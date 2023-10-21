2f5c6d8547d772a917a767c7fedaca962362d0c8
Alexander Batalov <alex.batalov@gmail.com>
2023-10-21 10:26:08 +0300

    Review bonebody.lua

--- src/scripts/bonebody.lua
+++ src/scripts/bonebody.lua
@@ -2,23 +2,11 @@ local fallout = require("fallout")
 
 local start
 
-local rndx = 0
-
 function start()
-    if (fallout.script_action() == 21) or (fallout.script_action() == 3) then
+    local script_action = fallout.script_action()
+    if script_action == 21 or script_action == 3 then
         fallout.script_overrides()
-        rndx = fallout.random(1, 3)
-        if rndx == 1 then
-            fallout.display_msg(fallout.message_str(293, 100))
-        else
-            if rndx == 2 then
-                fallout.display_msg(fallout.message_str(293, 101))
-            else
-                if rndx == 3 then
-                    fallout.display_msg(fallout.message_str(293, 102))
-                end
-            end
-        end
+        fallout.display_msg(fallout.message_str(293, 100 + fallout.random(0, 2)))
     end
 end
 
