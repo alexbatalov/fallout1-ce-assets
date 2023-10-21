671812f49b4bca8686554fec4aef0b9e12876253
Alexander Batalov <alex.batalov@gmail.com>
2023-10-21 08:32:11 +0300

    Review alphhotl.lua

--- src/scripts/alphhotl.lua
+++ src/scripts/alphhotl.lua
@@ -1,35 +1,43 @@
 local fallout = require("fallout")
 
 local start
+local map_enter_p_proc
+local map_update_p_proc
+
+local PIDS <const> = {
+    4,
+    18,
+    31,
+    40,
+}
 
 function start()
-    local v0 = 0
-    if fallout.script_action() == 15 then
-        v0 = fallout.create_object_sid(4, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), v0)
-        v0 = fallout.create_object_sid(18, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), v0)
-        v0 = fallout.create_object_sid(31, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), v0)
-        v0 = fallout.create_object_sid(40, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), v0)
+    local script_action = fallout.script_action()
+    if script_action == 15 then
+        map_enter_p_proc()
+    elseif script_action == 23 then
+        map_update_p_proc()
+    end
+end
+
+function map_enter_p_proc()
+    local dude_obj = fallout.dude_obj()
+    for index = 1, #PIDS do
+        local item_obj = fallout.create_object_sid(PIDS[index], 0, 0, -1)
+        fallout.add_obj_to_inven(dude_obj, item_obj)
+    end
+end
+
+function map_update_p_proc()
+    if fallout.elevation(fallout.dude_obj()) == 0 then
+        fallout.set_light_level(1)
     else
-        if fallout.script_action() == 16 then
-        else
-            if fallout.script_action() == 23 then
-                if fallout.elevation(fallout.dude_obj()) == 0 then
-                    fallout.set_light_level(1)
-                else
-                    fallout.set_light_level(100)
-                end
-            else
-                if fallout.script_action() == 22 then
-                end
-            end
-        end
+        fallout.set_light_level(100)
     end
 end
 
 local exports = {}
 exports.start = start
+exports.map_enter_p_proc = map_enter_p_proc
+exports.map_update_p_proc = map_update_p_proc
 return exports
