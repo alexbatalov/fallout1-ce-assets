4623c74d0b870fb326566e13bebc655f11da767a
Alexander Batalov <alex.batalov@gmail.com>
2023-10-21 08:01:23 +0300

    Review 1molrat1.lua

--- src/scripts/1molrat1.lua
+++ src/scripts/1molrat1.lua
@@ -2,8 +2,9 @@ local fallout = require("fallout")
 local reputation = require("lib.reputation")
 
 local start
+local critter_p_proc
+local destroy_p_proc
 
-local count = 0
 local initialized = false
 
 function start()
@@ -11,17 +12,27 @@ function start()
         fallout.critter_add_trait(fallout.self_obj(), 1, 6, 8)
         initialized = true
     end
-    if fallout.script_action() == 12 then
-        if fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) ~= 0 then
-            fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
-        end
-    else
-        if fallout.script_action() == 18 then
-            reputation.inc_evil_critter()
-        end
+
+    local script_action = fallout.script_action()
+    if script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
     end
+end
+
+function critter_p_proc()
+    if fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) then
+        fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
+    end
+end
+
+function destroy_p_proc()
+    reputation.inc_evil_critter()
 end
 
 local exports = {}
 exports.start = start
+exports.critter_p_proc = critter_p_proc
+exports.destroy_p_proc = destroy_p_proc
 return exports
