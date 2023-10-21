7f1b4ad374b4f78a05a8f2743dd29021f2240cfd
Alexander Batalov <alex.batalov@gmail.com>
2023-10-21 10:24:24 +0300

    Review blkroom.lua

--- src/scripts/blkroom.lua
+++ src/scripts/blkroom.lua
@@ -1,22 +1,28 @@
 local fallout = require("fallout")
 
 local start
+local spatial_p_proc
 
 function start()
     if fallout.script_action() == 2 then
-        if fallout.obj_type(fallout.source_obj()) == fallout.dude_obj() then
-            if fallout.local_var(0) == 0 then
-                if fallout.get_critter_stat(fallout.dude_obj(), 4) > 6 then
-                    fallout.display_msg(fallout.message_str(111, 100))
-                else
-                    fallout.display_msg(fallout.message_str(111, 101))
-                end
-                fallout.set_local_var(0, 1)
+        spatial_p_proc()
+    end
+end
+
+function spatial_p_proc()
+    if fallout.source_obj() == fallout.dude_obj() then
+        if fallout.local_var(0) == 0 then
+            if fallout.get_critter_stat(fallout.dude_obj(), 4) > 6 then
+                fallout.display_msg(fallout.message_str(111, 100))
+            else
+                fallout.display_msg(fallout.message_str(111, 101))
             end
+            fallout.set_local_var(0, 1)
         end
     end
 end
 
 local exports = {}
 exports.start = start
+exports.spatial_p_proc = spatial_p_proc
 return exports
