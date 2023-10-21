b939bac6280bf0f1b4d1e9dc55e142dc466d08a5
Alexander Batalov <alex.batalov@gmail.com>
2023-10-21 11:43:42 +0300

    Review boxguard.lua

--- src/scripts/boxguard.lua
+++ src/scripts/boxguard.lua
@@ -8,63 +8,56 @@ local map_enter_p_proc
 local talk_p_proc
 local timed_event_p_proc
 
-local home_tile = 7000
-local hostile = 0
-local sfx_name = 0
-
 function start()
-    if fallout.script_action() == 12 then
+    local script_action = fallout.script_action()
+    if script_action == 12 then
         critter_p_proc()
-    else
-        if fallout.script_action() == 18 then
-            destroy_p_proc()
-        else
-            if fallout.script_action() == 11 then
-                talk_p_proc()
-            else
-                if fallout.script_action() == 22 then
-                    timed_event_p_proc()
-                end
-            end
-        end
+    elseif script_action == 18 then
+        destroy_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 22 then
+        timed_event_p_proc()
     end
 end
 
 function critter_p_proc()
-    if hostile then
-        hostile = 0
-        fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
-    else
-        if fallout.external_var("Saul_loses") then
-            if (fallout.obj_pid(fallout.external_var("challenger_ptr")) == 16777227) or (fallout.obj_pid(fallout.external_var("challenger_ptr")) == 16777226) then
-                if fallout.external_var("shot_challenger") == 0 then
-                    fallout.set_external_var("Saul_loses", 0)
-                    fallout.anim(fallout.self_obj(), 1000, 5)
-                    fallout.float_msg(fallout.self_obj(), fallout.message_str(835, fallout.random(100, 103)), 0)
-                    fallout.reg_anim_func(2, fallout.self_obj())
-                    fallout.reg_anim_func(1, 1)
-                    fallout.reg_anim_animate(fallout.self_obj(), 43, -1)
-                    fallout.reg_anim_animate(fallout.self_obj(), 45, -1)
-                    sfx_name = fallout.sfx_build_weapon_name(1, fallout.obj_carrying_pid_obj(fallout.self_obj(), 8), 2, fallout.external_var("challenger_ptr"))
-                    fallout.reg_anim_play_sfx(fallout.dude_obj(), sfx_name, -1)
-                    fallout.reg_anim_animate(fallout.external_var("challenger_ptr"), 14, 4)
-                    fallout.reg_anim_animate(fallout.self_obj(), 43, -1)
-                    fallout.reg_anim_animate(fallout.self_obj(), 45, -1)
-                    sfx_name = fallout.sfx_build_weapon_name(1, fallout.obj_carrying_pid_obj(fallout.self_obj(), 8), 2, fallout.external_var("challenger_ptr"))
-                    fallout.reg_anim_play_sfx(fallout.dude_obj(), sfx_name, -1)
-                    fallout.reg_anim_animate(fallout.external_var("challenger_ptr"), 34, 4)
-                    fallout.reg_anim_animate(fallout.self_obj(), 44, -1)
-                    fallout.reg_anim_func(3, 0)
-                    fallout.rm_timer_event(fallout.self_obj())
-                    fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(5), 1)
-                end
-            else
-                fallout.set_external_var("shot_challenger", 1)
+    if fallout.external_var("Saul_loses") ~= 0 then
+        local challenger_obj = fallout.external_var("challenger_ptr")
+        local challenger_pid = fallout.obj_pid(challenger_obj)
+        if challenger_pid == 16777227 or challenger_pid == 16777226 then
+            if fallout.external_var("shot_challenger") == 0 then
+                local dude_obj = fallout.dude_obj()
+                local self_obj = fallout.self_obj()
+                local weapon_obj = fallout.obj_carrying_pid_obj(self_obj, 8)
+                fallout.set_external_var("Saul_loses", 0)
+                fallout.anim(self_obj, 1000, 5)
+                fallout.float_msg(self_obj, fallout.message_str(835, fallout.random(100, 103)), 0)
+                fallout.reg_anim_func(2, self_obj)
+                fallout.reg_anim_func(1, 1)
+                fallout.reg_anim_animate(self_obj, 43, -1)
+                fallout.reg_anim_animate(self_obj, 45, -1)
+                fallout.reg_anim_play_sfx(dude_obj,
+                    fallout.sfx_build_weapon_name(1, weapon_obj, 2, challenger_obj),
+                    -1)
+                fallout.reg_anim_animate(challenger_obj, 14, 4)
+                fallout.reg_anim_animate(self_obj, 43, -1)
+                fallout.reg_anim_animate(self_obj, 45, -1)
+                fallout.reg_anim_play_sfx(dude_obj,
+                    fallout.sfx_build_weapon_name(1, weapon_obj, 2, challenger_obj),
+                    -1)
+                fallout.reg_anim_animate(challenger_obj, 34, 4)
+                fallout.reg_anim_animate(self_obj, 44, -1)
+                fallout.reg_anim_func(3, 0)
+                fallout.rm_timer_event(self_obj)
+                fallout.add_timer_event(self_obj, fallout.game_ticks(5), 1)
             end
         else
-            if fallout.external_var("Saul_wins") then
-                fallout.set_external_var("shot_challenger", 1)
-            end
+            fallout.set_external_var("shot_challenger", 1)
+        end
+    else
+        if fallout.external_var("Saul_wins") ~= 0 then
+            fallout.set_external_var("shot_challenger", 1)
         end
     end
 end
@@ -74,16 +67,16 @@ function destroy_p_proc()
 end
 
 function map_enter_p_proc()
+    local self_obj = fallout.self_obj()
     if fallout.global_var(15) == 1 then
-        fallout.kill_critter(fallout.self_obj(), 49)
+        fallout.kill_critter(self_obj, 49)
     end
     if fallout.local_var(0) == 0 then
-        fallout.set_local_var(0, fallout.tile_num(fallout.self_obj()))
+        fallout.set_local_var(0, fallout.tile_num(self_obj))
     end
-    home_tile = fallout.local_var(0)
-    fallout.critter_add_trait(fallout.self_obj(), 1, 6, 25)
-    if fallout.item_caps_total(fallout.self_obj()) == 0 then
-        fallout.item_caps_adjust(fallout.self_obj(), fallout.random(10, 25))
+    fallout.critter_add_trait(self_obj, 1, 6, 25)
+    if fallout.item_caps_total(self_obj) == 0 then
+        fallout.item_caps_adjust(self_obj, fallout.random(10, 25))
     end
 end
 
