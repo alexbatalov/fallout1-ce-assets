cb8f49532d5f40f49b1dac89c87f542df11179aa
Alexander Batalov <alex.batalov@gmail.com>
2023-10-22 09:03:53 +0300

    Review bygreg.lua

--- src/scripts/bygreg.lua
+++ src/scripts/bygreg.lua
@@ -22,12 +22,11 @@ local initialized = false
 local PsstTime = 0
 local DisplayMessage = 100
 
-local exit_line = 0
-
 function start()
     if not initialized then
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 47)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 27)
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 47)
+        fallout.critter_add_trait(self_obj, 1, 5, 27)
         initialized = true
     end
 end
@@ -53,33 +52,35 @@ end
 function talk_p_proc()
     if fallout.global_var(253) == 1 then
         fallout.float_msg(fallout.self_obj(), fallout.message_str(669, fallout.random(100, 105)), 2)
+    elseif fallout.local_var(5) == 1 and fallout.get_critter_stat(fallout.dude_obj(), 4) < 4 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(925, fallout.random(115, 119)), 0)
     else
-        if (fallout.local_var(5) == 1) and (fallout.get_critter_stat(fallout.dude_obj(), 4) < 4) then
-            fallout.float_msg(fallout.self_obj(), fallout.message_str(925, fallout.random(115, 119)), 0)
+        reaction.get_reaction()
+        fallout.start_gdialog(925, fallout.self_obj(), -1, -1, -1)
+        fallout.gsay_start()
+        if fallout.local_var(4) == 0 then
+            DisplayMessage = 102
         else
-            reaction.get_reaction()
-            fallout.start_gdialog(925, fallout.self_obj(), -1, -1, -1)
-            fallout.gsay_start()
-            if fallout.local_var(4) == 0 then
-                DisplayMessage = 102
-            else
-                DisplayMessage = 103
-            end
-            BYGreg03()
-            fallout.gsay_end()
-            fallout.end_dialogue()
+            DisplayMessage = 103
         end
+        BYGreg03()
+        fallout.gsay_end()
+        fallout.end_dialogue()
     end
 end
 
 function critter_p_proc()
-    if ((time.game_time_in_seconds() - PsstTime) >= 30) and (fallout.tile_distance_objs(fallout.self_obj(), fallout.dude_obj()) <= 4) and (fallout.local_var(5) == 1) then
-        fallout.float_msg(fallout.self_obj(), fallout.message_str(925, fallout.random(115, 119)), 0)
+    local dude_obj = fallout.dude_obj()
+    local self_obj = fallout.self_obj()
+    if time.game_time_in_seconds() - PsstTime >= 30
+        and fallout.tile_distance_objs(self_obj, dude_obj) <= 4
+        and fallout.local_var(5) == 1 then
+        fallout.float_msg(self_obj, fallout.message_str(925, fallout.random(115, 119)), 0)
         PsstTime = time.game_time_in_seconds()
     end
-    if fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) then
+    if fallout.obj_can_see_obj(self_obj, dude_obj) then
         if fallout.global_var(253) == 1 then
-            fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
+            fallout.attack(dude_obj, 0, 1, 0, 0, 30000, 0, 0)
         end
     end
 end
