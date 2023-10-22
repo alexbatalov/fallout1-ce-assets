037f2d59c8e169b0007fa45d0e430b562a1fb914
Alexander Batalov <alex.batalov@gmail.com>
2023-10-22 14:06:54 +0300

    Review chepslot.lua

--- src/scripts/chepslot.lua
+++ src/scripts/chepslot.lua
@@ -5,22 +5,14 @@ local look_at_p_proc
 local map_enter_p_proc
 local use_p_proc
 
-local rndx = 0
-local bonus = 0
-local test = 0
-local money = 0
-
 function start()
-    if fallout.script_action() == 21 then
+    local script_action = fallout.script_action()
+    if script_action == 21 then
         look_at_p_proc()
-    else
-        if fallout.script_action() == 15 then
-            map_enter_p_proc()
-        else
-            if fallout.script_action() == 6 then
-                use_p_proc()
-            end
-        end
+    elseif script_action == 15 then
+        map_enter_p_proc()
+    elseif script_action == 6 then
+        use_p_proc()
     end
 end
 
@@ -36,25 +28,28 @@ function map_enter_p_proc()
 end
 
 function use_p_proc()
+    local dude_obj = fallout.dude_obj()
+    local self_obj = fallout.self_obj()
     fallout.script_overrides()
-    if fallout.item_caps_total(fallout.dude_obj()) < 10 then
+    if fallout.item_caps_total(dude_obj) < 10 then
         fallout.display_msg(fallout.message_str(354, 108))
     else
         fallout.display_msg(fallout.message_str(354, 101))
-        fallout.item_caps_adjust(fallout.dude_obj(), -10)
-        bonus = fallout.random(1, 50) - fallout.random(1, 50)
-        test = fallout.roll_vs_skill(fallout.dude_obj(), 16, bonus)
-        if fallout.is_success(test) then
-            if fallout.is_critical(test) then
-                rndx = fallout.random(15, 30)
-                fallout.float_msg(fallout.self_obj(), fallout.message_str(354, 102), 0)
-                fallout.display_msg(fallout.message_str(354, 103) .. rndx .. fallout.message_str(354, 104))
+        fallout.item_caps_adjust(dude_obj, -10)
+        local bonus = fallout.random(1, 50) - fallout.random(1, 50)
+        local roll = fallout.roll_vs_skill(dude_obj, 16, bonus)
+        if fallout.is_success(roll) then
+            local money
+            if fallout.is_critical(roll) then
+                money = fallout.random(15, 30)
+                fallout.float_msg(self_obj, fallout.message_str(354, 102), 0)
+                fallout.display_msg(fallout.message_str(354, 103) .. money .. fallout.message_str(354, 104))
             else
-                fallout.float_msg(fallout.self_obj(), fallout.message_str(354, 105), 0)
+                fallout.float_msg(self_obj, fallout.message_str(354, 105), 0)
                 fallout.display_msg(fallout.message_str(354, 106))
-                rndx = 15
+                money = 15
             end
-            fallout.item_caps_adjust(fallout.dude_obj(), rndx)
+            fallout.item_caps_adjust(fallout.dude_obj(), money)
         else
             fallout.display_msg(fallout.message_str(354, 107))
         end
