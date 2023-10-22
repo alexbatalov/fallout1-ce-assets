f7bc574bff0916f016c419233c5302d86324729a
Alexander Batalov <alex.batalov@gmail.com>
2023-10-22 10:15:18 +0300

    Review charbody.lua

--- src/scripts/charbody.lua
+++ src/scripts/charbody.lua
@@ -1,36 +1,30 @@
 local fallout = require("fallout")
 
 local start
-
-local rndx = 0
+local use_p_proc
+local look_at_p_proc
 
 function start()
-    if (fallout.script_action() == 21) or (fallout.script_action() == 3) then
-        fallout.script_overrides()
-        rndx = fallout.random(1, 4)
-        if rndx == 1 then
-            fallout.display_msg(fallout.message_str(292, 100))
-        else
-            if rndx == 2 then
-                fallout.display_msg(fallout.message_str(292, 101))
-            else
-                if rndx == 3 then
-                    fallout.display_msg(fallout.message_str(292, 102))
-                else
-                    if rndx == 4 then
-                        fallout.display_msg(fallout.message_str(292, 103))
-                    end
-                end
-            end
-        end
-    else
-        if fallout.script_action() == 6 then
-            fallout.display_msg(fallout.message_str(292, 104))
-            fallout.script_overrides()
-        end
+    local script_action = fallout.script_action()
+    if script_action == 21 or script_action == 3 then
+        look_at_p_proc()
+    elseif script_action == 6 then
+        use_p_proc()
     end
+end
+
+function use_p_proc()
+    fallout.display_msg(fallout.message_str(292, 104))
+    fallout.script_overrides()
+end
+
+function look_at_p_proc()
+    fallout.script_overrides()
+    fallout.display_msg(fallout.message_str(292, fallout.random(100, 103)))
 end
 
 local exports = {}
 exports.start = start
+exports.use_p_proc = use_p_proc
+exports.look_at_p_proc = look_at_p_proc
 return exports
