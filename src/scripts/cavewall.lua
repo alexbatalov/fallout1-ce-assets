7fe7c951d785bf2a9c0783c4d56260cc777b1b38
Alexander Batalov <alex.batalov@gmail.com>
2023-10-22 10:00:37 +0300

    Review cavewall.lua

--- src/scripts/cavewall.lua
+++ src/scripts/cavewall.lua
@@ -5,13 +5,38 @@ local look_at_p_proc
 local description_p_proc
 local damage_p_proc
 
+local HEXES <const> = {
+    22349,
+    21544,
+    21744,
+    21944,
+    22144,
+    22344,
+    22544,
+    22744,
+    22944,
+    21139,
+    21339,
+    21539,
+    21739,
+    21939,
+    22139,
+    22339,
+    22539,
+    22739,
+    22939,
+    22940,
+    22941,
+    22942,
+    22943,
+}
+
 function start()
-    if (fallout.script_action() == 21) or (fallout.script_action() == 3) then
+    local script_action = fallout.script_action()
+    if script_action == 21 or script_action == 3 then
         look_at_p_proc()
-    else
-        if fallout.script_action() == 14 then
-            damage_p_proc()
-        end
+    elseif script_action == 14 then
+        damage_p_proc()
     end
 end
 
@@ -22,7 +47,8 @@ end
 
 function description_p_proc()
     fallout.script_overrides()
-    if fallout.is_success(fallout.do_check(fallout.dude_obj(), 1, 0)) or fallout.is_success(fallout.do_check(fallout.dude_obj(), 6, -1)) then
+    local dude_obj = fallout.dude_obj()
+    if fallout.is_success(fallout.do_check(dude_obj, 1, 0)) or fallout.is_success(fallout.do_check(dude_obj, 6, -1)) then
         fallout.display_msg(fallout.message_str(465, 101))
     else
         fallout.display_msg(fallout.message_str(465, 100))
@@ -30,50 +56,28 @@ function description_p_proc()
 end
 
 function damage_p_proc()
-    local v0 = 0
-    local v1 = 0
     if fallout.map_var(0) == 0 then
         fallout.set_map_var(0, 1)
         fallout.gfade_out(600)
-        v0 = fallout.create_object_sid(33554839, 0, 0, -1)
-        fallout.move_to(v0, 21744, 0)
-        fallout.create_object_sid(33554499, 22349, 0, -1)
-        fallout.create_object_sid(33554499, 21544, 0, -1)
-        fallout.create_object_sid(33554499, 21744, 0, -1)
-        fallout.create_object_sid(33554499, 21944, 0, -1)
-        fallout.create_object_sid(33554499, 22144, 0, -1)
-        fallout.create_object_sid(33554499, 22344, 0, -1)
-        fallout.create_object_sid(33554499, 22544, 0, -1)
-        fallout.create_object_sid(33554499, 22744, 0, -1)
-        fallout.create_object_sid(33554499, 22944, 0, -1)
-        fallout.create_object_sid(33554499, 21139, 0, -1)
-        fallout.create_object_sid(33554499, 21339, 0, -1)
-        fallout.create_object_sid(33554499, 21539, 0, -1)
-        fallout.create_object_sid(33554499, 21739, 0, -1)
-        fallout.create_object_sid(33554499, 21939, 0, -1)
-        fallout.create_object_sid(33554499, 22139, 0, -1)
-        fallout.create_object_sid(33554499, 22339, 0, -1)
-        fallout.create_object_sid(33554499, 22539, 0, -1)
-        fallout.create_object_sid(33554499, 22739, 0, -1)
-        fallout.create_object_sid(33554499, 22939, 0, -1)
-        fallout.create_object_sid(33554499, 22940, 0, -1)
-        fallout.create_object_sid(33554499, 22941, 0, -1)
-        fallout.create_object_sid(33554499, 22942, 0, -1)
-        fallout.create_object_sid(33554499, 22943, 0, -1)
+        local scenery_obj = fallout.create_object_sid(33554839, 0, 0, -1)
+        fallout.move_to(scenery_obj, 21744, 0)
+        for index = 1, #HEXES do
+            fallout.create_object_sid(33554499, HEXES[index], 0, -1)
+        end
         fallout.set_global_var(155, fallout.global_var(155) + 3)
-        v1 = (fallout.global_var(2) * 75) + 75
-        if v1 < 300 then
-            v1 = 300
+        local xp = fallout.global_var(2) * 75 + 75
+        if xp < 300 then
+            xp = 300
         end
         if fallout.global_var(2) > 0 then
-            v1 = v1 + 500
+            xp = xp + 500
         end
         fallout.set_global_var(28, 2)
         fallout.set_global_var(43, 2)
-        fallout.display_msg(fallout.message_str(766, 188) .. " " .. fallout.message_str(766, 103) .. " " .. v1 .. " " .. fallout.message_str(766, 104))
-        fallout.give_exp_points(v1)
+        fallout.display_msg(fallout.message_str(766, 188) .. " " .. fallout.message_str(766, 103) .. " " .. xp .. " " .. fallout.message_str(766, 104))
+        fallout.give_exp_points(xp)
         fallout.gfade_in(600)
-        if not(fallout.tile_distance(fallout.tile_num(fallout.dude_obj()), 21155) < 15) then
+        if fallout.tile_distance(fallout.tile_num(fallout.dude_obj()), 21155) >= 15 then
             fallout.metarule(13, 0)
         end
     end
