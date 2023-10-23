617eb8f4633c349060e6a812625f7fc25ff93682
Alexander Batalov <alex.batalov@gmail.com>
2023-10-23 16:26:22 +0300

    Review brolock.lua

--- src/scripts/brolock.lua
+++ src/scripts/brolock.lua
@@ -4,15 +4,12 @@ local start
 local pickup_p_proc
 local use_skill_on_p_proc
 
-local Test = 0
-
 function start()
-    if fallout.script_action() == 8 then
+    local script_action = fallout.script_action()
+    if script_action == 8 then
         use_skill_on_p_proc()
-    else
-        if fallout.script_action() == pickup_p_proc() then
-            pickup_p_proc()
-        end
+    elseif script_action == 4 then
+        pickup_p_proc()
     end
 end
 
@@ -22,11 +19,11 @@ end
 
 function use_skill_on_p_proc()
     if fallout.global_var(250) == 0 then
-        Test = fallout.roll_vs_skill(fallout.dude_obj(), 10, 0)
         if fallout.action_being_used() == 10 then
-            if not(fallout.is_success(Test)) then
+            local roll = fallout.roll_vs_skill(fallout.dude_obj(), 10, 0)
+            if not fallout.is_success(roll) then
                 fallout.script_overrides()
-                if fallout.is_critical(Test) then
+                if fallout.is_critical(roll) then
                     fallout.set_global_var(250, 1)
                     fallout.display_msg(fallout.message_str(766, 181))
                 else
