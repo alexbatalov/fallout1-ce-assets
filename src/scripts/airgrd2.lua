af313365d479d9bd8403df6e2ad8ca3ceb14b3ac
Alexander Batalov <alex.batalov@gmail.com>
2023-10-23 15:58:09 +0300

    Review airgrd2.lua

--- src/scripts/airgrd2.lua
+++ src/scripts/airgrd2.lua
@@ -2,51 +2,74 @@ local fallout = require("fallout")
 local reputation = require("lib.reputation")
 
 local start
-local do_dialogue
+local pickup_p_proc
+local talk_p_proc
+local critter_p_proc
+local damage_p_proc
+local destroy_p_proc
+local look_at_p_proc
 
-local HOSTILE = 0
+local hostile = false
 local initialized = false
 
 function start()
     if not initialized then
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 34)
+        fallout.critter_add_trait(self_obj, 1, 5, 46)
         initialized = true
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 34)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 46)
-    else
-        if fallout.script_action() == 14 then
-            if fallout.global_var(245) == 0 then
-                fallout.set_global_var(245, 1)
-            end
-        else
-            if fallout.script_action() == 11 then
-                do_dialogue()
-            else
-                if fallout.script_action() == 4 then
-                    HOSTILE = 1
-                end
-            end
-        end
     end
-    if fallout.script_action() == 12 then
-        if HOSTILE then
-            HOSTILE = 0
-            fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
-        end
-    else
-        if (fallout.script_action() == 21) or (fallout.script_action() == 3) then
-            fallout.script_overrides()
-        else
-            if fallout.script_action() == 18 then
-                reputation.inc_evil_critter()
-            end
-        end
+
+    local script_action = fallout.script_action()
+    if script_action == 14 then
+        damage_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 21 or script_action == 3 then
+        look_at_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
     end
 end
 
-function do_dialogue()
+function pickup_p_proc()
+    hostile = true
+end
+
+function talk_p_proc()
     fallout.float_msg(fallout.self_obj(), fallout.message_str(673, 143), 2)
+end
+
+function critter_p_proc()
+    if hostile then
+        hostile = false
+        fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
+    end
+end
+
+function damage_p_proc()
+    if fallout.global_var(245) == 0 then
+        fallout.set_global_var(245, 1)
+    end
+end
+
+function destroy_p_proc()
+    reputation.inc_evil_critter()
+end
+
+function look_at_p_proc()
+    fallout.script_overrides()
 end
 
 local exports = {}
 exports.start = start
+exports.pickup_p_proc = pickup_p_proc
+exports.talk_p_proc = talk_p_proc
+exports.critter_p_proc = critter_p_proc
+exports.damage_p_proc = damage_p_proc
+exports.destroy_p_proc = destroy_p_proc
 return exports
