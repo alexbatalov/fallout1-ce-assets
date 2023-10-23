f1358fb8c23bd99690c049b2f45bf27435d83734
Alexander Batalov <alex.batalov@gmail.com>
2023-10-23 14:30:27 +0300

    Review corecomp.lua

--- src/scripts/corecomp.lua
+++ src/scripts/corecomp.lua
@@ -7,16 +7,13 @@ local use_p_proc
 local use_skill_on_p_proc
 
 function start()
-    if fallout.script_action() == 3 then
+    local script_action = fallout.script_action()
+    if script_action == 3 then
         description_p_proc()
-    else
-        if fallout.script_action() == 6 then
-            use_p_proc()
-        else
-            if fallout.script_action() == 8 then
-                use_skill_on_p_proc()
-            end
-        end
+    elseif script_action == 6 then
+        use_p_proc()
+    elseif script_action == 8 then
+        use_skill_on_p_proc()
     end
 end
 
@@ -31,9 +28,10 @@ function use_p_proc()
 end
 
 function use_skill_on_p_proc()
-    if fallout.action_being_used() == 12 then
+    local skill = fallout.action_being_used()
+    if skill == 12 then
         fallout.script_overrides()
-        if fallout.is_success(fallout.roll_vs_skill(fallout.dude_obj(), fallout.action_being_used(), 0)) then
+        if fallout.is_success(fallout.roll_vs_skill(fallout.dude_obj(), skill, 0)) then
             if time.game_time_in_days() >= 90 then
                 fallout.display_msg(fallout.message_str(133, 101))
             else
