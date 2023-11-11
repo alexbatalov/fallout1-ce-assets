082c0116b5cb1d8c44f5789ea020dcb9f3f96061
Alexander Batalov <alex.batalov@gmail.com>
2023-11-11 11:35:04 +0300

    Review dethcave.lua

--- src/scripts/dethcave.lua
+++ src/scripts/dethcave.lua
@@ -3,9 +3,8 @@ local light = require("lib.light")
 local party = require("lib.party")
 
 local start
-
-local party_elevation = 0
-local dude_start_hex = 0
+local map_enter_p_proc
+local map_exit_p_proc
 
 fallout.create_external_var("Ian_ptr")
 fallout.create_external_var("Dog_ptr")
@@ -13,18 +12,33 @@ fallout.create_external_var("Tycho_ptr")
 fallout.create_external_var("Katja_ptr")
 fallout.create_external_var("Tandi_ptr")
 
+fallout.set_external_var("Ian_ptr", nil)
+fallout.set_external_var("Dog_ptr", nil)
+fallout.set_external_var("Tycho_ptr", nil)
+fallout.set_external_var("Katja_ptr", nil)
+fallout.set_external_var("Tandi_ptr", nil)
+
 function start()
-    if fallout.script_action() == 15 then
-        fallout.override_map_start(129, 70, 0, 0)
-        light.darkness()
-        party_elevation = party.add_party()
-    else
-        if fallout.script_action() == 16 then
-            party.remove_party()
-        end
+    local script_action = fallout.script_action()
+    if script_action == 15 then
+        map_enter_p_proc()
+    elseif script_action == 16 then
+        map_exit_p_proc()
     end
+end
+
+function map_enter_p_proc()
+    fallout.override_map_start(129, 70, 0, 0)
+    light.darkness()
+    party.add_party()
+end
+
+function map_exit_p_proc()
+    party.remove_party()
 end
 
 local exports = {}
 exports.start = start
+exports.map_enter_p_proc = map_enter_p_proc
+exports.map_exit_p_proc = map_exit_p_proc
 return exports
