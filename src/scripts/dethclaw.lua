2983f7e9151cf9739d52f3a5328971a4c42833de
Alexander Batalov <alex.batalov@gmail.com>
2023-11-11 11:36:04 +0300

    Review dethclaw.lua

--- src/scripts/dethclaw.lua
+++ src/scripts/dethclaw.lua
@@ -5,18 +5,18 @@ local critter_p_proc
 local destroy_p_proc
 
 function start()
-    if fallout.script_action() == 12 then
+    local script_action = fallout.script_action()
+    if script_action == 12 then
         critter_p_proc()
-    else
-        if fallout.script_action() == 18 then
-            destroy_p_proc()
-        end
+    elseif script_action == 18 then
+        destroy_p_proc()
     end
 end
 
 function critter_p_proc()
-    if fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) then
-        fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
+    local dude_obj = fallout.dude_obj()
+    if fallout.obj_can_see_obj(fallout.self_obj(), dude_obj) then
+        fallout.attack(dude_obj, 0, 1, 0, 0, 30000, 0, 0)
     end
 end
 
