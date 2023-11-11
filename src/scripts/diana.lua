fb106505f4155eb9be4a2beaadea4a9ff44114e1
Alexander Batalov <alex.batalov@gmail.com>
2023-11-11 11:45:33 +0300

    Review diana.lua

--- src/scripts/diana.lua
+++ src/scripts/diana.lua
@@ -3,6 +3,12 @@ local reaction = require("lib.reaction")
 local reputation = require("lib.reputation")
 
 local start
+local pickup_p_proc
+local talk_p_proc
+local critter_p_proc
+local damage_p_proc
+local destroy_p_proc
+local look_at_p_proc
 local do_dialogue
 local Diana00
 local Diana01
@@ -14,53 +20,48 @@ local Diana10
 local Diana11
 local Diana12
 local Dianaend
-local combat
-local talk_p_proc
-local Critter_Action
-local damage_p_proc
 
-local hostile = 0
+local hostile = false
 local initialized = false
-
-local exit_line = 0
 
 function start()
     if not initialized then
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 6)
+        fallout.critter_add_trait(self_obj, 1, 5, 21)
         initialized = true
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 6)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 21)
     end
-    if fallout.script_action() == 11 then
+
+    local script_action = fallout.script_action()
+    if script_action == 11 then
         talk_p_proc()
-    else
-        if fallout.script_action() == 21 then
-            fallout.script_overrides()
-            fallout.display_msg(fallout.message_str(142, 100))
-        else
-            if fallout.script_action() == 4 then
-                hostile = 1
-            else
-                if fallout.script_action() == 12 then
-                    Critter_Action()
-                    if hostile then
-                        hostile = 0
-                        fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
-                    end
-                else
-                    if fallout.script_action() == 14 then
-                        damage_p_proc()
-                    else
-                        if fallout.script_action() == 18 then
-                            reputation.inc_evil_critter()
-                            fallout.set_global_var(254, 1)
-                            fallout.set_global_var(611, 0)
-                            fallout.set_global_var(115, fallout.global_var(115) - 1)
-                        end
-                    end
-                end
-            end
-        end
+    elseif script_action == 21 then
+        look_at_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 14 then
+        damage_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
     end
+end
+
+function pickup_p_proc()
+    hostile = true
+end
+
+function destroy_p_proc()
+    reputation.inc_evil_critter()
+    fallout.set_global_var(254, 1)
+    fallout.set_global_var(611, 0)
+    fallout.set_global_var(115, fallout.global_var(115) - 1)
+end
+
+function look_at_p_proc()
+    fallout.script_overrides()
+    fallout.display_msg(fallout.message_str(142, 100))
 end
 
 function do_dialogue()
@@ -145,13 +146,6 @@ end
 function Dianaend()
 end
 
-function combat()
-    if fallout.global_var(116) == 1 then
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 87)
-    end
-    hostile = 1
-end
-
 function talk_p_proc()
     if fallout.global_var(116) == 1 then
         fallout.float_msg(fallout.self_obj(), fallout.message_str(136, fallout.random(102, 106)), 8)
@@ -160,27 +154,29 @@ function talk_p_proc()
     end
 end
 
-function Critter_Action()
-    local v0 = 0
+function critter_p_proc()
+    local dude_obj = fallout.dude_obj()
+    local self_obj = fallout.self_obj()
     if fallout.global_var(26) == 2 then
         fallout.set_global_var(254, 1)
     else
         if fallout.global_var(116) ~= 0 then
             fallout.set_global_var(254, 0)
-            if fallout.tile_distance_objs(fallout.self_obj(), fallout.dude_obj()) < 3 then
-                v0 = fallout.tile_num_in_direction(fallout.tile_num(fallout.self_obj()), fallout.random(0, 5), 3)
-                if fallout.tile_distance(fallout.tile_num(fallout.self_obj()), v0) > 2 then
+            if fallout.tile_distance_objs(self_obj, dude_obj) < 3 then
+                local self_tile_num = fallout.tile_num(self_obj)
+                local dest_tile_num = fallout.tile_num_in_direction(self_tile_num, fallout.random(0, 5), 3)
+                if fallout.tile_distance(self_tile_num, dest_tile_num) > 2 then
                     if fallout.random(0, 9) == 0 then
-                        fallout.float_msg(fallout.self_obj(), fallout.message_str(136, fallout.random(102, 106)), 8)
+                        fallout.float_msg(self_obj, fallout.message_str(136, fallout.random(102, 106)), 8)
                     end
-                    fallout.animate_move_obj_to_tile(fallout.self_obj(), v0, 0)
+                    fallout.animate_move_obj_to_tile(self_obj, dest_tile_num, 0)
                 end
             end
         else
             if fallout.global_var(213) ~= 0 then
                 fallout.set_global_var(254, 1)
             end
-            if fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) then
+            if fallout.obj_can_see_obj(self_obj, dude_obj) then
                 if fallout.global_var(214) ~= 0 then
                     fallout.set_global_var(254, 1)
                 end
@@ -190,11 +186,15 @@ function Critter_Action()
             end
         end
     end
-    if fallout.global_var(254) and fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) then
-        hostile = 1
+    if fallout.global_var(254) and fallout.obj_can_see_obj(self_obj, dude_obj) then
+        hostile = true
     end
-    if fallout.tile_distance_objs(fallout.self_obj(), fallout.dude_obj()) > 12 then
-        hostile = 0
+    if fallout.tile_distance_objs(self_obj, dude_obj) > 12 then
+        hostile = false
+    end
+    if hostile then
+        hostile = false
+        fallout.attack(dude_obj, 0, 1, 0, 0, 30000, 0, 0)
     end
 end
 
@@ -206,6 +206,10 @@ end
 
 local exports = {}
 exports.start = start
+exports.pickup_p_proc = pickup_p_proc
 exports.talk_p_proc = talk_p_proc
+exports.critter_p_proc = critter_p_proc
 exports.damage_p_proc = damage_p_proc
+exports.destroy_p_proc = destroy_p_proc
+exports.look_at_p_proc = look_at_p_proc
 return exports
