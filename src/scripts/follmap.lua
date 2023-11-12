4956189d087458631ee2c7493656d660ea30e650
Alexander Batalov <alex.batalov@gmail.com>
2023-11-12 12:30:13 +0300

    Review follmap.lua

--- src/scripts/follmap.lua
+++ src/scripts/follmap.lua
@@ -9,35 +9,35 @@ local map_update_p_proc
 local map_exit_p_proc
 
 local party_elevation = 0
-local dude_start_hex = 0
+local dude_carrying_bomb = false
 
 fallout.create_external_var("Ian_ptr")
 fallout.create_external_var("Dog_ptr")
 fallout.create_external_var("Tycho_ptr")
 fallout.create_external_var("Katja_ptr")
 fallout.create_external_var("Tandi_ptr")
-
-local Player_Elevation = 0
-local dude_carrying_bomb = 0
-
 fallout.create_external_var("removal_ptr")
 
+fallout.set_external_var("Ian_ptr", nil)
+fallout.set_external_var("Dog_ptr", nil)
+fallout.set_external_var("Tycho_ptr", nil)
+fallout.set_external_var("Katja_ptr", nil)
+fallout.set_external_var("Tandi_ptr", nil)
+fallout.set_external_var("removal_ptr", nil)
+
 function start()
-    if fallout.script_action() == 15 then
+    local script_action = fallout.script_action()
+    if script_action == 15 then
         map_enter_p_proc()
-    else
-        if fallout.script_action() == 23 then
-            map_update_p_proc()
-        else
-            if fallout.script_action() == 16 then
-                map_exit_p_proc()
-            end
-        end
+    elseif script_action == 23 then
+        map_update_p_proc()
+    elseif script_action == 16 then
+        map_exit_p_proc()
     end
 end
 
 function map_enter_p_proc()
-    if fallout.metarule(14, 0) then
+    if fallout.metarule(14, 0) ~= 0 then
         fallout.display_msg(fallout.message_str(194, 114))
     end
     if fallout.global_var(129) == 2 then
@@ -54,21 +54,22 @@ function map_enter_p_proc()
     end
     fallout.override_map_start(82, 114, 0, 5)
     if fallout.obj_is_carrying_obj_pid(fallout.dude_obj(), 72) ~= 0 then
-        dude_carrying_bomb = 1
+        dude_carrying_bomb = true
     end
 end
 
 function map_update_p_proc()
     light.lighting()
     party_elevation = party.update_party(party_elevation)
-    if fallout.external_var("removal_ptr") then
-        fallout.destroy_object(fallout.external_var("removal_ptr"))
+    local removal_obj = fallout.external_var("removal_ptr")
+    if removal_obj ~= nil then
+        fallout.destroy_object(removal_obj)
         fallout.set_external_var("removal_ptr", 0)
     end
 end
 
 function map_exit_p_proc()
-    if dude_carrying_bomb == 1 then
+    if dude_carrying_bomb then
         if fallout.obj_is_carrying_obj_pid(fallout.dude_obj(), 72) == 0 then
             fallout.set_global_var(129, 2)
         end
