e1772244d66be22dc68abd7916bfa291b8e2b594
Alexander Batalov <alex.batalov@gmail.com>
2023-11-12 12:52:40 +0300

    Review fsauser.lua

--- src/scripts/fsauser.lua
+++ src/scripts/fsauser.lua
@@ -2,21 +2,32 @@ local fallout = require("fallout")
 local light = require("lib.light")
 
 local start
+local map_enter_p_proc
+local map_update_p_proc
 
 function start()
-    if fallout.script_action() == 15 then
-        light.lighting()
-        if fallout.metarule(14, 0) then
-            fallout.override_map_start(109, 121, 0, 5)
-            fallout.display_msg(fallout.message_str(112, 314))
-        end
-    else
-        if fallout.script_action() == 23 then
-            light.lighting()
-        end
+    local script_action = fallout.script_action()
+    if script_action == 15 then
+        map_enter_p_proc()
+    elseif script_action == 23 then
+        map_update_p_proc()
     end
+end
+
+function map_enter_p_proc()
+    light.lighting()
+    if fallout.metarule(14, 0) ~= 0 then
+        fallout.override_map_start(109, 121, 0, 5)
+        fallout.display_msg(fallout.message_str(112, 314))
+    end
+end
+
+function map_update_p_proc()
+    light.lighting()
 end
 
 local exports = {}
 exports.start = start
+exports.map_enter_p_proc = map_enter_p_proc
+exports.map_update_p_proc = map_update_p_proc
 return exports
