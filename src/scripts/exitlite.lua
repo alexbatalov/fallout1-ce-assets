3946ac6bf7e7e2b92a026e20eabcecdcff7adf29
Alexander Batalov <alex.batalov@gmail.com>
2023-11-12 08:24:35 +0300

    Review exitlite.lua

--- src/scripts/exitlite.lua
+++ src/scripts/exitlite.lua
@@ -13,18 +13,12 @@ end
 function map_update_p_proc()
     if time.is_day() then
         fallout.obj_set_light_level(fallout.self_obj(), 100, 8)
-    else
-        if time.is_night() then
-            fallout.obj_set_light_level(fallout.self_obj(), 40, 8)
-        else
-            if time.is_morning() then
-                fallout.obj_set_light_level(fallout.self_obj(), fallout.game_time_hour() - 600 + 40, 8)
-            else
-                if time.is_evening() then
-                    fallout.obj_set_light_level(fallout.self_obj(), 100 - (fallout.game_time_hour() - 1800), 8)
-                end
-            end
-        end
+    elseif time.is_night() then
+        fallout.obj_set_light_level(fallout.self_obj(), 40, 8)
+    elseif time.is_morning() then
+        fallout.obj_set_light_level(fallout.self_obj(), fallout.game_time_hour() - 600 + 40, 8)
+    elseif time.is_evening() then
+        fallout.obj_set_light_level(fallout.self_obj(), 100 - (fallout.game_time_hour() - 1800), 8)
     end
 end
 
