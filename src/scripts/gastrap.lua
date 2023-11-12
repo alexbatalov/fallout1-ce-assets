6df32a183821d03d70a14b03e40bfc49588ef388
Alexander Batalov <alex.batalov@gmail.com>
2023-11-12 13:08:43 +0300

    Review gastrap

--- src/scripts/gastrap.lua
+++ src/scripts/gastrap.lua
@@ -1,23 +1,26 @@
 local fallout = require("fallout")
 
 local start
-
-local Damage = 0
+local spatial_p_proc
 
 function start()
     if fallout.script_action() == 2 then
-        if fallout.global_var(139) and fallout.global_var(140) then
-            if fallout.local_var(0) == 0 then
-                fallout.set_local_var(0, 1)
-                Damage = fallout.random(1, 6) - 3
-                fallout.float_msg(fallout.dude_obj(), fallout.message_str(335, 100), 0)
-                if Damage < 2 then
-                    fallout.display_msg(fallout.message_str(335, 101))
-                    fallout.critter_dmg(fallout.dude_obj(), 1, 0)
-                else
-                    fallout.display_msg(fallout.message_str(335, 102) .. Damage .. fallout.message_str(335, 103))
-                    fallout.critter_dmg(fallout.dude_obj(), Damage, 0)
-                end
+        spatial_p_proc()
+    end
+end
+
+function spatial_p_proc()
+    if fallout.global_var(139) ~= 0 and fallout.global_var(140) ~= 0 then
+        if fallout.local_var(0) == 0 then
+            fallout.set_local_var(0, 1)
+            local damage = fallout.random(1, 6) - 3
+            fallout.float_msg(fallout.dude_obj(), fallout.message_str(335, 100), 0)
+            if damage < 2 then
+                fallout.display_msg(fallout.message_str(335, 101))
+                fallout.critter_dmg(fallout.dude_obj(), 1, 0)
+            else
+                fallout.display_msg(fallout.message_str(335, 102) .. damage .. fallout.message_str(335, 103))
+                fallout.critter_dmg(fallout.dude_obj(), damage, 0)
             end
         end
     end
@@ -25,4 +28,5 @@ end
 
 local exports = {}
 exports.start = start
+exports.spatial_p_proc = spatial_p_proc
 return exports
