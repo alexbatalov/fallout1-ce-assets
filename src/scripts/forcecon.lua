d985e77734095fcd6c2300a5ea01f59d9b3275ae
Alexander Batalov <alex.batalov@gmail.com>
2023-11-12 12:38:17 +0300

    Review forcecon.lua

--- src/scripts/forcecon.lua
+++ src/scripts/forcecon.lua
@@ -5,12 +5,11 @@ local description_p_proc
 local use_p_proc
 
 function start()
-    if fallout.script_action() == 3 then
+    local script_action = fallout.script_action()
+    if script_action == 3 then
         description_p_proc()
-    else
-        if fallout.script_action() == 6 then
-            use_p_proc()
-        end
+    elseif script_action == 6 then
+        use_p_proc()
     end
 end
 
@@ -21,48 +20,37 @@ end
 
 function use_p_proc()
     fallout.set_external_var("field_change", "toggle")
-    if fallout.cur_map_index() == 31 then
-        if fallout.elevation(fallout.self_obj()) == 0 then
-            if fallout.tile_num(fallout.self_obj()) == 16308 then
+
+    local self_obj = fallout.self_obj()
+    local self_tile_num = fallout.tile_num(self_obj)
+    local self_elevation = fallout.elevation(self_obj)
+    local cur_map_index = fallout.cur_map_index()
+    if cur_map_index == 31 then
+        if self_elevation == 0 then
+            if self_tile_num == 16308 then
                 fallout.use_obj(fallout.map_var(0))
-            else
-                if fallout.tile_num(fallout.self_obj()) == 16280 then
-                    fallout.use_obj(fallout.map_var(1))
-                else
-                    if fallout.tile_num(fallout.self_obj()) == 25912 then
-                        fallout.use_obj(fallout.map_var(2))
-                    else
-                        if fallout.tile_num(fallout.self_obj()) == 25878 then
-                            fallout.use_obj(fallout.map_var(7))
-                        else
-                            if fallout.tile_num(fallout.self_obj()) == 21664 then
-                                fallout.use_obj(fallout.map_var(8))
-                            end
-                        end
-                    end
-                end
+            elseif self_tile_num == 16280 then
+                fallout.use_obj(fallout.map_var(1))
+            elseif self_tile_num == 25912 then
+                fallout.use_obj(fallout.map_var(2))
+            elseif self_tile_num == 25878 then
+                fallout.use_obj(fallout.map_var(7))
+            elseif self_tile_num == 21664 then
+                fallout.use_obj(fallout.map_var(8))
             end
-        else
-            if fallout.elevation(fallout.self_obj()) == 1 then
-                if fallout.tile_num(fallout.self_obj()) == 15502 then
-                    fallout.use_obj(fallout.map_var(10))
-                else
-                    if fallout.tile_num(fallout.self_obj()) == 26706 then
-                        fallout.use_obj(fallout.map_var(11))
-                    end
-                end
+        elseif self_elevation == 1 then
+            if self_tile_num == 15502 then
+                fallout.use_obj(fallout.map_var(10))
+            elseif self_tile_num == 26706 then
+                fallout.use_obj(fallout.map_var(11))
             end
         end
-    else
-        if fallout.cur_map_index() == 32 then
-            if fallout.elevation(fallout.self_obj()) == 0 then
-                if fallout.tile_num(fallout.self_obj()) == 17120 then
-                    fallout.use_obj(fallout.map_var(0))
-                else
-                    if fallout.tile_num(fallout.self_obj()) == 24326 then
-                        fallout.use_obj(fallout.map_var(1))
-                    end
-                end
+    elseif cur_map_index == 32 then
+        if self_elevation == 0 then
+            if self_tile_num == 17120 then
+                fallout.use_obj(fallout.map_var(0))
+            elseif self_tile_num == 24326 then
+                fallout.use_obj(fallout.map_var(1))
             end
         end
     end
