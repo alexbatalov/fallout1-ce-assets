300a8a8afbb27f4e1ecc98ec10d58729b4f4c78b
Alexander Batalov <alex.batalov@gmail.com>
2023-11-12 10:35:22 +0300

    Review floater.lua

--- src/scripts/floater.lua
+++ src/scripts/floater.lua
@@ -5,31 +5,28 @@ local start
 local destroy_p_proc
 local critter_p_proc
 
-local start_init = 0
-local hostile = 0
+local initialized = false
+local hostile = false
 
 function start()
-    local v0 = 0
-    if not(start_init) and fallout.metarule(14, 0) then
-        v0 = fallout.obj_pid(fallout.self_obj())
-        if v0 == 16777261 then
-            fallout.critter_add_trait(fallout.self_obj(), 1, 5, 73)
-            fallout.critter_add_trait(fallout.self_obj(), 1, 6, 43)
-        else
-            if v0 == 16777259 then
-                fallout.critter_add_trait(fallout.self_obj(), 1, 5, 72)
-                fallout.critter_add_trait(fallout.self_obj(), 1, 6, 43)
-            end
+    if not initialized and fallout.metarule(14, 0) ~= 0 then
+        local self_obj = fallout.self_obj()
+        local pid = fallout.obj_pid(self_obj)
+        if pid == 16777261 then
+            fallout.critter_add_trait(self_obj, 1, 5, 73)
+            fallout.critter_add_trait(self_obj, 1, 6, 43)
+        elseif pid == 16777259 then
+            fallout.critter_add_trait(self_obj, 1, 5, 72)
+            fallout.critter_add_trait(self_obj, 1, 6, 43)
         end
-        start_init = 1
-    else
-        if fallout.script_action() == 18 then
-            destroy_p_proc()
-        else
-            if fallout.script_action() == 12 then
-                critter_p_proc()
-            end
-        end
+        initialized = true
+    end
+
+    local script_action = fallout.script_action()
+    if script_action == 18 then
+        destroy_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
     end
 end
 
@@ -40,9 +37,9 @@ function destroy_p_proc()
 end
 
 function critter_p_proc()
-    if not(hostile) then
+    if not hostile then
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
-        hostile = 1
+        hostile = true
     end
 end
 
