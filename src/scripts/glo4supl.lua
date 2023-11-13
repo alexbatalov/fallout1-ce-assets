61017d17266f76766b129c82409ab01377468fb9
Alexander Batalov <alex.batalov@gmail.com>
2023-11-13 15:55:56 +0300

    Review glo4supl.lua

--- src/scripts/glo4supl.lua
+++ src/scripts/glo4supl.lua
@@ -1,73 +1,65 @@
 local fallout = require("fallout")
 
 local start
-local open_door
-local pick_lock
-local disarm_electronics
+local use_p_proc
+local use_obj_on_p_proc
+local use_skill_on_p_proc
 local arm_doors
 local shock
 
-local skill_roll = 0
-
 function start()
-    if fallout.script_action() == 6 then
-        open_door()
-    else
-        if fallout.script_action() == 7 then
-            pick_lock()
-        else
-            if fallout.script_action() == 8 then
-                disarm_electronics()
-            end
-        end
+    local script_action = fallout.script_action()
+    if script_action == 6 then
+        use_p_proc()
+    elseif script_action == 7 then
+        use_obj_on_p_proc()
+    elseif script_action == 8 then
+        use_skill_on_p_proc()
     end
 end
 
-function open_door()
+function use_p_proc()
     if fallout.local_var(3) == 1 then
         fallout.script_overrides()
         shock()
-    else
-        if fallout.local_var(0) == 0 then
-            arm_doors()
-        else
-            if fallout.local_var(1) == 0 then
-                fallout.script_overrides()
-                fallout.display_msg(fallout.message_str(503, 105))
-            end
-        end
+    elseif fallout.local_var(0) == 0 then
+        arm_doors()
+    elseif fallout.local_var(1) == 0 then
+        fallout.script_overrides()
+        fallout.display_msg(fallout.message_str(503, 105))
     end
 end
 
-function pick_lock()
+function use_obj_on_p_proc()
     fallout.script_overrides()
     if fallout.local_var(3) == 0 then
         shock()
+    elseif fallout.local_var(0) == 0 then
+        arm_doors()
     else
-        if fallout.local_var(0) == 0 then
-            arm_doors()
+        local skill_roll
+        -- FIXME: Comparing object and pid.
+        if fallout.obj_being_used_with() == 84 then
+            skill_roll = fallout.roll_vs_skill(fallout.dude_obj(), 9, 0)
         else
-            if fallout.obj_being_used_with() == 84 then
-                skill_roll = fallout.roll_vs_skill(fallout.dude_obj(), 9, 0)
+            skill_roll = fallout.roll_vs_skill(fallout.dude_obj(), 9, 3)
+        end
+        if fallout.local_var(1) == 1 then
+            fallout.display_msg(fallout.message_str(503, 100))
+        else
+            if fallout.is_success(skill_roll) then
+                fallout.display_msg(fallout.message_str(503, 101))
+                fallout.set_local_var(1, 1)
             else
-                skill_roll = fallout.roll_vs_skill(fallout.dude_obj(), 9, 3)
-            end
-            if fallout.local_var(1) == 1 then
-                fallout.display_msg(fallout.message_str(503, 100))
-            else
-                if fallout.is_success(skill_roll) then
-                    fallout.display_msg(fallout.message_str(503, 101))
-                    fallout.set_local_var(1, 1)
-                else
-                    fallout.display_msg(fallout.message_str(503, 102))
-                end
+                fallout.display_msg(fallout.message_str(503, 102))
             end
         end
     end
 end
 
-function disarm_electronics()
-    if (fallout.action_being_used() ~= 12) and (fallout.action_being_used() ~= 11) then
+function use_skill_on_p_proc()
+    local skill = fallout.action_being_used()
+    if skill ~= 12 and skill ~= 11 then
         if fallout.local_var(3) == 1 then
             shock()
         else
@@ -77,7 +69,7 @@ function disarm_electronics()
         if fallout.local_var(0) == 1 then
             fallout.display_msg(fallout.message_str(503, 106))
         else
-            if fallout.is_success(fallout.roll_vs_skill(fallout.dude_obj(), fallout.action_being_used(), 0)) then
+            if fallout.is_success(fallout.roll_vs_skill(fallout.dude_obj(), skill, 0)) then
                 fallout.display_msg(fallout.message_str(503, 107))
                 fallout.set_local_var(3, 0)
                 fallout.set_local_var(0, 1)
@@ -104,4 +96,7 @@ end
 
 local exports = {}
 exports.start = start
+exports.use_p_proc = use_p_proc
+exports.use_obj_on_p_proc = use_obj_on_p_proc
+exports.use_skill_on_p_proc = use_skill_on_p_proc
 return exports
