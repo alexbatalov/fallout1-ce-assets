78d0b24599907033dd740b8708c8662df4d36654
Alexander Batalov <alex.batalov@gmail.com>
2023-11-13 16:56:00 +0300

    Review glocomp.lua

--- src/scripts/glocomp.lua
+++ src/scripts/glocomp.lua
@@ -1,30 +1,21 @@
 local fallout = require("fallout")
 
 local start
+local look_at_p_proc
 
 function start()
-    local v0 = 0
-    if (fallout.script_action() == 21) or (fallout.script_action() == 3) then
-        fallout.script_overrides()
-        v0 = fallout.random(1, 4)
-        if v0 == 1 then
-            fallout.display_msg(fallout.message_str(299, 100))
-        else
-            if v0 == 2 then
-                fallout.display_msg(fallout.message_str(299, 101))
-            else
-                if v0 == 3 then
-                    fallout.display_msg(fallout.message_str(299, 102))
-                else
-                    if v0 == 4 then
-                        fallout.display_msg(fallout.message_str(299, 103))
-                    end
-                end
-            end
-        end
+    local script_action = fallout.script_action()
+    if script_action == 21 or script_action == 3 then
+        look_at_p_proc()
     end
+end
+
+function look_at_p_proc()
+    fallout.script_overrides()
+    fallout.display_msg(fallout.message_str(299, fallout.random(100, 103)))
 end
 
 local exports = {}
 exports.start = start
+exports.look_at_p_proc = look_at_p_proc
 return exports
