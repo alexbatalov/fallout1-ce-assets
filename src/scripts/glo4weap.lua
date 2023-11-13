5400f893913a74d07914c4ac54361c2323678428
Alexander Batalov <alex.batalov@gmail.com>
2023-11-13 16:00:48 +0300

    Review glo4weap.lua

--- src/scripts/glo4weap.lua
+++ src/scripts/glo4weap.lua
@@ -1,6 +1,10 @@
 local fallout = require("fallout")
 
 local start
+local use_p_proc
+local use_obj_on_p_proc
+local use_skill_on_p_proc
+local look_at_p_proc
 local Traps
 local Sciences
 local Picklocks
@@ -12,59 +16,69 @@ function start()
     if fallout.global_var(142) == 2 then
         fallout.set_local_var(1, 1)
     end
-    if (fallout.script_action() == 21) or (fallout.script_action() == 3) then
-        fallout.script_overrides()
-        fallout.display_msg(fallout.message_str(321, 100))
-    else
-        if fallout.script_action() == 6 then
-            if fallout.global_var(142) ~= 2 then
-                fallout.set_global_var(142, 1)
-            end
-            Using_Door()
+
+    local script_action = fallout.script_action()
+    if script_action == 21 or script_action == 3 then
+        look_at_p_proc()
+    elseif script_action == 6 then
+        use_p_proc()
+    elseif script_action == 8 then
+        use_skill_on_p_proc()
+    elseif script_action == 7 then
+        use_obj_on_p_proc()
+    end
+end
+
+function use_p_proc()
+    if fallout.global_var(142) ~= 2 then
+        fallout.set_global_var(142, 1)
+    end
+    Using_Door()
+end
+
+function use_obj_on_p_proc()
+    if fallout.global_var(142) ~= 2 then
+        fallout.set_global_var(142, 1)
+    end
+    if fallout.obj_pid(fallout.obj_being_used_with()) == 96 then
+        if fallout.local_var(1) == 0 then
+            fallout.script_overrides()
+            local damage = fallout.random(3, 7)
+            fallout.critter_dmg(fallout.dude_obj(), damage, 0)
+            fallout.display_msg(fallout.message_str(321, 102) .. damage .. fallout.message_str(321, 103))
+            fallout.set_local_var(0, fallout.local_var(0) + 1)
         else
-            if fallout.script_action() == 8 then
-                if fallout.global_var(142) ~= 2 then
-                    fallout.set_global_var(142, 1)
-                end
-                if fallout.action_being_used() == 9 then
-                    Picklocks()
-                else
-                    if fallout.action_being_used() == 11 then
-                        Traps()
-                    else
-                        if fallout.action_being_used() == 12 then
-                            Sciences()
-                        else
-                            fallout.display_msg(fallout.message_str(321, 101))
-                        end
-                    end
-                end
-            else
-                if fallout.script_action() == 7 then
-                    if fallout.global_var(142) ~= 2 then
-                        fallout.set_global_var(142, 1)
-                    end
-                    if fallout.obj_pid(fallout.obj_being_used_with()) == 96 then
-                        if fallout.local_var(1) == 0 then
-                            fallout.script_overrides()
-                            Shocked = fallout.random(3, 7)
-                            fallout.critter_dmg(fallout.dude_obj(), Shocked, 0)
-                            fallout.display_msg(fallout.message_str(321, 102) .. Shocked .. fallout.message_str(321, 103))
-                            fallout.set_local_var(0, fallout.local_var(0) + 1)
-                        else
-                            if fallout.local_var(2) == 0 then
-                                fallout.script_overrides()
-                                fallout.set_local_var(2, 1)
-                                fallout.display_msg(fallout.message_str(321, 104))
-                            end
-                        end
-                    else
-                        fallout.display_msg(fallout.message_str(321, 105))
-                    end
-                end
+            if fallout.local_var(2) == 0 then
+                fallout.script_overrides()
+                fallout.set_local_var(2, 1)
+                fallout.display_msg(fallout.message_str(321, 104))
             end
         end
+    else
+        fallout.display_msg(fallout.message_str(321, 105))
     end
+end
+
+function use_skill_on_p_proc()
+    if fallout.global_var(142) ~= 2 then
+        fallout.set_global_var(142, 1)
+    end
+
+    local skill = fallout.action_being_used()
+    if skill == 9 then
+        Picklocks()
+    elseif skill == 11 then
+        Traps()
+    elseif skill == 12 then
+        Sciences()
+    else
+        fallout.display_msg(fallout.message_str(321, 101))
+    end
+end
+
+function look_at_p_proc()
+    fallout.script_overrides()
+    fallout.display_msg(fallout.message_str(321, 100))
 end
 
 function Traps()
@@ -74,9 +88,9 @@ function Traps()
             fallout.display_msg(fallout.message_str(321, 113))
         else
             fallout.set_local_var(0, 5)
-            Shocked = fallout.random(2, fallout.local_var(0))
-            fallout.critter_dmg(fallout.dude_obj(), Shocked, 0)
-            fallout.display_msg(fallout.message_str(321, 114) .. Shocked .. fallout.message_str(321, 115))
+            local damage = fallout.random(2, fallout.local_var(0))
+            fallout.critter_dmg(fallout.dude_obj(), damage, 0)
+            fallout.display_msg(fallout.message_str(321, 114) .. damage .. fallout.message_str(321, 115))
             fallout.set_local_var(0, fallout.local_var(0) + 1)
         end
     else
@@ -104,9 +118,9 @@ end
 function Picklocks()
     if fallout.local_var(1) == 0 then
         if fallout.local_var(0) > 2 then
-            Shocked = fallout.random(2, fallout.local_var(0))
-            fallout.critter_dmg(fallout.dude_obj(), Shocked, 0)
-            fallout.display_msg(fallout.message_str(321, 121) .. Shocked .. fallout.message_str(321, 122))
+            local damage = fallout.random(2, fallout.local_var(0))
+            fallout.critter_dmg(fallout.dude_obj(), damage, 0)
+            fallout.display_msg(fallout.message_str(321, 121) .. damage .. fallout.message_str(321, 122))
             fallout.set_local_var(0, fallout.local_var(0) + 1)
         else
             fallout.set_local_var(0, fallout.local_var(0) + 1)
@@ -126,26 +140,24 @@ function Using_Door()
     if fallout.local_var(1) == 0 then
         fallout.script_overrides()
         if fallout.local_var(0) > 2 then
-            Shocked = fallout.random(2, fallout.local_var(0))
-            fallout.critter_dmg(fallout.dude_obj(), Shocked, 0)
-            fallout.display_msg(fallout.message_str(321, 106) .. Shocked .. fallout.message_str(321, 107))
+            local damage = fallout.random(2, fallout.local_var(0))
+            fallout.critter_dmg(fallout.dude_obj(), damage, 0)
+            fallout.display_msg(fallout.message_str(321, 106) .. damage .. fallout.message_str(321, 107))
             fallout.set_local_var(0, fallout.local_var(0) + 1)
         else
             fallout.set_local_var(0, fallout.local_var(0) + 1)
             fallout.display_msg(fallout.message_str(321, 108))
         end
-    else
-        if fallout.local_var(2) == 0 then
-            fallout.script_overrides()
-            fallout.display_msg(fallout.message_str(321, 109))
-            if fallout.is_success(fallout.do_check(fallout.dude_obj(), 1, 0)) then
-                fallout.display_msg(fallout.message_str(321, 110))
+    elseif fallout.local_var(2) == 0 then
+        fallout.script_overrides()
+        fallout.display_msg(fallout.message_str(321, 109))
+        if fallout.is_success(fallout.do_check(fallout.dude_obj(), 1, 0)) then
+            fallout.display_msg(fallout.message_str(321, 110))
+        else
+            if fallout.is_success(fallout.roll_vs_skill(fallout.dude_obj(), 11, 0)) then
+                fallout.display_msg(fallout.message_str(321, 111))
             else
-                if fallout.is_success(fallout.roll_vs_skill(fallout.dude_obj(), 11, 0)) then
-                    fallout.display_msg(fallout.message_str(321, 111))
-                else
-                    fallout.display_msg(fallout.message_str(321, 112))
-                end
+                fallout.display_msg(fallout.message_str(321, 112))
             end
         end
     end
@@ -153,4 +165,8 @@ end
 
 local exports = {}
 exports.start = start
+exports.use_p_proc = use_p_proc
+exports.use_obj_on_p_proc = use_obj_on_p_proc
+exports.use_skill_on_p_proc = use_skill_on_p_proc
+exports.look_at_p_proc = look_at_p_proc
 return exports
