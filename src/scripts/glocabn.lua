cfbd75e77b4c6fd096bde18515c71c86447bd773
Alexander Batalov <alex.batalov@gmail.com>
2023-11-13 16:54:35 +0300

    Review glocabn.lua

--- src/scripts/glocabn.lua
+++ src/scripts/glocabn.lua
@@ -1,41 +1,52 @@
 local fallout = require("fallout")
 
 local start
+local pickup_p_proc
+local use_skill_on_p_proc
+local look_at_p_proc
 
 function start()
-    if (fallout.script_action() == 21) or (fallout.script_action() == 3) then
-        fallout.script_overrides()
-        fallout.display_msg(fallout.message_str(334, 100))
-    else
-        if fallout.script_action() == 4 then
-            if fallout.local_var(0) == 0 then
-                fallout.script_overrides()
-                fallout.display_msg(fallout.message_str(334, 101))
-            end
-        else
-            if fallout.script_action() == 8 then
-                if fallout.action_being_used() == 9 then
-                    if fallout.is_success(fallout.roll_vs_skill(fallout.dude_obj(), 9, -15)) then
-                        fallout.set_local_var(0, 1)
-                        fallout.display_msg(fallout.message_str(334, 102))
-                    else
-                        fallout.display_msg(fallout.message_str(334, 103))
-                    end
-                else
-                    if fallout.action_being_used() == 12 then
-                        if fallout.is_success(fallout.roll_vs_skill(fallout.dude_obj(), 12, -5)) then
-                            fallout.set_local_var(0, 1)
-                            fallout.display_msg(fallout.message_str(334, 104))
-                        else
-                            fallout.display_msg(fallout.message_str(334, 105))
-                        end
-                    else
-                        fallout.display_msg(fallout.message_str(334, 106))
-                    end
-                end
-            end
-        end
+    local script_action = fallout.script_action()
+    if script_action == 21 or script_action == 3 then
+        look_at_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 8 then
+        use_skill_on_p_proc()
     end
+end
+
+function pickup_p_proc()
+    if fallout.local_var(0) == 0 then
+        fallout.script_overrides()
+        fallout.display_msg(fallout.message_str(334, 101))
+    end
+end
+
+function use_skill_on_p_proc()
+    local skill = fallout.action_being_used()
+    if skill == 9 then
+        if fallout.is_success(fallout.roll_vs_skill(fallout.dude_obj(), 9, -15)) then
+            fallout.set_local_var(0, 1)
+            fallout.display_msg(fallout.message_str(334, 102))
+        else
+            fallout.display_msg(fallout.message_str(334, 103))
+        end
+    elseif skill == 12 then
+        if fallout.is_success(fallout.roll_vs_skill(fallout.dude_obj(), 12, -5)) then
+            fallout.set_local_var(0, 1)
+            fallout.display_msg(fallout.message_str(334, 104))
+        else
+            fallout.display_msg(fallout.message_str(334, 105))
+        end
+    else
+        fallout.display_msg(fallout.message_str(334, 106))
+    end
+end
+
+function look_at_p_proc()
+    fallout.script_overrides()
+    fallout.display_msg(fallout.message_str(334, 100))
 end
 
 local exports = {}
