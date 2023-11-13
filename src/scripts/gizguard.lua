1383cd7dc54ec1ded8f361e2844ad0a0614b132c
Alexander Batalov <alex.batalov@gmail.com>
2023-11-13 15:35:13 +0300

    Review gizguard.lua

--- src/scripts/gizguard.lua
+++ src/scripts/gizguard.lua
@@ -20,49 +20,42 @@ local GizGuard07
 local GizGuard08
 local GizGuardEnd
 
-local hostile = 0
+local hostile = false
 local initialized = false
-local sneak_checked = 0
+local sneak_checked = false
 
 function start()
-    if fallout.script_action() == 12 then
+    local script_action = fallout.script_action()
+    if script_action == 12 then
         critter_p_proc()
-    else
-        if fallout.script_action() == 18 then
-            destroy_p_proc()
-        else
-            if fallout.script_action() == 15 then
-                map_enter_p_proc()
-            else
-                if fallout.script_action() == 4 then
-                    pickup_p_proc()
-                else
-                    if fallout.script_action() == 11 then
-                        talk_p_proc()
-                    else
-                        if fallout.script_action() == 22 then
-                            timed_event_p_proc()
-                        end
-                    end
-                end
-            end
-        end
+    elseif script_action == 18 then
+        destroy_p_proc()
+    elseif script_action == 15 then
+        map_enter_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 22 then
+        timed_event_p_proc()
     end
 end
 
 function critter_p_proc()
+    local dude_obj = fallout.dude_obj()
+    local self_obj = fallout.self_obj()
     if hostile then
-        if fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) then
-            hostile = 0
-            fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
+        if fallout.obj_can_see_obj(self_obj, dude_obj) then
+            hostile = false
+            fallout.attack(dude_obj, 0, 1, 0, 0, 30000, 0, 0)
         end
     else
-        if fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) then
-            if fallout.tile_distance_objs(fallout.self_obj(), fallout.dude_obj()) < 12 then
-                if (fallout.obj_item_subtype(fallout.critter_inven_obj(fallout.dude_obj(), 1)) == 3) or (fallout.obj_item_subtype(fallout.critter_inven_obj(fallout.dude_obj(), 2)) == 3) and not(fallout.external_var("weapon_checked")) then
+        if fallout.obj_can_see_obj(self_obj, dude_obj) then
+            if fallout.tile_distance_objs(self_obj, dude_obj) < 12 then
+                if (fallout.obj_item_subtype(fallout.critter_inven_obj(dude_obj, 1)) == 3) or (fallout.obj_item_subtype(fallout.critter_inven_obj(dude_obj, 2)) == 3) and not (fallout.external_var("weapon_checked")) then
                     fallout.dialogue_system_enter()
                 else
-                    if fallout.using_skill(fallout.dude_obj(), 8) and not(sneak_checked) then
+                    if fallout.using_skill(fallout.dude_obj(), 8) and not (sneak_checked) then
                         fallout.dialogue_system_enter()
                     end
                 end
@@ -70,8 +63,8 @@ function critter_p_proc()
         end
     end
     if fallout.global_var(347) == 1 then
-        if fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) then
-            fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
+        if fallout.obj_can_see_obj(self_obj, dude_obj) then
+            fallout.attack(dude_obj, 0, 1, 0, 0, 30000, 0, 0)
         end
     end
 end
@@ -84,33 +77,35 @@ function destroy_p_proc()
 end
 
 function map_enter_p_proc()
-    if not(fallout.obj_is_carrying_obj_pid(fallout.self_obj(), 41)) then
-        fallout.item_caps_adjust(fallout.self_obj(), fallout.random(0, 10))
+    local self_obj = fallout.self_obj()
+    if fallout.obj_is_carrying_obj_pid(self_obj, 41) == 0 then
+        fallout.item_caps_adjust(self_obj, fallout.random(0, 10))
     end
     if fallout.global_var(32) == 4 then
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 87)
+        fallout.critter_add_trait(self_obj, 1, 6, 87)
     else
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 13)
+        fallout.critter_add_trait(self_obj, 1, 6, 13)
     end
     if (fallout.global_var(38) == 1) or (fallout.global_var(104) == 2) then
-        fallout.set_obj_visibility(fallout.self_obj(), 1)
-        fallout.move_to(fallout.self_obj(), 7000, 0)
-        fallout.set_external_var("removal_ptr", fallout.self_obj())
+        fallout.set_obj_visibility(self_obj, true)
+        fallout.move_to(self_obj, 7000, 0)
+        fallout.set_external_var("removal_ptr", self_obj)
     end
 end
 
 function pickup_p_proc()
-    hostile = 1
+    hostile = true
 end
 
 function talk_p_proc()
-    if (fallout.obj_item_subtype(fallout.critter_inven_obj(fallout.dude_obj(), 1)) == 3) or (fallout.obj_item_subtype(fallout.critter_inven_obj(fallout.dude_obj(), 2)) == 3) then
+    local dude_obj = fallout.dude_obj()
+    if fallout.obj_item_subtype(fallout.critter_inven_obj(dude_obj, 1)) == 3 or fallout.obj_item_subtype(fallout.critter_inven_obj(dude_obj, 2)) == 3 then
         GizGuard00()
     else
-        if fallout.using_skill(fallout.dude_obj(), 8) then
+        if fallout.using_skill(dude_obj, 8) then
             GizGuard01()
         else
-            if not(fallout.local_var(0)) then
+            if fallout.local_var(0) == 0 then
                 fallout.start_gdialog(639, fallout.self_obj(), 4, -1, -1)
                 fallout.gsay_start()
                 GizGuard02()
@@ -124,20 +119,19 @@ function talk_p_proc()
 end
 
 function timed_event_p_proc()
-    if fallout.fixed_param() == 1 then
-        if (fallout.obj_item_subtype(fallout.critter_inven_obj(fallout.dude_obj(), 1)) == 3) or (fallout.obj_item_subtype(fallout.critter_inven_obj(fallout.dude_obj(), 2)) == 3) then
+    local event = fallout.fixed_param()
+    if event == 1 then
+        if fallout.obj_item_subtype(fallout.critter_inven_obj(fallout.dude_obj(), 1)) == 3 or fallout.obj_item_subtype(fallout.critter_inven_obj(fallout.dude_obj(), 2)) == 3 then
             fallout.float_msg(fallout.self_obj(), fallout.message_str(639, fallout.random(100, 103)), 2)
-            hostile = 1
+            hostile = true
         else
             fallout.set_external_var("weapon_checked", 0)
         end
-    else
-        if fallout.fixed_param() == 2 then
-            if fallout.using_skill(fallout.dude_obj(), 8) then
-                hostile = 1
-            else
-                sneak_checked = 0
-            end
+    elseif event == 2 then
+        if fallout.using_skill(fallout.dude_obj(), 8) then
+            hostile = true
+        else
+            sneak_checked = false
         end
     end
 end
@@ -156,7 +150,7 @@ end
 
 function GizGuard01()
     fallout.float_msg(fallout.self_obj(), fallout.message_str(639, fallout.random(104, 106)), 2)
-    sneak_checked = 1
+    sneak_checked = true
     fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(5), 2)
 end
 
