2f0c2b83b2f3e10dcb27ff9750846e5a0795f6af
Alexander Batalov <alex.batalov@gmail.com>
2023-11-19 11:23:42 +0300

    Review valtcomp.lua

--- src/scripts/valtcomp.lua
+++ src/scripts/valtcomp.lua
@@ -5,12 +5,11 @@ local use_p_proc
 local use_skill_on_p_proc
 
 function start()
-    if fallout.script_action() == 6 then
+    local script_action = fallout.script_action()
+    if script_action == 6 then
         use_p_proc()
-    else
-        if fallout.script_action() == 8 then
-            use_skill_on_p_proc()
-        end
+    elseif script_action == 8 then
+        use_skill_on_p_proc()
     end
 end
 
