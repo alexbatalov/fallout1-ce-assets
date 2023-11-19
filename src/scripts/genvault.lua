e5d5dffc1bca92e8ab932fd6d13056dc5dcd3605
Alexander Batalov <alex.batalov@gmail.com>
2023-11-19 09:54:32 +0300

    Review genvault.lua

--- src/scripts/genvault.lua
+++ src/scripts/genvault.lua
@@ -36,67 +36,55 @@ local wake_time = 0
 local sleep_time = 0
 local home_tile = 0
 local sleep_tile = 0
-local gword1 = 0
-local gword2 = 0
-local PICK = 0
-local hostile = 0
-local sleeping_disabled = 0
+local hostile = false
+local sleeping_disabled = false
 local ration_tile = 0
 
-local exit_line = 0
-
 function start()
-    if fallout.script_action() == 12 then
+    local script_action = fallout.script_action()
+    if script_action == 12 then
         critter_p_proc()
-    else
-        if fallout.script_action() == 14 then
-            damage_p_proc()
-        else
-            if fallout.script_action() == 18 then
-                destroy_p_proc()
-            else
-                if fallout.script_action() == 15 then
-                    map_enter_p_proc()
-                else
-                    if fallout.script_action() == 4 then
-                        pickup_p_proc()
-                    else
-                        if fallout.script_action() == 11 then
-                            talk_p_proc()
-                        else
-                            if fallout.script_action() == 22 then
-                                timed_event_p_proc()
-                            end
-                        end
-                    end
-                end
-            end
-        end
+    elseif script_action == 14 then
+        damage_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
+    elseif script_action == 15 then
+        map_enter_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 22 then
+        timed_event_p_proc()
     end
 end
 
 function critter_p_proc()
+    local dude_obj = fallout.dude_obj()
     if hostile then
-        hostile = 0
-        fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
+        hostile = false
+        fallout.attack(dude_obj, 0, 1, 0, 0, 30000, 0, 0)
     else
-        if fallout.local_var(7) and (fallout.tile_distance_objs(fallout.self_obj(), fallout.dude_obj()) < 8) then
+        local self_obj = fallout.self_obj()
+        if fallout.local_var(7) and (fallout.tile_distance_objs(self_obj, dude_obj) < 8) then
             behaviour.flee_dude(1)
         end
         if fallout.global_var(101) == 0 then
             if fallout.local_var(5) == 0 then
-                if (fallout.elevation(fallout.self_obj()) == fallout.elevation(fallout.external_var("WtrGrd_ptr"))) and (fallout.elevation(fallout.self_obj()) == fallout.elevation(fallout.dude_obj())) then
-                    if (fallout.game_time_hour() > 700) and (fallout.game_time_hour() < 900) then
+                local self_elevation = fallout.elevation(self_obj)
+                if self_elevation == fallout.elevation(fallout.external_var("WtrGrd_ptr"))
+                    and self_elevation == fallout.elevation(dude_obj) then
+                    if fallout.game_time_hour() > 700 and fallout.game_time_hour() < 900 then
                         get_rations()
                     end
                 end
             end
         end
         if not time.is_day() then
-            sleeping_disabled = 0
+            sleeping_disabled = false
             fallout.set_local_var(5, 0)
         end
-        if sleeping_disabled == 0 then
+        if not sleeping_disabled then
             behaviour.sleeping(6, night_person, wake_time, sleep_time, home_tile, sleep_tile)
         end
     end
@@ -128,7 +116,7 @@ function map_enter_p_proc()
 end
 
 function pickup_p_proc()
-    hostile = 1
+    hostile = true
 end
 
 function talk_p_proc()
@@ -139,48 +127,36 @@ function talk_p_proc()
         else
             fallout.display_msg(fallout.message_str(185, 167))
         end
+    elseif fallout.local_var(7) ~= 0 or fallout.global_var(261) ~= 0 then
+        Vault00()
+    elseif fallout.global_var(101) ~= 0 and fallout.global_var(101) ~= 1 then
+        Vault01()
+    elseif fallout.obj_item_subtype(fallout.critter_inven_obj(fallout.dude_obj(), 1)) == 3 or fallout.obj_item_subtype(fallout.critter_inven_obj(fallout.dude_obj(), 2)) == 3 then
+        Vault02()
+    elseif fallout.global_var(10) < 80 then
+        Vault00b()
+    elseif fallout.global_var(10) < 40 then
+        Vault00c()
     else
-        if fallout.local_var(7) or fallout.global_var(261) then
-            Vault00()
-        else
-            if (fallout.global_var(101) ~= 0) and (fallout.global_var(101) ~= 1) then
-                Vault01()
-            else
-                if (fallout.obj_item_subtype(fallout.critter_inven_obj(fallout.dude_obj(), 1)) == 3) or (fallout.obj_item_subtype(fallout.critter_inven_obj(fallout.dude_obj(), 2)) == 3) then
-                    Vault02()
-                else
-                    if fallout.global_var(10) < 80 then
-                        Vault00b()
-                    else
-                        if fallout.global_var(10) < 40 then
-                            Vault00c()
-                        else
-                            Vault00a()
-                        end
-                    end
-                end
-            end
-        end
+        Vault00a()
     end
 end
 
 function timed_event_p_proc()
-    if fallout.fixed_param() == 1 then
-        fallout.animate_move_obj_to_tile(fallout.self_obj(), fallout.tile_num_in_direction(fallout.tile_num(fallout.external_var("WtrGrd_ptr")), 2, 1), 0)
+    local event = fallout.fixed_param()
+    if event == 1 then
+        fallout.animate_move_obj_to_tile(fallout.self_obj(),
+            fallout.tile_num_in_direction(fallout.tile_num(fallout.external_var("WtrGrd_ptr")), 2, 1), 0)
         fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(3), 2)
-    else
-        if fallout.fixed_param() == 2 then
-            fallout.set_external_var("getting_ration", 1)
-            if fallout.random(0, 1) then
-                fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 165), 0)
-            end
-            fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(3), 3)
-        else
-            if fallout.fixed_param() == 3 then
-                fallout.set_external_var("recipient", 0)
-                fallout.animate_move_obj_to_tile(fallout.self_obj(), home_tile, 0)
-            end
+    elseif event == 2 then
+        fallout.set_external_var("getting_ration", 1)
+        if fallout.random(0, 1) then
+            fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 165), 0)
         end
+        fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(3), 3)
+    elseif event == 3 then
+        fallout.set_external_var("recipient", nil)
+        fallout.animate_move_obj_to_tile(fallout.self_obj(), home_tile, 0)
     end
 end
 
@@ -191,66 +167,52 @@ end
 function Vault00a()
     if fallout.local_var(1) == 3 then
         Vault03()
+    elseif fallout.local_var(1) == 2 then
+        Vault06()
     else
-        if fallout.local_var(1) == 2 then
-            Vault06()
-        else
-            Vault09()
-        end
+        Vault09()
     end
 end
 
 function Vault00b()
     if fallout.local_var(1) == 3 then
         Vault04()
+    elseif fallout.local_var(1) == 2 then
+        Vault07()
     else
-        if fallout.local_var(1) == 2 then
-            Vault07()
-        else
-            Vault10()
-        end
+        Vault10()
     end
 end
 
 function Vault00c()
     if fallout.local_var(1) == 3 then
         Vault05()
+    elseif fallout.local_var(1) == 2 then
+        Vault08()
     else
-        if fallout.local_var(1) == 2 then
-            Vault08()
-        else
-            Vault11()
-        end
+        Vault11()
     end
 end
 
 function Vault01()
-    PICK = fallout.random(1, 5)
-    if PICK == 1 then
+    local rnd = fallout.random(1, 5)
+    if rnd == 1 then
         fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 109), 0)
-    else
-        if PICK == 2 then
-            fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 110), 0)
+    elseif rnd == 2 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 110), 0)
+    elseif rnd == 3 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 111), 0)
+    elseif rnd == 4 then
+        if fallout.get_critter_stat(fallout.dude_obj(), 34) == 0 then
+            fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 112), 0)
         else
-            if PICK == 3 then
-                fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 111), 0)
-            else
-                if PICK == 4 then
-                    if fallout.get_critter_stat(fallout.dude_obj(), 34) == 0 then
-                        fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 112), 0)
-                    else
-                        fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 113), 0)
-                    end
-                else
-                    if PICK == 5 then
-                        if fallout.get_critter_stat(fallout.dude_obj(), 34) == 0 then
-                            fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 114), 0)
-                        else
-                            fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 115), 0)
-                        end
-                    end
-                end
-            end
+            fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 113), 0)
+        end
+    elseif rnd == 5 then
+        if fallout.get_critter_stat(fallout.dude_obj(), 34) == 0 then
+            fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 114), 0)
+        else
+            fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 115), 0)
         end
     end
 end
@@ -261,21 +223,17 @@ function Vault02()
 end
 
 function Vault03()
-    PICK = fallout.random(1, 4)
-    if PICK == 1 then
+    local rnd = fallout.random(1, 4)
+    if rnd == 1 then
         fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 120), 0)
-    else
-        if PICK == 2 then
-            fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 121), 0)
-        else
-            if PICK == 3 then
-                fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 122), 0)
-            else
-                if PICK == 4 then
-                    fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 123) .. fallout.proto_data(fallout.obj_pid(fallout.dude_obj()), 1) .. fallout.message_str(0, 124), 0)
-                end
-            end
-        end
+    elseif rnd == 2 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 121), 0)
+    elseif rnd == 3 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 122), 0)
+    elseif rnd == 4 then
+        fallout.float_msg(fallout.self_obj(),
+            fallout.message_str(185, 123) ..
+            fallout.proto_data(fallout.obj_pid(fallout.dude_obj()), 1) .. fallout.message_str(0, 124), 0)
     end
 end
 
@@ -288,25 +246,19 @@ function Vault05()
 end
 
 function Vault06()
-    PICK = fallout.random(1, 5)
-    if PICK == 1 then
+    local rnd = fallout.random(1, 5)
+    if rnd == 1 then
         fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 134), 0)
-    else
-        if PICK == 2 then
-            fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 135) .. fallout.proto_data(fallout.obj_pid(fallout.dude_obj()), 1) .. fallout.message_str(185, 136), 0)
-        else
-            if PICK == 3 then
-                fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 137), 0)
-            else
-                if PICK == 4 then
-                    fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 138), 0)
-                else
-                    if PICK == 5 then
-                        fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 139), 0)
-                    end
-                end
-            end
-        end
+    elseif rnd == 2 then
+        fallout.float_msg(fallout.self_obj(),
+            fallout.message_str(185, 135) ..
+            fallout.proto_data(fallout.obj_pid(fallout.dude_obj()), 1) .. fallout.message_str(185, 136), 0)
+    elseif rnd == 3 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 137), 0)
+    elseif rnd == 4 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 138), 0)
+    elseif rnd == 5 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 139), 0)
     end
 end
 
@@ -319,89 +271,79 @@ function Vault08()
 end
 
 function Vault09()
-    PICK = fallout.random(1, 5)
-    if PICK == 1 then
-        fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 150) .. fallout.proto_data(fallout.obj_pid(fallout.dude_obj()), 1) .. fallout.message_str(185, 151), 0)
+    local rnd = fallout.random(1, 5)
+    if rnd == 1 then
+        fallout.float_msg(fallout.self_obj(),
+            fallout.message_str(185, 150) ..
+            fallout.proto_data(fallout.obj_pid(fallout.dude_obj()), 1) .. fallout.message_str(185, 151), 0)
+    elseif rnd >= 4 then
+        fallout.display_msg(fallout.message_str(185, 150 + rnd))
     else
-        if PICK >= 4 then
-            fallout.display_msg(fallout.message_str(185, 150 + PICK))
-        else
-            fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 150 + PICK), 0)
-        end
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 150 + rnd), 0)
     end
 end
 
 function Vault10()
-    PICK = fallout.random(1, 5)
-    if PICK == 5 then
+    local rnd = fallout.random(1, 5)
+    if rnd == 5 then
         fallout.display_msg(fallout.message_str(185, 160))
     else
-        fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 155 + PICK), 0)
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 155 + rnd), 0)
     end
 end
 
 function Vault11()
-    PICK = fallout.random(1, 5)
-    if PICK == 1 then
+    local rnd = fallout.random(1, 5)
+    if rnd == 1 then
         fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 161), 0)
-    else
-        if PICK == 2 then
-            fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 162), 0)
-        else
-            if PICK == 3 then
-                fallout.display_msg(fallout.message_str(185, 163))
-            else
-                if PICK == 4 then
-                    fallout.display_msg(fallout.message_str(185, 164))
-                end
-            end
-        end
+    elseif rnd == 2 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 162), 0)
+    elseif rnd == 3 then
+        fallout.display_msg(fallout.message_str(185, 163))
+    elseif rnd == 4 then
+        fallout.display_msg(fallout.message_str(185, 164))
     end
+    -- FIXME: Missing 5.
 end
 
 function get_rations()
-    sleeping_disabled = 1
-    if fallout.tile_num(fallout.self_obj()) ~= ration_tile then
+    local self_obj = fallout.self_obj()
+    sleeping_disabled = true
+    if fallout.tile_num(self_obj) ~= ration_tile then
         if fallout.random(0, 1) then
-            fallout.animate_move_obj_to_tile(fallout.self_obj(), ration_tile, 0)
+            fallout.animate_move_obj_to_tile(self_obj, ration_tile, 0)
         else
-            fallout.animate_move_obj_to_tile(fallout.self_obj(), ration_tile, 1)
+            fallout.animate_move_obj_to_tile(self_obj, ration_tile, 1)
         end
     else
-        if not(fallout.external_var("recipient")) then
-            fallout.set_external_var("recipient", fallout.self_obj())
-            fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(3), 1)
+        if fallout.external_var("recipient") == nil then
+            fallout.set_external_var("recipient", self_obj)
+            fallout.add_timer_event(self_obj, fallout.game_ticks(3), 1)
             fallout.set_local_var(5, 1)
-            sleeping_disabled = 0
+            sleeping_disabled = false
         end
     end
 end
 
 function set_ration_tile()
-    if fallout.elevation(fallout.self_obj()) == 0 then
+    local self_elevation = fallout.elevation(fallout.self_obj())
+    if self_elevation == 0 then
         ration_tile = fallout.tile_num_in_direction(14704, fallout.random(0, 5), fallout.random(1, 2))
-    else
-        if fallout.elevation(fallout.self_obj()) == 1 then
-            ration_tile = fallout.tile_num_in_direction(21895, fallout.random(0, 5), fallout.random(1, 2))
-        else
-            if fallout.elevation(fallout.self_obj()) == 2 then
-                ration_tile = fallout.tile_num_in_direction(22513, fallout.random(0, 5), fallout.random(1, 2))
-            end
-        end
+    elseif self_elevation == 1 then
+        ration_tile = fallout.tile_num_in_direction(21895, fallout.random(0, 5), fallout.random(1, 2))
+    elseif self_elevation == 2 then
+        ration_tile = fallout.tile_num_in_direction(22513, fallout.random(0, 5), fallout.random(1, 2))
     end
 end
 
 function set_sleep_tile()
-    if fallout.elevation(fallout.self_obj()) == 0 then
+    local self_elevation = fallout.elevation(fallout.self_obj())
+    if self_elevation == 0 then
         sleep_tile = 7000
-    else
-        if fallout.elevation(fallout.self_obj()) == 1 then
-            sleep_tile = home_tile
-        else
-            if fallout.elevation(fallout.self_obj()) == 2 then
-                sleep_tile = 7000
-            end
-        end
+    elseif self_elevation == 1 then
+        sleep_tile = home_tile
+    elseif self_elevation == 2 then
+        sleep_tile = 7000
     end
 end
 
