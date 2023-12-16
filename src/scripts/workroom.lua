c348144215563b25ce431e234e46c52c76cd050b
Alexander Batalov <alex.batalov@gmail.com>
2023-12-16 12:20:31 +0300

    Review workroom.lua

--- src/scripts/workroom.lua
+++ src/scripts/workroom.lua
@@ -1,40 +1,49 @@
 local fallout = require("fallout")
 
 local start
-local initialize
+local use_p_proc
+local map_enter_p_proc
 local look_at_p_proc
+local timed_event_p_proc
 local explode
 
-local new_tile = 0
-
 function start()
-    if fallout.script_action() == 15 then
-        initialize()
-    else
-        if fallout.script_action() == 22 then
-            explode()
-        else
-            if fallout.script_action() == 6 then
-                explode()
-            end
-        end
+    local script_action = fallout.script_action()
+    if script_action == 15 then
+        map_enter_p_proc()
+    elseif script_action == 22 then
+        explode()
+    elseif script_action == 6 then
+        explode()
     end
 end
 
-function initialize()
+function use_p_proc()
+    explode()
+end
+
+function map_enter_p_proc()
     fallout.set_external_var("table_ptr", fallout.self_obj())
 end
 
 function look_at_p_proc()
 end
 
+function timed_event_p_proc()
+    explode()
+end
+
 function explode()
-    new_tile = fallout.tile_num_in_direction(fallout.tile_num(fallout.self_obj()), 1, 1)
-    fallout.explosion(new_tile, fallout.elevation(fallout.self_obj()), 0)
-    fallout.float_msg(fallout.self_obj(), fallout.message_str(736, fallout.random(108, 115)), 8)
+    local self_obj = fallout.self_obj()
+    local new_tile = fallout.tile_num_in_direction(fallout.tile_num(self_obj), 1, 1)
+    fallout.explosion(new_tile, fallout.elevation(self_obj), 0)
+    fallout.float_msg(self_obj, fallout.message_str(736, fallout.random(108, 115)), 8)
 end
 
 local exports = {}
 exports.start = start
+exports.use_p_proc = use_p_proc
+exports.map_enter_p_proc = map_enter_p_proc
 exports.look_at_p_proc = look_at_p_proc
+exports.timed_event_p_proc = timed_event_p_proc
 return exports
