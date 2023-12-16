389248567bc97a8e998859f2a66fb7e680038c5a
Alexander Batalov <alex.batalov@gmail.com>
2023-12-16 12:28:41 +0300

    Review wmgroup.lua

--- src/scripts/wmgroup.lua
+++ src/scripts/wmgroup.lua
@@ -3,7 +3,6 @@ local reaction = require("lib.reaction")
 local reputation = require("lib.reputation")
 
 local start
-local combat
 local critter_p_proc
 local pickup_p_proc
 local talk_p_proc
@@ -11,64 +10,50 @@ local destroy_p_proc
 local look_at_p_proc
 local damage_p_proc
 
-local hostile = 0
+local hostile = false
 local initialized = false
-
-local exit_line = 0
 
 function start()
     if not initialized then
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 35)
+        fallout.critter_add_trait(self_obj, 1, 5, 2)
         initialized = true
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 35)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 2)
     end
-    if fallout.script_action() == 21 then
-        look_at_p_proc()
-    else
-        if fallout.script_action() == 4 then
-            pickup_p_proc()
-        else
-            if fallout.script_action() == 11 then
-                talk_p_proc()
-            else
-                if fallout.script_action() == 12 then
-                    critter_p_proc()
-                else
-                    if fallout.script_action() == 18 then
-                        destroy_p_proc()
-                    end
-                end
-            end
-        end
-    end
-end
 
-function combat()
-    hostile = 1
+    local script_action = fallout.script_action()
+    if script_action == 21 then
+        look_at_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
+    end
 end
 
 function critter_p_proc()
     if hostile then
-        hostile = 0
+        hostile = false
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     end
 end
 
 function pickup_p_proc()
     if fallout.source_obj() == fallout.dude_obj() then
-        hostile = 1
+        hostile = true
     end
 end
 
 function talk_p_proc()
-    local v0 = 0
     reaction.get_reaction()
     if fallout.map_var(2) == 1 then
-        v0 = fallout.random(101, 104)
-        fallout.float_msg(fallout.self_obj(), fallout.message_str(833, v0), 2)
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(833, fallout.random(101, 104)), 2)
     else
-        v0 = fallout.random(105, 107)
-        fallout.float_msg(fallout.self_obj(), fallout.message_str(833, v0), 2)
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(833, fallout.random(105, 107)), 2)
     end
 end
 
@@ -82,9 +67,8 @@ function look_at_p_proc()
 end
 
 function damage_p_proc()
-    local v0 = 0
-    v0 = fallout.obj_pid(fallout.source_obj())
-    if fallout.party_member_obj(v0) ~= 0 then
+    local pid = fallout.obj_pid(fallout.source_obj())
+    if fallout.party_member_obj(pid) ~= nil then
         fallout.set_global_var(248, 1)
     end
 end
