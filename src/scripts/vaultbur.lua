b991113e11d4c97fe764cf9217fd5d3f0b5f6c7e
Alexander Batalov <alex.batalov@gmail.com>
2023-12-23 13:00:25 +0300

    Review vaultbur.lua

--- src/scripts/vaultbur.lua
+++ src/scripts/vaultbur.lua
@@ -7,7 +7,6 @@ local map_update_p_proc
 local map_exit_p_proc
 
 local party_elevation = 0
-local dude_start_hex = 0
 
 fallout.create_external_var("Ian_ptr")
 fallout.create_external_var("Dog_ptr")
@@ -15,17 +14,20 @@ fallout.create_external_var("Tycho_ptr")
 fallout.create_external_var("Katja_ptr")
 fallout.create_external_var("Tandi_ptr")
 
+fallout.set_external_var("Ian_ptr", nil)
+fallout.set_external_var("Dog_ptr", nil)
+fallout.set_external_var("Tycho_ptr", nil)
+fallout.set_external_var("Katja_ptr", nil)
+fallout.set_external_var("Tandi_ptr", nil)
+
 function start()
-    if fallout.script_action() == 15 then
+    local script_action = fallout.script_action()
+    if script_action == 15 then
         map_enter_p_proc()
-    else
-        if fallout.script_action() == 23 then
-            map_update_p_proc()
-        else
-            if fallout.script_action() == 16 then
-                map_exit_p_proc()
-            end
-        end
+    elseif script_action == 23 then
+        map_update_p_proc()
+    elseif script_action == 16 then
+        map_exit_p_proc()
     end
 end
 
@@ -33,29 +35,24 @@ function map_enter_p_proc()
     fallout.set_light_level(40)
     if fallout.global_var(32) == 1 then
         fallout.override_map_start(94, 95, 0, 5)
+    elseif fallout.global_var(32) == 2 then
+        fallout.override_map_start(109, 113, 1, 5)
+    elseif fallout.global_var(32) == 3 then
+        fallout.override_map_start(108, 93, 2, 5)
     else
-        if fallout.global_var(32) == 2 then
-            fallout.override_map_start(109, 113, 1, 5)
-        else
-            if fallout.global_var(32) == 3 then
-                fallout.override_map_start(108, 93, 2, 5)
-            else
-                fallout.override_map_start(119, 103, 0, 5)
-            end
-        end
+        fallout.override_map_start(119, 103, 0, 5)
     end
     party_elevation = party.add_party()
 end
 
 function map_update_p_proc()
-    if fallout.elevation(fallout.dude_obj()) == 0 then
+    local dude_elevation = fallout.elevation(fallout.dude_obj())
+    if dude_elevation == 0 then
         fallout.set_global_var(563, 1)
+    elseif dude_elevation == 1 then
+        fallout.set_global_var(564, 1)
     else
-        if fallout.elevation(fallout.dude_obj()) == 1 then
-            fallout.set_global_var(564, 1)
-        else
-            fallout.set_global_var(565, 1)
-        end
+        fallout.set_global_var(565, 1)
     end
     party_elevation = party.update_party(party_elevation)
 end
