94c57e9ecffd9fca5694801db9baf2898cb8558f
Alexander Batalov <alex.batalov@gmail.com>
2023-12-23 13:05:48 +0300

    Review vatalert.lua

--- src/scripts/vatalert.lua
+++ src/scripts/vatalert.lua
@@ -5,42 +5,33 @@ local description_p_proc
 local spatial_p_proc
 local use_skill_on_p_proc
 
-local initialized = false
-
 function start()
-    if not initialized then
-        initialized = true
-    else
-        if fallout.script_action() == 3 then
-            description_p_proc()
-        else
-            if fallout.script_action() == 2 then
-                spatial_p_proc()
-            else
-                if fallout.script_action() == 8 then
-                    use_skill_on_p_proc()
-                end
-            end
-        end
+    local script_action = fallout.script_action()
+    if script_action == 3 then
+        description_p_proc()
+    elseif script_action == 2 then
+        spatial_p_proc()
+    elseif script_action == 8 then
+        use_skill_on_p_proc()
     end
 end
 
 function description_p_proc()
-    local v0 = 0
-    v0 = fallout.message_str(611, 100)
-    if not(fallout.local_var(0)) then
-        if not(fallout.is_success(fallout.do_check(fallout.dude_obj(), 1, fallout.has_trait(0, fallout.dude_obj(), 0)))) then
-            v0 = fallout.message_str(611, 101)
+    local message = fallout.message_str(611, 100)
+    if fallout.local_var(0) == 0 then
+        local dude_obj = fallout.dude_obj()
+        if not (fallout.is_success(fallout.do_check(dude_obj, 1, fallout.has_trait(0, dude_obj, 0)))) then
+            message = fallout.message_str(611, 101)
         else
             fallout.set_local_var(0, 1)
         end
     end
-    fallout.display_msg(v0)
+    fallout.display_msg(message)
 end
 
 function spatial_p_proc()
     if fallout.map_var(7) == 0 then
-        if not(fallout.is_success(fallout.do_check(fallout.dude_obj(), 6, 0))) then
+        if not fallout.is_success(fallout.do_check(fallout.dude_obj(), 6, 0)) then
             fallout.set_global_var(146, 1)
             fallout.set_map_var(7, 1)
             fallout.display_msg(fallout.message_str(611, 102))
