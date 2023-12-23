3399c5a37305aa544f8db8ca625e53594244fa61
Alexander Batalov <alex.batalov@gmail.com>
2023-12-23 13:55:19 +0300

    Review valtctzn.lua

--- src/scripts/valtctzn.lua
+++ src/scripts/valtctzn.lua
@@ -21,49 +21,41 @@ local sleep_time = 0
 local home_tile = 0
 local sleep_tile = 0
 local rndx = 0
-local hostile = 0
-local sleeping_disabled = 0
+local hostile = false
+local sleeping_disabled = false
 local ration_tile = 0
 
 function start()
-    if fallout.script_action() == 12 then
+    local script_action = fallout.script_action()
+    if script_action == 12 then
         critter_p_proc()
-    else
-        if fallout.script_action() == 14 then
-            damage_p_proc()
-        else
-            if fallout.script_action() == 18 then
-                destroy_p_proc()
-            else
-                if fallout.script_action() == 15 then
-                    map_enter_p_proc()
-                else
-                    if fallout.script_action() == 4 then
-                        pickup_p_proc()
-                    else
-                        if fallout.script_action() == 11 then
-                            talk_p_proc()
-                        else
-                            if fallout.script_action() == 22 then
-                                timed_event_p_proc()
-                            end
-                        end
-                    end
-                end
-            end
-        end
+    elseif script_action == 14 then
+        damage_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
+    elseif script_action == 15 then
+        map_enter_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 22 then
+        timed_event_p_proc()
     end
 end
 
 function critter_p_proc()
     if hostile then
-        hostile = 0
+        hostile = false
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     else
         if fallout.global_var(101) == 0 then
             if fallout.local_var(5) == 0 then
-                if (fallout.elevation(fallout.self_obj()) == fallout.elevation(fallout.external_var("WtrGrd_ptr"))) and (fallout.elevation(fallout.self_obj()) == fallout.elevation(fallout.dude_obj())) then
-                    if (fallout.game_time_hour() > 700) and (fallout.game_time_hour() < 900) then
+                local self_elevation = fallout.elevation(fallout.self_obj())
+                local guard_elevation = fallout.elevation(fallout.external_var("WtrGrd_ptr"))
+                local dude_elevation = fallout.elevation(fallout.dude_obj())
+                if self_elevation == guard_elevation and self_elevation == dude_elevation then
+                    if fallout.game_time_hour() > 700 and fallout.game_time_hour() < 900 then
                         get_rations()
                     end
                 end
@@ -71,7 +63,7 @@ function critter_p_proc()
         end
         if not time.is_day() then
             fallout.set_local_var(5, 0)
-            sleeping_disabled = 0
+            sleeping_disabled = false
         end
         if sleeping_disabled == 0 then
             behaviour.sleeping(6, night_person, wake_time, sleep_time, home_tile, sleep_tile)
@@ -93,11 +85,12 @@ function destroy_p_proc()
 end
 
 function map_enter_p_proc()
-    if not(fallout.local_var(4)) then
-        fallout.set_local_var(4, fallout.tile_num(fallout.self_obj()))
+    local self_obj = fallout.self_obj()
+    if fallout.local_var(4) == 0 then
+        fallout.set_local_var(4, fallout.tile_num(self_obj))
     end
     home_tile = fallout.local_var(4)
-    fallout.critter_add_trait(fallout.self_obj(), 1, 6, 1)
+    fallout.critter_add_trait(self_obj, 1, 6, 1)
     set_ration_tile()
     set_sleep_tile()
     sleep_time = fallout.random(1900, 1930)
@@ -105,7 +98,7 @@ function map_enter_p_proc()
 end
 
 function pickup_p_proc()
-    hostile = 1
+    hostile = true
 end
 
 function talk_p_proc()
@@ -128,68 +121,62 @@ function talk_p_proc()
 end
 
 function timed_event_p_proc()
-    if fallout.fixed_param() == 1 then
-        fallout.animate_move_obj_to_tile(fallout.self_obj(), fallout.tile_num_in_direction(fallout.tile_num(fallout.external_var("WtrGrd_ptr")), 2, 1), 0)
-        fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(3), 2)
-    else
-        if fallout.fixed_param() == 2 then
-            fallout.set_external_var("getting_ration", 1)
-            if fallout.random(0, 1) then
-                fallout.float_msg(fallout.self_obj(), fallout.message_str(185, 165), 0)
-            end
-            fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(3), 3)
-        else
-            if fallout.fixed_param() == 3 then
-                fallout.set_external_var("recipient", 0)
-                fallout.animate_move_obj_to_tile(fallout.self_obj(), home_tile, 0)
-            end
+    local event = fallout.fixed_param()
+    local self_obj = fallout.self_obj()
+    if event == 1 then
+        fallout.animate_move_obj_to_tile(self_obj,
+            fallout.tile_num_in_direction(fallout.tile_num(fallout.external_var("WtrGrd_ptr")), 2, 1), 0)
+        fallout.add_timer_event(self_obj, fallout.game_ticks(3), 2)
+    elseif event == 2 then
+        fallout.set_external_var("getting_ration", 1)
+        if fallout.random(0, 1) ~= 0 then
+            fallout.float_msg(self_obj, fallout.message_str(185, 165), 0)
         end
+        fallout.add_timer_event(self_obj, fallout.game_ticks(3), 3)
+    elseif event == 3 then
+        fallout.set_external_var("recipient", nil)
+        fallout.animate_move_obj_to_tile(self_obj, home_tile, 0)
     end
 end
 
 function get_rations()
-    sleeping_disabled = 1
-    if fallout.tile_num(fallout.self_obj()) ~= ration_tile then
-        if fallout.random(0, 1) then
-            fallout.animate_move_obj_to_tile(fallout.self_obj(), ration_tile, 0)
+    sleeping_disabled = true
+    local self_obj = fallout.self_obj()
+    if fallout.tile_num(self_obj) ~= ration_tile then
+        if fallout.random(0, 1) ~= 0 then
+            fallout.animate_move_obj_to_tile(self_obj, ration_tile, 0)
         else
-            fallout.animate_move_obj_to_tile(fallout.self_obj(), ration_tile, 1)
+            fallout.animate_move_obj_to_tile(self_obj, ration_tile, 1)
         end
     else
-        if not(fallout.external_var("recipient")) then
-            fallout.set_external_var("recipient", fallout.self_obj())
-            fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(3), 1)
+        if fallout.external_var("recipient") == nil then
+            fallout.set_external_var("recipient", self_obj)
+            fallout.add_timer_event(self_obj, fallout.game_ticks(3), 1)
             fallout.set_local_var(5, 1)
-            sleeping_disabled = 0
+            sleeping_disabled = false
         end
     end
 end
 
 function set_ration_tile()
-    if fallout.elevation(fallout.self_obj()) == 0 then
+    local self_elevation = fallout.elevation(fallout.self_obj())
+    if self_elevation == 0 then
         ration_tile = fallout.tile_num_in_direction(14704, fallout.random(0, 5), fallout.random(1, 2))
-    else
-        if fallout.elevation(fallout.self_obj()) == 1 then
-            ration_tile = fallout.tile_num_in_direction(21895, fallout.random(0, 5), fallout.random(1, 2))
-        else
-            if fallout.elevation(fallout.self_obj()) == 2 then
-                ration_tile = fallout.tile_num_in_direction(22513, fallout.random(0, 5), fallout.random(1, 2))
-            end
-        end
+    elseif self_elevation == 1 then
+        ration_tile = fallout.tile_num_in_direction(21895, fallout.random(0, 5), fallout.random(1, 2))
+    elseif self_elevation == 2 then
+        ration_tile = fallout.tile_num_in_direction(22513, fallout.random(0, 5), fallout.random(1, 2))
     end
 end
 
 function set_sleep_tile()
-    if fallout.elevation(fallout.self_obj()) == 0 then
+    local self_elevation = fallout.elevation(fallout.self_obj())
+    if self_elevation == 0 then
         sleep_tile = 7000
-    else
-        if fallout.elevation(fallout.self_obj()) == 1 then
-            sleep_tile = home_tile
-        else
-            if fallout.elevation(fallout.self_obj()) == 2 then
-                sleep_tile = 7000
-            end
-        end
+    elseif self_elevation == 1 then
+        sleep_tile = home_tile
+    elseif self_elevation == 2 then
+        sleep_tile = 7000
     end
 end
 
