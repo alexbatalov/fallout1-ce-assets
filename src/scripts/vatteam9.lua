67f0f8d6735f79543c7fb0da68d5cf1e1ee58fd2
Alexander Batalov <alex.batalov@gmail.com>
2023-12-23 13:04:04 +0300

    Review vatteam9.lua

--- src/scripts/vatteam9.lua
+++ src/scripts/vatteam9.lua
@@ -7,42 +7,40 @@ local description_p_proc
 local destroy_p_proc
 local pickup_p_proc
 
-local hostile = 0
+local hostile = false
 local initialized = false
 
 function start()
     if not initialized then
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 34)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 48)
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 34)
+        fallout.critter_add_trait(self_obj, 1, 5, 48)
         initialized = true
     end
-    if fallout.script_action() == 12 then
+
+    local script_action = fallout.script_action()
+    if script_action == 12 then
         critter_p_proc()
-    else
-        if fallout.script_action() == 3 then
-            description_p_proc()
-        else
-            if fallout.script_action() == 18 then
-                destroy_p_proc()
-            else
-                if fallout.script_action() == 4 then
-                    pickup_p_proc()
-                end
-            end
-        end
+    elseif script_action == 3 then
+        description_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
     end
 end
 
 function critter_p_proc()
     if fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) then
         if fallout.global_var(146) ~= 0 then
-            hostile = 1
+            hostile = true
         else
-            if not(fallout.obj_pid(fallout.critter_inven_obj(fallout.dude_obj(), 0)) == 113) then
-                hostile = 1
+            if not (fallout.obj_pid(fallout.critter_inven_obj(fallout.dude_obj(), 0)) == 113) then
+                hostile = true
             end
         end
     end
+    -- FIXME: Missing hostility behaviour (`fallout.attack`)?
 end
 
 function description_p_proc()
@@ -55,7 +53,7 @@ function destroy_p_proc()
 end
 
 function pickup_p_proc()
-    hostile = 1
+    hostile = true
 end
 
 local exports = {}
