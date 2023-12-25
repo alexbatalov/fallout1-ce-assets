2cfac173444e46e70b22a48284bea1737769c3be
Alexander Batalov <alex.batalov@gmail.com>
2023-12-25 14:18:57 +0300

    Review thug.lua

--- src/scripts/thug.lua
+++ src/scripts/thug.lua
@@ -8,57 +8,53 @@ local talk_p_proc
 local destroy_p_proc
 
 local initialized = false
-local hostile = 0
-local my_script_mode = 0
+local hostile = false
+local my_script_mode = false
 
 function start()
     if not initialized then
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 29)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 11)
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 29)
+        fallout.critter_add_trait(self_obj, 1, 5, 11)
         if fallout.cur_map_index() ~= 29 then
             fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
-            my_script_mode = 1
+            my_script_mode = true
         else
             if fallout.global_var(123) == 3 then
-                fallout.move_to(fallout.self_obj(), 7000, 0)
-                fallout.set_obj_visibility(fallout.self_obj(), 1)
+                fallout.move_to(self_obj, 7000, 0)
+                fallout.set_obj_visibility(self_obj, true)
             end
         end
         initialized = true
+    end
+
+    local script_action = fallout.script_action()
+    if not my_script_mode then
+        if script_action == 18 then
+            destroy_p_proc()
+        elseif script_action == 12 then
+            critter_p_proc()
+        elseif script_action == 4 then
+            pickup_p_proc()
+        elseif script_action == 11 then
+            talk_p_proc()
+        end
     else
-        if not(my_script_mode) then
-            if fallout.script_action() == 18 then
-                destroy_p_proc()
-            else
-                if fallout.script_action() == 12 then
-                    critter_p_proc()
-                else
-                    if fallout.script_action() == 4 then
-                        pickup_p_proc()
-                    else
-                        if fallout.script_action() == 11 then
-                            talk_p_proc()
-                        end
-                    end
-                end
-            end
-        else
-            if fallout.script_action() == 18 then
-                destroy_p_proc()
-            end
+        if script_action == 18 then
+            destroy_p_proc()
         end
     end
 end
 
 function critter_p_proc()
     if hostile then
-        hostile = 0
+        hostile = false
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     end
 end
 
 function pickup_p_proc()
-    hostile = 1
+    hostile = true
 end
 
 function talk_p_proc()
