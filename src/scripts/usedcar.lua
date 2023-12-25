b6fa2fbf9ede067efc359b24299703517bda4349
Alexander Batalov <alex.batalov@gmail.com>
2023-12-25 08:04:58 +0300

    Review usedcar.lua

--- src/scripts/usedcar.lua
+++ src/scripts/usedcar.lua
@@ -1,8 +1,18 @@
 local fallout = require("fallout")
 local light = require("lib.light")
 
-local map_enter_p_proc
 local start
+local map_enter_p_proc
+local map_update_p_proc
+
+function start()
+    local script_action = fallout.script_action()
+    if script_action == 15 then
+        map_enter_p_proc()
+    elseif script_action == 23 then
+        map_update_p_proc()
+    end
+end
 
 function map_enter_p_proc()
     if fallout.metarule(14, 0) then
@@ -12,17 +22,12 @@ function map_enter_p_proc()
     light.lighting()
 end
 
-function start()
-    if fallout.script_action() == 15 then
-        map_enter_p_proc()
-    else
-        if fallout.script_action() == 23 then
-            light.lighting()
-        end
-    end
+function map_update_p_proc()
+    light.lighting()
 end
 
 local exports = {}
-exports.map_enter_p_proc = map_enter_p_proc
 exports.start = start
+exports.map_enter_p_proc = map_enter_p_proc
+exports.map_update_p_proc = map_update_p_proc
 return exports
