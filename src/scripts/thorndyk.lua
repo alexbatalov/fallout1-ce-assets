34fe93e4d9383c1aa4668acfc51212a8872c9557
Alexander Batalov <alex.batalov@gmail.com>
2023-12-25 14:37:18 +0300

    Review thorndyk.lua

--- src/scripts/thorndyk.lua
+++ src/scripts/thorndyk.lua
@@ -50,96 +50,81 @@ local Thorn27
 local Thorn28
 local Thorn29
 
-local hostile = 0
+local hostile = false
 local initialized = false
-local PlayerYellsOuch = 0
+local PlayerYellsOuch = false
 local DoctorPostTreatmentResponse = 0
-local HealPlayer = 0
-
-local exit_line = 0
+local HealPlayer = false
 
 function start()
     if not initialized then
+        local self_obj = fallout.self_obj()
+        fallout.set_external_var("Thorndyke_Ptr", self_obj)
+        fallout.critter_add_trait(self_obj, 1, 6, 72)
+        fallout.critter_add_trait(self_obj, 1, 5, 77)
         initialized = true
-        fallout.set_external_var("Thorndyke_Ptr", fallout.self_obj())
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 72)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 77)
     end
-    if fallout.script_action() == 21 then
+
+    local script_action = fallout.script_action()
+    if script_action == 21 then
         look_at_p_proc()
-    else
-        if fallout.script_action() == 4 then
-            pickup_p_proc()
-        else
-            if fallout.script_action() == 11 then
-                talk_p_proc()
-            else
-                if fallout.script_action() == 12 then
-                    critter_p_proc()
-                else
-                    if fallout.script_action() == 18 then
-                        destroy_p_proc()
-                    end
-                end
-            end
-        end
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
     end
 end
 
 function combat()
-    hostile = 1
+    hostile = true
 end
 
 function critter_p_proc()
     if hostile then
-        hostile = 0
+        hostile = false
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     end
-    if (fallout.map_var(6) == 1) and (fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) == 1) then
+    if fallout.map_var(6) == 1 and fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) then
         combat()
     end
 end
 
 function pickup_p_proc()
     if fallout.source_obj() == fallout.dude_obj() then
-        hostile = 1
+        hostile = true
     end
 end
 
 function talk_p_proc()
-    local v0 = 0
     reaction.get_reaction()
     if fallout.global_var(195) == 1 then
         Thorn00()
+    elseif fallout.local_var(5) == 1 then
+        Thorn28()
+    elseif fallout.map_var(0) == 1 then
+        Thorn29()
+    elseif fallout.global_var(158) == 1 or fallout.global_var(156) == 1 then
+        Thorn01()
     else
-        if fallout.local_var(5) == 1 then
-            Thorn28()
-        else
-            if fallout.map_var(0) == 1 then
-                Thorn29()
-            else
-                if (fallout.global_var(158) == 1) or (fallout.global_var(156) == 1) then
-                    Thorn01()
-                else
-                    fallout.start_gdialog(603, fallout.self_obj(), 4, -1, -1)
-                    fallout.gsay_start()
-                    Thorn02()
-                    fallout.gsay_end()
-                    fallout.end_dialogue()
-                end
-            end
-        end
+        fallout.start_gdialog(603, fallout.self_obj(), 4, -1, -1)
+        fallout.gsay_start()
+        Thorn02()
+        fallout.gsay_end()
+        fallout.end_dialogue()
     end
-    if HealPlayer == 1 then
-        HealPlayer = 0
+    if HealPlayer then
+        HealPlayer = false
         fallout.set_local_var(6, time.game_time_in_hours())
-        if PlayerYellsOuch == 1 then
-            PlayerYellsOuch = 0
+        if PlayerYellsOuch then
+            PlayerYellsOuch = false
             fallout.float_msg(fallout.dude_obj(), 168, 9)
         end
         fallout.float_msg(fallout.self_obj(), fallout.message_str(603, DoctorPostTreatmentResponse), 2)
-        v0 = fallout.random(5, 15) + 10
-        fallout.critter_heal(fallout.dude_obj(), v0)
+        fallout.critter_heal(fallout.dude_obj(), fallout.random(5, 15) + 10)
     end
 end
 
@@ -171,11 +156,9 @@ function Thorn01()
 end
 
 function Thorn02()
-    local v0 = 0
-    local v1 = 0
-    v0 = fallout.get_critter_stat(fallout.dude_obj(), 35)
-    v1 = fallout.get_critter_stat(fallout.dude_obj(), 7)
-    if v0 == v1 then
+    local current_hp = fallout.get_critter_stat(fallout.dude_obj(), 35)
+    local max_hp = fallout.get_critter_stat(fallout.dude_obj(), 7)
+    if current_hp == max_hp then
         if fallout.get_critter_stat(fallout.dude_obj(), 34) == 0 then
             fallout.gsay_reply(603, 103)
         else
@@ -187,52 +170,46 @@ function Thorn02()
         fallout.giq_option(4, 603, 108, Thorn07, 50)
         fallout.giq_option(4, 603, 109, Thorn08, 51)
         fallout.giq_option(-3, 603, 110, Thorn09, 51)
-    else
-        if (v0 < v1) and ((time.game_time_in_hours() - fallout.local_var(6)) < 24) then
-            if fallout.get_critter_stat(fallout.dude_obj(), 34) == 0 then
-                fallout.gsay_reply(603, 111)
-            else
-                fallout.gsay_reply(603, 112)
-            end
-            fallout.giq_option(7, 603, 113, Thorn02a, 50)
-            fallout.giq_option(7, 603, 114, Thorn05, 50)
-            fallout.giq_option(4, 603, 115, Thorn27, 51)
-            fallout.giq_option(4, 603, 116, Thorn07, 49)
-            fallout.giq_option(4, 603, 117, Thorn08, 51)
-            fallout.giq_option(-3, 603, 118, Thorn09, 50)
+    elseif current_hp < max_hp and time.game_time_in_hours() - fallout.local_var(6) < 24 then
+        if fallout.get_critter_stat(fallout.dude_obj(), 34) == 0 then
+            fallout.gsay_reply(603, 111)
         else
-            if (v0 > (v1 // 2)) and ((time.game_time_in_hours() - fallout.local_var(6)) > 24) then
-                if fallout.get_critter_stat(fallout.dude_obj(), 34) == 0 then
-                    fallout.gsay_reply(603, 119)
-                else
-                    fallout.gsay_reply(603, 120)
-                end
-                fallout.giq_option(7, 603, 121, Thorn10, 50)
-                fallout.giq_option(7, 603, 122, Thorn10, 50)
-                fallout.giq_option(4, 603, 123, Thorn11, 50)
-                fallout.giq_option(4, 603, 124, Thorn12, 50)
-                fallout.giq_option(4, 603, 125, Thorn13, 50)
-                fallout.giq_option(4, 603, 126, Thorn14, 50)
-                fallout.giq_option(-3, 603, 127, Thorn15, 50)
-                fallout.giq_option(-3, 603, 128, Thorn16, 50)
-            else
-                if (v0 < (v1 // 2)) and ((time.game_time_in_hours() - fallout.local_var(6)) > 24) then
-                    if fallout.get_critter_stat(fallout.dude_obj(), 34) == 0 then
-                        fallout.gsay_reply(603, 129)
-                    else
-                        fallout.gsay_reply(603, 130)
-                    end
-                    fallout.giq_option(7, 603, 131, Thorn10, 50)
-                    fallout.giq_option(7, 603, 132, Thorn10, 50)
-                    fallout.giq_option(4, 603, 133, Thorn11, 50)
-                    fallout.giq_option(4, 603, 124, Thorn12, 50)
-                    fallout.giq_option(4, 603, 134, Thorn13, 50)
-                    fallout.giq_option(4, 603, 135, Thorn14, 50)
-                    fallout.giq_option(-3, 603, 136, Thorn15, 50)
-                    fallout.giq_option(-3, 603, 137, Thorn16, 50)
-                end
-            end
+            fallout.gsay_reply(603, 112)
         end
+        fallout.giq_option(7, 603, 113, Thorn02a, 50)
+        fallout.giq_option(7, 603, 114, Thorn05, 50)
+        fallout.giq_option(4, 603, 115, Thorn27, 51)
+        fallout.giq_option(4, 603, 116, Thorn07, 49)
+        fallout.giq_option(4, 603, 117, Thorn08, 51)
+        fallout.giq_option(-3, 603, 118, Thorn09, 50)
+    elseif current_hp > max_hp // 2 and time.game_time_in_hours() - fallout.local_var(6) > 24 then
+        if fallout.get_critter_stat(fallout.dude_obj(), 34) == 0 then
+            fallout.gsay_reply(603, 119)
+        else
+            fallout.gsay_reply(603, 120)
+        end
+        fallout.giq_option(7, 603, 121, Thorn10, 50)
+        fallout.giq_option(7, 603, 122, Thorn10, 50)
+        fallout.giq_option(4, 603, 123, Thorn11, 50)
+        fallout.giq_option(4, 603, 124, Thorn12, 50)
+        fallout.giq_option(4, 603, 125, Thorn13, 50)
+        fallout.giq_option(4, 603, 126, Thorn14, 50)
+        fallout.giq_option(-3, 603, 127, Thorn15, 50)
+        fallout.giq_option(-3, 603, 128, Thorn16, 50)
+    elseif current_hp < max_hp // 2 and time.game_time_in_hours() - fallout.local_var(6) > 24 then
+        if fallout.get_critter_stat(fallout.dude_obj(), 34) == 0 then
+            fallout.gsay_reply(603, 129)
+        else
+            fallout.gsay_reply(603, 130)
+        end
+        fallout.giq_option(7, 603, 131, Thorn10, 50)
+        fallout.giq_option(7, 603, 132, Thorn10, 50)
+        fallout.giq_option(4, 603, 133, Thorn11, 50)
+        fallout.giq_option(4, 603, 124, Thorn12, 50)
+        fallout.giq_option(4, 603, 134, Thorn13, 50)
+        fallout.giq_option(4, 603, 135, Thorn14, 50)
+        fallout.giq_option(-3, 603, 136, Thorn15, 50)
+        fallout.giq_option(-3, 603, 137, Thorn16, 50)
     end
 end
 
@@ -321,8 +298,8 @@ end
 
 function Thorn12()
     DoctorPostTreatmentResponse = 164
-    PlayerYellsOuch = 0
-    HealPlayer = 1
+    PlayerYellsOuch = false
+    HealPlayer = true
     fallout.gsay_message(603, 161, 50)
 end
 
@@ -336,8 +313,8 @@ end
 
 function Thorn15()
     DoctorPostTreatmentResponse = 169
-    PlayerYellsOuch = 1
-    HealPlayer = 1
+    PlayerYellsOuch = true
+    HealPlayer = true
     fallout.gsay_message(603, 167, 50)
 end
 
