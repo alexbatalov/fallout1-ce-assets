1e3f3868f7bba8d9a263f88412a23352756d3488
Alexander Batalov <alex.batalov@gmail.com>
2023-12-28 23:42:14 +0300

    Review spotlite.lua

--- src/scripts/spotlite.lua
+++ src/scripts/spotlite.lua
@@ -6,36 +6,31 @@ local map_enter_p_proc
 local map_update_p_proc
 
 function start()
-    if fallout.script_action() == 15 then
+    local script_action = fallout.script_action()
+    if script_action == 15 then
         map_enter_p_proc()
-    else
-        if fallout.script_action() == 23 then
-            map_update_p_proc()
-        end
+    elseif script_action == 23 then
+        map_update_p_proc()
     end
 end
 
 function map_enter_p_proc()
     if fallout.global_var(224) == 2 then
         fallout.set_light_level(100)
+    elseif fallout.global_var(224) == 1 then
+        light.darkness()
     else
-        if fallout.global_var(224) == 1 then
-            light.darkness()
-        else
-            fallout.set_light_level(1)
-        end
+        fallout.set_light_level(1)
     end
 end
 
 function map_update_p_proc()
     if fallout.global_var(224) == 2 then
         fallout.set_light_level(100)
+    elseif fallout.global_var(224) == 1 then
+        light.darkness()
     else
-        if fallout.global_var(224) == 1 then
-            light.darkness()
-        else
-            fallout.set_light_level(1)
-        end
+        fallout.set_light_level(1)
     end
 end
 
