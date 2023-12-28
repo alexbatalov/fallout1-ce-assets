9362b9dc1fe42eda160a42f344911101a24971bd
Alexander Batalov <alex.batalov@gmail.com>
2023-12-28 23:35:31 +0300

    Review sswell.lua

--- src/scripts/sswell.lua
+++ src/scripts/sswell.lua
@@ -6,16 +6,13 @@ local use_obj_on_p_proc
 local use_skill_on_p_proc
 
 function start()
-    if (fallout.script_action() == 21) or (fallout.script_action() == 3) then
+    local script_action = fallout.script_action()
+    if script_action == 21 or script_action == 3 then
         look_at_p_proc()
-    else
-        if fallout.script_action() == 7 then
-            use_obj_on_p_proc()
-        else
-            if fallout.script_action() == 8 then
-                use_skill_on_p_proc()
-            end
-        end
+    elseif script_action == 7 then
+        use_obj_on_p_proc()
+    elseif script_action == 8 then
+        use_skill_on_p_proc()
     end
 end
 
@@ -25,15 +22,14 @@ function look_at_p_proc()
 end
 
 function use_obj_on_p_proc()
-    if fallout.obj_pid(fallout.obj_being_used_with()) == 127 then
+    local pid = fallout.obj_pid(fallout.obj_being_used_with())
+    if pid == 127 then
         fallout.script_overrides()
         fallout.display_msg(fallout.message_str(14, 101))
-    else
-        if fallout.obj_pid(fallout.obj_being_used_with()) == 41 then
-            fallout.script_overrides()
-            fallout.display_msg(fallout.message_str(14, 103))
-            fallout.item_caps_adjust(fallout.dude_obj(), -1)
-        end
+    elseif pid == 41 then
+        fallout.script_overrides()
+        fallout.display_msg(fallout.message_str(14, 103))
+        fallout.item_caps_adjust(fallout.dude_obj(), -1)
     end
 end
 
