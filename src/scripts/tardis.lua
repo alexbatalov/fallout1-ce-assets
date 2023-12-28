f8d23fac07d51422b8f70592e3cad54c2ab11a85
Alexander Batalov <alex.batalov@gmail.com>
2023-12-28 22:55:34 +0300

    Review tardis.lua

--- src/scripts/tardis.lua
+++ src/scripts/tardis.lua
@@ -6,24 +6,22 @@ local spatial_p_proc
 local use_p_proc
 
 function start()
-    if fallout.script_action() == 15 then
+    local script_action = fallout.script_action()
+    if script_action == 15 then
         map_enter_p_proc()
-    else
-        if fallout.script_action() == 2 then
-            spatial_p_proc()
-        else
-            if fallout.script_action() == 6 then
-                use_p_proc()
-            end
-        end
+    elseif script_action == 2 then
+        spatial_p_proc()
+    elseif script_action == 6 then
+        use_p_proc()
     end
 end
 
 function map_enter_p_proc()
-    if fallout.obj_pid(fallout.self_obj()) == 33555090 then
-        fallout.set_external_var("Tardis_ptr", fallout.self_obj())
+    local self_obj = fallout.self_obj()
+    if fallout.obj_pid(self_obj) == 33555090 then
+        fallout.set_external_var("Tardis_ptr", self_obj)
     end
-    if fallout.metarule(14, 0) then
+    if fallout.metarule(14, 0) ~= 0 then
         fallout.override_map_start(113, 107, 0, 0)
         fallout.display_msg(fallout.message_str(112, 317))
     end
