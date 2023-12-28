47ed146a4d103f3c3448b1dc1e7fb90de7f7cd05
Alexander Batalov <alex.batalov@gmail.com>
2023-12-28 23:16:32 +0300

    Review supambsh.lua

--- src/scripts/supambsh.lua
+++ src/scripts/supambsh.lua
@@ -5,30 +5,29 @@ local start
 local critter_p_proc
 local destroy_p_proc
 
-local start_init = 0
-local crit_init = 0
-local Item = 0
+local initialized = false
+local crit_init = false
 
 function start()
-    if not(start_init) then
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 43)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 49)
-        start_init = 1
-    else
-        if fallout.script_action() == 12 then
-            critter_p_proc()
-        else
-            if fallout.script_action() == 18 then
-                destroy_p_proc()
-            end
-        end
+    if not initialized then
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 43)
+        fallout.critter_add_trait(self_obj, 1, 5, 49)
+        initialized = true
+    end
+
+    local script_action = fallout.script_action()
+    if script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
     end
 end
 
 function critter_p_proc()
-    if not(crit_init) then
+    if not crit_init then
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
-        crit_init = 1
+        crit_init = true
     end
 end
 
