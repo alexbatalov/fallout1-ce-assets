15708efc47bec3759ade60b9bf9ddc423910dda4
Alexander Batalov <alex.batalov@gmail.com>
2023-12-28 23:38:51 +0300

    Review ssradsco.lua

--- src/scripts/ssradsco.lua
+++ src/scripts/ssradsco.lua
@@ -2,56 +2,57 @@ local fallout = require("fallout")
 
 local start
 local critter_p_proc
+local combat_p_proc
 local destroy_p_proc
 local timed_event_p_proc
 
 local rndx = 0
 local rndy = 0
 local critter_tile = 0
-local hostile = 0
+local hostile = false
 local initialized = false
-local Start_Moving = 0
+local start_moving = false
 
 function start()
     if not initialized then
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 5, 14)
+        fallout.critter_add_trait(self_obj, 1, 6, 5)
         initialized = true
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 14)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 5)
-    else
-        if fallout.script_action() == 13 then
-            if fallout.fixed_param() == 2 then
-                if not(fallout.is_success(fallout.do_check(fallout.dude_obj(), 6, -1))) then
-                    rndx = fallout.random(1, 6)
-                    fallout.poison(fallout.target_obj(), rndx)
-                end
-            end
-        else
-            if fallout.script_action() == 18 then
-                destroy_p_proc()
-            else
-                if fallout.script_action() == 22 then
-                    timed_event_p_proc()
-                else
-                    if fallout.script_action() == 12 then
-                        critter_p_proc()
-                    end
-                end
-            end
-        end
+    end
+
+    local script_action = fallout.script_action()
+    if script_action == 13 then
+        combat_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
+    elseif script_action == 22 then
+        timed_event_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
     end
 end
 
 function critter_p_proc()
-    if (fallout.random(1, 10) == 1) and (Start_Moving == 0) then
-        Start_Moving = 1
+    if fallout.random(1, 10) == 1 and not start_moving then
+        start_moving = true
         fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(5), 1)
     end
     if fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) then
-        hostile = 1
+        hostile = true
     end
     if hostile then
-        hostile = 0
+        hostile = false
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
+    end
+end
+
+function combat_p_proc()
+    if fallout.fixed_param() == 2 then
+        if not fallout.is_success(fallout.do_check(fallout.dude_obj(), 6, -1)) then
+            local dmg = fallout.random(1, 6)
+            fallout.poison(fallout.target_obj(), dmg)
+        end
     end
 end
 
@@ -68,13 +69,13 @@ function destroy_p_proc()
 end
 
 function timed_event_p_proc()
-    local v0 = 0
-    if Start_Moving == 1 then
-        v0 = fallout.tile_num_in_direction(fallout.tile_num(fallout.self_obj()), fallout.random(0, 5), 3)
-        Start_Moving = 0
-        fallout.reg_anim_func(2, fallout.self_obj())
+    if start_moving then
+        local self_obj = fallout.self_obj()
+        local tile = fallout.tile_num_in_direction(fallout.tile_num(self_obj), fallout.random(0, 5), 3)
+        start_moving = false
+        fallout.reg_anim_func(2, self_obj)
         fallout.reg_anim_func(1, 1)
-        fallout.reg_anim_obj_move_to_tile(fallout.self_obj(), v0, -1)
+        fallout.reg_anim_obj_move_to_tile(self_obj, tile, -1)
         fallout.reg_anim_func(3, 0)
     end
 end
@@ -82,6 +83,7 @@ end
 local exports = {}
 exports.start = start
 exports.critter_p_proc = critter_p_proc
+exports.combat_p_proc = combat_p_proc
 exports.destroy_p_proc = destroy_p_proc
 exports.timed_event_p_proc = timed_event_p_proc
 return exports
