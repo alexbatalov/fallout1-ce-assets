7ac5237ec4ca2854f2214d09337eda5e90505051
Alexander Batalov <alex.batalov@gmail.com>
2023-12-28 22:51:25 +0300

    Review teacher.lua

--- src/scripts/teacher.lua
+++ src/scripts/teacher.lua
@@ -8,64 +8,57 @@ local pickup_p_proc
 local talk_p_proc
 local destroy_p_proc
 local look_at_p_proc
-local background
+local timed_event_p_proc
 
-local hostile = 0
+local hostile = false
 local initialized = false
 
 function start()
     if not initialized then
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 44)
+        fallout.critter_add_trait(self_obj, 1, 5, 65)
+        fallout.add_timer_event(self_obj, fallout.game_ticks(30), 2)
         initialized = true
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 44)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 65)
-        fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(30), 2)
     end
-    if fallout.script_action() == 21 then
+
+    local script_action = fallout.script_action()
+    if script_action == 21 then
         look_at_p_proc()
-    else
-        if fallout.script_action() == 4 then
-            pickup_p_proc()
-        else
-            if fallout.script_action() == 11 then
-                talk_p_proc()
-            else
-                if fallout.script_action() == 22 then
-                    background()
-                else
-                    if fallout.script_action() == 12 then
-                        critter_p_proc()
-                    else
-                        if fallout.script_action() == 18 then
-                            destroy_p_proc()
-                        end
-                    end
-                end
-            end
-        end
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 22 then
+        timed_event_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
     end
 end
 
 function combat()
-    hostile = 1
+    hostile = true
 end
 
 function critter_p_proc()
     if fallout.global_var(250) ~= 0 then
-        hostile = 1
+        hostile = true
     end
     if fallout.tile_distance_objs(fallout.self_obj(), fallout.dude_obj()) > 12 then
-        hostile = 0
+        hostile = false
     end
     if hostile then
         fallout.set_global_var(250, 1)
-        hostile = 0
+        hostile = false
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     end
 end
 
 function pickup_p_proc()
     if fallout.source_obj() == fallout.dude_obj() then
-        hostile = 1
+        hostile = true
     end
 end
 
@@ -84,9 +77,10 @@ function look_at_p_proc()
     fallout.display_msg(fallout.message_str(319, 400))
 end
 
-function background()
-    fallout.float_msg(fallout.self_obj(), fallout.message_str(319, fallout.random(101, 104)), 8)
-    fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(180), 2)
+function timed_event_p_proc()
+    local self_obj = fallout.self_obj()
+    fallout.float_msg(self_obj, fallout.message_str(319, fallout.random(101, 104)), 8)
+    fallout.add_timer_event(self_obj, fallout.game_ticks(180), 2)
 end
 
 local exports = {}
@@ -95,5 +89,6 @@ exports.critter_p_proc = critter_p_proc
 exports.pickup_p_proc = pickup_p_proc
 exports.talk_p_proc = talk_p_proc
 exports.destroy_p_proc = destroy_p_proc
+exports.timed_event_p_proc = timed_event_p_proc
 exports.look_at_p_proc = look_at_p_proc
 return exports
