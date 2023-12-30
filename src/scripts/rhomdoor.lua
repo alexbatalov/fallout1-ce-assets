f1b9f0474e691e4c0b6fcb96987295766e9d462e
Alexander Batalov <alex.batalov@gmail.com>
2023-12-30 23:12:09 +0300

    Review rhomdoor.lua

--- src/scripts/rhomdoor.lua
+++ src/scripts/rhomdoor.lua
@@ -7,29 +7,18 @@ local hear_check
 local anger
 local anger2
 
-local failure = 0
-local bonus = 0
-local Test = 0
-
 function start()
-    if fallout.script_action() == 14 then
+    local script_action = fallout.script_action()
+    if script_action == 14 then
         damage_p_proc()
-    else
-        if fallout.script_action() == 22 then
-            timed_event_p_proc()
-        else
-            if fallout.script_action() == 8 then
-                hear_check()
-            else
-                if fallout.script_action() == 6 then
-                    hear_check()
-                else
-                    if fallout.script_action() == 7 then
-                        hear_check()
-                    end
-                end
-            end
-        end
+    elseif script_action == 22 then
+        timed_event_p_proc()
+    elseif script_action == 8 then
+        hear_check()
+    elseif script_action == 6 then
+        hear_check()
+    elseif script_action == 7 then
+        hear_check()
     end
 end
 
@@ -37,49 +26,47 @@ function damage_p_proc()
     fallout.set_global_var(250, 1)
     fallout.set_local_var(0, fallout.local_var(0) + 1)
     if fallout.local_var(0) == 3 then
-        fallout.set_obj_visibility(fallout.self_obj(), 1)
+        fallout.set_obj_visibility(fallout.self_obj(), true)
     end
 end
 
 function timed_event_p_proc()
-    if fallout.fixed_param() == 1 then
+    local self_obj = fallout.self_obj()
+    local event = fallout.fixed_param()
+    if event == 1 then
         fallout.set_map_var(19, 0)
-        if not(fallout.combat_is_initialized()) then
-            fallout.obj_close(fallout.self_obj())
+        if not fallout.combat_is_initialized() then
+            fallout.obj_close(self_obj)
         end
-        fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(3), 2)
-    else
-        if fallout.fixed_param() == 2 then
-            if fallout.obj_is_open(fallout.self_obj()) then
-                fallout.obj_close(fallout.self_obj())
-                fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(3), 3)
-            else
-                fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(3), 2)
-            end
+        fallout.add_timer_event(self_obj, fallout.game_ticks(3), 2)
+    elseif event == 2 then
+        if fallout.obj_is_open(self_obj) then
+            fallout.obj_close(self_obj)
+            fallout.add_timer_event(self_obj, fallout.game_ticks(3), 3)
         else
-            if fallout.fixed_param() == 3 then
-                fallout.rm_timer_event(fallout.self_obj())
-            end
+            fallout.add_timer_event(self_obj, fallout.game_ticks(3), 2)
         end
+    elseif event == 3 then
+        fallout.rm_timer_event(self_obj)
     end
 end
 
 function hear_check()
-    bonus = -35
-    failure = 1
-    if (fallout.metarule(16, 0) > 1) and (fallout.source_obj() == fallout.dude_obj()) then
+    local bonus = -35
+    local failure = true
+    if fallout.metarule(16, 0) > 1 and fallout.source_obj() == fallout.dude_obj() then
         fallout.set_map_var(19, 2)
     else
         if fallout.source_obj() == fallout.dude_obj() then
             if fallout.using_skill(fallout.dude_obj(), 8) then
-                Test = fallout.roll_vs_skill(fallout.dude_obj(), 8, bonus)
-                if fallout.is_success(Test) then
-                    failure = 0
+                local roll = fallout.roll_vs_skill(fallout.dude_obj(), 8, bonus)
+                if fallout.is_success(roll) then
+                    failure = false
                 else
-                    failure = 1
+                    failure = true
                 end
                 if fallout.has_skill(fallout.dude_obj(), 8) < 40 then
-                    failure = 1
+                    failure = true
                 end
             end
         end
@@ -91,13 +78,13 @@ function hear_check()
 end
 
 function anger()
-    if (fallout.map_var(19) < 1) and (fallout.source_obj() == fallout.dude_obj()) then
+    if fallout.map_var(19) < 1 and fallout.source_obj() == fallout.dude_obj() then
         fallout.set_map_var(19, 1)
     end
 end
 
 function anger2()
-    if (fallout.map_var(19) < 1) and (fallout.source_obj() == fallout.dude_obj()) then
+    if fallout.map_var(19) < 1 and fallout.source_obj() == fallout.dude_obj() then
         fallout.set_map_var(19, 2)
     end
 end
