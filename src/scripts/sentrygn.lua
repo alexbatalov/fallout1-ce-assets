b4488e05a9cf06f0cb40a639eefcce7864397c9f
Alexander Batalov <alex.batalov@gmail.com>
2023-12-30 18:49:15 +0300

    Review sentrygn.lua

--- src/scripts/sentrygn.lua
+++ src/scripts/sentrygn.lua
@@ -1,56 +1,70 @@
 local fallout = require("fallout")
 
 local start
+local spatial_p_proc
+local timed_event_p_proc
 
 function start()
-    local v0 = 0
-    local v1 = 0
-    if fallout.script_action() == 2 then
-        if (fallout.local_var(0) == 0) and (fallout.obj_type(fallout.source_obj()) == fallout.dude_obj()) then
-            fallout.display_msg(fallout.message_str(304, 100))
-            fallout.set_local_var(0, 1)
-        end
-        if (fallout.local_var(1) == 0) and (fallout.obj_type(fallout.source_obj()) == fallout.dude_obj()) and fallout.global_var(139) and fallout.global_var(140) then
-            fallout.display_msg(fallout.message_str(304, 101))
-            fallout.set_local_var(1, 1)
-            fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(7), 1)
+    local script_action = fallout.script_action()
+    if script_action == 2 then
+        spatial_p_proc()
+    elseif script_action == 22 then
+        timed_event_p_proc()
+    end
+end
+
+function spatial_p_proc()
+    local source_obj = fallout.source_obj()
+    local dude_obj = fallout.dude_obj()
+
+    if fallout.local_var(0) == 0 and source_obj == dude_obj then
+        fallout.display_msg(fallout.message_str(304, 100))
+        fallout.set_local_var(0, 1)
+    end
+    if fallout.local_var(1) == 0 and source_obj == dude_obj and fallout.global_var(139) ~= 0 and fallout.global_var(140) ~= 0 then
+        fallout.display_msg(fallout.message_str(304, 101))
+        fallout.set_local_var(1, 1)
+        fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(7), 1)
+    end
+end
+
+function timed_event_p_proc()
+    local source_obj = fallout.source_obj()
+    local dude_obj = fallout.dude_obj()
+    local damage = fallout.random(1, 6) + 2
+    local roll = fallout.do_check(source_obj, 5, 0)
+    if fallout.is_success(roll) then
+        if fallout.is_critical(roll) then
+            if source_obj == dude_obj then
+                fallout.display_msg(fallout.message_str(304, 102))
+            end
+        else
+            if source_obj == dude_obj then
+                fallout.display_msg(fallout.message_str(304, 103))
+            end
+            fallout.critter_dmg(source_obj, damage, 0)
+            fallout.display_msg(fallout.message_str(304, 104) .. damage .. fallout.message_str(304, 105))
         end
     else
-        if fallout.script_action() == 22 then
-            v0 = fallout.random(1, 6) + 2
-            v1 = fallout.do_check(fallout.source_obj(), 5, 0)
-            if fallout.is_success(v1) then
-                if fallout.is_critical(v1) then
-                    if fallout.source_obj() == fallout.dude_obj() then
-                        fallout.display_msg(fallout.message_str(304, 102))
-                    end
-                else
-                    if fallout.source_obj() == fallout.dude_obj() then
-                        fallout.display_msg(fallout.message_str(304, 103))
-                    end
-                    fallout.critter_dmg(fallout.source_obj(), v0, 0)
-                    fallout.display_msg(fallout.message_str(304, 104) .. v0 .. fallout.message_str(304, 105))
-                end
-            else
-                if fallout.is_critical(v1) then
-                    if fallout.source_obj() == fallout.dude_obj() then
-                        fallout.display_msg(fallout.message_str(304, 106))
-                    end
-                    fallout.critter_dmg(fallout.source_obj(), v0, 0)
-                    fallout.display_msg(fallout.message_str(304, 107) .. v0 .. fallout.message_str(304, 108))
-                else
-                    if fallout.source_obj() == fallout.dude_obj() then
-                        fallout.display_msg(fallout.message_str(304, 109))
-                    end
-                    fallout.critter_dmg(fallout.source_obj(), v0, 0)
-                    fallout.display_msg(fallout.message_str(304, 110) .. v0 .. fallout.message_str(304, 111))
-                end
+        if fallout.is_critical(roll) then
+            if source_obj == dude_obj then
+                fallout.display_msg(fallout.message_str(304, 106))
             end
-            fallout.set_local_var(1, 0)
+            fallout.critter_dmg(source_obj, damage, 0)
+            fallout.display_msg(fallout.message_str(304, 107) .. damage .. fallout.message_str(304, 108))
+        else
+            if source_obj == dude_obj then
+                fallout.display_msg(fallout.message_str(304, 109))
+            end
+            fallout.critter_dmg(fallout.source_obj(), damage, 0)
+            fallout.display_msg(fallout.message_str(304, 110) .. damage .. fallout.message_str(304, 111))
         end
     end
+    fallout.set_local_var(1, 0)
 end
 
 local exports = {}
 exports.start = start
+exports.spatial_p_proc = spatial_p_proc
+exports.timed_event_p_proc = timed_event_p_proc
 return exports
