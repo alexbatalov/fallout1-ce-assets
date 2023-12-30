01d7dc7b40e7c447c04cc8eab6743ce91baee5db
Alexander Batalov <alex.batalov@gmail.com>
2023-12-31 00:37:54 +0300

    Review psycon.lua

--- src/scripts/psycon.lua
+++ src/scripts/psycon.lua
@@ -1,38 +1,33 @@
 local fallout = require("fallout")
 
 local start
+local use_p_proc
 local look_at_p_proc
 local map_enter_p_proc
 local map_update_p_proc
 
-local initialized = false
-local spot1 = 17120
-local field1 = 0
-local swtch = 0
-
 function start()
-    if fallout.script_action() == 15 then
+    local script_action = fallout.script_action()
+    if script_action == 15 then
         map_enter_p_proc()
-    else
-        if fallout.script_action() == 23 then
-            map_update_p_proc()
-        end
-    end
-    if fallout.script_action() == 6 then
-        if fallout.local_var(0) ~= 0 then
-            fallout.set_obj_visibility(fallout.external_var("Psy_Field_Ptr"), 0)
-            fallout.set_map_var(6, 0)
-            fallout.set_local_var(0, 0)
-        else
-            fallout.set_obj_visibility(fallout.external_var("Psy_Field_Ptr"), 1)
-            fallout.set_map_var(6, 1)
-            fallout.set_local_var(0, 1)
-        end
-    end
-    if fallout.script_action() == 21 then
+    elseif script_action == 23 then
+        map_update_p_proc()
+    elseif script_action == 6 then
+        use_p_proc()
+    elseif script_action == 21 then
         look_at_p_proc()
     end
-    if fallout.script_action() == 16 then
+end
+
+function use_p_proc()
+    if fallout.local_var(0) ~= 0 then
+        fallout.set_obj_visibility(fallout.external_var("Psy_Field_Ptr"), false)
+        fallout.set_map_var(6, 0)
+        fallout.set_local_var(0, 0)
+    else
+        fallout.set_obj_visibility(fallout.external_var("Psy_Field_Ptr"), true)
+        fallout.set_map_var(6, 1)
+        fallout.set_local_var(0, 1)
     end
 end
 
@@ -50,6 +45,7 @@ end
 
 local exports = {}
 exports.start = start
+exports.use_p_proc = use_p_proc
 exports.look_at_p_proc = look_at_p_proc
 exports.map_enter_p_proc = map_enter_p_proc
 exports.map_update_p_proc = map_update_p_proc
