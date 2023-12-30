e5bd63b0f72ae4ae958292873bf3374d26a2989f
Alexander Batalov <alex.batalov@gmail.com>
2023-12-30 18:53:35 +0300

    Review semicomp.lua

--- src/scripts/semicomp.lua
+++ src/scripts/semicomp.lua
@@ -1,71 +1,54 @@
 local fallout = require("fallout")
 
 local start
-local search
-local skills
-
-local use_skill = 0
+local look_at_p_proc
+local use_skill_on_p_proc
 
 function start()
     if fallout.local_var(0) == 0 then
         fallout.set_local_var(0, fallout.random(1, 9))
     end
-    if fallout.script_action() == 21 then
-        fallout.script_overrides()
-        search()
-    else
-        if fallout.script_action() == 8 then
-            use_skill = fallout.action_being_used()
-            fallout.script_overrides()
-            skills()
-        end
+
+    local script_action = fallout.script_action()
+    if script_action == 21 then
+        look_at_p_proc()
+    elseif script_action == 8 then
+        use_skill_on_p_proc()
     end
 end
 
-function search()
+function look_at_p_proc()
+    fallout.script_overrides()
     if fallout.tile_distance_objs(fallout.self_obj(), fallout.dude_obj()) > 3 then
         fallout.display_msg(fallout.message_str(418, 100))
     else
-        if fallout.local_var(0) == 1 then
+        local msg = fallout.local_var(0)
+        if msg == 1 then
             fallout.display_msg(fallout.message_str(418, 102))
-        else
-            if fallout.local_var(0) == 2 then
-                fallout.display_msg(fallout.message_str(418, 103))
-            else
-                if fallout.local_var(0) == 3 then
-                    fallout.display_msg(fallout.message_str(418, 104))
-                else
-                    if fallout.local_var(0) == 4 then
-                        fallout.display_msg(fallout.message_str(418, 105))
-                    else
-                        if fallout.local_var(0) == 5 then
-                            fallout.display_msg(fallout.message_str(418, 106))
-                        else
-                            if fallout.local_var(0) == 6 then
-                                fallout.display_msg(fallout.message_str(418, 107))
-                            else
-                                if fallout.local_var(0) == 7 then
-                                    fallout.display_msg(fallout.message_str(418, 108))
-                                else
-                                    if fallout.local_var(0) == 8 then
-                                        fallout.display_msg(fallout.message_str(418, 109))
-                                    else
-                                        if fallout.local_var(0) == 9 then
-                                            fallout.display_msg(fallout.message_str(418, 111))
-                                        end
-                                    end
-                                end
-                            end
-                        end
-                    end
-                end
-            end
+        elseif msg == 2 then
+            fallout.display_msg(fallout.message_str(418, 103))
+        elseif msg == 3 then
+            fallout.display_msg(fallout.message_str(418, 104))
+        elseif msg == 4 then
+            fallout.display_msg(fallout.message_str(418, 105))
+        elseif msg == 5 then
+            fallout.display_msg(fallout.message_str(418, 106))
+        elseif msg == 6 then
+            fallout.display_msg(fallout.message_str(418, 107))
+        elseif msg == 7 then
+            fallout.display_msg(fallout.message_str(418, 108))
+        elseif msg == 8 then
+            fallout.display_msg(fallout.message_str(418, 109))
+        elseif msg == 9 then
+            fallout.display_msg(fallout.message_str(418, 111))
         end
     end
 end
 
-function skills()
-    if (use_skill == 13) or (use_skill == 12) then
+function use_skill_on_p_proc()
+    local skill = fallout.action_being_used()
+    fallout.script_overrides()
+    if skill == 13 or skill == 12 then
         fallout.display_msg(fallout.message_str(418, 101))
     else
         fallout.display_msg(fallout.message_str(418, 102))
@@ -74,4 +57,6 @@ end
 
 local exports = {}
 exports.start = start
+exports.look_at_p_proc = look_at_p_proc
+exports.use_skill_on_p_proc = use_skill_on_p_proc
 return exports
