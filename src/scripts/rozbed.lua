49032d856f5f23ed4762d3fd2b718ac51fc7a8eb
Alexander Batalov <alex.batalov@gmail.com>
2023-12-30 22:40:20 +0300

    Review rozbed.lua

--- src/scripts/rozbed.lua
+++ src/scripts/rozbed.lua
@@ -2,31 +2,23 @@ local fallout = require("fallout")
 
 local start
 local use_p_proc
-local chairend
 local look_at_p_proc
 local description_p_proc
 
 function start()
-    local v0 = 0
-    if fallout.script_action() == 6 then
+    local script_action = fallout.script_action()
+    if script_action == 6 then
         use_p_proc()
-    else
-        if fallout.script_action() == 3 then
-            description_p_proc()
-        else
-            if fallout.script_action() == 21 then
-                look_at_p_proc()
-            end
-        end
+    elseif script_action == 3 then
+        description_p_proc()
+    elseif script_action == 21 then
+        look_at_p_proc()
     end
 end
 
 function use_p_proc()
     fallout.script_overrides()
     fallout.display_msg(fallout.message_str(118, 102))
-end
-
-function chairend()
 end
 
 function look_at_p_proc()
