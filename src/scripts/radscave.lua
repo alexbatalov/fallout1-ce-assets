4dfa8eff3e8727e4b2ccb2c4c2660a58f9b15aa0
Alexander Batalov <alex.batalov@gmail.com>
2023-12-31 00:03:48 +0300

    Review radscave.lua

--- src/scripts/radscave.lua
+++ src/scripts/radscave.lua
@@ -1,13 +1,11 @@
 local fallout = require("fallout")
 local party = require("lib.party")
 
-local HEREBEFORE = 0
-local time = 0
-
 local start
+local map_enter_p_proc
+local map_exit_p_proc
 
 local party_elevation = 0
-local dude_start_hex = 0
 
 fallout.create_external_var("Ian_ptr")
 fallout.create_external_var("Dog_ptr")
@@ -15,26 +13,41 @@ fallout.create_external_var("Tycho_ptr")
 fallout.create_external_var("Katja_ptr")
 fallout.create_external_var("Tandi_ptr")
 
+fallout.set_external_var("Ian_ptr", nil)
+fallout.set_external_var("Dog_ptr", nil)
+fallout.set_external_var("Tycho_ptr", nil)
+fallout.set_external_var("Katja_ptr", nil)
+fallout.set_external_var("Tandi_ptr", nil)
+
 function start()
-    if fallout.script_action() == 15 then
-        if fallout.metarule(14, 0) then
-            fallout.display_msg(fallout.message_str(766, 189))
-        end
-        fallout.set_light_level(40)
-        if fallout.global_var(32) == 1 then
-            fallout.override_map_start(156, 106, 0, 1)
-        else
-            fallout.override_map_start(156, 106, 0, 1)
-        end
-        party_elevation = party.add_party()
-    else
-        if fallout.script_action() == 16 then
-            fallout.game_time_advance(fallout.game_ticks(60 * 30))
-            party.remove_party()
-        end
+    local script_action = fallout.script_action()
+    if script_action == 15 then
+        map_enter_p_proc()
+    elseif script_action == 16 then
+        map_exit_p_proc()
     end
+end
+
+function map_enter_p_proc()
+    if fallout.metarule(14, 0) ~= 0 then
+        fallout.display_msg(fallout.message_str(766, 189))
+    end
+    fallout.set_light_level(40)
+    if fallout.global_var(32) == 1 then
+        fallout.override_map_start(156, 106, 0, 1)
+    else
+        fallout.override_map_start(156, 106, 0, 1)
+    end
+    party_elevation = party.add_party()
+end
+
+function map_exit_p_proc()
+    fallout.game_time_advance(fallout.game_ticks(60 * 30))
+    party.remove_party()
 end
 
 local exports = {}
 exports.start = start
+exports.map_enter_p_proc = map_enter_p_proc
+exports.map_exit_p_proc = map_exit_p_proc
 return exports
