529cc932faf4fb44874b8e745b112391351eb23a
Alexander Batalov <alex.batalov@gmail.com>
2023-12-30 19:06:46 +0300

    Review scribec.lua

--- src/scripts/scribec.lua
+++ src/scripts/scribec.lua
@@ -9,80 +9,72 @@ local talk_p_proc
 local destroy_p_proc
 local look_at_p_proc
 
-local hostile = 0
+local hostile = false
 local initialized = false
 
 function start()
     if not initialized then
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 44)
+        fallout.critter_add_trait(self_obj, 1, 5, 63)
         initialized = true
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 44)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 63)
     end
-    if fallout.script_action() == 21 then
+
+    local script_action = fallout.script_action()
+    if script_action == 21 then
         look_at_p_proc()
-    else
-        if fallout.script_action() == 4 then
-            pickup_p_proc()
-        else
-            if fallout.script_action() == 11 then
-                talk_p_proc()
-            else
-                if fallout.script_action() == 12 then
-                    critter_p_proc()
-                else
-                    if fallout.script_action() == 18 then
-                        destroy_p_proc()
-                    end
-                end
-            end
-        end
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
     end
 end
 
 function ScribeCRandom()
     local v0 = 0
-    local v1 = 0
-    if not(v0) then
+    if v0 == 0 then
         v0 = fallout.random(1, 4)
     end
     if v0 > 4 then
         v0 = 1
     end
-    v1 = fallout.message_str(381, 101) .. fallout.proto_data(fallout.obj_pid(fallout.dude_obj()), 1) .. fallout.message_str(381, 102)
+    local msg = fallout.message_str(381, 101) ..
+        fallout.proto_data(fallout.obj_pid(fallout.dude_obj()), 1) .. fallout.message_str(381, 102)
     if v0 == 2 then
-        v1 = fallout.message_str(381, 103) .. fallout.proto_data(fallout.obj_pid(fallout.dude_obj()), 1) .. fallout.message_str(381, 104)
+        msg = fallout.message_str(381, 103) ..
+            fallout.proto_data(fallout.obj_pid(fallout.dude_obj()), 1) .. fallout.message_str(381, 104)
+    elseif v0 == 3 then
+        msg = fallout.message_str(381, 105)
+    elseif v0 == 4 then
+        msg = fallout.message_str(381, 106)
     else
-        if v0 == 3 then
-            v1 = fallout.message_str(381, 105)
-        else
-            if v0 == 4 then
-                v1 = fallout.message_str(381, 106)
-            else
-                v0 = 1
-            end
-        end
+        v0 = 1
     end
     v0 = v0 + 1
-    fallout.float_msg(fallout.self_obj(), v1, 0)
+    fallout.float_msg(fallout.self_obj(), msg, 0)
 end
 
 function critter_p_proc()
     if fallout.global_var(250) ~= 0 then
-        hostile = 1
+        hostile = true
     end
     if fallout.tile_distance_objs(fallout.self_obj(), fallout.dude_obj()) > 12 then
-        hostile = 0
+        hostile = false
     end
     if hostile then
         fallout.set_global_var(250, 1)
-        hostile = 0
+        hostile = false
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     end
 end
 
 function pickup_p_proc()
     if fallout.source_obj() == fallout.dude_obj() then
-        hostile = 1
+        hostile = true
     end
 end
 
