dfc6598e817d66da7507b68051f2cefdb2c8838a
Alexander Batalov <alex.batalov@gmail.com>
2023-12-30 13:36:27 +0300

    Review setdoor1.lua

--- src/scripts/setdoor1.lua
+++ src/scripts/setdoor1.lua
@@ -1,46 +1,40 @@
 local fallout = require("fallout")
 
 local start
-local do_stuff
-local do_skill
-local see_stuff
-local doorend
-
-local lockpicks = 0
-local testa = 0
+local use_p_proc
+local use_obj_on_p_proc
+local look_at_p_proc
 
 function start()
-    local v0 = 0
-    if fallout.script_action() == 21 then
-        fallout.script_overrides()
-        see_stuff()
-    end
-    if fallout.script_action() == 6 then
-        fallout.script_overrides()
-        do_stuff()
-    end
-    if fallout.script_action() == 7 then
-        lockpicks = fallout.obj_being_used_with()
-        fallout.script_overrides()
-        do_skill()
+    local script_action = fallout.script_action()
+    if script_action == 21 then
+        look_at_p_proc()
+    elseif script_action == 6 then
+        use_p_proc()
+    elseif script_action == 7 then
+        use_obj_on_p_proc()
     end
 end
 
-function do_stuff()
+function use_p_proc()
+    fallout.script_overrides()
     fallout.display_msg(fallout.message_str(94, 107))
 end
 
-function do_skill()
-    if fallout.has_skill(fallout.dude_obj(), 9) then
-        testa = fallout.roll_vs_skill(fallout.dude_obj(), 9, -10)
-        if fallout.is_success(testa) then
+function use_obj_on_p_proc()
+    local dude_obj = fallout.dude_obj()
+    local item_obj = fallout.obj_being_used_with()
+    fallout.script_overrides()
+    if fallout.has_skill(dude_obj, 9) then
+        local roll = fallout.roll_vs_skill(dude_obj, 9, -10)
+        if fallout.is_success(roll) then
             fallout.display_msg(fallout.message_str(94, 100))
             fallout.set_local_var(0, 1)
         else
             fallout.display_msg(fallout.message_str(94, 101))
-            if fallout.is_critical(testa) then
+            if fallout.is_critical(roll) then
                 fallout.display_msg(fallout.message_str(94, 102))
-                fallout.rm_obj_from_inven(fallout.dude_obj(), lockpicks)
+                fallout.rm_obj_from_inven(dude_obj, item_obj)
             end
         end
     else
@@ -48,7 +42,8 @@ function do_skill()
     end
 end
 
-function see_stuff()
+function look_at_p_proc()
+    fallout.script_overrides()
     if fallout.local_var(0) == 0 then
         fallout.display_msg(fallout.message_str(94, 104))
     else
@@ -60,9 +55,9 @@ function see_stuff()
     end
 end
 
-function doorend()
-end
-
 local exports = {}
 exports.start = start
+exports.use_p_proc = use_p_proc
+exports.use_obj_on_p_proc = use_obj_on_p_proc
+exports.look_at_p_proc = look_at_p_proc
 return exports
