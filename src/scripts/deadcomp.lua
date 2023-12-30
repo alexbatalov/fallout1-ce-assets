2172105e5cc25838e96967c0a6b9f45f3bfce378
Alexander Batalov <alex.batalov@gmail.com>
2023-12-30 14:11:29 +0300

    Review deadcomp.lua

--- src/scripts/deadcomp.lua
+++ src/scripts/deadcomp.lua
@@ -1,19 +1,21 @@
 local fallout = require("fallout")
 
 local start
-local do_stuff
+local use_p_proc
 
 function start()
-    if fallout.script_action() == 6 then
-        fallout.script_overrides()
-        do_stuff()
+    local script_action = fallout.script_action()
+    if script_action == 6 then
+        use_p_proc()
     end
 end
 
-function do_stuff()
+function use_p_proc()
+    fallout.script_overrides()
     fallout.display_msg(fallout.message_str(62, 100))
 end
 
 local exports = {}
 exports.start = start
+exports.use_p_proc = use_p_proc
 return exports
