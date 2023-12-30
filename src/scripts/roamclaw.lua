782ab67a671e8afe383bd5db6d024241e8d9ea0c
Alexander Batalov <alex.batalov@gmail.com>
2023-12-30 22:59:49 +0300

    Review roamclaw.lua

--- src/scripts/roamclaw.lua
+++ src/scripts/roamclaw.lua
@@ -1,14 +1,11 @@
 local fallout = require("fallout")
 
 local start
+local critter_p_proc
 local destroy_p_proc
 local timed_event_p_proc
 
 local initialized = false
-
-local exit_line = 0
-
-local critter_p_proc
 
 function start()
     if not initialized then
@@ -22,13 +19,18 @@ function destroy_p_proc()
 end
 
 function timed_event_p_proc()
-    fallout.animate_move_obj_to_tile(fallout.self_obj(), fallout.tile_num_in_direction(fallout.tile_num(fallout.self_obj()), fallout.random(0, 5), fallout.random(1, 5)), 0)
-    fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(fallout.random(3, 5)), 1)
+    local self_obj = fallout.self_obj()
+    local rotation = fallout.random(0, 5)
+    local distance = fallout.random(1, 5)
+    fallout.animate_move_obj_to_tile(self_obj,
+        fallout.tile_num_in_direction(fallout.tile_num(self_obj), rotation, distance), 0)
+    fallout.add_timer_event(self_obj, fallout.game_ticks(fallout.random(3, 5)), 1)
 end
 
 function critter_p_proc()
-    if fallout.tile_distance_objs(fallout.self_obj(), fallout.dude_obj()) <= 8 then
-        fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
+    local dude_obj = fallout.dude_obj()
+    if fallout.tile_distance_objs(fallout.self_obj(), dude_obj) <= 8 then
+        fallout.attack(dude_obj, 0, 1, 0, 0, 30000, 0, 0)
     end
 end
 
