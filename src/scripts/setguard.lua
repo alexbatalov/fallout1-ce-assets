31ac5509b749244bc531bc314b474f8ab1c97db3
Alexander Batalov <alex.batalov@gmail.com>
2023-12-30 13:32:38 +0300

    Review setguard.lua

--- src/scripts/setguard.lua
+++ src/scripts/setguard.lua
@@ -2,36 +2,57 @@ local fallout = require("fallout")
 local reputation = require("lib.reputation")
 
 local start
+local pickup_p_proc
+local talk_p_proc
+local critter_p_proc
+local destroy_p_proc
 
-local Hostile = 0
+local hostile = false
 local initialized = false
 
 function start()
     if not initialized then
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 30)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 42)
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 30)
+        fallout.critter_add_trait(self_obj, 1, 5, 42)
         initialized = true
     end
-    if fallout.script_action() == 11 then
-        fallout.display_msg(fallout.message_str(95, 100))
-    else
-        if fallout.script_action() == 4 then
-            Hostile = 1
-        else
-            if fallout.script_action() == 12 then
-                if Hostile then
-                    Hostile = 0
-                    fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
-                end
-            else
-                if fallout.script_action() == 18 then
-                    reputation.inc_evil_critter()
-                end
-            end
-        end
+
+    local script_action = fallout.script_action()
+    if script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
     end
+end
+
+function pickup_p_proc()
+    hostile = true
+end
+
+function talk_p_proc()
+    fallout.display_msg(fallout.message_str(95, 100))
+end
+
+function critter_p_proc()
+    if hostile then
+        hostile = false
+        fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
+    end
+end
+
+function destroy_p_proc()
+    reputation.inc_evil_critter()
 end
 
 local exports = {}
 exports.start = start
+exports.pickup_p_proc = pickup_p_proc
+exports.talk_p_proc = talk_p_proc
+exports.critter_p_proc = critter_p_proc
+exports.destroy_p_proc = destroy_p_proc
 return exports
