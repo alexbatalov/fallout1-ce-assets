01e513bcffa1793f254a51eb702742a884b0bf02
Alexander Batalov <alex.batalov@gmail.com>
2023-12-30 13:22:16 +0300

    Review seth2.lua

--- src/scripts/seth2.lua
+++ src/scripts/seth2.lua
@@ -2,52 +2,80 @@ local fallout = require("fallout")
 local reputation = require("lib.reputation")
 
 local start
+local pickup_p_proc
+local talk_p_proc
+local critter_p_proc
+local destroy_p_proc
+local look_at_p_proc
 
 local initialized = false
-local hostile = 0
+local hostile = false
 
 function start()
     if not initialized then
-        initialized = true
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 0)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 4)
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 0)
+        fallout.critter_add_trait(self_obj, 1, 5, 4)
         if fallout.global_var(124) == 3 then
-            fallout.set_obj_visibility(fallout.self_obj(), 1)
+            fallout.set_obj_visibility(self_obj, true)
         end
+        initialized = true
     end
-    if fallout.script_action() == 11 then
-        if fallout.global_var(43) == 2 then
-            fallout.float_msg(fallout.self_obj(), fallout.message_str(197, 102), 8)
-        else
-            fallout.float_msg(fallout.self_obj(), fallout.message_str(197, 101), 8)
-        end
+
+    local script_action = fallout.script_action()
+    if script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 21 then
+        look_at_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
+    end
+end
+
+function pickup_p_proc()
+    hostile = true
+end
+
+function talk_p_proc()
+    if fallout.global_var(43) == 2 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(197, 102), 8)
     else
-        if fallout.script_action() == 12 then
-            if hostile then
-                hostile = 0
-                fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
-            end
-            if fallout.tile_distance_objs(fallout.self_obj(), fallout.dude_obj()) > 3 then
-                fallout.animate_move_obj_to_tile(fallout.self_obj(), fallout.tile_num_in_direction(fallout.tile_num(fallout.dude_obj()), fallout.random(0, 5), 1), 0)
-            end
-        else
-            if fallout.script_action() == 4 then
-                hostile = 1
-            else
-                if fallout.script_action() == 21 then
-                    fallout.script_overrides()
-                    fallout.display_msg(fallout.message_str(197, 100))
-                else
-                    if fallout.script_action() == 18 then
-                        fallout.set_global_var(124, 3)
-                        reputation.inc_good_critter()
-                    end
-                end
-            end
-        end
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(197, 101), 8)
     end
+end
+
+function critter_p_proc()
+    local self_obj = fallout.self_obj()
+    local dude_obj = fallout.dude_obj()
+    if hostile then
+        hostile = false
+        fallout.attack(dude_obj, 0, 1, 0, 0, 30000, 0, 0)
+    end
+    if fallout.tile_distance_objs(self_obj, dude_obj) > 3 then
+        fallout.animate_move_obj_to_tile(self_obj,
+            fallout.tile_num_in_direction(fallout.tile_num(dude_obj), fallout.random(0, 5), 1), 0)
+    end
+end
+
+function destroy_p_proc()
+    fallout.set_global_var(124, 3)
+    reputation.inc_good_critter()
+end
+
+function look_at_p_proc()
+    fallout.script_overrides()
+    fallout.display_msg(fallout.message_str(197, 100))
 end
 
 local exports = {}
 exports.start = start
+exports.pickup_p_proc = pickup_p_proc
+exports.talk_p_proc = talk_p_proc
+exports.critter_p_proc = critter_p_proc
+exports.destroy_p_proc = destroy_p_proc
+exports.look_at_p_proc = look_at_p_proc
 return exports
