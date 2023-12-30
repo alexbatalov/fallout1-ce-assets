98525306fdd5ab4f4205e03f567ec12b96916b23
Alexander Batalov <alex.batalov@gmail.com>
2023-12-30 23:06:04 +0300

    Review ripper.lua

--- src/scripts/ripper.lua
+++ src/scripts/ripper.lua
@@ -7,43 +7,40 @@ local critter_p_proc
 local destroy_p_proc
 local pickup_p_proc
 
-local stealing = 0
+local stealing = false
 local initialized = false
 
 function start()
     if not initialized then
         fallout.critter_add_trait(fallout.self_obj(), 1, 6, 45)
         initialized = true
-    else
-        if fallout.script_action() == 13 then
-            combat_p_proc()
-        else
-            if fallout.script_action() == 12 then
-                critter_p_proc()
-            else
-                if fallout.script_action() == 18 then
-                    destroy_p_proc()
-                else
-                    if fallout.script_action() == 4 then
-                        pickup_p_proc()
-                    end
-                end
-            end
-        end
+    end
+
+    local script_action = fallout.script_action()
+    if script_action == 13 then
+        combat_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
     end
 end
 
 function combat_p_proc()
     if fallout.fixed_param() == 2 then
-        if fallout.obj_pid(fallout.critter_inven_obj(fallout.dude_obj(), 2)) == 4 then
-            fallout.poison(fallout.dude_obj(), fallout.random(1, 6))
+        local dude_obj = fallout.dude_obj()
+        if fallout.obj_pid(fallout.critter_inven_obj(dude_obj, 2)) == 4 then
+            fallout.poison(dude_obj, fallout.random(1, 6))
         end
     end
 end
 
 function critter_p_proc()
-    if fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) or stealing then
-        fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
+    local dude_obj = fallout.dude_obj()
+    if fallout.obj_can_see_obj(fallout.self_obj(), dude_obj) or stealing then
+        fallout.attack(dude_obj, 0, 1, 0, 0, 30000, 0, 0)
     end
 end
 
@@ -52,7 +49,7 @@ function destroy_p_proc()
 end
 
 function pickup_p_proc()
-    stealing = 1
+    stealing = true
 end
 
 local exports = {}
