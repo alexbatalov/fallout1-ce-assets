a9dc88e976a73cbe777af9d8e255cf8b8b22e9c9
Alexander Batalov <alex.batalov@gmail.com>
2023-12-31 18:05:58 +0300

    Review lagunrun.lua

--- src/scripts/lagunrun.lua
+++ src/scripts/lagunrun.lua
@@ -4,43 +4,62 @@ local party = require("lib.party")
 local time = require("lib.time")
 
 local start
+local map_enter_p_proc
+local map_exit_p_proc
+local map_update_p_proc
 
 local party_elevation = 0
-local dude_start_hex = 0
 
 fallout.create_external_var("Ian_ptr")
 fallout.create_external_var("Dog_ptr")
 fallout.create_external_var("Tycho_ptr")
 fallout.create_external_var("Katja_ptr")
 fallout.create_external_var("Tandi_ptr")
-fallout.create_external_var("get_Gabriel")
-fallout.create_external_var("peaceful")
 fallout.create_external_var("Locker_Ptr")
-fallout.create_external_var("Being_Looted")
+
+fallout.set_external_var("Ian_ptr", nil)
+fallout.set_external_var("Dog_ptr", nil)
+fallout.set_external_var("Tycho_ptr", nil)
+fallout.set_external_var("Katja_ptr", nil)
+fallout.set_external_var("Tandi_ptr", nil)
+fallout.set_external_var("Locker_Ptr", nil)
 
 function start()
-    if fallout.script_action() == 15 then
-        if fallout.metarule(14, 0) then
-            fallout.display_msg(fallout.message_str(194, 111))
-        end
-        fallout.set_global_var(597, 1)
-        fallout.override_map_start(99, 143, 0, 5)
-        if time.game_time_in_days() >= fallout.global_var(148) then
-            fallout.set_global_var(7, 1)
-        end
-        light.lighting()
-        party_elevation = party.add_party()
-    else
-        if fallout.script_action() == 23 then
-            light.lighting()
-        else
-            if fallout.script_action() == 16 then
-                party.remove_party()
-            end
-        end
+    local script_action = fallout.script_action()
+    if script_action == 15 then
+        map_enter_p_proc()
+    elseif script_action == 23 then
+        map_update_p_proc()
+    elseif script_action == 16 then
+        map_exit_p_proc()
     end
+end
+
+function map_enter_p_proc()
+    if fallout.metarule(14, 0) then
+        fallout.display_msg(fallout.message_str(194, 111))
+    end
+    fallout.set_global_var(597, 1)
+    fallout.override_map_start(99, 143, 0, 5)
+    if time.game_time_in_days() >= fallout.global_var(148) then
+        fallout.set_global_var(7, 1)
+    end
+    light.lighting()
+    party_elevation = party.add_party()
+end
+
+function map_update_p_proc()
+    light.lighting()
+    -- FIXME: Probably missing `party.update_party`.
+end
+
+function map_exit_p_proc()
+    party.remove_party()
 end
 
 local exports = {}
 exports.start = start
+exports.map_enter_p_proc = map_enter_p_proc
+exports.map_update_p_proc = map_update_p_proc
+exports.map_exit_p_proc = map_exit_p_proc
 return exports
