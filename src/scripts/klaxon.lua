b8c84acfffb1773d7cdc4f32b6f6bd8d4ee13122
Alexander Batalov <alex.batalov@gmail.com>
2023-12-31 19:41:12 +0300

    Review klaxon.lua

--- src/scripts/klaxon.lua
+++ src/scripts/klaxon.lua
@@ -10,13 +10,13 @@ function start()
 end
 
 function map_update_p_proc()
-    local v0 = 0
-    if fallout.global_var(146) and (fallout.local_var(0) == 0) then
+    if fallout.global_var(146) ~= 0 and fallout.local_var(0) == 0 then
         fallout.set_local_var(0, 1)
-        if fallout.obj_pid(fallout.self_obj()) == 33555260 then
-            v0 = fallout.create_object_sid(33555340, fallout.tile_num(fallout.self_obj()), fallout.elevation(fallout.self_obj()), -1)
+        local self_obj = fallout.self_obj()
+        if fallout.obj_pid(self_obj) == 33555260 then
+            fallout.create_object_sid(33555340, fallout.tile_num(self_obj), fallout.elevation(self_obj), -1)
         else
-            v0 = fallout.create_object_sid(33555339, fallout.tile_num(fallout.self_obj()), fallout.elevation(fallout.self_obj()), -1)
+            fallout.create_object_sid(33555339, fallout.tile_num(self_obj), fallout.elevation(self_obj), -1)
         end
     end
 end
