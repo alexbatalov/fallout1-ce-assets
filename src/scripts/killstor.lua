cce5865d10e0e24f6ed24d9c6380daae0fd4a06f
Alexander Batalov <alex.batalov@gmail.com>
2023-12-31 19:43:40 +0300

    Review killstor.lua

--- src/scripts/killstor.lua
+++ src/scripts/killstor.lua
@@ -7,39 +7,32 @@ local use_p_proc
 local use_skill_on_p_proc
 
 function start()
-    if fallout.script_action() == 15 then
+    local script_action = fallout.script_action()
+    if script_action == 15 then
         map_enter_p_proc()
-    else
-        if fallout.script_action() == 6 then
-            use_p_proc()
-        else
-            if fallout.script_action() == 8 then
-                use_skill_on_p_proc()
-            end
-        end
+    elseif script_action == 6 then
+        use_p_proc()
+    elseif script_action == 8 then
+        use_skill_on_p_proc()
     end
 end
 
 function map_enter_p_proc()
-    if fallout.tile_num(fallout.self_obj()) == 25880 then
+    local self_obj = fallout.self_obj()
+    local self_tile_num = fallout.tile_num(self_obj)
+    if self_tile_num == 25880 then
         fallout.set_external_var("Killian_store1_ptr", fallout.self_obj())
-    else
-        if fallout.tile_num(fallout.self_obj()) == 25874 then
-            fallout.set_external_var("Killian_store2_ptr", fallout.self_obj())
-        else
-            if fallout.tile_num(fallout.self_obj()) == 26874 then
-                fallout.set_external_var("Killian_store3_ptr", fallout.self_obj())
-            else
-                if fallout.tile_num(fallout.self_obj()) == 27470 then
-                    fallout.set_external_var("Killian_store4_ptr", fallout.self_obj())
-                end
-            end
-        end
+    elseif self_tile_num == 25874 then
+        fallout.set_external_var("Killian_store2_ptr", fallout.self_obj())
+    elseif self_tile_num == 26874 then
+        fallout.set_external_var("Killian_store3_ptr", fallout.self_obj())
+    elseif self_tile_num == 27470 then
+        fallout.set_external_var("Killian_store4_ptr", fallout.self_obj())
     end
 end
 
 function pickup_p_proc()
-    if (fallout.action_being_used() ~= 10) and (fallout.global_var(37) == 0) then
+    if fallout.action_being_used() ~= 10 and fallout.global_var(37) == 0 then
         fallout.script_overrides()
     end
 end
@@ -47,27 +40,22 @@ end
 function use_p_proc()
     if fallout.global_var(37) == 0 then
         fallout.script_overrides()
-        if fallout.tile_num(fallout.self_obj()) == 25880 then
+        local self_tile_num = fallout.tile_num(fallout.self_obj())
+        if self_tile_num == 25880 then
             fallout.set_external_var("Killian_barter_var", 1)
-        else
-            if fallout.tile_num(fallout.self_obj()) == 25874 then
-                fallout.set_external_var("Killian_barter_var", 2)
-            else
-                if fallout.tile_num(fallout.self_obj()) == 26874 then
-                    fallout.set_external_var("Killian_barter_var", 3)
-                else
-                    if fallout.tile_num(fallout.self_obj()) == 27470 then
-                        fallout.set_external_var("Killian_barter_var", 4)
-                    end
-                end
-            end
+        elseif self_tile_num == 25874 then
+            fallout.set_external_var("Killian_barter_var", 2)
+        elseif self_tile_num == 26874 then
+            fallout.set_external_var("Killian_barter_var", 3)
+        elseif self_tile_num == 27470 then
+            fallout.set_external_var("Killian_barter_var", 4)
         end
     end
 end
 
 function use_skill_on_p_proc()
-    if (fallout.action_being_used() == 10) and (fallout.global_var(37) == 0) then
-        if not(fallout.is_success(fallout.roll_vs_skill(fallout.source_obj(), fallout.action_being_used(), -10))) then
+    if fallout.action_being_used() == 10 and fallout.global_var(37) == 0 then
+        if not fallout.is_success(fallout.roll_vs_skill(fallout.source_obj(), 10, -10)) then
             fallout.script_overrides()
             fallout.set_external_var("Killian_barter_var", -1)
         end
