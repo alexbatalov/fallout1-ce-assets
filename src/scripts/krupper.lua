04fd9858f2e245f2724b138d60c5c9f8a551f285
Alexander Batalov <alex.batalov@gmail.com>
2023-12-31 18:10:41 +0300

    Review krupper.lua

--- src/scripts/krupper.lua
+++ src/scripts/krupper.lua
@@ -15,48 +15,39 @@ local krupper03
 local krupper04
 local kruppercombat
 
-local hostile = 0
+local hostile = false
 local initialized = false
-
-local exit_line = 0
 
 function start()
     if not initialized then
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 34)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 48)
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 34)
+        fallout.critter_add_trait(self_obj, 1, 5, 48)
         initialized = true
     end
-    if fallout.script_action() == 12 then
+
+    local script_action = fallout.script_action()
+    if script_action == 12 then
         critter_p_proc()
-    else
-        if fallout.script_action() == 21 then
-            look_at_p_proc()
-        else
-            if fallout.script_action() == 4 then
-                pickup_p_proc()
-            else
-                if fallout.script_action() == 11 then
-                    talk_p_proc()
-                else
-                    if fallout.script_action() == destroy_p_proc() then
-                        destroy_p_proc()
-                    else
-                        if fallout.script_action() == 22 then
-                            timed_event_p_proc()
-                        end
-                    end
-                end
-            end
-        end
+    elseif script_action == 21 then
+        look_at_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
+    elseif script_action == 22 then
+        timed_event_p_proc()
     end
 end
 
 function critter_p_proc()
-    if (fallout.local_var(4) == 0) and fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) then
+    if fallout.local_var(4) == 0 and fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) then
         fallout.dialogue_system_enter()
     end
     if hostile then
-        hostile = 0
+        hostile = false
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     end
 end
@@ -67,7 +58,7 @@ function look_at_p_proc()
 end
 
 function pickup_p_proc()
-    hostile = 1
+    hostile = true
 end
 
 function talk_p_proc()
@@ -81,7 +72,7 @@ end
 
 function timed_event_p_proc()
     if fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) then
-        hostile = 1
+        hostile = true
     end
 end
 
@@ -122,7 +113,7 @@ function krupper04()
 end
 
 function kruppercombat()
-    hostile = 1
+    hostile = true
 end
 
 local exports = {}
