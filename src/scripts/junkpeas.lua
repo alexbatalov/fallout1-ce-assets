bae12a2ac8d1e996a45cd52020a3c25469acc022
Alexander Batalov <alex.batalov@gmail.com>
2024-01-01 13:30:52 +0300

    Review junkpeas.lua

--- src/scripts/junkpeas.lua
+++ src/scripts/junkpeas.lua
@@ -9,54 +9,39 @@ local destroy_p_proc
 local pickup_p_proc
 local talk_p_proc
 
-local night_person = false
-local wake_time = 0
-local sleep_time = 0
-local home_tile = 0
-local sleep_tile = 0
 local initialized = false
 
 function start()
-    local v0 = 0
     if not initialized then
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 24)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 6)
-        if fallout.obj_is_carrying_obj_pid(fallout.self_obj(), 41) == 0 then
-            v0 = fallout.create_object_sid(41, 0, 0, -1)
-            fallout.add_mult_objs_to_inven(fallout.self_obj(), v0, fallout.random(0, 20))
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 24)
+        fallout.critter_add_trait(self_obj, 1, 5, 6)
+        if fallout.obj_is_carrying_obj_pid(self_obj, 41) == 0 then
+            local item_obj = fallout.create_object_sid(41, 0, 0, -1)
+            fallout.add_mult_objs_to_inven(self_obj, item_obj, fallout.random(0, 20))
         end
         if fallout.local_var(3) == 0 then
-            fallout.set_local_var(3, fallout.tile_num(fallout.self_obj()))
+            fallout.set_local_var(3, fallout.tile_num(self_obj))
         end
-        sleep_tile = 7000
-        wake_time = fallout.random(610, 650)
-        sleep_time = fallout.random(1710, 1750)
         initialized = true
-    else
-        if fallout.script_action() == 12 then
-            critter_p_proc()
-        else
-            if fallout.script_action() == 14 then
-                damage_p_proc()
-            else
-                if fallout.script_action() == 18 then
-                    destroy_p_proc()
-                else
-                    if fallout.script_action() == 4 then
-                        pickup_p_proc()
-                    else
-                        if fallout.script_action() == 11 then
-                            talk_p_proc()
-                        end
-                    end
-                end
-            end
-        end
+    end
+
+    local script_action = fallout.script_action()
+    if script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 14 then
+        damage_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
     end
 end
 
 function critter_p_proc()
-    if fallout.local_var(1) and fallout.tile_distance_objs(fallout.self_obj(), fallout.dude_obj()) then
+    if fallout.local_var(1) ~= 0 and fallout.tile_distance_objs(fallout.self_obj(), fallout.dude_obj()) ~= 0 then
         behaviour.flee_dude(1)
     end
 end
@@ -84,7 +69,7 @@ function pickup_p_proc()
 end
 
 function talk_p_proc()
-    if (fallout.local_var(1) == 1) or (fallout.global_var(247) == 1) then
+    if fallout.local_var(1) == 1 or fallout.global_var(247) == 1 then
         fallout.float_msg(fallout.self_obj(), fallout.message_str(669, fallout.random(100, 105)), 2)
     else
         if fallout.local_var(0) == 0 then
