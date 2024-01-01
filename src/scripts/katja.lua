de867935d214a70b52625f6e996e97295d623a50
Alexander Batalov <alex.batalov@gmail.com>
2024-01-01 13:13:43 +0300

    Review katja.lua

--- src/scripts/katja.lua
+++ src/scripts/katja.lua
@@ -48,49 +48,39 @@ local map_commentary
 local pick_lock
 local set_lock
 
-local hostile = 0
-local item = 0
-local unwield_flag = 0
-local lock = 0
+local hostile = false
+local unwield_flag = false
+local lock_obj = nil
 local prev_tile = 7000
 local dest_tile = 7000
-local line173flag = 0
-local line174flag = 0
-local line176flag = 0
-local line184flag = 0
-local line186flag = 0
-local line187flag = 0
-local line188flag = 0
+local line173flag = false
+local line174flag = false
+local line176flag = false
+local line184flag = false
+local line186flag = false
+local line187flag = false
+local line188flag = false
 
 function start()
-    if fallout.script_action() == 12 then
+    local script_action = fallout.script_action()
+    if script_action == 12 then
         critter_p_proc()
-    else
-        if fallout.script_action() == 18 then
-            destroy_p_proc()
-        else
-            if fallout.script_action() == 15 then
-                map_enter_p_proc()
-            else
-                if fallout.script_action() == 7 then
-                    use_obj_on_p_proc()
-                else
-                    if fallout.script_action() == 11 then
-                        talk_p_proc()
-                    else
-                        if fallout.script_action() == 22 then
-                            timed_event_p_proc()
-                        end
-                    end
-                end
-            end
-        end
+    elseif script_action == 18 then
+        destroy_p_proc()
+    elseif script_action == 15 then
+        map_enter_p_proc()
+    elseif script_action == 7 then
+        use_obj_on_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 22 then
+        timed_event_p_proc()
     end
 end
 
 function critter_p_proc()
     if hostile then
-        hostile = 0
+        hostile = false
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     else
         map_commentary()
@@ -103,30 +93,51 @@ function destroy_p_proc()
 end
 
 function map_enter_p_proc()
+    local self_obj = fallout.self_obj()
     if fallout.global_var(244) < 2 then
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 49)
+        fallout.critter_add_trait(self_obj, 1, 6, 49)
     else
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 0)
-        fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(2), 1)
+        fallout.critter_add_trait(self_obj, 1, 6, 0)
+        fallout.add_timer_event(self_obj, fallout.game_ticks(2), 1)
     end
-    fallout.set_external_var("Katja_ptr", fallout.self_obj())
+    fallout.set_external_var("Katja_ptr", self_obj)
 end
 
 function use_obj_on_p_proc()
     if fallout.global_var(244) == 2 then
-        item = fallout.obj_pid(fallout.obj_being_used_with())
-        if (item ~= 40) and (item ~= 47) and (item ~= 91) then
+        local self_obj = fallout.self_obj()
+        local dude_obj = fallout.dude_obj()
+        local item_obj = fallout.obj_being_used_with()
+        local item_pid = fallout.obj_pid(item_obj)
+        if item_pid ~= 40
+            and item_pid ~= 47
+            and item_pid ~= 91 then
             fallout.script_overrides()
-            if fallout.obj_item_subtype(fallout.obj_being_used_with()) == 3 then
-                if (item == 4) or (item == 9) or (item == 22) or (item == 45) or (item == 7) or (item == 159) or (item == 25) or (item == 26) or (item == 27) or (item == 19) or (item == 21) or (item == 234) or (item == 235) or (item == 116) or (item == 236) or (item == 241) then
-                    fallout.rm_obj_from_inven(fallout.dude_obj(), fallout.obj_being_used_with())
-                    fallout.add_obj_to_inven(fallout.self_obj(), fallout.obj_being_used_with())
+            if fallout.obj_item_subtype(item_obj) == 3 then
+                if item_pid == 4
+                    or item_pid == 9
+                    or item_pid == 22
+                    or item_pid == 45
+                    or item_pid == 7
+                    or item_pid == 159
+                    or item_pid == 25
+                    or item_pid == 26
+                    or item_pid == 27
+                    or item_pid == 19
+                    or item_pid == 21
+                    or item_pid == 234
+                    or item_pid == 235
+                    or item_pid == 116
+                    or item_pid == 236
+                    or item_pid == 241 then
+                    fallout.rm_obj_from_inven(dude_obj, item_obj)
+                    fallout.add_obj_to_inven(self_obj, item_obj)
                 else
-                    fallout.float_msg(fallout.self_obj(), fallout.message_str(634, 109), 5)
+                    fallout.float_msg(self_obj, fallout.message_str(634, 109), 5)
                 end
             else
-                fallout.rm_obj_from_inven(fallout.dude_obj(), fallout.obj_being_used_with())
-                fallout.add_obj_to_inven(fallout.self_obj(), fallout.obj_being_used_with())
+                fallout.rm_obj_from_inven(dude_obj, item_obj)
+                fallout.add_obj_to_inven(self_obj, item_obj)
             end
         end
     end
@@ -137,24 +148,20 @@ function talk_p_proc()
     fallout.gsay_start()
     if fallout.global_var(244) == 3 then
         Katja28()
+    elseif fallout.global_var(244) == 2 then
+        Katja24()
+    elseif fallout.global_var(244) == 1 then
+        Katja25()
     else
-        if fallout.global_var(244) == 2 then
-            Katja24()
-        else
-            if fallout.global_var(244) == 1 then
-                Katja25()
-            else
-                Katja01()
-            end
-        end
+        Katja01()
     end
     fallout.gsay_end()
     fallout.end_dialogue()
-    if unwield_flag == 1 then
-        unwield_flag = 0
+    if unwield_flag then
+        unwield_flag = false
         fallout.inven_unwield()
     end
-    if lock then
+    if lock_obj ~= nil then
         pick_lock()
     end
     if fallout.global_var(244) == 2 then
@@ -175,7 +182,9 @@ end
 function Katja01()
     fallout.set_global_var(244, 1)
     fallout.gsay_reply(623, 102)
-    fallout.giq_option(4, 623, fallout.message_str(623, 103) .. fallout.proto_data(fallout.obj_pid(fallout.dude_obj()), 1) .. fallout.message_str(623, 104), Katja02, 50)
+    fallout.giq_option(4, 623,
+        fallout.message_str(623, 103) ..
+        fallout.proto_data(fallout.obj_pid(fallout.dude_obj()), 1) .. fallout.message_str(623, 104), Katja02, 50)
     fallout.giq_option(4, 623, 105, Katja02, 50)
     fallout.giq_option(5, 623, 106, Katja05, 50)
     fallout.giq_option(-3, 623, 189, KatjaExit, 50)
@@ -186,7 +195,7 @@ function Katja02()
     fallout.giq_option(5, 623, 108, Katja03, 50)
     fallout.giq_option(5, 623, 109, Katja04, 50)
     fallout.giq_option(4, 623, 110, Katja07, 50)
-    if (fallout.global_var(101) ~= 2) and (fallout.obj_is_carrying_obj_pid(fallout.dude_obj(), 55) == 0) then
+    if fallout.global_var(101) ~= 2 and fallout.obj_is_carrying_obj_pid(fallout.dude_obj(), 55) == 0 then
         fallout.giq_option(4, 623, 111, Katja06, 50)
     end
 end
@@ -195,7 +204,7 @@ function Katja03()
     fallout.gsay_reply(623, 112)
     fallout.giq_option(5, 623, 109, Katja04, 50)
     fallout.giq_option(4, 623, 110, Katja07, 50)
-    if (fallout.global_var(101) ~= 2) and (fallout.obj_is_carrying_obj_pid(fallout.dude_obj(), 55) == 0) then
+    if fallout.global_var(101) ~= 2 and fallout.obj_is_carrying_obj_pid(fallout.dude_obj(), 55) == 0 then
         fallout.giq_option(4, 623, 111, Katja06, 50)
     end
 end
@@ -209,7 +218,9 @@ end
 
 function Katja05()
     fallout.gsay_reply(623, 117)
-    fallout.giq_option(4, 623, fallout.message_str(623, 103) .. fallout.proto_data(fallout.obj_pid(fallout.dude_obj()), 1) .. fallout.message_str(623, 104), Katja02, 50)
+    fallout.giq_option(4, 623,
+        fallout.message_str(623, 103) ..
+        fallout.proto_data(fallout.obj_pid(fallout.dude_obj()), 1) .. fallout.message_str(623, 104), Katja02, 50)
     fallout.giq_option(4, 623, 118, Katja07, 50)
     fallout.giq_option(4, 623, 115, KatjaEnd, 50)
 end
@@ -233,7 +244,7 @@ end
 
 function Katja09()
     fallout.gsay_reply(623, 127)
-    if (fallout.global_var(101) ~= 2) and (fallout.obj_is_carrying_obj_pid(fallout.dude_obj(), 55) == 0) then
+    if fallout.global_var(101) ~= 2 and fallout.obj_is_carrying_obj_pid(fallout.dude_obj(), 55) == 0 then
         fallout.giq_option(4, 623, 128, Katja06, 50)
     end
     fallout.giq_option(4, 623, 129, Katja10, 50)
@@ -282,7 +293,7 @@ end
 
 function Katja17()
     fallout.gsay_reply(623, 144)
-    if not(fallout.global_var(77)) then
+    if fallout.global_var(77) == 0 then
         fallout.set_global_var(77, 1)
     end
     fallout.giq_option(4, 623, 145, Katja18, 50)
@@ -303,10 +314,11 @@ function Katja19()
 end
 
 function Katja20()
+    local self_obj = fallout.self_obj()
     fallout.set_global_var(244, 2)
-    fallout.party_add(fallout.self_obj())
-    fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(1), 1)
-    fallout.critter_add_trait(fallout.self_obj(), 1, 6, 0)
+    fallout.party_add(self_obj)
+    fallout.add_timer_event(self_obj, fallout.game_ticks(1), 1)
+    fallout.critter_add_trait(self_obj, 1, 6, 0)
     fallout.gsay_message(623, 153, 50)
 end
 
@@ -325,7 +337,7 @@ end
 function Katja23()
     fallout.gsay_reply(623, 159)
     fallout.giq_option(4, 623, 160, Katja09, 50)
-    if (fallout.global_var(101) == 0) and (fallout.obj_is_carrying_obj_pid(fallout.dude_obj(), 55) == 0) then
+    if fallout.global_var(101) == 0 and fallout.obj_is_carrying_obj_pid(fallout.dude_obj(), 55) == 0 then
         fallout.giq_option(4, 623, 161, Katja06, 50)
     end
     fallout.giq_option(4, 623, 162, KatjaEnd, 50)
@@ -338,7 +350,7 @@ function Katja24()
             fallout.giq_option(4, 623, 164, set_lock, 50)
         end
     end
-    if (fallout.cur_map_index() == 10) and (fallout.elevation(fallout.dude_obj()) == 1) then
+    if fallout.cur_map_index() == 10 and fallout.elevation(fallout.dude_obj()) == 1 then
         if fallout.map_var(4) ~= 0 then
             fallout.giq_option(4, 623, 164, set_lock, 50)
         end
@@ -364,7 +376,7 @@ function Katja26()
 end
 
 function Katja27()
-    unwield_flag = 1
+    unwield_flag = true
     fallout.gsay_message(623, 179, 50)
     Katja24()
 end
@@ -409,85 +421,83 @@ function KatjaExit()
 end
 
 function follow_player()
-    local v0 = 0
+    local dude_obj = fallout.dude_obj()
+    local dude_tile_num = fallout.tile_num(dude_obj)
+    local self_obj = fallout.self_obj()
+
     prev_tile = dest_tile
-    v0 = (fallout.has_trait(1, fallout.dude_obj(), 10) + fallout.random(2, 4)) % 6
-    dest_tile = fallout.tile_num_in_direction(fallout.tile_num(fallout.dude_obj()), v0, (fallout.global_var(279) * 2) + fallout.random(1, 2))
-    if (fallout.tile_distance_objs(fallout.self_obj(), fallout.dude_obj()) > ((fallout.global_var(279) + 1) * 2)) or (fallout.random(0, 3) == 3) then
-        if fallout.tile_distance(prev_tile, fallout.tile_num(fallout.dude_obj())) > fallout.tile_distance(dest_tile, fallout.tile_num(fallout.dude_obj())) then
-            if fallout.tile_distance_objs(fallout.self_obj(), fallout.dude_obj()) > 8 then
-                fallout.animate_move_obj_to_tile(fallout.self_obj(), dest_tile, 1 | 16)
+    dest_tile = fallout.tile_num_in_direction(dude_tile_num,
+        (fallout.has_trait(1, dude_obj, 10) + fallout.random(2, 4)) % 6,
+        (fallout.global_var(279) * 2) + fallout.random(1, 2))
+
+    local distance_prev_to_dude = fallout.tile_distance(prev_tile, dude_tile_num)
+    local distance_dest_to_dude = fallout.tile_distance(dest_tile, dude_tile_num)
+    local distance_self_to_dude = fallout.tile_distance_objs(self_obj, dude_obj)
+    if distance_self_to_dude > (fallout.global_var(279) + 1) * 2 or fallout.random(0, 3) == 3 then
+        if distance_prev_to_dude > distance_dest_to_dude then
+            if distance_self_to_dude > 8 then
+                fallout.animate_move_obj_to_tile(self_obj, dest_tile, 1 | 16)
             else
-                fallout.animate_move_obj_to_tile(fallout.self_obj(), dest_tile, 0 | 16)
+                fallout.animate_move_obj_to_tile(self_obj, dest_tile, 0 | 16)
             end
         else
-            if fallout.tile_distance_objs(fallout.self_obj(), fallout.dude_obj()) > 8 then
-                fallout.animate_move_obj_to_tile(fallout.self_obj(), dest_tile, 1)
+            if distance_self_to_dude > 8 then
+                fallout.animate_move_obj_to_tile(self_obj, dest_tile, 1)
             else
-                fallout.animate_move_obj_to_tile(fallout.self_obj(), dest_tile, 0)
+                fallout.animate_move_obj_to_tile(self_obj, dest_tile, 0)
             end
         end
     end
-    fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(1), 1)
+    fallout.add_timer_event(self_obj, fallout.game_ticks(1), 1)
 end
 
 function map_commentary()
-    if fallout.cur_map_index() == 33 then
-        if (fallout.tile_distance(fallout.tile_num(fallout.self_obj()), 27317) < 4) and (line173flag == 0) then
-            fallout.float_msg(fallout.self_obj(), fallout.message_str(623, 173), 5)
-            line173flag = 1
+    local self_obj = fallout.self_obj()
+    local cur_map_index = fallout.cur_map_index()
+    if cur_map_index == 33 then
+        if fallout.tile_distance(fallout.tile_num(self_obj), 27317) < 4 and not line173flag then
+            fallout.float_msg(self_obj, fallout.message_str(623, 173), 5)
+            line173flag = true
         end
-    else
-        if fallout.cur_map_index() == 6 then
-            if fallout.elevation(fallout.self_obj()) == 1 then
-                if fallout.tile_distance(fallout.tile_num(fallout.self_obj()), 19111) < 4 then
-                    if (line174flag == 0) and (fallout.global_var(238) ~= 2) and (fallout.external_var("Ian_ptr") ~= 0) then
-                        fallout.float_msg(fallout.self_obj(), fallout.message_str(623, 172), 5)
-                        line174flag = 1
-                    end
+    elseif cur_map_index == 6 then
+        if fallout.elevation(self_obj) == 1 then
+            if fallout.tile_distance(fallout.tile_num(self_obj), 19111) < 4 then
+                if not line174flag and fallout.global_var(238) ~= 2 and fallout.external_var("Ian_ptr") ~= nil then
+                    fallout.float_msg(self_obj, fallout.message_str(623, 172), 5)
+                    line174flag = true
                 end
             end
-        else
-            if fallout.cur_map_index() == 38 then
-                if (fallout.tile_distance(fallout.tile_num(fallout.self_obj()), 28104) < 5) and (line176flag == 0) then
-                    fallout.float_msg(fallout.self_obj(), fallout.message_str(623, 176), 5)
-                    if fallout.global_var(121) == 2 then
-                        fallout.float_msg(fallout.external_var("Tycho_ptr"), fallout.message_str(389, 167), 12)
-                    end
-                    line176flag = 1
-                end
-            else
-                if fallout.cur_map_index() == 32 then
-                    if line184flag == 0 then
-                        fallout.float_msg(fallout.self_obj(), fallout.message_str(623, 184), 5)
-                        if fallout.global_var(118) == 2 then
-                            fallout.float_msg(fallout.external_var("Ian_ptr"), fallout.message_str(235, 175), 0)
-                        end
-                        line184flag = 1
-                    end
-                else
-                    if fallout.cur_map_index() == 34 then
-                        if line186flag == 0 then
-                            fallout.float_msg(fallout.self_obj(), fallout.message_str(623, 186), 5)
-                            line186flag = 1
-                        end
-                    else
-                        if fallout.cur_map_index() == 3 then
-                            if (fallout.tile_distance(fallout.tile_num(fallout.self_obj()), 17274) < 4) and (line187flag == 0) then
-                                fallout.float_msg(fallout.self_obj(), fallout.message_str(623, 187), 5)
-                                line187flag = 1
-                            end
-                        else
-                            if fallout.cur_map_index() == 45 then
-                                if line188flag == 0 then
-                                    fallout.float_msg(fallout.self_obj(), fallout.message_str(623, 188), 5)
-                                    line188flag = 1
-                                end
-                            end
-                        end
-                    end
-                end
+        end
+    elseif cur_map_index == 38 then
+        if fallout.tile_distance(fallout.tile_num(self_obj), 28104) < 5 and not line176flag then
+            fallout.float_msg(self_obj, fallout.message_str(623, 176), 5)
+            if fallout.global_var(121) == 2 then
+                fallout.float_msg(fallout.external_var("Tycho_ptr"), fallout.message_str(389, 167), 12)
             end
+            line176flag = true
+        end
+    elseif cur_map_index == 32 then
+        if not line184flag then
+            fallout.float_msg(self_obj, fallout.message_str(623, 184), 5)
+            if fallout.global_var(118) == 2 then
+                fallout.float_msg(fallout.external_var("Ian_ptr"), fallout.message_str(235, 175), 0)
+            end
+            line184flag = true
+        end
+    elseif cur_map_index == 34 then
+        if not line186flag then
+            fallout.float_msg(self_obj, fallout.message_str(623, 186), 5)
+            line186flag = true
+        end
+    elseif cur_map_index == 3 then
+        if fallout.tile_distance(fallout.tile_num(self_obj), 17274) < 4 and not line187flag then
+            fallout.float_msg(self_obj, fallout.message_str(623, 187), 5)
+            line187flag = true
+        end
+    elseif cur_map_index == 45 then
+        if not line188flag then
+            fallout.float_msg(self_obj, fallout.message_str(623, 188), 5)
+            line188flag = true
         end
     end
 end
@@ -495,21 +505,19 @@ end
 function pick_lock()
     fallout.reg_anim_func(2, fallout.self_obj())
     fallout.reg_anim_func(1, 1)
-    fallout.reg_anim_obj_move_to_obj(fallout.self_obj(), lock, -1)
+    fallout.reg_anim_obj_move_to_obj(fallout.self_obj(), lock_obj, -1)
     fallout.reg_anim_animate(fallout.self_obj(), 11, -1)
     fallout.reg_anim_func(3, 0)
-    fallout.use_obj(lock)
-    fallout.obj_unlock(lock)
-    lock = 0
+    fallout.use_obj(lock_obj)
+    fallout.obj_unlock(lock_obj)
+    lock_obj = nil
 end
 
 function set_lock()
     if fallout.cur_map_index() == 30 then
-        lock = fallout.map_var(3)
-    else
-        if fallout.cur_map_index() == 10 then
-            lock = fallout.map_var(4)
-        end
+        lock_obj = fallout.map_var(3)
+    elseif fallout.cur_map_index() == 10 then
+        lock_obj = fallout.map_var(4)
     end
 end
 
