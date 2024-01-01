5134c1d6381e5d951886f0c355d65185f44a023f
Alexander Batalov <alex.batalov@gmail.com>
2024-01-01 17:43:48 +0300

    Review hubsmity.lua

--- src/scripts/hubsmity.lua
+++ src/scripts/hubsmity.lua
@@ -10,52 +10,45 @@ local talk_p_proc
 local destroy_p_proc
 local look_at_p_proc
 
-local hostile = 0
+local hostile = false
 local initialized = false
-
-local exit_line = 0
 
 function start()
     if not initialized then
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 39)
+        fallout.critter_add_trait(self_obj, 1, 5, 52)
         initialized = true
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 39)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 52)
     end
-    if fallout.script_action() == 21 then
+
+    local script_action = fallout.script_action()
+    if script_action == 21 then
         look_at_p_proc()
-    else
-        if fallout.script_action() == 4 then
-            pickup_p_proc()
-        else
-            if fallout.script_action() == 11 then
-                talk_p_proc()
-            else
-                if fallout.script_action() == 12 then
-                    critter_p_proc()
-                else
-                    if fallout.script_action() == 18 then
-                        destroy_p_proc()
-                    end
-                end
-            end
-        end
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
     end
 end
 
 function combat()
-    hostile = 1
+    hostile = true
 end
 
 function critter_p_proc()
     if hostile then
-        hostile = 0
+        hostile = false
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     end
 end
 
 function pickup_p_proc()
     if fallout.source_obj() == fallout.dude_obj() then
-        hostile = 1
+        hostile = true
     end
 end
 
@@ -63,12 +56,10 @@ function talk_p_proc()
     reaction.get_reaction()
     if fallout.map_var(1) == 1 then
         fallout.float_msg(fallout.self_obj(), fallout.message_str(591, 101), 2)
+    elseif fallout.global_var(107) == 2 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(591, 104), 2)
     else
-        if fallout.global_var(107) == 2 then
-            fallout.float_msg(fallout.self_obj(), fallout.message_str(591, 104), 2)
-        else
-            fallout.float_msg(fallout.self_obj(), fallout.message_str(591, 103), 2)
-        end
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(591, 103), 2)
     end
 end
 
