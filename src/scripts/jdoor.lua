05139e807ef293f313b98dbd27d4129a2ff89ec2
Alexander Batalov <alex.batalov@gmail.com>
2024-01-01 15:30:08 +0300

    Review jdoor.lua

--- src/scripts/jdoor.lua
+++ src/scripts/jdoor.lua
@@ -8,62 +8,44 @@ local use_skill_p_proc
 local map_update_p_proc
 local map_enter_p_proc
 local damage_p_proc
-
-local cantopen = 0
+local timed_event_p_proc
 
 function start()
-    if fallout.script_action() == 15 then
+    local script_action = fallout.script_action()
+    if script_action == 15 then
         map_enter_p_proc()
-    else
-        if (fallout.script_action() == 21) or (fallout.script_action() == 3) then
-            look_at_p_proc()
-        else
-            if fallout.script_action() == 6 then
-                use_p_proc()
-            else
-                if fallout.script_action() == 8 then
-                    use_skill_p_proc()
-                else
-                    if fallout.script_action() == 7 then
-                        use_obj_on_p_proc()
-                    else
-                        if fallout.script_action() == 14 then
-                            damage_p_proc()
-                        else
-                            if fallout.script_action() == 22 then
-                                if not(fallout.combat_is_initialized()) then
-                                    fallout.obj_close(fallout.self_obj())
-                                end
-                            else
-                                if fallout.script_action() == 23 then
-                                    map_update_p_proc()
-                                end
-                            end
-                        end
-                    end
-                end
-            end
-        end
+    elseif script_action == 21 or script_action == 3 then
+        look_at_p_proc()
+    elseif script_action == 6 then
+        use_p_proc()
+    elseif script_action == 8 then
+        use_skill_p_proc()
+    elseif script_action == 7 then
+        use_obj_on_p_proc()
+    elseif script_action == 14 then
+        damage_p_proc()
+    elseif script_action == 22 then
+        timed_event_p_proc()
+    elseif script_action == 23 then
+        map_update_p_proc()
     end
 end
 
 function use_p_proc()
-    cantopen = 0
-    if fallout.source_obj() == fallout.dude_obj() then
-        cantopen = 1
-        if fallout.tile_num(fallout.dude_obj()) == 27913 then
-            cantopen = 0
-        else
-            if fallout.tile_num(fallout.dude_obj()) == 28113 then
-                cantopen = 0
-            else
-                if fallout.tile_num(fallout.dude_obj()) == 28313 then
-                    cantopen = 0
-                end
-            end
+    local cantopen = false
+    local dude_obj = fallout.dude_obj()
+    if fallout.source_obj() == dude_obj then
+        cantopen = true
+        local dude_tile_num = fallout.tile_num(dude_obj)
+        if dude_tile_num == 27913 then
+            cantopen = false
+        elseif dude_tile_num == 28113 then
+            cantopen = false
+        elseif dude_tile_num == 28313 then
+            cantopen = false
         end
     end
-    if (fallout.local_var(0) == 0) and cantopen then
+    if fallout.local_var(0) == 0 and cantopen then
         fallout.script_overrides()
         fallout.display_msg(fallout.message_str(63, 104))
     end
@@ -76,11 +58,8 @@ function look_at_p_proc()
 end
 
 function use_obj_on_p_proc()
-    local v0 = 0
-    local v1 = 0
-    v0 = fallout.obj_being_used_with()
-    v1 = fallout.roll_vs_skill(fallout.dude_obj(), 9, 20)
-    if fallout.obj_pid(v0) == 84 then
+    local item_obj = fallout.obj_being_used_with()
+    if fallout.obj_pid(item_obj) == 84 then
         fallout.script_overrides()
         fallout.display_msg(fallout.message_str(63, 200))
     end
@@ -100,8 +79,14 @@ function map_enter_p_proc()
 end
 
 function damage_p_proc()
-    fallout.set_obj_visibility(fallout.self_obj(), 1)
+    fallout.set_obj_visibility(fallout.self_obj(), true)
     fallout.set_local_var(1, 1)
+end
+
+function timed_event_p_proc()
+    if not fallout.combat_is_initialized() then
+        fallout.obj_close(fallout.self_obj())
+    end
 end
 
 local exports = {}
@@ -113,4 +98,5 @@ exports.use_skill_p_proc = use_skill_p_proc
 exports.map_update_p_proc = map_update_p_proc
 exports.map_enter_p_proc = map_enter_p_proc
 exports.damage_p_proc = damage_p_proc
+exports.timed_event_p_proc = timed_event_p_proc
 return exports
