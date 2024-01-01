f8384b80c87400b3d8f1204564b3148333af5e08
Alexander Batalov <alex.batalov@gmail.com>
2024-01-01 12:56:33 +0300

    Review kenny.lua

--- src/scripts/kenny.lua
+++ src/scripts/kenny.lua
@@ -3,7 +3,6 @@ local reaction = require("lib.reaction")
 local reputation = require("lib.reputation")
 
 local start
-local combat
 local critter_p_proc
 local pickup_p_proc
 local talk_p_proc
@@ -12,52 +11,41 @@ local look_at_p_proc
 local damage_p_proc
 local Kenny00
 
-local hostile = 0
+local hostile = false
 local initialized = false
-
-local exit_line = 0
 
 function start()
     if not initialized then
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 40)
+        fallout.critter_add_trait(self_obj, 1, 5, 86)
         initialized = true
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 40)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 86)
     end
-    if fallout.script_action() == 21 then
-        look_at_p_proc()
-    else
-        if fallout.script_action() == 4 then
-            pickup_p_proc()
-        else
-            if fallout.script_action() == 11 then
-                talk_p_proc()
-            else
-                if fallout.script_action() == 12 then
-                    critter_p_proc()
-                else
-                    if fallout.script_action() == 18 then
-                        destroy_p_proc()
-                    end
-                end
-            end
-        end
-    end
-end
 
-function combat()
-    hostile = 1
+    local script_action = fallout.script_action()
+    if script_action == 21 then
+        look_at_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
+    end
 end
 
 function critter_p_proc()
     if hostile then
-        hostile = 0
+        hostile = false
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     end
 end
 
 function pickup_p_proc()
     if fallout.source_obj() == fallout.dude_obj() then
-        hostile = 1
+        hostile = true
     end
 end
 
@@ -90,16 +78,14 @@ function look_at_p_proc()
 end
 
 function damage_p_proc()
-    local v0 = 0
-    v0 = fallout.obj_pid(fallout.source_obj())
-    if fallout.party_member_obj(v0) ~= 0 then
+    local pid = fallout.obj_pid(fallout.source_obj())
+    if fallout.party_member_obj(pid) ~= nil then
         fallout.set_global_var(248, 1)
     end
 end
 
 function Kenny00()
-    local v0 = 0
-    v0 = fallout.item_caps_adjust(fallout.dude_obj(), 1000)
+    fallout.item_caps_adjust(fallout.dude_obj(), 1000)
     fallout.set_global_var(155, fallout.global_var(155) + 4)
     fallout.give_exp_points(1400)
     fallout.display_msg(fallout.message_str(766, 103) .. 1400 .. fallout.message_str(766, 104))
