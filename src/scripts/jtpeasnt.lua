478c9e770c2cddec469c596578dd36be94feb844
Alexander Batalov <alex.batalov@gmail.com>
2024-01-01 13:56:24 +0300

    Review jtpeasnt.lua

--- src/scripts/jtpeasnt.lua
+++ src/scripts/jtpeasnt.lua
@@ -2,57 +2,58 @@ local fallout = require("fallout")
 local reputation = require("lib.reputation")
 
 local start
-local do_dialogue
+local talk_p_proc
+local destroy_p_proc
+local look_at_p_proc
 local guard00
-local first
-local notfirst
 
 function start()
-    local v0 = 0
-    if fallout.script_action() == 11 then
-        do_dialogue()
+    local script_action = fallout.script_action()
+    if script_action == 11 then
+        talk_p_proc()
     else
-        if fallout.script_action() == 18 then
-            reputation.inc_good_critter()
+        if script_action == 18 then
+            destroy_p_proc()
         else
-            if (fallout.script_action() == 21) or (fallout.script_action() == 3) then
-                fallout.script_overrides()
-                fallout.display_msg(fallout.message_str(38, 100))
+            if script_action == 21 or script_action == 3 then
+                look_at_p_proc()
             end
         end
     end
 end
 
-function do_dialogue()
+function talk_p_proc()
     guard00()
 end
 
+function destroy_p_proc()
+    reputation.inc_good_critter()
+end
+
+function look_at_p_proc()
+    fallout.script_overrides()
+    fallout.display_msg(fallout.message_str(38, 100))
+end
+
 function guard00()
-    local v0 = 0
+    local rnd
     if fallout.get_critter_stat(fallout.dude_obj(), 34) == 0 then
-        v0 = fallout.random(1, 2)
+        rnd = fallout.random(1, 2)
     else
-        v0 = fallout.random(1, 3)
+        rnd = fallout.random(1, 3)
     end
-    if v0 == 1 then
+    if rnd == 1 then
         fallout.float_msg(fallout.self_obj(), fallout.message_str(38, 103), 0)
-    else
-        if v0 == 2 then
-            fallout.float_msg(fallout.self_obj(), fallout.message_str(38, 104), 0)
-        else
-            if v0 == 3 then
-                fallout.float_msg(fallout.self_obj(), fallout.message_str(38, 105), 0)
-            end
-        end
+    elseif rnd == 2 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(38, 104), 0)
+    elseif rnd == 3 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(38, 105), 0)
     end
-end
-
-function first()
-end
-
-function notfirst()
 end
 
 local exports = {}
 exports.start = start
+exports.talk_p_proc = talk_p_proc
+exports.destroy_p_proc = destroy_p_proc
+exports.look_at_p_proc = look_at_p_proc
 return exports
