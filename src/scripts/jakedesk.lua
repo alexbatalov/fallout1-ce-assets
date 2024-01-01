3d947a9b1b118c2bd7a3f3a624d2c8633b3dff84
Alexander Batalov <alex.batalov@gmail.com>
2024-01-01 16:08:26 +0300

    Review jakedesk.lua

--- src/scripts/jakedesk.lua
+++ src/scripts/jakedesk.lua
@@ -2,11 +2,11 @@ local fallout = require("fallout")
 
 local start
 
-local OnceOnly = 1
+local initialized = false
 
 function start()
-    if OnceOnly then
-        OnceOnly = 0
+    if not initialized then
+        initialized = true
         fallout.set_external_var("Jake_Desk_Ptr", fallout.self_obj())
     end
 end
