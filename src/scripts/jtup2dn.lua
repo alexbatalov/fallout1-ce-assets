c46f47f202831029dac00e6474155c16d9a385e4
Alexander Batalov <alex.batalov@gmail.com>
2024-01-01 13:42:42 +0300

    Review jtup2dn

--- src/scripts/jtup2dn.lua
+++ src/scripts/jtup2dn.lua
@@ -9,17 +9,19 @@ function start()
     if not initialized then
         fallout.set_external_var("ladder_down", fallout.self_obj())
         initialized = true
-    else
-        if fallout.script_action() == 6 then
-            use_p_proc()
-        end
+    end
+
+    local script_action = fallout.script_action()
+    if script_action == 6 then
+        use_p_proc()
     end
 end
 
 function use_p_proc()
-    fallout.move_to(fallout.source_obj(), 12498, 1)
-    if fallout.combat_is_initialized() and (fallout.source_obj() ~= fallout.dude_obj()) then
-        fallout.critter_stop_attacking(fallout.source_obj())
+    local source_obj = fallout.source_obj()
+    fallout.move_to(source_obj, 12498, 1)
+    if fallout.combat_is_initialized() and source_obj ~= fallout.dude_obj() then
+        fallout.critter_stop_attacking(source_obj)
     end
 end
 
