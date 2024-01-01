4548f7a02910a5f547daf4c69a8eef2107ceb348
Alexander Batalov <alex.batalov@gmail.com>
2024-01-01 17:07:20 +0300

    Review izo.lua

--- src/scripts/izo.lua
+++ src/scripts/izo.lua
@@ -9,62 +9,55 @@ local pickup_p_proc
 local talk_p_proc
 
 local initialized = false
-local hostile = 0
-local line103flag = 0
+local hostile = false
+local line103flag = false
 
 function start()
     if not initialized then
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 13)
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 13)
         if fallout.global_var(104) == 2 then
-            fallout.kill_critter(fallout.self_obj(), 62)
+            fallout.kill_critter(self_obj, 62)
         end
         initialized = true
-    else
-        if fallout.script_action() == 12 then
-            critter_p_proc()
-        else
-            if fallout.script_action() == 3 then
-                description_p_proc()
-            else
-                if fallout.script_action() == 18 then
-                    destroy_p_proc()
-                else
-                    if fallout.script_action() == 11 then
-                        talk_p_proc()
-                    end
-                end
-            end
-        end
+    end
+
+    local script_action = fallout.script_action()
+    if script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 3 then
+        description_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
     end
 end
 
 function critter_p_proc()
     if hostile then
-        hostile = 0
+        hostile = false
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     else
-        if fallout.external_var("Gizmo_is_angry") then
+        if fallout.external_var("Gizmo_is_angry") ~= 0 then
             fallout.set_external_var("Gizmo_is_angry", 0)
-            hostile = 1
-        else
-            if fallout.external_var("show_to_door") then
-                if fallout.tile_distance_objs(fallout.dude_obj(), fallout.external_var("Gizmo_ptr")) > 8 then
-                    if fallout.tile_num(fallout.self_obj()) ~= 16520 then
-                        fallout.animate_move_obj_to_tile(fallout.self_obj(), 16520, 0)
-                    else
-                        fallout.anim(fallout.self_obj(), 1000, 2)
-                        line103flag = 0
-                        fallout.set_external_var("show_to_door", 0)
-                    end
+            hostile = true
+        elseif fallout.external_var("show_to_door") ~= 0 then
+            local self_obj = fallout.self_obj()
+            if fallout.tile_distance_objs(fallout.dude_obj(), fallout.external_var("Gizmo_ptr")) > 8 then
+                if fallout.tile_num(self_obj) ~= 16520 then
+                    fallout.animate_move_obj_to_tile(self_obj, 16520, 0)
                 else
-                    if fallout.tile_num(fallout.self_obj()) ~= 18720 then
-                        fallout.animate_move_obj_to_tile(fallout.self_obj(), 18720, 0)
-                    else
-                        if not(line103flag) then
-                            line103flag = 1
-                            fallout.display_msg(fallout.message_str(622, 103))
-                        end
-                    end
+                    fallout.anim(self_obj, 1000, 2)
+                    line103flag = false
+                    fallout.set_external_var("show_to_door", 0)
+                end
+            else
+                if fallout.tile_num(self_obj) ~= 18720 then
+                    fallout.animate_move_obj_to_tile(self_obj, 18720, 0)
+                elseif not line103flag then
+                    line103flag = true
+                    fallout.display_msg(fallout.message_str(622, 103))
                 end
             end
         end
@@ -76,22 +69,23 @@ function destroy_p_proc()
 end
 
 function description_p_proc()
-    if fallout.is_success(fallout.do_check(fallout.dude_obj(), 1, fallout.has_trait(0, fallout.dude_obj(), 0))) then
+    local dude_obj = fallout.dude_obj()
+    if fallout.is_success(fallout.do_check(dude_obj, 1, fallout.has_trait(0, dude_obj, 0))) then
         fallout.display_msg(fallout.message_str(622, 100))
     end
 end
 
 function pickup_p_proc()
-    hostile = 1
+    hostile = true
 end
 
 function talk_p_proc()
-    local v0 = 0
     fallout.display_msg(fallout.message_str(622, 101))
-    if fallout.external_var("payment") > 0 then
+    local amount = fallout.external_var("payment")
+    if amount > 0 then
         fallout.use_obj(fallout.dude_obj())
-        v0 = fallout.create_object_sid(41, 0, 0, -1)
-        fallout.add_mult_objs_to_inven(fallout.dude_obj(), v0, fallout.external_var("payment"))
+        local item_obj = fallout.create_object_sid(41, 0, 0, -1)
+        fallout.add_mult_objs_to_inven(fallout.dude_obj(), item_obj, amount)
         fallout.display_msg(fallout.message_str(622, 102))
         fallout.set_external_var("payment", 0)
     end
