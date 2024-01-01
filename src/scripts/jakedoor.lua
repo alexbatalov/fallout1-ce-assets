51c160ae932b7e629779fac6a2512ef5936e654a
Alexander Batalov <alex.batalov@gmail.com>
2024-01-01 16:08:00 +0300

    Review jakedoor.lua

--- src/scripts/jakedoor.lua
+++ src/scripts/jakedoor.lua
@@ -8,32 +8,26 @@ local map_update_p_proc
 local damage_p_proc
 local use_skill_on_p_proc
 
-local OnceOnly = 1
+local initialized = false
 
 function start()
-    if OnceOnly then
-        OnceOnly = 0
+    if not initialized then
         fallout.set_external_var("jake_door_ptr", fallout.self_obj())
         map_update_p_proc()
+        initialized = true
     end
-    if (fallout.script_action() == 21) or (fallout.script_action() == 3) then
+
+    local script_action = fallout.script_action()
+    if script_action == 21 or script_action == 3 then
         look_at_p_proc()
-    else
-        if fallout.script_action() == 7 then
-            use_obj_on_p_proc()
-        else
-            if fallout.script_action() == 8 then
-                use_skill_on_p_proc()
-            else
-                if fallout.script_action() == 23 then
-                    map_update_p_proc()
-                else
-                    if fallout.script_action() == 14 then
-                        damage_p_proc()
-                    end
-                end
-            end
-        end
+    elseif script_action == 7 then
+        use_obj_on_p_proc()
+    elseif script_action == 8 then
+        use_skill_on_p_proc()
+    elseif script_action == 23 then
+        map_update_p_proc()
+    elseif script_action == 14 then
+        damage_p_proc()
     end
 end
 
@@ -45,19 +39,17 @@ function look_at_p_proc()
 end
 
 function use_obj_on_p_proc()
-    local v0 = 0
-    local v1 = 0
-    v0 = fallout.obj_being_used_with()
-    v1 = fallout.roll_vs_skill(fallout.dude_obj(), 9, -20)
-    if fallout.obj_pid(v0) == 84 then
+    local item_obj = fallout.obj_being_used_with()
+    local roll = fallout.roll_vs_skill(fallout.dude_obj(), 9, -20)
+    if fallout.obj_pid(item_obj) == 84 then
         fallout.script_overrides()
-        if fallout.is_success(v1) then
+        if fallout.is_success(roll) then
             fallout.set_local_var(0, 1)
             fallout.display_msg(fallout.message_str(589, 204))
         else
-            if fallout.is_critical(v1) then
-                fallout.rm_obj_from_inven(fallout.dude_obj(), v0)
-                fallout.destroy_object(v0)
+            if fallout.is_critical(roll) then
+                fallout.rm_obj_from_inven(fallout.dude_obj(), item_obj)
+                fallout.destroy_object(item_obj)
                 fallout.display_msg(fallout.message_str(589, 207))
             else
                 fallout.display_msg(fallout.message_str(589, 205))
@@ -67,21 +59,22 @@ function use_obj_on_p_proc()
 end
 
 function map_update_p_proc()
+    local self_obj = fallout.self_obj()
     if not time.is_night() then
         fallout.set_local_var(0, 1)
     end
     if fallout.local_var(0) == 0 then
-        fallout.obj_lock(fallout.self_obj())
+        fallout.obj_lock(self_obj)
     else
-        fallout.obj_unlock(fallout.self_obj())
+        fallout.obj_unlock(self_obj)
     end
     fallout.set_map_var(7, 0)
     if fallout.map_var(6) == 0 then
-        if fallout.obj_is_open(fallout.self_obj()) == 1 then
+        if fallout.obj_is_open(self_obj) == 1 then
             fallout.set_map_var(7, 1)
         end
     end
-    if fallout.obj_is_locked(fallout.self_obj()) == 1 then
+    if fallout.obj_is_locked(self_obj) == 1 then
         fallout.set_local_var(0, 1)
     else
         fallout.set_local_var(0, 0)
@@ -89,18 +82,17 @@ function map_update_p_proc()
 end
 
 function damage_p_proc()
-    fallout.set_obj_visibility(fallout.self_obj(), 1)
+    fallout.set_obj_visibility(fallout.self_obj(), true)
     fallout.set_local_var(1, 1)
     fallout.set_map_var(6, 1)
 end
 
 function use_skill_on_p_proc()
-    local v0 = 0
     if fallout.local_var(0) == 0 then
         if fallout.action_being_used() == 9 then
             fallout.script_overrides()
-            v0 = fallout.roll_vs_skill(fallout.dude_obj(), 9, -40)
-            if fallout.is_success(v0) then
+            local roll = fallout.roll_vs_skill(fallout.dude_obj(), 9, -40)
+            if fallout.is_success(roll) then
                 fallout.set_local_var(0, 1)
                 fallout.display_msg(fallout.message_str(589, 204))
             else
