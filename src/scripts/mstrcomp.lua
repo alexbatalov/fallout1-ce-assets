7128e36ae40108f0db137b83a5c598ad0c9e1ef1
Alexander Batalov <alex.batalov@gmail.com>
2024-01-02 10:25:46 +0300

    Review mstrcomp.lua

--- src/scripts/mstrcomp.lua
+++ src/scripts/mstrcomp.lua
@@ -1,25 +1,38 @@
 local fallout = require("fallout")
 
 local start
+local use_p_proc
+local use_skill_on_p_proc
 local look_at_p_proc
-local skills
-
-local use_skill = 0
-local temp = 0
 
 function start()
-    if fallout.script_action() == 21 then
+    local script_action = fallout.script_action()
+    if script_action == 21 then
         look_at_p_proc()
-    else
-        if fallout.script_action() == 8 then
-            use_skill = fallout.action_being_used()
-            fallout.script_overrides()
-            skills()
-        else
-            if fallout.script_action() == 6 then
-                fallout.display_msg(fallout.message_str(725, 201))
-            end
+    elseif script_action == 8 then
+        use_skill_on_p_proc()
+    elseif script_action == 6 then
+        use_p_proc()
+    end
+end
+
+function use_p_proc()
+    fallout.display_msg(fallout.message_str(725, 201))
+end
+
+function use_skill_on_p_proc()
+    local skill = fallout.action_being_used()
+    fallout.script_overrides()
+    if skill == 12 then
+        fallout.display_msg(fallout.message_str(725, 203))
+        if fallout.global_var(78) ~= 1 then
+            fallout.set_global_var(78, 1)
+            local xp = 1250
+            fallout.display_msg(fallout.message_str(725, 204) .. xp .. fallout.message_str(725, 205))
+            fallout.give_exp_points(xp)
         end
+    elseif skill == 13 then
+        fallout.display_msg(fallout.message_str(725, 202))
     end
 end
 
@@ -28,23 +41,9 @@ function look_at_p_proc()
     fallout.display_msg(fallout.message_str(725, 200))
 end
 
-function skills()
-    if use_skill == 12 then
-        fallout.display_msg(fallout.message_str(725, 203))
-        if fallout.global_var(78) ~= 1 then
-            fallout.set_global_var(78, 1)
-            temp = 1250
-            fallout.display_msg(fallout.message_str(725, 204) .. temp .. fallout.message_str(725, 205))
-            fallout.give_exp_points(temp)
-        end
-    else
-        if use_skill == 13 then
-            fallout.display_msg(fallout.message_str(725, 202))
-        end
-    end
-end
-
 local exports = {}
 exports.start = start
+exports.use_p_proc = use_p_proc
+exports.use_skill_on_p_proc = use_skill_on_p_proc
 exports.look_at_p_proc = look_at_p_proc
 return exports
