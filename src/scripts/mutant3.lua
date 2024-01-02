143e688adc60ea257e9345fef04ef55cc6cbef26
Alexander Batalov <alex.batalov@gmail.com>
2024-01-02 10:17:31 +0300

    Review mutant3.lua

--- src/scripts/mutant3.lua
+++ src/scripts/mutant3.lua
@@ -2,73 +2,81 @@ local fallout = require("fallout")
 local reputation = require("lib.reputation")
 
 local start
-local timeforwhat
-local supercbt
-local superx
+local pickup_p_proc
+local critter_p_proc
+local destroy_p_proc
+local look_at_p_proc
+local timed_event_p_proc
 
-local Hostile = 0
+local hostile = false
 local initialized = false
-local couple_of_minutes = 0
 
 function start()
     if not initialized then
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 34)
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 34)
         initialized = true
+    end
+
+    local script_action = fallout.script_action()
+    if script_action == 22 then
+        timed_event_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
+    elseif script_action == 21 then
+        look_at_p_proc()
+    end
+end
+
+function pickup_p_proc()
+    hostile = true
+end
+
+function critter_p_proc()
+    if hostile then
+        hostile = false
+        fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     else
-        if fallout.script_action() == 22 then
-            timeforwhat()
+        if fallout.global_var(13) == 0 then
+            fallout.set_obj_visibility(fallout.self_obj(), true)
         else
-            if fallout.script_action() == 4 then
-                Hostile = 1
-            else
-                if fallout.script_action() == 12 then
-                    if Hostile then
-                        Hostile = 0
-                        fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
-                    else
-                        if fallout.global_var(13) == 0 then
-                            fallout.set_obj_visibility(fallout.self_obj(), 1)
-                        else
-                            if fallout.local_var(0) ~= 1 then
-                                couple_of_minutes = 150 + fallout.random(1, 60)
-                                fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(couple_of_minutes), 0)
-                            end
-                        end
-                    end
-                else
-                    if fallout.script_action() == 18 then
-                        fallout.set_global_var(35, fallout.global_var(35) + 1)
-                        reputation.inc_evil_critter()
-                    else
-                        if fallout.script_action() == 21 then
-                            fallout.display_msg(fallout.message_str(232, 100))
-                        end
-                    end
-                end
+            if fallout.local_var(0) ~= 1 then
+                local couple_of_minutes = 150 + fallout.random(1, 60)
+                fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(couple_of_minutes), 0)
             end
         end
     end
 end
 
-function timeforwhat()
+function destroy_p_proc()
+    fallout.set_global_var(35, fallout.global_var(35) + 1)
+    reputation.inc_evil_critter()
+end
+
+function look_at_p_proc()
+    fallout.display_msg(fallout.message_str(232, 100))
+end
+
+function timed_event_p_proc()
+    local self_obj = fallout.self_obj()
     if fallout.local_var(0) == 1 then
-        fallout.animate_move_obj_to_tile(fallout.self_obj(), 16929, 0)
+        fallout.animate_move_obj_to_tile(self_obj, 16929, 0)
         fallout.set_local_var(0, 0)
     else
-        fallout.animate_move_obj_to_tile(fallout.self_obj(), 10917, 0)
+        fallout.animate_move_obj_to_tile(self_obj, 10917, 0)
         fallout.set_local_var(0, 1)
-        fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(40), 0)
+        fallout.add_timer_event(self_obj, fallout.game_ticks(40), 0)
     end
-end
-
-function supercbt()
-    Hostile = 1
-end
-
-function superx()
-    supercbt()
 end
 
 local exports = {}
 exports.start = start
+exports.pickup_p_proc = pickup_p_proc
+exports.critter_p_proc = critter_p_proc
+exports.destroy_p_proc = destroy_p_proc
+exports.look_at_p_proc = look_at_p_proc
 return exports
