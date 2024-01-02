532d92dd78b5260e03f900105f6a5efcc67bf6fb
Alexander Batalov <alex.batalov@gmail.com>
2024-01-02 09:57:47 +0300

    Review mvnuke.lua

--- src/scripts/mvnuke.lua
+++ src/scripts/mvnuke.lua
@@ -1,63 +1,42 @@
 local fallout = require("fallout")
 
---
--- Some unreferenced imported varables found.
--- Because of it it is impossible to specify
--- the real names of global variables.
---
-
-local g0 = 0
-
 local start
-local skills
-local reset
-
--- ?import? variable KEY_USED
--- ?import? variable SKILL_USED
--- ?import? variable MASTER_HAS_ACTIVATED
--- ?import? variable BOMB_ARMED
--- ?import? variable use_skill
+local use_skill_on_p_proc
+local look_at_p_proc
 
 function start()
-    if fallout.script_action() == 21 then
-        fallout.script_overrides()
-        fallout.display_msg(fallout.message_str(449, 100))
-    else
-        if fallout.script_action() == 8 then
-            fallout.set_map_var(5, 1)
-            g0 = fallout.action_being_used()
-            fallout.script_overrides()
-            skills()
-        end
+    local script_action = fallout.script_action()
+    if script_action == 21 then
+        look_at_p_proc()
+    elseif script_action == 8 then
+        use_skill_on_p_proc()
     end
 end
 
-function skills()
-    if g0 == 12 then
+function use_skill_on_p_proc()
+    fallout.set_map_var(5, 1)
+    local skill = fallout.action_being_used()
+    fallout.script_overrides()
+    if skill == 12 then
         fallout.display_msg(fallout.message_str(449, 108))
+    elseif skill == 13 then
+        fallout.display_msg(fallout.message_str(449, 109))
+    elseif skill == 9 then
+        fallout.display_msg(fallout.message_str(449, 110))
+    elseif skill == 11 then
+        fallout.display_msg(fallout.message_str(449, 111))
     else
-        if g0 == 13 then
-            fallout.display_msg(fallout.message_str(449, 109))
-        else
-            if g0 == 9 then
-                fallout.display_msg(fallout.message_str(449, 110))
-            else
-                if g0 == 11 then
-                    fallout.display_msg(fallout.message_str(449, 111))
-                else
-                    fallout.display_msg(fallout.message_str(449, 105))
-                end
-            end
-        end
+        fallout.display_msg(fallout.message_str(449, 105))
     end
 end
 
-function reset()
-    fallout.display_msg(fallout.message_str(449, 103))
-    fallout.set_global_var(55, 0)
-    fallout.set_global_var(18, 0)
+function look_at_p_proc()
+    fallout.script_overrides()
+    fallout.display_msg(fallout.message_str(449, 100))
 end
 
 local exports = {}
 exports.start = start
+exports.use_skill_on_p_proc = use_skill_on_p_proc
+exports.look_at_p_proc = look_at_p_proc
 return exports
