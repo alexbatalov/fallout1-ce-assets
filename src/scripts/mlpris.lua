85e4b06611fa183a441857a450e21b888c13e798
Alexander Batalov <alex.batalov@gmail.com>
2024-01-02 14:28:04 +0300

    Review mlpris.lua

--- src/scripts/mlpris.lua
+++ src/scripts/mlpris.lua
@@ -1,36 +1,26 @@
 local fallout = require("fallout")
 
 local start
+local use_p_proc
 local look_at_p_proc
 
-local initialized = false
-local spot1 = 27096
-local spot2 = 27104
-local spot3 = 26312
-local spot4 = 26320
-local field1 = 0
-local field2 = 0
-local field3 = 0
-local field4 = 0
-local swtch = 0
-
 function start()
-    if fallout.script_action() == 15 then
-    end
-    if fallout.script_action() == 6 then
-        fallout.display_msg(fallout.message_str(766, 187))
-        if fallout.local_var(0) ~= 0 then
-            fallout.set_local_var(0, 0)
-            fallout.set_map_var(7, 0)
-        else
-            fallout.set_local_var(0, 1)
-            fallout.set_map_var(7, 1)
-        end
-    end
-    if fallout.script_action() == 21 then
+    local script_action = fallout.script_action()
+    if script_action == 6 then
+        use_p_proc()
+    elseif script_action == 21 then
         look_at_p_proc()
     end
-    if fallout.script_action() == 16 then
+end
+
+function use_p_proc()
+    fallout.display_msg(fallout.message_str(766, 187))
+    if fallout.local_var(0) ~= 0 then
+        fallout.set_local_var(0, 0)
+        fallout.set_map_var(7, 0)
+    else
+        fallout.set_local_var(0, 1)
+        fallout.set_map_var(7, 1)
     end
 end
 
@@ -40,5 +30,6 @@ end
 
 local exports = {}
 exports.start = start
+exports.use_p_proc = use_p_proc
 exports.look_at_p_proc = look_at_p_proc
 return exports
