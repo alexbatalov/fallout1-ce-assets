544211c4b095eb0e72fc00afb23dd3d2fe1d78be
Alexander Batalov <alex.batalov@gmail.com>
2024-01-02 09:51:53 +0300

    Review mvsctshf.lua

--- src/scripts/mvsctshf.lua
+++ src/scripts/mvsctshf.lua
@@ -1,29 +1,28 @@
 local fallout = require("fallout")
 
 local start
-local use_stuff
+local use_skill_on_p_proc
+local look_at_p_proc
 
-local open = 0
-local unlocked = 0
+local open = false
+local unlocked = false
 
 function start()
-    if (fallout.script_action() == 21) or (fallout.script_action() == 3) then
-        fallout.script_overrides()
-        fallout.display_msg(fallout.message_str(428, 100))
-    else
-        if fallout.script_action() == 8 then
-            use_stuff()
-        end
+    local script_action = fallout.script_action()
+    if script_action == 21 or script_action == 3 then
+        look_at_p_proc()
+    elseif script_action == 8 then
+        use_skill_on_p_proc()
     end
 end
 
-function use_stuff()
+function use_skill_on_p_proc()
     fallout.script_overrides()
-    if not(unlocked) then
+    if not unlocked then
         fallout.display_msg(fallout.message_str(428, 101))
         if fallout.is_success(fallout.roll_vs_skill(fallout.dude_obj(), 11, 0)) or fallout.is_success(fallout.roll_vs_skill(fallout.dude_obj(), 9, 0)) then
             fallout.display_msg(fallout.message_str(429, 100))
-            unlocked = 1
+            unlocked = true
         end
     else
         fallout.display_msg(fallout.message_str(429, 101))
@@ -32,14 +31,21 @@ function use_stuff()
         end
         if open then
             fallout.animate_stand_reverse_obj(fallout.self_obj())
-            open = 0
+            open = false
         else
             fallout.animate_stand_obj(fallout.self_obj())
-            open = 1
+            open = true
         end
     end
 end
 
+function look_at_p_proc()
+    fallout.script_overrides()
+    fallout.display_msg(fallout.message_str(428, 100))
+end
+
 local exports = {}
 exports.start = start
+exports.use_skill_on_p_proc = use_skill_on_p_proc
+exports.look_at_p_proc = look_at_p_proc
 return exports
