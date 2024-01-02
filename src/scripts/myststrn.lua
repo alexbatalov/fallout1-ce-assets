0dee263c0edaa983e4d2e1a67833d7948335be1b
Alexander Batalov <alex.batalov@gmail.com>
2024-01-02 09:49:02 +0300

    Review myststrn.lua

--- src/scripts/myststrn.lua
+++ src/scripts/myststrn.lua
@@ -5,24 +5,23 @@ local destroy_p_proc
 local talk_p_proc
 
 local initialized = false
-local Item = 0
 
 function start()
-    if not initialized and fallout.metarule(14, 0) then
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 0)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 92)
-        Item = fallout.create_object_sid(fallout.global_var(288), 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.self_obj(), Item)
-        fallout.float_msg(fallout.self_obj(), fallout.message_str(766, 182 + fallout.random(0, 1)), 0)
+    if not initialized and fallout.metarule(14, 0) ~= 0 then
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 0)
+        fallout.critter_add_trait(self_obj, 1, 5, 92)
+        local item_obj = fallout.create_object_sid(fallout.global_var(288), 0, 0, -1)
+        fallout.add_obj_to_inven(self_obj, item_obj)
+        fallout.float_msg(self_obj, fallout.message_str(766, 182 + fallout.random(0, 1)), 0)
         initialized = true
-    else
-        if fallout.script_action() == 18 then
-            destroy_p_proc()
-        else
-            if fallout.script_action() == 11 then
-                talk_p_proc()
-            end
-        end
+    end
+
+    local script_action = fallout.script_action()
+    if script_action == 18 then
+        destroy_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
     end
 end
 
