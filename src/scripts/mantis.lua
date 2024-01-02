939587b0c2bb90eec4878ce410a6cdcc1d97c7b4
Alexander Batalov <alex.batalov@gmail.com>
2024-01-02 20:29:17 +0300

    Review mantis.lua

--- src/scripts/mantis.lua
+++ src/scripts/mantis.lua
@@ -9,54 +9,50 @@ local timed_event_p_proc
 local use_obj_on_p_proc
 local use_skill_on_p_proc
 
-local hostile = 0
+local hostile = false
 local initialized = false
-local scared = 0
+local scared = false
 
 function start()
     if not initialized then
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 55)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 8)
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 55)
+        fallout.critter_add_trait(self_obj, 1, 5, 8)
         if fallout.local_var(0) == 0 then
-            fallout.set_local_var(0, fallout.tile_num(fallout.self_obj()))
+            fallout.set_local_var(0, fallout.tile_num(self_obj))
         end
-        fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(fallout.random(1, 5)), 1)
+        fallout.add_timer_event(self_obj, fallout.game_ticks(fallout.random(1, 5)), 1)
         initialized = true
-    else
-        if fallout.script_action() == 12 then
-            critter_p_proc()
-        else
-            if fallout.script_action() == 18 then
-                destroy_p_proc()
-            else
-                if fallout.script_action() == 22 then
-                    timed_event_p_proc()
-                else
-                    if fallout.script_action() == 7 then
-                        use_obj_on_p_proc()
-                    else
-                        if fallout.script_action() == 8 then
-                            use_skill_on_p_proc()
-                        end
-                    end
-                end
-            end
-        end
+    end
+
+    local script_action = fallout.script_action()
+    if script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
+    elseif script_action == 22 then
+        timed_event_p_proc()
+    elseif script_action == 7 then
+        use_obj_on_p_proc()
+    elseif script_action == 8 then
+        use_skill_on_p_proc()
     end
 end
 
 function critter_p_proc()
     if hostile then
-        hostile = 0
-        scared = 1
+        hostile = false
+        scared = true
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     else
-        if fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) then
-            if fallout.tile_distance_objs(fallout.self_obj(), fallout.dude_obj()) < 6 then
+        local self_obj = fallout.self_obj()
+        local dude_obj = fallout.dude_obj()
+        if fallout.obj_can_see_obj(self_obj, dude_obj) then
+            if fallout.tile_distance_objs(self_obj, dude_obj) < 6 then
                 if scared then
                     behaviour.flee_dude(1)
                 else
-                    hostile = 1
+                    hostile = true
                 end
             end
         end
@@ -68,16 +64,23 @@ function destroy_p_proc()
 end
 
 function timed_event_p_proc()
-    fallout.animate_move_obj_to_tile(fallout.self_obj(), fallout.tile_num_in_direction(fallout.local_var(0), fallout.random(0, 5), fallout.random(1, 3)), 0)
-    fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(fallout.random(1, 5)), 0)
+    local self_obj = fallout.self_obj()
+    local rotation = fallout.random(0, 5)
+    local distance = fallout.random(1, 3)
+    fallout.animate_move_obj_to_tile(self_obj,
+        fallout.tile_num_in_direction(fallout.local_var(0), rotation, distance), 0)
+    fallout.add_timer_event(self_obj, fallout.game_ticks(fallout.random(1, 5)), 0)
 end
 
 function use_obj_on_p_proc()
-    if (fallout.obj_pid(fallout.obj_being_used_with()) == 124) or (fallout.obj_pid(fallout.obj_being_used_with()) == 106) then
+    local item_obj = fallout.obj_being_used_with()
+    local item_pid = fallout.obj_pid(item_obj)
+    if item_pid == 124 or item_pid == 106 then
         fallout.script_overrides()
-        fallout.rm_obj_from_inven(fallout.source_obj(), fallout.obj_being_used_with())
-        fallout.destroy_object(fallout.obj_being_used_with())
-        fallout.critter_dmg(fallout.self_obj(), fallout.get_critter_stat(fallout.self_obj(), 35) + 10, 6)
+        fallout.rm_obj_from_inven(fallout.source_obj(), item_obj)
+        fallout.destroy_object(item_obj)
+        local self_obj = fallout.self_obj()
+        fallout.critter_dmg(self_obj, fallout.get_critter_stat(self_obj, 35) + 10, 6)
     end
 end
 
