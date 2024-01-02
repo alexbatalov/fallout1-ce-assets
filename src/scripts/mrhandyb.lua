4de44e1911fb467eb1fd1dfee0d761374f790c62
Alexander Batalov <alex.batalov@gmail.com>
2024-01-02 10:44:13 +0300

    Review mrhandyb.lua

--- src/scripts/mrhandyb.lua
+++ src/scripts/mrhandyb.lua
@@ -4,76 +4,78 @@ local start
 local critter_p_proc
 local use_skill_on_p_proc
 
-local hostile = 0
+local hostile = false
 local initialized = false
 
 function start()
+    local self_obj = fallout.self_obj()
     if not initialized then
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 34)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 71)
+        fallout.critter_add_trait(self_obj, 1, 6, 34)
+        fallout.critter_add_trait(self_obj, 1, 5, 71)
         initialized = true
     end
     if fallout.global_var(273) == 2 then
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 85)
+        fallout.critter_add_trait(self_obj, 1, 6, 85)
     end
-    if fallout.script_action() == 12 then
+
+    local script_action = fallout.script_action()
+    if script_action == 12 then
         critter_p_proc()
-    else
-        if fallout.script_action() == 8 then
-            use_skill_on_p_proc()
-        end
+    elseif script_action == 8 then
+        use_skill_on_p_proc()
     end
 end
 
 function critter_p_proc()
+    local self_obj = fallout.self_obj()
+    local dude_obj = fallout.dude_obj()
     if hostile then
-        hostile = 0
-        fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
+        hostile = false
+        fallout.attack(dude_obj, 0, 1, 0, 0, 30000, 0, 0)
     end
     if fallout.global_var(275) == 0 then
-        if (fallout.critter_state(fallout.self_obj()) & 64) == 0 then
-            fallout.critter_injure(fallout.self_obj(), 64)
+        if fallout.critter_state(self_obj) & 64 == 0 then
+            fallout.critter_injure(self_obj, 64)
         end
     end
     if fallout.global_var(274) ~= 0 then
-        if (fallout.critter_state(fallout.self_obj()) & 4) == 0 then
-            fallout.critter_injure(fallout.self_obj(), 4)
+        if fallout.critter_state(self_obj) & 4 == 0 then
+            fallout.critter_injure(self_obj, 4)
         end
     end
     if fallout.global_var(274) == 2 then
-        if (fallout.critter_state(fallout.self_obj()) & 8) == 0 then
-            fallout.critter_injure(fallout.self_obj(), 8)
+        if fallout.critter_state(self_obj) & 8 == 0 then
+            fallout.critter_injure(self_obj, 8)
         end
     end
     if fallout.global_var(146) == 1 then
         if fallout.global_var(273) == 0 then
-            if fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) then
-                hostile = 1
+            if fallout.obj_can_see_obj(self_obj, dude_obj) then
+                hostile = true
             end
-        else
-            if fallout.global_var(273) == 1 then
-                if fallout.obj_can_see_obj(fallout.self_obj(), fallout.external_var("valid_target")) then
-                    fallout.attack(fallout.external_var("valid_target"), 0, 1, 0, 0, 30000, 0, 0)
-                end
+        elseif fallout.global_var(273) == 1 then
+            local valid_target_obj = fallout.external_var("valid_target")
+            if fallout.obj_can_see_obj(self_obj, valid_target_obj) then
+                fallout.attack(valid_target_obj, 0, 1, 0, 0, 30000, 0, 0)
+            end
+        elseif fallout.global_var(273) == 2 then
+            local valid_target_obj = fallout.external_var("valid_target")
+            if fallout.obj_can_see_obj(self_obj, valid_target_obj) then
+                fallout.attack(valid_target_obj, 0, 1, 0, 0, 30000, 0, 0)
+            end
+        elseif fallout.global_var(273) == 3 then
+            if fallout.obj_can_see_obj(self_obj, dude_obj) then
+                hostile = true
             else
-                if fallout.global_var(273) == 2 then
-                    if fallout.obj_can_see_obj(fallout.self_obj(), fallout.external_var("valid_target")) then
-                        fallout.attack(fallout.external_var("valid_target"), 0, 1, 0, 0, 30000, 0, 0)
-                    end
-                else
-                    if fallout.global_var(273) == 3 then
-                        if fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) then
-                            hostile = 1
-                        else
-                            if fallout.obj_can_see_obj(fallout.self_obj(), fallout.external_var("valid_target")) then
-                                fallout.attack(fallout.external_var("valid_target"), 0, 1, 0, 0, 30000, 0, 0)
-                            end
-                        end
-                    end
+                local valid_target_obj = fallout.external_var("valid_target")
+                if fallout.obj_can_see_obj(self_obj, valid_target_obj) then
+                    fallout.attack(valid_target_obj, 0, 1, 0, 0, 30000, 0, 0)
                 end
             end
         end
-        fallout.animate_move_obj_to_tile(fallout.self_obj(), fallout.tile_num_in_direction(fallout.tile_num(fallout.self_obj()), fallout.random(0, 5), fallout.random(1, 3)), 0)
+        fallout.animate_move_obj_to_tile(self_obj,
+            fallout.tile_num_in_direction(fallout.tile_num(self_obj), fallout.random(0, 5),
+                fallout.random(1, 3)), 0)
     end
 end
 
