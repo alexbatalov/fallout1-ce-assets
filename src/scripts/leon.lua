b6fb486a34b0c5652558e0e196791281abe7170b
Alexander Batalov <alex.batalov@gmail.com>
2024-01-02 22:51:57 +0300

    Review leon.lua

--- src/scripts/leon.lua
+++ src/scripts/leon.lua
@@ -3,7 +3,6 @@ local reaction = require("lib.reaction")
 local reputation = require("lib.reputation")
 
 local start
-local combat
 local critter_p_proc
 local pickup_p_proc
 local talk_p_proc
@@ -14,57 +13,44 @@ local Leon00
 local Leon01
 local Leon02
 
-local hostile = 0
+local hostile = false
 local initialized = false
-
-local exit_line = 0
 
 function start()
     if not initialized then
+        local self_obj = fallout.self_obj()
+        fallout.set_external_var("Leon_Ptr", self_obj)
+        fallout.critter_add_trait(self_obj, 1, 6, 73)
+        fallout.critter_add_trait(self_obj, 1, 5, 4)
         initialized = true
-        fallout.set_external_var("Leon_Ptr", fallout.self_obj())
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 73)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 4)
     end
-    if fallout.script_action() == 21 then
-        look_at_p_proc()
-    else
-        if fallout.script_action() == 4 then
-            pickup_p_proc()
-        else
-            if fallout.script_action() == 11 then
-                talk_p_proc()
-            else
-                if fallout.script_action() == 12 then
-                    critter_p_proc()
-                else
-                    if fallout.script_action() == 22 then
-                        timed_event_p_proc()
-                    else
-                        if fallout.script_action() == 18 then
-                            destroy_p_proc()
-                        end
-                    end
-                end
-            end
-        end
-    end
-end
 
-function combat()
-    hostile = 1
+    local script_action = fallout.script_action()
+    if script_action == 21 then
+        look_at_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 22 then
+        timed_event_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
+    end
 end
 
 function critter_p_proc()
     if hostile then
-        hostile = 0
+        hostile = false
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     end
 end
 
 function pickup_p_proc()
     if fallout.source_obj() == fallout.dude_obj() then
-        hostile = 1
+        hostile = true
     end
 end
 
@@ -94,10 +80,12 @@ function timed_event_p_proc()
 end
 
 function Leon00()
-    fallout.float_msg(fallout.external_var("Guido_Ptr"), fallout.message_str(576, 101), 2)
-    fallout.float_msg(fallout.self_obj(), fallout.message_str(596, 105), 3)
-    fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(3), 1)
-    fallout.add_timer_event(fallout.external_var("Guido_Ptr"), fallout.game_ticks(3), 1)
+    local guido_obj = fallout.external_var("Guido_Ptr")
+    local self_obj = fallout.self_obj()
+    fallout.float_msg(guido_obj, fallout.message_str(576, 101), 2)
+    fallout.float_msg(self_obj, fallout.message_str(596, 105), 3)
+    fallout.add_timer_event(self_obj, fallout.game_ticks(3), 1)
+    fallout.add_timer_event(guido_obj, fallout.game_ticks(3), 1)
 end
 
 function Leon01()
