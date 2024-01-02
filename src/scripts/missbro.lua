638f2967d9367e4e117796a3c86ac1fc4b338747
Alexander Batalov <alex.batalov@gmail.com>
2024-01-02 14:32:12 +0300

    Review missbro.lua

--- src/scripts/missbro.lua
+++ src/scripts/missbro.lua
@@ -24,77 +24,68 @@ local Brother11
 local Brother12
 local BrotherEnd
 
-local hostile = 0
+local hostile = false
 local initialized = false
-local visible = 1
-
-local exit_line = 0
 
 function start()
     if not initialized then
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 44)
+        fallout.critter_add_trait(self_obj, 1, 5, 62)
         initialized = true
-        if fallout.local_var(5) == 1 then
-            visible = 0
-        end
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 44)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 62)
     end
-    if fallout.script_action() == 21 then
+
+    local script_action = fallout.script_action()
+    if script_action == 21 then
         look_at_p_proc()
-    else
-        if fallout.script_action() == 4 then
-            pickup_p_proc()
-        else
-            if fallout.script_action() == 11 then
-                talk_p_proc()
-            else
-                if fallout.script_action() == 12 then
-                    critter_p_proc()
-                else
-                    if fallout.script_action() == 18 then
-                        destroy_p_proc()
-                    end
-                end
-            end
-        end
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
     end
 end
 
 function combat()
-    hostile = 1
+    hostile = true
 end
 
 function critter_p_proc()
     if hostile then
-        hostile = 0
+        hostile = false
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     end
     if fallout.local_var(5) == 1 then
-        if fallout.tile_num(fallout.self_obj()) ~= 17145 then
-            fallout.animate_move_obj_to_tile(fallout.self_obj(), 17145, 0)
+        local self_obj = fallout.self_obj()
+        if fallout.tile_num(self_obj) ~= 17145 then
+            fallout.animate_move_obj_to_tile(self_obj, 17145, 0)
         end
     end
 end
 
 function pickup_p_proc()
     if fallout.source_obj() == fallout.dude_obj() then
-        hostile = 1
+        hostile = true
     end
 end
 
 function talk_p_proc()
+    local self_obj = fallout.self_obj()
     reaction.get_reaction()
     if fallout.local_var(4) == 1 then
-        fallout.float_msg(fallout.self_obj(), fallout.message_str(651, fallout.random(134, 136)), 2)
+        fallout.float_msg(self_obj, fallout.message_str(651, fallout.random(134, 136)), 2)
     else
         fallout.set_local_var(4, 1)
-        fallout.start_gdialog(651, fallout.self_obj(), 4, -1, -1)
+        fallout.start_gdialog(651, self_obj, 4, -1, -1)
         fallout.gsay_start()
         Brother00()
         fallout.gsay_end()
         fallout.end_dialogue()
     end
-    fallout.animate_move_obj_to_tile(fallout.self_obj(), 17145, 0)
+    fallout.animate_move_obj_to_tile(self_obj, 17145, 0)
     fallout.set_local_var(5, 1)
 end
 
