e46116091988f5f9a27390f07d8546465d1c8590
Alexander Batalov <alex.batalov@gmail.com>
2024-01-02 13:12:16 +0300

    Review morpcomp.lua

--- src/scripts/morpcomp.lua
+++ src/scripts/morpcomp.lua
@@ -4,28 +4,22 @@ local start
 local use_p_proc
 local use_skill_on_p_proc
 
-local Hacked = 0
+local hacked = false
 
 function start()
-    if fallout.script_action() == 21 then
+    local script_action = fallout.script_action()
+    if script_action == 21 then
         fallout.script_overrides()
         fallout.display_msg(fallout.message_str(110, 100))
-    else
-        if fallout.script_action() == 6 then
-            use_p_proc()
-        else
-            if fallout.script_action() == 8 then
-                if fallout.action_being_used() == 12 then
-                    use_skill_on_p_proc()
-                end
-            end
-        end
+    elseif script_action == 6 then
+        use_p_proc()
+    elseif script_action == 8 then
+        use_skill_on_p_proc()
     end
 end
 
 function use_p_proc()
-    local v0 = 0
-    if Hacked == 0 then
+    if not hacked then
         fallout.script_overrides()
         fallout.display_msg(fallout.message_str(910, 105))
     else
@@ -35,15 +29,16 @@ function use_p_proc()
 end
 
 function use_skill_on_p_proc()
-    local v0 = 0
-    v0 = fallout.roll_vs_skill(fallout.dude_obj(), 12, 0)
-    if fallout.is_success(v0) then
-        fallout.script_overrides()
-        fallout.display_msg(fallout.message_str(910, 107))
-        Hacked = 1
-    else
-        fallout.script_overrides()
-        fallout.display_msg(fallout.message_str(910, 106))
+    if fallout.action_being_used() == 12 then
+        local roll = fallout.roll_vs_skill(fallout.dude_obj(), 12, 0)
+        if fallout.is_success(roll) then
+            fallout.script_overrides()
+            fallout.display_msg(fallout.message_str(910, 107))
+            hacked = true
+        else
+            fallout.script_overrides()
+            fallout.display_msg(fallout.message_str(910, 106))
+        end
     end
 end
 
