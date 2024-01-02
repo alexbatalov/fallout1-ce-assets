5809eeacca06bdf8d95dc6006ba6d6a2712e708e
Alexander Batalov <alex.batalov@gmail.com>
2024-01-02 20:41:29 +0300

    Review madbrot.lua

--- src/scripts/madbrot.lua
+++ src/scripts/madbrot.lua
@@ -2,71 +2,53 @@ local fallout = require("fallout")
 local reputation = require("lib.reputation")
 
 local start
-local do_dialogue
-local combat
-local do_action
-local timetomove
+local pickup_p_proc
+local critter_p_proc
+local destroy_p_proc
+local timed_event_p_proc
 
-local Hostile = 0
-local init = 0
+local hostile = false
+local initialized = false
 local maxleash = 10
-local noevent = 0
-local loopcount = 0
-local rndx = 0
-local rndy = 0
-local rndz = 0
-local dist = 0
-local new_tile = 0
 
 function start()
-    if not(init) then
+    if not initialized then
+        local self_obj = fallout.self_obj()
         if fallout.local_var(3) == 0 then
-            fallout.set_local_var(3, fallout.tile_num(fallout.self_obj()))
+            fallout.set_local_var(3, fallout.tile_num(self_obj))
             fallout.set_local_var(1, 1)
         end
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 32)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 64)
-        init = 1
-    else
-        if fallout.script_action() == 22 then
-            timetomove()
-        else
-            if fallout.script_action() == 11 then
-                do_dialogue()
-            else
-                if fallout.script_action() == 4 then
-                    Hostile = 1
-                else
-                    if fallout.script_action() == 12 then
-                        do_action()
-                    else
-                        if fallout.script_action() == 18 then
-                            reputation.inc_good_critter()
-                        end
-                    end
-                end
-            end
-        end
+        fallout.critter_add_trait(self_obj, 1, 6, 32)
+        fallout.critter_add_trait(self_obj, 1, 5, 64)
+        initialized = true
+    end
+
+    local script_action = fallout.script_action()
+    if script_action == 22 then
+        timed_event_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+
     end
 end
 
-function do_dialogue()
+function pickup_p_proc()
+    hostile = true
 end
 
-function combat()
-    Hostile = 1
-end
-
-function do_action()
+function critter_p_proc()
     if fallout.global_var(250) ~= 0 then
-        Hostile = 1
+        hostile = true
     end
     if fallout.tile_distance_objs(fallout.self_obj(), fallout.dude_obj()) > 12 then
-        Hostile = 0
+        hostile = false
     end
-    if Hostile then
+    if hostile then
         fallout.set_global_var(250, 1)
-        Hostile = 0
+        hostile = false
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     else
         if fallout.local_var(1) ~= 0 then
@@ -74,22 +56,30 @@ function do_action()
                 if fallout.random(1, 10) < 2 then
                     fallout.use_obj(fallout.external_var("table_ptr"))
                 else
-                    timetomove()
+                    timed_event_p_proc()
                 end
             end
         end
     end
 end
 
-function timetomove()
-    noevent = 0
-    fallout.set_local_var(4, fallout.tile_num(fallout.self_obj()))
-    new_tile = fallout.tile_num_in_direction(fallout.local_var(4), fallout.random(0, 5), 2)
+function destroy_p_proc()
+    reputation.inc_good_critter()
+end
+
+function timed_event_p_proc()
+    local self_obj = fallout.self_obj()
+    fallout.set_local_var(4, fallout.tile_num(self_obj))
+    local new_tile = fallout.tile_num_in_direction(fallout.local_var(4), fallout.random(0, 5), 2)
     if fallout.tile_distance(fallout.local_var(3), new_tile) < maxleash then
-        fallout.animate_move_obj_to_tile(fallout.self_obj(), new_tile, 0)
+        fallout.animate_move_obj_to_tile(self_obj, new_tile, 0)
     end
 end
 
 local exports = {}
 exports.start = start
+exports.pickup_p_proc = pickup_p_proc
+exports.critter_p_proc = critter_p_proc
+exports.destroy_p_proc = destroy_p_proc
+exports.timed_event_p_proc = timed_event_p_proc
 return exports
