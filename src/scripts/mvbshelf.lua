a46729d5b89c6d99498fa4a7f6d128a9b8ce2064
Alexander Batalov <alex.batalov@gmail.com>
2024-01-02 10:00:30 +0300

    Review mvbshelf.lua

--- src/scripts/mvbshelf.lua
+++ src/scripts/mvbshelf.lua
@@ -1,19 +1,30 @@
 local fallout = require("fallout")
 
 local start
+local use_skill_on_p_proc
+local look_at_p_proc
 
 function start()
-    if (fallout.script_action() == 21) or (fallout.script_action() == 3) then
-        fallout.script_overrides()
-        fallout.display_msg(fallout.message_str(428, 100))
-    else
-        if fallout.script_action() == 8 then
-            fallout.script_overrides()
-            fallout.display_msg(fallout.message_str(428, 101))
-        end
+    local script_action = fallout.script_action()
+    if script_action == 21 or script_action == 3 then
+        look_at_p_proc()
+    elseif script_action == 8 then
+        use_skill_on_p_proc()
     end
+end
+
+function look_at_p_proc()
+    fallout.script_overrides()
+    fallout.display_msg(fallout.message_str(428, 100))
+end
+
+function use_skill_on_p_proc()
+    fallout.script_overrides()
+    fallout.display_msg(fallout.message_str(428, 101))
 end
 
 local exports = {}
 exports.start = start
+exports.use_skill_on_p_proc = use_skill_on_p_proc
+exports.look_at_p_proc = look_at_p_proc
 return exports
