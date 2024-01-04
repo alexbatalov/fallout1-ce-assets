8ab1be91908e30e3442c44820413d6dcd09dda19
Alexander Batalov <alex.batalov@gmail.com>
2024-01-04 09:24:19 +0300

    Review barfly.lua

--- src/scripts/barfly.lua
+++ src/scripts/barfly.lua
@@ -15,62 +15,61 @@ local wake_time = 0
 local sleep_time = 0
 local home_tile = 0
 local sleep_tile = 0
-local hostile = 0
+local hostile = false
 
 function start()
-    if fallout.script_action() == 12 then
+    local script_action = fallout.script_action()
+    if script_action == 12 then
         critter_p_proc()
-    else
-        if fallout.script_action() == 15 then
-            map_enter_p_proc()
-        else
-            if fallout.script_action() == 4 then
-                pickup_p_proc()
-            else
-                if fallout.script_action() == 11 then
-                    talk_p_proc()
-                end
-            end
-        end
+    elseif script_action == 15 then
+        map_enter_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
     end
 end
 
 function critter_p_proc()
+    local self_obj = fallout.self_obj()
+    local dude_obj = fallout.dude_obj()
     if hostile then
-        hostile = 0
-        fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
+        hostile = false
+        fallout.attack(dude_obj, 0, 1, 0, 0, 30000, 0, 0)
     end
     behaviour.sleeping(0, night_person, wake_time, sleep_time, home_tile, sleep_tile)
-    if (fallout.game_time_hour() > (wake_time + 20)) or (fallout.game_time_hour() < sleep_time) then
-        if fallout.tile_num(fallout.self_obj()) ~= home_tile then
-            fallout.animate_move_obj_to_tile(fallout.self_obj(), home_tile, 0)
+    local game_time_hour = fallout.game_time_hour()
+    if game_time_hour > wake_time + 20 or game_time_hour < sleep_time then
+        if fallout.tile_num(self_obj) ~= home_tile then
+            fallout.animate_move_obj_to_tile(self_obj, home_tile, 0)
         end
     end
     if fallout.global_var(247) == 1 then
-        if fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) then
-            fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
+        if fallout.obj_can_see_obj(self_obj, dude_obj) then
+            fallout.attack(dude_obj, 0, 1, 0, 0, 30000, 0, 0)
         end
     end
 end
 
 function map_enter_p_proc()
+    local self_obj = fallout.self_obj()
     if fallout.global_var(15) == 1 then
-        fallout.destroy_object(fallout.self_obj())
+        fallout.destroy_object(self_obj)
     end
     night_person = true
     wake_time = fallout.random(1610, 1640)
     sleep_time = fallout.random(310, 340)
     sleep_tile = 7000
     home_tile = 20675
-    fallout.critter_add_trait(fallout.self_obj(), 1, 6, 26)
-    fallout.critter_add_trait(fallout.self_obj(), 1, 5, 5)
+    fallout.critter_add_trait(self_obj, 1, 6, 26)
+    fallout.critter_add_trait(self_obj, 1, 5, 5)
     if fallout.map_var(2) == 1 then
-        fallout.destroy_object(fallout.self_obj())
+        fallout.destroy_object(self_obj)
     end
 end
 
 function pickup_p_proc()
-    hostile = 1
+    hostile = true
 end
 
 function talk_p_proc()
