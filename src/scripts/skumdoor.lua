a4f8abffb801f13b73f089ec7973f7e4e1749858
Alexander Batalov <alex.batalov@gmail.com>
2024-01-04 17:52:12 +0300

    Review skumdoor.lua

--- src/scripts/skumdoor.lua
+++ src/scripts/skumdoor.lua
@@ -21,59 +21,54 @@ local use_skill_on_p_proc
 -- ?import? variable messing_with_SkumDoor
 
 function start()
-    if fallout.script_action() == 14 then
+    local script_action = fallout.script_action()
+    if script_action == 14 then
         damage_p_proc()
-    else
-        if fallout.script_action() == 18 then
-            destroy_p_proc()
-        else
-            if fallout.script_action() == 15 then
-                map_enter_p_proc()
-            else
-                if fallout.script_action() == 6 then
-                    use_p_proc()
-                else
-                    if fallout.script_action() == 7 then
-                        use_obj_on_p_proc()
-                    else
-                        if fallout.script_action() == 8 then
-                            use_skill_on_p_proc()
-                        end
-                    end
-                end
-            end
-        end
+    elseif script_action == 18 then
+        destroy_p_proc()
+    elseif script_action == 15 then
+        map_enter_p_proc()
+    elseif script_action == 6 then
+        use_p_proc()
+    elseif script_action == 7 then
+        use_obj_on_p_proc()
+    elseif script_action == 8 then
+        use_skill_on_p_proc()
     end
 end
 
 function damage_p_proc()
-    fallout.set_external_var("SkumDoor_ptr", 0)
+    fallout.set_external_var("SkumDoor_ptr", nil)
     fallout.destroy_object(fallout.self_obj())
 end
 
 function destroy_p_proc()
-    fallout.set_external_var("SkumDoor_ptr", 0)
+    fallout.set_external_var("SkumDoor_ptr", nil)
 end
 
 function map_enter_p_proc()
-    fallout.set_external_var("SkumDoor_ptr", fallout.self_obj())
-    if (fallout.game_time_hour() > 410) and (fallout.game_time_hour() < 1400) then
-        fallout.obj_close(fallout.self_obj())
-        fallout.obj_lock(fallout.self_obj())
+    local self_obj = fallout.self_obj()
+    local game_time_hour = fallout.game_time_hour()
+    fallout.set_external_var("SkumDoor_ptr", self_obj)
+    if game_time_hour > 410 and game_time_hour < 1400 then
+        fallout.obj_close(self_obj)
+        fallout.obj_lock(self_obj)
     else
-        fallout.obj_unlock(fallout.self_obj())
+        fallout.obj_unlock(self_obj)
     end
     fallout.set_local_var(0, 0)
 end
 
 function map_update_p_proc()
-    if (fallout.game_time_hour() > 410) and (fallout.game_time_hour() < 1400) then
+    local game_time_hour = fallout.game_time_hour()
+    local self_obj = fallout.self_obj()
+    if game_time_hour > 410 and game_time_hour < 1400 then
         if fallout.local_var(0) == 0 then
-            fallout.obj_close(fallout.self_obj())
-            fallout.obj_lock(fallout.self_obj())
+            fallout.obj_close(self_obj)
+            fallout.obj_lock(self_obj)
         end
     else
-        fallout.obj_unlock(fallout.self_obj())
+        fallout.obj_unlock(self_obj)
     end
 end
 
@@ -84,17 +79,19 @@ function use_p_proc()
             fallout.display_msg(fallout.message_str(853, 100))
         end
     else
-        if fallout.external_var("Neal_ptr") ~= 0 then
-            if fallout.source_obj() == fallout.external_var("Neal_ptr") then
+        local neal_obj = fallout.external_var("Neal_ptr")
+        if neal_obj ~= nil then
+            if fallout.source_obj() == neal_obj then
                 fallout.set_local_var(0, 0)
+                local self_obj = fallout.self_obj()
                 if fallout.external_var("Neal_closing_door") == 0 then
                     fallout.script_overrides()
-                    fallout.obj_open(fallout.self_obj())
-                    fallout.obj_unlock(fallout.self_obj())
+                    fallout.obj_open(self_obj)
+                    fallout.obj_unlock(self_obj)
                 else
                     fallout.script_overrides()
-                    fallout.obj_close(fallout.self_obj())
-                    fallout.obj_lock(fallout.self_obj())
+                    fallout.obj_close(self_obj)
+                    fallout.obj_lock(self_obj)
                 end
             end
         end
@@ -103,16 +100,17 @@ end
 
 function use_obj_on_p_proc()
     if fallout.obj_pid(fallout.obj_being_used_with()) == 84 then
-        if fallout.obj_is_locked(fallout.self_obj()) then
-            g0 = fallout.roll_vs_skill(fallout.source_obj(), 9, 0)
-            if fallout.is_success(g0) then
+        local self_obj = fallout.self_obj()
+        if fallout.obj_is_locked(self_obj) then
+            local roll = fallout.roll_vs_skill(fallout.source_obj(), 9, 0)
+            if fallout.is_success(roll) then
                 fallout.display_msg(fallout.message_str(853, 101))
-                fallout.obj_unlock(fallout.self_obj())
+                fallout.obj_unlock(self_obj)
                 fallout.set_local_var(0, 1)
             else
-                if fallout.is_critical(g0) then
+                if fallout.is_critical(roll) then
                     fallout.display_msg(fallout.message_str(853, 103))
-                    fallout.jam_lock(fallout.self_obj())
+                    fallout.jam_lock(self_obj)
                 else
                     fallout.display_msg(fallout.message_str(853, 102))
                 end
@@ -124,16 +122,17 @@ function use_obj_on_p_proc()
 end
 
 function use_skill_on_p_proc()
-    if fallout.action_being_used() == 9 then
+    local skill = fallout.action_being_used()
+    if skill == 9 then
         fallout.script_overrides()
         if fallout.obj_is_locked(fallout.self_obj()) then
-            g0 = fallout.roll_vs_skill(fallout.source_obj(), fallout.action_being_used(), -10)
-            if fallout.is_success(g0) then
+            local roll = fallout.roll_vs_skill(fallout.source_obj(), skill, -10)
+            if fallout.is_success(roll) then
                 fallout.display_msg(fallout.message_str(853, 101))
                 fallout.obj_unlock(fallout.self_obj())
                 fallout.set_local_var(0, 1)
             else
-                if fallout.is_critical(g0) then
+                if fallout.is_critical(roll) then
                     fallout.display_msg(fallout.message_str(853, 103))
                     fallout.jam_lock(fallout.self_obj())
                 else
