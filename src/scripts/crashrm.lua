74be35d55dfe2c679807130298bd748d12c13bcf
Alexander Batalov <alex.batalov@gmail.com>
2024-01-04 09:33:20 +0300

    Review crashrm.lua

--- src/scripts/crashrm.lua
+++ src/scripts/crashrm.lua
@@ -8,20 +8,17 @@ local CrashRm01
 local CrashRm02
 local CrashRmEnd
 
-local item = 0
-
 function start()
-    if fallout.script_action() == 2 then
+    local script_action = fallout.script_action()
+    if script_action == 2 then
         spatial_p_proc()
-    else
-        if fallout.script_action() == 11 then
-            talk_p_proc()
-        end
+    elseif script_action == 11 then
+        talk_p_proc()
     end
 end
 
 function spatial_p_proc()
-    if (fallout.source_obj() == fallout.dude_obj()) and (fallout.global_var(168) > time.game_time_in_days()) then
+    if fallout.source_obj() == fallout.dude_obj() and fallout.global_var(168) > time.game_time_in_days() then
         fallout.dialogue_system_enter()
     end
 end
@@ -54,17 +51,20 @@ function CrashRm02()
     end
     fallout.critter_heal(fallout.dude_obj(), fallout.random(10, 15))
     fallout.display_msg(fallout.message_str(807, 103))
-    if (fallout.map_var(3) == 0) and (fallout.map_var(0) ~= 2) then
-        fallout.set_external_var("JTRaider_ptr", fallout.create_object_sid(16777449, 17484, 0, 337))
-        fallout.move_to(fallout.external_var("Sinthia_ptr"), 17485, 0)
-        fallout.anim(fallout.external_var("JTRaider_ptr"), 1000, 3)
-        fallout.anim(fallout.external_var("Sinthia_ptr"), 1000, 3)
-        item = fallout.create_object_sid(8, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.external_var("JTRaider_ptr"), item)
-        fallout.wield_obj_critter(fallout.external_var("JTRaider_ptr"), item)
-        item = fallout.create_object_sid(1, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.external_var("JTRaider_ptr"), item)
-        fallout.wield_obj_critter(fallout.external_var("JTRaider_ptr"), item)
+    if fallout.map_var(3) == 0 and fallout.map_var(0) ~= 2 then
+        local jtraider_obj = fallout.create_object_sid(16777449, 17484, 0, 337)
+        local sinthia_obj = fallout.external_var("Sinthia_ptr")
+        local item_obj
+        fallout.set_external_var("JTRaider_ptr", jtraider_obj)
+        fallout.move_to(sinthia_obj, 17485, 0)
+        fallout.anim(jtraider_obj, 1000, 3)
+        fallout.anim(sinthia_obj, 1000, 3)
+        item_obj = fallout.create_object_sid(8, 0, 0, -1)
+        fallout.add_obj_to_inven(jtraider_obj, item_obj)
+        fallout.wield_obj_critter(jtraider_obj, item_obj)
+        item_obj = fallout.create_object_sid(1, 0, 0, -1)
+        fallout.add_obj_to_inven(jtraider_obj, item_obj)
+        fallout.wield_obj_critter(jtraider_obj, item_obj)
         fallout.set_map_var(3, 1)
     end
     fallout.gfade_in(600)
