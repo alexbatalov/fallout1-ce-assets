8bd309edf6dde09519f57fdeaa9c8bac7e397b6a
Alexander Batalov <alex.batalov@gmail.com>
2024-01-06 13:30:22 +0300

    Review ismarc.lua

--- src/scripts/ismarc.lua
+++ src/scripts/ismarc.lua
@@ -30,69 +30,60 @@ local wake_time = 0
 local sleep_time = 0
 local home_tile = 0
 local sleep_tile = 0
-local hostile = 0
-local initialized = false
-local singing = 0
+local hostile = false
+local singing = false
 local song_line_base = 0
 local song_line_offset = 0
-local line05flag = 0
-local line06flag = 0
-local line07flag = 0
-local line08flag = 0
-local line11flag = 0
-local line12flag = 0
-
-local exit_line = 0
+local line05flag = false
+local line06flag = false
+local line07flag = false
+local line08flag = false
+local line11flag = false
+local line12flag = false
 
 function start()
-    if fallout.script_action() == 12 then
+    local script_action = fallout.script_action()
+    if script_action == 12 then
         critter_p_proc()
-    else
-        if fallout.script_action() == 18 then
-            destroy_p_proc()
-        else
-            if fallout.script_action() == 15 then
-                map_enter_p_proc()
-            else
-                if fallout.script_action() == 4 then
-                    pickup_p_proc()
-                else
-                    if fallout.script_action() == 11 then
-                        talk_p_proc()
-                    else
-                        if fallout.script_action() == 22 then
-                            timed_event_p_proc()
-                        end
-                    end
-                end
-            end
-        end
+    elseif script_action == 18 then
+        destroy_p_proc()
+    elseif script_action == 15 then
+        map_enter_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 22 then
+        timed_event_p_proc()
     end
 end
 
 function critter_p_proc()
+    local self_obj = fallout.self_obj()
+    local dude_obj = fallout.dude_obj()
     if hostile then
-        hostile = 0
-        fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
+        hostile = false
+        fallout.attack(dude_obj, 0, 1, 0, 0, 30000, 0, 0)
     else
         if fallout.local_var(4) == 0 then
-            if fallout.tile_num(fallout.self_obj()) ~= home_tile then
-                fallout.animate_move_obj_to_tile(fallout.self_obj(), home_tile, 0)
+            if fallout.tile_num(self_obj) ~= home_tile then
+                fallout.animate_move_obj_to_tile(self_obj, home_tile, 0)
             else
-                if fallout.has_trait(1, fallout.self_obj(), 10) ~= 2 then
-                    fallout.anim(fallout.self_obj(), 1000, 2)
+                if fallout.has_trait(1, self_obj, 10) ~= 2 then
+                    fallout.anim(self_obj, 1000, 2)
                 end
             end
         end
-        if (fallout.game_time_hour() >= 1500) and (fallout.game_time_hour() <= 1900) and not(singing) then
-            fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(fallout.random(30, 60)), 1)
-            singing = 1
+        local game_time_hour = fallout.game_time_hour()
+        if game_time_hour >= 1500 and game_time_hour <= 1900 and not singing then
+            fallout.add_timer_event(self_obj, fallout.game_ticks(fallout.random(30, 60)), 1)
+            singing = true
         end
         behaviour.sleeping(4, night_person, wake_time, sleep_time, home_tile, sleep_tile)
     end
     if fallout.global_var(247) == 1 then
-        if fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) then
-            fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
+        if fallout.obj_can_see_obj(self_obj, dude_obj) then
+            fallout.attack(dude_obj, 0, 1, 0, 0, 30000, 0, 0)
         end
     end
 end
@@ -111,12 +102,13 @@ function destroy_p_proc()
 end
 
 function map_enter_p_proc()
+    local self_obj = fallout.self_obj()
     if fallout.global_var(15) == 1 then
-        fallout.destroy_object(fallout.self_obj())
+        fallout.destroy_object(self_obj)
     end
-    fallout.critter_add_trait(fallout.self_obj(), 1, 6, 26)
-    if fallout.obj_is_carrying_obj_pid(fallout.self_obj(), 41) == 0 then
-        fallout.item_caps_adjust(fallout.self_obj(), fallout.random(20, 50))
+    fallout.critter_add_trait(self_obj, 1, 6, 26)
+    if fallout.obj_is_carrying_obj_pid(self_obj, 41) == 0 then
+        fallout.item_caps_adjust(self_obj, fallout.random(20, 50))
     end
     sleep_tile = 7000
     home_tile = 19089
@@ -125,7 +117,7 @@ function map_enter_p_proc()
 end
 
 function pickup_p_proc()
-    hostile = 1
+    hostile = true
 end
 
 function talk_p_proc()
@@ -138,31 +130,31 @@ function talk_p_proc()
 end
 
 function timed_event_p_proc()
-    if fallout.fixed_param() == 1 then
+    local event = fallout.fixed_param()
+    if event == 1 then
         if song_line_base == 0 then
             song_line_base = (fallout.random(0, 2) * 4) + 127
         end
-        fallout.reg_anim_func(2, fallout.self_obj())
+        local self_obj = fallout.self_obj()
+        fallout.reg_anim_func(2, self_obj)
         fallout.reg_anim_func(1, 1)
-        fallout.reg_anim_animate(fallout.self_obj(), 13, -1)
-        fallout.reg_anim_animate(fallout.self_obj(), 15, 3)
-        fallout.reg_anim_animate(fallout.self_obj(), 14, 3)
-        fallout.reg_anim_animate(fallout.self_obj(), 13, 3)
-        fallout.reg_anim_animate(fallout.self_obj(), 0, 3)
+        fallout.reg_anim_animate(self_obj, 13, -1)
+        fallout.reg_anim_animate(self_obj, 15, 3)
+        fallout.reg_anim_animate(self_obj, 14, 3)
+        fallout.reg_anim_animate(self_obj, 13, 3)
+        fallout.reg_anim_animate(self_obj, 0, 3)
         fallout.reg_anim_func(3, 0)
-        fallout.float_msg(fallout.self_obj(), fallout.message_str(668, song_line_base + song_line_offset), 0)
+        fallout.float_msg(self_obj, fallout.message_str(668, song_line_base + song_line_offset), 0)
         song_line_offset = song_line_offset + 1
         if song_line_offset < 4 then
-            fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(5), 1)
+            fallout.add_timer_event(self_obj, fallout.game_ticks(5), 1)
         else
-            fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(fallout.random(1800, 3600)), 2)
+            fallout.add_timer_event(self_obj, fallout.game_ticks(fallout.random(1800, 3600)), 2)
         end
-    else
-        if fallout.fixed_param() == 2 then
-            singing = 0
-            song_line_offset = 0
-            song_line_base = 0
-        end
+    elseif event == 2 then
+        singing = false
+        song_line_offset = 0
+        song_line_base = 0
     end
 end
 
@@ -197,13 +189,13 @@ function Ismarc04()
 end
 
 function Ismarc05()
-    line05flag = 1
+    line05flag = true
     fallout.gsay_reply(668, 115)
     Ismarc09()
 end
 
 function Ismarc06()
-    line06flag = 1
+    line06flag = true
     if fallout.global_var(73) < 1 then
         fallout.set_global_var(73, 1)
     end
@@ -212,7 +204,7 @@ function Ismarc06()
 end
 
 function Ismarc07()
-    line07flag = 1
+    line07flag = true
     if fallout.global_var(74) < 1 then
         fallout.set_global_var(74, 1)
     end
@@ -221,7 +213,7 @@ function Ismarc07()
 end
 
 function Ismarc08()
-    line08flag = 1
+    line08flag = true
     if fallout.global_var(75) < 1 then
         fallout.set_global_var(75, 1)
     end
@@ -230,38 +222,38 @@ function Ismarc08()
 end
 
 function Ismarc09()
-    if not(line06flag) then
+    if not line06flag then
         fallout.giq_option(4, 668, 116, Ismarc06, 50)
     end
-    if not(line07flag) then
+    if not line07flag then
         fallout.giq_option(4, 668, 117, Ismarc07, 50)
     end
-    if not(line08flag) then
+    if not line08flag then
         fallout.giq_option(4, 668, 118, Ismarc08, 50)
     end
     fallout.giq_option(4, 668, 119, IsmarcEnd, 50)
 end
 
 function Ismarc10()
-    if not(line05flag) then
+    if not line05flag then
         fallout.giq_option(4, 668, 110, Ismarc05, 50)
     end
-    if not(line11flag) then
+    if not line11flag then
         fallout.giq_option(4, 668, 111, Ismarc11, 49)
     end
-    if not(line12flag) then
+    if not line12flag then
         fallout.giq_option(4, 668, 112, Ismarc12, 50)
     end
     fallout.giq_option(4, 668, 113, IsmarcEnd, 50)
 end
 
 function Ismarc11()
-    local v0 = 0
-    if fallout.item_caps_total(fallout.dude_obj()) >= 5 then
+    local dude_obj = fallout.dude_obj()
+    if fallout.item_caps_total(dude_obj) >= 5 then
         reaction.UpReact()
-        v0 = fallout.rm_mult_objs_from_inven(fallout.dude_obj(), fallout.obj_carrying_pid_obj(fallout.dude_obj(), 41), 5)
-        v0 = fallout.create_object_sid(41, 0, 0, -1)
-        fallout.add_mult_objs_to_inven(fallout.self_obj(), v0, 5)
+        fallout.rm_mult_objs_from_inven(dude_obj, fallout.obj_carrying_pid_obj(dude_obj, 41), 5)
+        local item_obj = fallout.create_object_sid(41, 0, 0, -1)
+        fallout.add_mult_objs_to_inven(fallout.self_obj(), item_obj, 5)
         fallout.gsay_reply(668, 123)
         fallout.giq_option(0, 634, 106, Ismarc10, 49)
     else
