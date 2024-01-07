67c5c4552b2ab0184ff21c81a8d060bd6f379241
Alexander Batalov <alex.batalov@gmail.com>
2024-01-07 20:01:03 +0300

    Review nwdn2up2.lua

--- src/scripts/nwdn2up2.lua
+++ src/scripts/nwdn2up2.lua
@@ -1,11 +1,16 @@
 local fallout = require("fallout")
 
 local start
+local use_p_proc
 
 function start()
     if fallout.script_action() == 6 then
-        fallout.move_to(fallout.dude_obj(), 22531, 1)
+        use_p_proc()
     end
+end
+
+function use_p_proc()
+    fallout.move_to(fallout.dude_obj(), 22531, 1)
     if fallout.source_obj() == fallout.dude_obj() then
         fallout.anim(fallout.source_obj(), 0, 0)
     end
@@ -13,4 +18,5 @@ end
 
 local exports = {}
 exports.start = start
+exports.use_p_proc = use_p_proc
 return exports
