350d57a7837af2c5106daa1888befda4a538801d
Alexander Batalov <alex.batalov@gmail.com>
2024-01-07 12:30:15 +0300

    Review garl.lua

--- src/scripts/garl.lua
+++ src/scripts/garl.lua
@@ -3,7 +3,11 @@ local reaction = require("lib.reaction")
 local reputation = require("lib.reputation")
 
 local start
-local do_dialogue
+local pickup_p_proc
+local talk_p_proc
+local destroy_p_proc
+local timed_event_p_proc
+local look_at_p_proc
 local first
 local notfirst
 local garl00
@@ -72,130 +76,124 @@ local honorcbt
 local honorarea
 local garlbarter
 local return_to_map
-local Critter_Action
+local critter_p_proc
 local damage_p_proc
 
-local HOSTILE = 0
+local hostile = false
 local initialized = false
-local temp = 0
-local tandi_pid_ptr = 0
-
-local exit_line = 0
+local tandi_pid_ptr = nil
 
 function start()
     if not initialized then
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 6)
+        fallout.critter_add_trait(self_obj, 1, 5, 22)
         initialized = true
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 6)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 22)
     end
-    if fallout.script_action() == 11 then
-        do_dialogue()
-    else
-        if fallout.script_action() == 22 then
-            if fallout.fixed_param() == 1 then
-                if fallout.global_var(611) ~= 1 then
-                    fallout.dialogue_system_enter()
-                end
-            else
-                if fallout.fixed_param() == 2 then
-                    freetandi()
-                else
-                    if fallout.fixed_param() == 3 then
-                        honorarea()
-                    else
-                        if fallout.fixed_param() == 4 then
-                            return_to_map()
-                            freetandi()
-                        end
-                    end
-                end
-            end
-        else
-            if fallout.script_action() == 21 then
-                fallout.script_overrides()
-                fallout.display_msg(fallout.message_str(137, 100))
-            else
-                if fallout.script_action() == 4 then
-                    HOSTILE = 1
-                else
-                    if fallout.script_action() == 12 then
-                        Critter_Action()
-                        if HOSTILE then
-                            HOSTILE = 0
-                            fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
-                        end
-                    else
-                        if fallout.script_action() == 14 then
-                            damage_p_proc()
-                        else
-                            if fallout.script_action() == 18 then
-                                if fallout.local_var(7) == 1 then
-                                    fallout.move_to(fallout.dude_obj(), fallout.local_var(6), 0)
-                                    fallout.move_obj_inven_to_obj(fallout.external_var("Garls_Inven_Ptr"), fallout.dude_obj())
-                                    fallout.set_global_var(26, 5)
-                                    fallout.set_map_var(2, 2)
-                                    fallout.obj_unlock(fallout.external_var("Cell_Door_Ptr"))
-                                end
-                                reputation.inc_evil_critter()
-                                if fallout.local_var(7) == 0 then
-                                    fallout.set_global_var(254, 1)
-                                    fallout.set_global_var(611, 0)
-                                end
-                                fallout.set_global_var(114, 1)
-                                fallout.set_global_var(115, fallout.global_var(115) - 1)
-                            end
-                        end
-                    end
-                end
-            end
-        end
+
+    local script_action = fallout.script_action()
+    if script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 22 then
+        timed_event_p_proc()
+    elseif script_action == 21 then
+        look_at_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 14 then
+        damage_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
     end
 end
 
-function do_dialogue()
-    if fallout.elevation(fallout.self_obj()) == 1 then
-        fallout.float_msg(fallout.self_obj(), fallout.message_str(137, 208), 2)
-        fallout.add_timer_event(fallout.self_obj(), 5, 4)
+function pickup_p_proc()
+    hostile = true
+end
+
+function talk_p_proc()
+    local self_obj = fallout.self_obj()
+    local dude_obj = fallout.dude_obj()
+    if fallout.elevation(self_obj) == 1 then
+        fallout.float_msg(self_obj, fallout.message_str(137, 208), 2)
+        fallout.add_timer_event(self_obj, 5, 4)
     else
         if fallout.external_var("killing_women") then
             garl39a()
         else
             fallout.set_local_var(3, 1)
             reaction.get_reaction()
-            fallout.start_gdialog(137, fallout.self_obj(), 4, -1, -1)
+            fallout.start_gdialog(137, self_obj, 4, -1, -1)
             fallout.gsay_start()
             if fallout.external_var("women_killed") > 1 then
                 garl11()
+            elseif fallout.global_var(116) ~= 0 then
+                garl01()
+            elseif fallout.local_var(4) < 1 then
+                first()
+                fallout.set_local_var(4, 1)
             else
-                if fallout.global_var(116) ~= 0 then
-                    garl01()
-                else
-                    if fallout.local_var(4) < 1 then
-                        first()
-                        fallout.set_local_var(4, 1)
-                    else
-                        notfirst()
-                    end
-                end
+                notfirst()
             end
             fallout.gsay_end()
             fallout.end_dialogue()
         end
     end
-    if fallout.obj_carrying_pid_obj(fallout.dude_obj(), 212) then
-        fallout.rm_obj_from_inven(fallout.dude_obj(), tandi_pid_ptr)
+    if fallout.obj_carrying_pid_obj(dude_obj, 212) then
+        fallout.rm_obj_from_inven(dude_obj, tandi_pid_ptr)
         fallout.destroy_object(tandi_pid_ptr)
         fallout.set_global_var(26, 5)
         fallout.set_map_var(2, 2)
         fallout.obj_unlock(fallout.external_var("Cell_Door_Ptr"))
-        fallout.add_timer_event(fallout.self_obj(), 1, 2)
+        fallout.add_timer_event(self_obj, 1, 2)
     else
-        if fallout.obj_carrying_pid_obj(fallout.self_obj(), 212) then
-            fallout.rm_obj_from_inven(fallout.self_obj(), tandi_pid_ptr)
+        if fallout.obj_carrying_pid_obj(self_obj, 212) then
+            fallout.rm_obj_from_inven(self_obj, tandi_pid_ptr)
             fallout.destroy_object(tandi_pid_ptr)
             garl20()
         end
     end
+end
+
+function destroy_p_proc()
+    if fallout.local_var(7) == 1 then
+        fallout.move_to(fallout.dude_obj(), fallout.local_var(6), 0)
+        fallout.move_obj_inven_to_obj(fallout.external_var("Garls_Inven_Ptr"),
+            fallout.dude_obj())
+        fallout.set_global_var(26, 5)
+        fallout.set_map_var(2, 2)
+        fallout.obj_unlock(fallout.external_var("Cell_Door_Ptr"))
+    end
+    reputation.inc_evil_critter()
+    if fallout.local_var(7) == 0 then
+        fallout.set_global_var(254, 1)
+        fallout.set_global_var(611, 0)
+    end
+    fallout.set_global_var(114, 1)
+    fallout.set_global_var(115, fallout.global_var(115) - 1)
+end
+
+function timed_event_p_proc()
+    local event = fallout.fixed_param()
+    if event == 1 then
+        if fallout.global_var(611) ~= 1 then
+            fallout.dialogue_system_enter()
+        end
+    elseif event == 2 then
+        freetandi()
+    elseif event == 3 then
+        honorarea()
+    elseif event == 4 then
+        return_to_map()
+        freetandi()
+    end
+end
+
+function look_at_p_proc()
+    fallout.script_overrides()
+    fallout.display_msg(fallout.message_str(137, 100))
 end
 
 function first()
@@ -306,7 +304,8 @@ function garl06()
 end
 
 function garl06a()
-    if fallout.is_success(fallout.do_check(fallout.dude_obj(), 3, 0)) or fallout.is_success(fallout.roll_vs_skill(fallout.dude_obj(), 14, 10)) then
+    local dude_obj = fallout.dude_obj()
+    if fallout.is_success(fallout.do_check(dude_obj, 3, 0)) or fallout.is_success(fallout.roll_vs_skill(dude_obj, 14, 10)) then
         garl08()
     else
         garl09()
@@ -360,7 +359,8 @@ function garl12a()
 end
 
 function garl12b()
-    if fallout.is_success(fallout.roll_vs_skill(fallout.dude_obj(), 14, 10)) or fallout.is_success(fallout.do_check(fallout.dude_obj(), 3, 0)) then
+    local dude_obj = fallout.dude_obj()
+    if fallout.is_success(fallout.roll_vs_skill(dude_obj, 14, 10)) or fallout.is_success(fallout.do_check(dude_obj, 3, 0)) then
         garl15()
     else
         garl10()
@@ -378,7 +378,8 @@ function garl15()
 end
 
 function garl15a()
-    if fallout.is_success(fallout.roll_vs_skill(fallout.dude_obj(), 14, 10)) or fallout.is_success(fallout.do_check(fallout.dude_obj(), 3, 0)) then
+    local dude_obj = fallout.dude_obj()
+    if fallout.is_success(fallout.roll_vs_skill(dude_obj, 14, 10)) or fallout.is_success(fallout.do_check(dude_obj, 3, 0)) then
         garl16()
     else
         garl17()
@@ -563,9 +564,9 @@ function garl40()
     fallout.set_external_var("women_killed", 0)
     reaction.TopReact()
     fallout.float_msg(fallout.self_obj(), fallout.message_str(137, 191), 2)
-    temp = 500
-    fallout.display_msg(fallout.message_str(238, 100) .. temp .. fallout.message_str(238, 102))
-    fallout.give_exp_points(temp)
+    local xp = 500
+    fallout.display_msg(fallout.message_str(238, 100) .. xp .. fallout.message_str(238, 102))
+    fallout.give_exp_points(xp)
     fallout.set_global_var(155, fallout.global_var(155) + -2)
     fallout.set_global_var(611, 1)
 end
@@ -603,42 +604,44 @@ function freetandi()
 end
 
 function garlcbt()
-    HOSTILE = 1
+    hostile = true
 end
 
 function honorcbt()
     fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(1), 3)
 end
 
+local PARTY_MEMBER_LOCATIONS = {
+    [16777292] = 0,
+    [16777518] = 1,
+    [16777426] = 2,
+    [16777338] = 4,
+    [16777279] = 5,
+}
+
 function honorarea()
-    local v0 = 0
-    fallout.set_local_var(6, fallout.tile_num(fallout.dude_obj()))
+    local self_obj = fallout.self_obj()
+    local dude_obj = fallout.dude_obj()
+    local garls_inven_obj = fallout.external_var("Garls_Inven_Ptr")
+    fallout.set_local_var(6, fallout.tile_num(dude_obj))
     fallout.set_local_var(7, 1)
-    fallout.move_to(fallout.dude_obj(), 20102, 1)
-    fallout.move_to(fallout.self_obj(), 20301, 1)
-    v0 = fallout.critter_inven_obj(fallout.dude_obj(), 0)
-    fallout.rm_obj_from_inven(fallout.dude_obj(), v0)
-    fallout.move_obj_inven_to_obj(fallout.self_obj(), fallout.external_var("Garls_Inven_Ptr"))
-    fallout.move_obj_inven_to_obj(fallout.dude_obj(), fallout.external_var("Garls_Inven_Ptr"))
-    fallout.add_obj_to_inven(fallout.dude_obj(), v0)
-    fallout.wield_obj_critter(fallout.dude_obj(), v0)
-    if fallout.party_member_obj(16777292) ~= 0 then
-        fallout.move_to(fallout.party_member_obj(16777292), fallout.tile_num_in_direction(fallout.tile_num(fallout.dude_obj()), 0, 10), 1)
+    fallout.move_to(dude_obj, 20102, 1)
+    fallout.move_to(self_obj, 20301, 1)
+    local item_obj = fallout.critter_inven_obj(dude_obj, 0)
+    fallout.rm_obj_from_inven(dude_obj, item_obj)
+    fallout.move_obj_inven_to_obj(self_obj, garls_inven_obj)
+    fallout.move_obj_inven_to_obj(dude_obj, garls_inven_obj)
+    fallout.add_obj_to_inven(dude_obj, item_obj)
+    fallout.wield_obj_critter(dude_obj, item_obj)
+    local dude_tile_num = fallout.tile_num(dude_obj)
+    for pid, rotation in ipairs(PARTY_MEMBER_LOCATIONS) do
+        local party_member_obj = fallout.party_member_obj(pid)
+        if party_member_obj ~= nil then
+            fallout.move_to(party_member_obj, fallout.tile_num_in_direction(dude_tile_num, rotation, 10), 1)
+        end
     end
-    if fallout.party_member_obj(16777518) ~= 0 then
-        fallout.move_to(fallout.party_member_obj(16777518), fallout.tile_num_in_direction(fallout.tile_num(fallout.dude_obj()), 1, 10), 1)
-    end
-    if fallout.party_member_obj(16777426) ~= 0 then
-        fallout.move_to(fallout.party_member_obj(16777426), fallout.tile_num_in_direction(fallout.tile_num(fallout.dude_obj()), 2, 10), 1)
-    end
-    if fallout.party_member_obj(16777338) ~= 0 then
-        fallout.move_to(fallout.party_member_obj(16777338), fallout.tile_num_in_direction(fallout.tile_num(fallout.dude_obj()), 4, 10), 1)
-    end
-    if fallout.party_member_obj(16777279) ~= 0 then
-        fallout.move_to(fallout.party_member_obj(16777279), fallout.tile_num_in_direction(fallout.tile_num(fallout.dude_obj()), 5, 10), 1)
-    end
-    fallout.critter_add_trait(fallout.self_obj(), 1, 6, 87)
-    fallout.critter_add_trait(fallout.self_obj(), 1, 5, 43)
+    fallout.critter_add_trait(self_obj, 1, 6, 87)
+    fallout.critter_add_trait(self_obj, 1, 5, 43)
     garlcbt()
 end
 
@@ -655,28 +658,33 @@ function return_to_map()
     fallout.obj_unlock(fallout.external_var("Cell_Door_Ptr"))
 end
 
-function Critter_Action()
+function critter_p_proc()
+    local self_obj = fallout.self_obj()
+    local dude_obj = fallout.dude_obj()
+    local self_can_see_dude = fallout.obj_can_see_obj(self_obj, dude_obj)
     if fallout.global_var(26) == 2 then
         fallout.set_global_var(254, 1)
+    elseif fallout.global_var(116) ~= 0 then
+        fallout.set_global_var(254, 0)
     else
-        if fallout.global_var(116) ~= 0 then
-            fallout.set_global_var(254, 0)
-        else
-            if fallout.global_var(213) ~= 0 then
+        if fallout.global_var(213) ~= 0 then
+            fallout.set_global_var(254, 1)
+        end
+        if self_can_see_dude then
+            if fallout.global_var(214) ~= 0 then
                 fallout.set_global_var(254, 1)
-            end
-            if fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) then
-                if fallout.global_var(214) ~= 0 then
-                    fallout.set_global_var(254, 1)
-                end
             end
         end
     end
-    if fallout.global_var(254) and fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) then
-        HOSTILE = 1
+    if fallout.global_var(254) ~= 0 and self_can_see_dude then
+        hostile = true
     end
-    if fallout.tile_distance_objs(fallout.self_obj(), fallout.dude_obj()) > 12 then
-        HOSTILE = 0
+    if fallout.tile_distance_objs(self_obj, dude_obj) > 12 then
+        hostile = false
+    end
+    if hostile then
+        hostile = false
+        fallout.attack(dude_obj, 0, 1, 0, 0, 30000, 0, 0)
     end
 end
 
@@ -688,5 +696,11 @@ end
 
 local exports = {}
 exports.start = start
+exports.pickup_p_proc = pickup_p_proc
+exports.talk_p_proc = talk_p_proc
+exports.critter_p_proc = critter_p_proc
 exports.damage_p_proc = damage_p_proc
+exports.destroy_p_proc = destroy_p_proc
+exports.timed_event_p_proc = timed_event_p_proc
+exports.look_at_p_proc = look_at_p_proc
 return exports
