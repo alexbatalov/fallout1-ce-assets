4705e7233dd291af9ecb3e56934e6cef56977a1d
Alexander Batalov <alex.batalov@gmail.com>
2024-01-07 20:58:21 +0300

    Review lookout.lua

--- src/scripts/lookout.lua
+++ src/scripts/lookout.lua
@@ -4,48 +4,34 @@ local reputation = require("lib.reputation")
 local start
 local spies
 local guilt
+local pickup_p_proc
 local talk_p_proc
+local critter_p_proc
+local destroy_p_proc
+local look_at_p_proc
 
-local Hostile = 0
+local hostile = false
 local initialized = false
 
 function start()
     if not initialized then
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 31)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 41)
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 31)
+        fallout.critter_add_trait(self_obj, 1, 5, 41)
         initialized = true
     end
-    if fallout.script_action() == 11 then
+
+    local script_action = fallout.script_action()
+    if script_action == 11 then
         talk_p_proc()
-    else
-        if (fallout.script_action() == 21) or (fallout.script_action() == 3) then
-            fallout.script_overrides()
-            fallout.display_msg(fallout.message_str(109, 100))
-        else
-            if fallout.script_action() == 4 then
-                Hostile = 1
-            else
-                if fallout.script_action() == 12 then
-                    if Hostile then
-                        Hostile = 0
-                        fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
-                    else
-                        if fallout.local_var(0) == 0 then
-                            if (fallout.global_var(30) == 1) and (fallout.global_var(31) ~= 2) then
-                                if fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) and (fallout.tile_distance_objs(fallout.self_obj(), fallout.dude_obj()) < 5) then
-                                    fallout.set_local_var(0, 1)
-                                    guilt()
-                                end
-                            end
-                        end
-                    end
-                else
-                    if fallout.script_action() == 18 then
-                        reputation.inc_good_critter()
-                    end
-                end
-            end
-        end
+    elseif script_action == 21 or script_action == 3 then
+        look_at_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
     end
 end
 
@@ -54,22 +40,57 @@ function spies()
 end
 
 function guilt()
-    if fallout.random(0, 1) then
+    if fallout.random(0, 1) ~= 0 then
         fallout.float_msg(fallout.self_obj(), fallout.message_str(109, 200), 0)
     else
         fallout.float_msg(fallout.self_obj(), fallout.message_str(109, 201), 0)
     end
 end
 
+function pickup_p_proc()
+    hostile = true
+end
+
 function talk_p_proc()
-    if (fallout.global_var(30) == 1) and (fallout.global_var(31) ~= 2) then
+    if fallout.global_var(30) == 1 and fallout.global_var(31) ~= 2 then
         guilt()
     else
         spies()
     end
 end
 
+function critter_p_proc()
+    if hostile then
+        hostile = false
+        fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
+    else
+        if fallout.local_var(0) == 0 then
+            if fallout.global_var(30) == 1 and fallout.global_var(31) ~= 2 then
+                local self_obj = fallout.self_obj()
+                local dude_obj = fallout.dude_obj()
+                if fallout.obj_can_see_obj(self_obj, dude_obj) and fallout.tile_distance_objs(self_obj, dude_obj) < 5 then
+                    fallout.set_local_var(0, 1)
+                    guilt()
+                end
+            end
+        end
+    end
+end
+
+function destroy_p_proc()
+    reputation.inc_good_critter()
+end
+
+function look_at_p_proc()
+    fallout.script_overrides()
+    fallout.display_msg(fallout.message_str(109, 100))
+end
+
 local exports = {}
 exports.start = start
+exports.pickup_p_proc = pickup_p_proc
 exports.talk_p_proc = talk_p_proc
+exports.critter_p_proc = critter_p_proc
+exports.destroy_p_proc = destroy_p_proc
+exports.look_at_p_proc = look_at_p_proc
 return exports
