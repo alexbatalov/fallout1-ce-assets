dbe66cb269017af17f82df592974e0c19811af42
Alexander Batalov <alex.batalov@gmail.com>
2024-01-07 19:49:36 +0300

    Refactor stuff.lua

--- src/scripts/stuff.lua
+++ src/scripts/stuff.lua
@@ -1,21 +1,46 @@
 local fallout = require("fallout")
 
 local start
+local pickup_p_proc
+local use_p_proc
+local use_obj_on_p_proc
+local use_skill_on_p_proc
+local timed_event_p_proc
 local anger
 
 function start()
     local script_action = fallout.script_action()
     if script_action == 22 then
-        fallout.set_map_var(5, 0)
+        timed_event_p_proc()
     elseif script_action == 6 then
-        anger()
+        use_p_proc()
     elseif script_action == 8 then
-        anger()
+        use_skill_on_p_proc()
     elseif script_action == 7 then
-        anger()
+        use_obj_on_p_proc()
     elseif script_action == 4 then
-        anger()
+        pickup_p_proc()
     end
+end
+
+function pickup_p_proc()
+    anger()
+end
+
+function use_p_proc()
+    anger()
+end
+
+function use_obj_on_p_proc()
+    anger()
+end
+
+function use_skill_on_p_proc()
+    anger()
+end
+
+function timed_event_p_proc()
+    fallout.set_map_var(5, 0)
 end
 
 function anger()
@@ -25,4 +50,9 @@ end
 
 local exports = {}
 exports.start = start
+exports.pickup_p_proc = pickup_p_proc
+exports.use_p_proc = use_p_proc
+exports.use_obj_on_p_proc = use_obj_on_p_proc
+exports.use_skill_on_p_proc = use_skill_on_p_proc
+exports.timed_event_p_proc = timed_event_p_proc
 return exports
