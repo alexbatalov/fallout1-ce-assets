5c36fc457b7321d9e90f15bed1c40812b1bcfa03
Alexander Batalov <alex.batalov@gmail.com>
2024-01-07 12:42:00 +0300

    Review gwen.lua

--- src/scripts/gwen.lua
+++ src/scripts/gwen.lua
@@ -3,7 +3,10 @@ local reaction = require("lib.reaction")
 local reputation = require("lib.reputation")
 
 local start
+local pickup_p_proc
 local do_dialogue
+local destroy_p_proc
+local look_at_p_proc
 local gwen00
 local gwen01
 local gwen02
@@ -17,51 +20,39 @@ local gwen10
 local gwenend
 local talk_p_proc
 local combat
-local Critter_Action
+local critter_p_proc
 local damage_p_proc
 
-local hostile = 0
+local hostile = false
 local initialized = false
-
-local exit_line = 0
 
 function start()
     if not initialized then
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 6)
+        fallout.critter_add_trait(self_obj, 1, 5, 20)
         initialized = true
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 6)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 20)
     end
-    if fallout.script_action() == 21 then
-        fallout.display_msg(fallout.message_str(141, 100))
-        fallout.script_overrides()
-    else
-        if fallout.script_action() == 11 then
-            do_dialogue()
-        else
-            if fallout.script_action() == 4 then
-                hostile = 1
-            else
-                if fallout.script_action() == 12 then
-                    Critter_Action()
-                    if hostile then
-                        hostile = 0
-                        fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
-                    end
-                else
-                    if fallout.script_action() == 14 then
-                        damage_p_proc()
-                    else
-                        if fallout.script_action() == 18 then
-                            reputation.inc_evil_critter()
-                            fallout.set_global_var(254, 1)
-                            fallout.set_global_var(611, 0)
-                            fallout.set_global_var(115, fallout.global_var(115) - 1)
-                        end
-                    end
-                end
-            end
-        end
+
+    local script_action = fallout.script_action()
+    if script_action == 21 then
+        look_at_p_proc()
+    elseif script_action == 11 then
+        -- FIXME: Conflicts with `talk_p_proc`.
+        do_dialogue()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 14 then
+        damage_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
     end
+end
+
+function pickup_p_proc()
+    hostile = true
 end
 
 function do_dialogue()
@@ -79,9 +70,23 @@ function do_dialogue()
     fallout.end_dialogue()
 end
 
+function destroy_p_proc()
+    reputation.inc_evil_critter()
+    fallout.set_global_var(254, 1)
+    fallout.set_global_var(611, 0)
+    fallout.set_global_var(115, fallout.global_var(115) - 1)
+end
+
+function look_at_p_proc()
+    fallout.display_msg(fallout.message_str(141, 100))
+    fallout.script_overrides()
+end
+
 function gwen00()
     fallout.gsay_reply(141, 101)
-    fallout.giq_option(3, 141, fallout.message_str(141, 102) .. fallout.proto_data(fallout.obj_pid(fallout.dude_obj()), 1) .. fallout.message_str(141, 103), gwen01, 50)
+    fallout.giq_option(3, 141,
+        fallout.message_str(141, 102) ..
+        fallout.proto_data(fallout.obj_pid(fallout.dude_obj()), 1) .. fallout.message_str(141, 103), gwen01, 50)
     fallout.giq_option(7, 141, 104, gwen02, 50)
     fallout.giq_option(-3, 141, 105, gwen03, 50)
     if fallout.get_critter_stat(fallout.dude_obj(), 34) == 0 then
@@ -151,44 +156,50 @@ function combat()
     if fallout.global_var(116) == 1 then
         fallout.critter_add_trait(fallout.self_obj(), 1, 6, 87)
     end
-    hostile = 1
+    hostile = true
 end
 
-function Critter_Action()
-    local v0 = 0
+function critter_p_proc()
+    local self_obj = fallout.self_obj()
+    local dude_obj = fallout.dude_obj()
+    local distance_self_to_dude = fallout.tile_distance_objs(self_obj, dude_obj)
+    local self_can_see_dude = fallout.obj_can_see_obj(self_obj, dude_obj)
     if fallout.global_var(26) == 2 then
         fallout.set_global_var(254, 1)
+    elseif fallout.global_var(116) ~= 0 then
+        fallout.set_global_var(254, 0)
+        if distance_self_to_dude < 3 then
+            local self_tile_num = fallout.tile_num(self_obj)
+            local dest = fallout.tile_num_in_direction(self_tile_num, fallout.random(0, 5), 3)
+            if fallout.tile_distance(self_tile_num, dest) > 2 then
+                if fallout.random(0, 9) == 0 then
+                    fallout.float_msg(self_obj, fallout.message_str(136, fallout.random(102, 106)), 8)
+                end
+                fallout.animate_move_obj_to_tile(self_obj, dest, 0)
+            end
+        end
     else
-        if fallout.global_var(116) ~= 0 then
-            fallout.set_global_var(254, 0)
-            if fallout.tile_distance_objs(fallout.self_obj(), fallout.dude_obj()) < 3 then
-                v0 = fallout.tile_num_in_direction(fallout.tile_num(fallout.self_obj()), fallout.random(0, 5), 3)
-                if fallout.tile_distance(fallout.tile_num(fallout.self_obj()), v0) > 2 then
-                    if fallout.random(0, 9) == 0 then
-                        fallout.float_msg(fallout.self_obj(), fallout.message_str(136, fallout.random(102, 106)), 8)
-                    end
-                    fallout.animate_move_obj_to_tile(fallout.self_obj(), v0, 0)
-                end
-            end
-        else
-            if fallout.global_var(213) ~= 0 then
-                fallout.set_global_var(254, 1)
-            end
-            if fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) then
-                if fallout.global_var(214) ~= 0 then
-                    fallout.set_global_var(254, 1)
-                end
-            end
-            if fallout.map_var(2) == 1 then
+        if fallout.global_var(213) ~= 0 then
+            fallout.set_global_var(254, 1)
+        end
+        if self_can_see_dude then
+            if fallout.global_var(214) ~= 0 then
                 fallout.set_global_var(254, 1)
             end
         end
+        if fallout.map_var(2) == 1 then
+            fallout.set_global_var(254, 1)
+        end
     end
-    if fallout.global_var(254) and fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) then
-        hostile = 1
+    if fallout.global_var(254) ~= 0 and self_can_see_dude then
+        hostile = true
     end
-    if fallout.tile_distance_objs(fallout.self_obj(), fallout.dude_obj()) > 12 then
-        hostile = 0
+    if distance_self_to_dude > 12 then
+        hostile = false
+    end
+    if hostile then
+        hostile = false
+        fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     end
 end
 
@@ -200,6 +211,10 @@ end
 
 local exports = {}
 exports.start = start
+exports.pickup_p_proc = pickup_p_proc
 exports.talk_p_proc = talk_p_proc
+exports.critter_p_proc = critter_p_proc
 exports.damage_p_proc = damage_p_proc
+exports.destroy_p_proc = destroy_p_proc
+exports.look_at_p_proc = look_at_p_proc
 return exports
