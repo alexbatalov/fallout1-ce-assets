d24b44693cccb73a5280682eeb426919e6884488
Alexander Batalov <alex.batalov@gmail.com>
2024-01-07 20:47:03 +0300

    Review larry.lua

--- src/scripts/larry.lua
+++ src/scripts/larry.lua
@@ -3,59 +3,69 @@ local reputation = require("lib.reputation")
 local time = require("lib.time")
 
 local start
-local do_dialogue
+local pickup_p_proc
+local talk_p_proc
+local destroy_p_proc
+local look_at_p_proc
 local mutant00
 local mutant01
 local mutant02
 local combat
-local Critter_Action
+local critter_p_proc
 
-local Hostile = 0
+local hostile = false
 local initialized = false
-local noevent = 0
-local loopcount = 0
 
 function start()
     if not initialized then
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 34)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 79)
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 34)
+        fallout.critter_add_trait(self_obj, 1, 5, 79)
         fallout.set_local_var(1, 0)
         initialized = true
     end
-    if (fallout.script_action() == 11) and (fallout.global_var(35) < 1) then
+
+    local script_action = fallout.script_action()
+    if (script_action == 11) then
+        talk_p_proc()
+    elseif script_action == 21 then
+        look_at_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
+    end
+end
+
+function pickup_p_proc()
+    hostile = true
+end
+
+function talk_p_proc()
+    if fallout.global_var(35) < 1 then
         if fallout.global_var(306) == 0 then
-            do_dialogue()
+            mutant00()
         else
             fallout.display_msg(fallout.message_str(80, 108))
-        end
-    else
-        if fallout.script_action() == 21 then
-            fallout.script_overrides()
-            fallout.display_msg(fallout.message_str(80, 101))
-        else
-            if fallout.script_action() == 4 then
-                Hostile = 1
-            else
-                if fallout.script_action() == 12 then
-                    Critter_Action()
-                else
-                    if fallout.script_action() == 18 then
-                        fallout.set_global_var(35, fallout.global_var(35) + 1)
-                        if fallout.global_var(35) > fallout.global_var(551) then
-                            fallout.set_global_var(155, fallout.global_var(155) + 3)
-                            fallout.set_global_var(29, 2)
-                            fallout.set_global_var(225, time.game_time_in_days())
-                        end
-                        reputation.inc_evil_critter()
-                    end
-                end
-            end
         end
     end
 end
 
-function do_dialogue()
-    mutant00()
+function destroy_p_proc()
+    fallout.set_global_var(35, fallout.global_var(35) + 1)
+    if fallout.global_var(35) > fallout.global_var(551) then
+        fallout.set_global_var(155, fallout.global_var(155) + 3)
+        fallout.set_global_var(29, 2)
+        fallout.set_global_var(225, time.game_time_in_days())
+    end
+    reputation.inc_evil_critter()
+end
+
+function look_at_p_proc()
+    fallout.script_overrides()
+    fallout.display_msg(fallout.message_str(80, 101))
 end
 
 function mutant00()
@@ -73,35 +83,37 @@ function mutant02()
 end
 
 function combat()
-    Hostile = 1
+    hostile = true
 end
 
-function Critter_Action()
-    if (fallout.global_var(35) > 0) and fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) then
-        Hostile = 1
+function critter_p_proc()
+    local self_obj = fallout.self_obj()
+    local dude_obj = fallout.dude_obj()
+    if fallout.global_var(35) > 0 and fallout.obj_can_see_obj(self_obj, dude_obj) then
+        hostile = true
     end
-    if Hostile > 0 then
-        Hostile = 0
-        fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
+    if hostile then
+        hostile = false
+        fallout.attack(dude_obj, 0, 1, 0, 0, 30000, 0, 0)
     else
-        if (fallout.global_var(306) == 1) and (fallout.tile_num(fallout.self_obj()) ~= 15507) then
-            fallout.animate_move_obj_to_tile(fallout.self_obj(), 15507, 0)
-        else
-            if (fallout.global_var(306) == 1) and (fallout.tile_num(fallout.self_obj()) == 15507) then
-                fallout.set_global_var(306, 2)
-            else
-                if (fallout.global_var(306) == 2) and (fallout.tile_num(fallout.self_obj()) ~= 12536) then
-                    fallout.animate_move_obj_to_tile(fallout.self_obj(), 12536, 0)
-                else
-                    if (fallout.tile_num(fallout.self_obj()) == 12536) and (fallout.global_var(306) == 2) then
-                        fallout.set_obj_visibility(fallout.self_obj(), 1)
-                    end
-                end
-            end
+        local self_tile_num = fallout.tile_num(self_obj)
+        if fallout.global_var(306) == 1 and self_tile_num ~= 15507 then
+            fallout.animate_move_obj_to_tile(self_obj, 15507, 0)
+        elseif fallout.global_var(306) == 1 and self_tile_num == 15507 then
+            fallout.set_global_var(306, 2)
+        elseif fallout.global_var(306) == 2 and self_tile_num ~= 12536 then
+            fallout.animate_move_obj_to_tile(self_obj, 12536, 0)
+        elseif fallout.global_var(306) == 2 and self_tile_num == 12536 then
+            fallout.set_obj_visibility(self_obj, true)
         end
     end
 end
 
 local exports = {}
 exports.start = start
+exports.pickup_p_proc = pickup_p_proc
+exports.talk_p_proc = talk_p_proc
+exports.critter_p_proc = critter_p_proc
+exports.destroy_p_proc = destroy_p_proc
+exports.look_at_p_proc = look_at_p_proc
 return exports
