4434f11e3b566f1db03201aa10cf2b00a2aec018
Alexander Batalov <alex.batalov@gmail.com>
2024-01-07 19:54:12 +0300

    Review sdebris.lua

--- src/scripts/sdebris.lua
+++ src/scripts/sdebris.lua
@@ -1,18 +1,23 @@
 local fallout = require("fallout")
 
 local start
+local spatial_p_proc
 
 function start()
     if fallout.script_action() == 2 then
-        if fallout.source_obj() == fallout.dude_obj() then
-            if fallout.map_var(1) == 0 then
-                fallout.set_map_var(1, 1)
-                fallout.set_map_var(4, fallout.map_var(4) + 1)
-                if fallout.map_var(4) > 3 then
-                    fallout.display_msg(fallout.message_str(824, 104))
-                else
-                    fallout.display_msg(fallout.message_str(824, 101))
-                end
+        spatial_p_proc()
+    end
+end
+
+function spatial_p_proc()
+    if fallout.source_obj() == fallout.dude_obj() then
+        if fallout.map_var(1) == 0 then
+            fallout.set_map_var(1, 1)
+            fallout.set_map_var(4, fallout.map_var(4) + 1)
+            if fallout.map_var(4) > 3 then
+                fallout.display_msg(fallout.message_str(824, 104))
+            else
+                fallout.display_msg(fallout.message_str(824, 101))
             end
         end
     end
@@ -20,4 +25,5 @@ end
 
 local exports = {}
 exports.start = start
+exports.spatial_p_proc = spatial_p_proc
 return exports
