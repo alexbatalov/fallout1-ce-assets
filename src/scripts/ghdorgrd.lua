5adad5f7c429ec52833e218a3e850c1f6948b5ac
Alexander Batalov <alex.batalov@gmail.com>
2024-01-07 19:46:12 +0300

    Review ghdorgrd.lua

--- src/scripts/ghdorgrd.lua
+++ src/scripts/ghdorgrd.lua
@@ -3,7 +3,10 @@ local reputation = require("lib.reputation")
 local time = require("lib.time")
 
 local start
-local do_dialogue
+local pickup_p_proc
+local talk_p_proc
+local destroy_p_proc
+local look_at_p_proc
 local ghoulend
 local ghoulcbt
 local ghoul00a
@@ -13,88 +16,89 @@ local ghoul02
 local ghoul03
 local ghoul03a
 local ghoul05
-local Critter_Action
+local critter_p_proc
 local timetomove
 
-local Hostile = 0
-local init = 0
+local hostile = false
+local initialized = false
 local maxleash = 9
-local noevent = 0
+local noevent = false
 local loopcount = 0
 local new_tile = 0
 
-local exit_line = 0
-
 function start()
-    if not(init) then
+    if not initialized then
+        local self_obj = fallout.self_obj()
         if fallout.local_var(7) == 0 then
-            fallout.set_local_var(7, fallout.tile_num(fallout.self_obj()))
+            fallout.set_local_var(7, fallout.tile_num(self_obj))
             fallout.set_local_var(5, 0)
         end
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 30)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 40)
-        init = 1
-    else
-        if fallout.script_action() == 11 then
-            if fallout.global_var(249) ~= 0 then
-                fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
-            else
-                do_dialogue()
-            end
-        else
-            if fallout.script_action() == 21 then
-                fallout.script_overrides()
-                fallout.display_msg(fallout.message_str(70, 100))
-            else
-                if fallout.script_action() == 4 then
-                    Hostile = 1
-                else
-                    if fallout.script_action() == 12 then
-                        if fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) and fallout.global_var(249) then
-                            Hostile = 1
-                        end
-                        Critter_Action()
-                    else
-                        if fallout.script_action() == 18 then
-                            reputation.inc_evil_critter()
-                        end
-                    end
-                end
-            end
-        end
+        fallout.critter_add_trait(self_obj, 1, 6, 30)
+        fallout.critter_add_trait(self_obj, 1, 5, 40)
+        initialized = true
+    end
+
+    local script_action = fallout.script_action()
+    if script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 21 then
+        look_at_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
     end
 end
 
-function do_dialogue()
-    fallout.start_gdialog(70, fallout.self_obj(), 4, -1, -1)
-    fallout.gsay_start()
-    if fallout.local_var(4) ~= 0 then
-        ghoul02()
-    end
-    if time.is_night() then
-        if fallout.local_var(1) >= 3 then
-            ghoul00b()
-        else
-            fallout.set_local_var(4, 1)
-            ghoul03()
-        end
+function pickup_p_proc()
+    hostile = true
+end
+
+function talk_p_proc()
+    if fallout.global_var(249) ~= 0 then
+        fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     else
-        if fallout.local_var(1) >= 3 then
-            ghoul01()
-        else
-            fallout.set_local_var(4, 1)
-            ghoul03()
+        fallout.start_gdialog(70, fallout.self_obj(), 4, -1, -1)
+        fallout.gsay_start()
+        if fallout.local_var(4) ~= 0 then
+            ghoul02()
         end
+        if time.is_night() then
+            if fallout.local_var(1) >= 3 then
+                ghoul00b()
+            else
+                fallout.set_local_var(4, 1)
+                ghoul03()
+            end
+        else
+            if fallout.local_var(1) >= 3 then
+                ghoul01()
+            else
+                fallout.set_local_var(4, 1)
+                ghoul03()
+            end
+        end
+        fallout.gsay_end()
+        fallout.end_dialogue()
     end
-    fallout.gsay_end()
-    fallout.end_dialogue()
+end
+
+function destroy_p_proc()
+    reputation.inc_evil_critter()
+end
+
+function look_at_p_proc()
+    fallout.script_overrides()
+    fallout.display_msg(fallout.message_str(70, 100))
 end
 
 function ghoulend()
 end
 
 function ghoulcbt()
-    Hostile = 1
+    hostile = true
 end
 
 function ghoul00a()
@@ -135,17 +139,20 @@ function ghoul05()
     fallout.gsay_message(70, 116, 50)
 end
 
-function Critter_Action()
-    if Hostile > 0 then
-        Hostile = 0
+function critter_p_proc()
+    if fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) and fallout.global_var(249) then
+        hostile = true
+    end
+    if hostile then
+        hostile = false
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     else
         if fallout.local_var(5) ~= 0 then
             loopcount = loopcount + 1
             if loopcount > 40 then
                 loopcount = 0
-                if noevent == 0 then
-                    noevent = 1
+                if not noevent then
+                    noevent = true
                     fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(fallout.random(5, 13)), 0)
                 end
             end
@@ -154,14 +161,20 @@ function Critter_Action()
 end
 
 function timetomove()
-    noevent = 0
-    fallout.set_local_var(6, fallout.tile_num(fallout.self_obj()))
+    noevent = false
+    local self_obj = fallout.self_obj()
+    fallout.set_local_var(6, fallout.tile_num(self_obj))
     new_tile = fallout.tile_num_in_direction(fallout.local_var(6), fallout.random(0, 5), 2)
     if fallout.tile_distance(fallout.local_var(7), new_tile) < maxleash then
-        fallout.animate_move_obj_to_tile(fallout.self_obj(), new_tile, 0)
+        fallout.animate_move_obj_to_tile(self_obj, new_tile, 0)
     end
 end
 
 local exports = {}
 exports.start = start
+exports.pickup_p_proc = pickup_p_proc
+exports.talk_p_proc = talk_p_proc
+exports.critter_p_proc = critter_p_proc
+exports.destroy_p_proc = destroy_p_proc
+exports.look_at_p_proc = look_at_p_proc
 return exports
