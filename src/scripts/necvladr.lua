92b109450e208f35fa2e34926463c703f88e9083
Alexander Batalov <alex.batalov@gmail.com>
2024-01-07 20:16:10 +0300

    Review necvladr.lua

--- src/scripts/necvladr.lua
+++ src/scripts/necvladr.lua
@@ -1,18 +1,29 @@
 local fallout = require("fallout")
 
 local start
+local use_p_proc
+local timed_event_p_proc
 
 function start()
-    if fallout.script_action() == 6 then
-        fallout.script_overrides()
-        fallout.add_timer_event(fallout.self_obj(), 2, 1)
-    else
-        if fallout.script_action() == 22 then
-            fallout.load_map("Watrshd.map", 12)
-        end
+    local script_action = fallout.script_action()
+    if script_action == 6 then
+        use_p_proc()
+    elseif script_action == 22 then
+        timed_event_p_proc()
     end
+end
+
+function use_p_proc()
+    fallout.script_overrides()
+    fallout.add_timer_event(fallout.self_obj(), 2, 1)
+end
+
+function timed_event_p_proc()
+    fallout.load_map("Watrshd.map", 12)
 end
 
 local exports = {}
 exports.start = start
+exports.use_p_proc = use_p_proc
+exports.timed_event_p_proc = timed_event_p_proc
 return exports
