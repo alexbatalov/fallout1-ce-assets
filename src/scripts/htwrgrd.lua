39f4512b955ec158055f2f53ff1388d2542fcb58
Alexander Batalov <alex.batalov@gmail.com>
2024-01-08 18:37:59 +0300

    Review htwrgrd.lua

--- src/scripts/htwrgrd.lua
+++ src/scripts/htwrgrd.lua
@@ -12,97 +12,86 @@ local destroy_p_proc
 local look_at_p_proc
 local damage_p_proc
 
-local hostile = 0
+local hostile = false
 local initialized = false
 local guardNumber = 0
-local threatened = 0
-local visible = 1
-
-local exit_line = 0
+local threatened = false
+local visible = true
 
 function start()
     if not initialized then
-        initialized = true
-        if fallout.tile_num(fallout.self_obj()) == 19511 then
+        local self_obj = fallout.self_obj()
+        if fallout.tile_num(self_obj) == 19511 then
             guardNumber = 1
         end
-        if fallout.tile_num(fallout.self_obj()) == 19935 then
+        if fallout.tile_num(self_obj) == 19935 then
             guardNumber = 2
         end
         if fallout.global_var(111) == 5 then
-            fallout.set_obj_visibility(fallout.self_obj(), 1)
-            visible = 0
+            fallout.set_obj_visibility(self_obj, true)
+            visible = false
         end
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 42)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 16)
+        fallout.critter_add_trait(self_obj, 1, 6, 42)
+        fallout.critter_add_trait(self_obj, 1, 5, 16)
+        initialized = true
     end
-    if fallout.script_action() == 21 then
+
+    local script_action = fallout.script_action()
+    if script_action == 21 then
         look_at_p_proc()
-    else
-        if fallout.script_action() == 4 then
-            pickup_p_proc()
-        else
-            if fallout.script_action() == 11 then
-                talk_p_proc()
-            else
-                if fallout.script_action() == 12 then
-                    critter_p_proc()
-                else
-                    if fallout.script_action() == 18 then
-                        destroy_p_proc()
-                    else
-                        if fallout.script_action() == 14 then
-                            damage_p_proc()
-                        end
-                    end
-                end
-            end
-        end
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
+    elseif script_action == 14 then
+        damage_p_proc()
     end
 end
 
 function combat()
-    hostile = 1
+    hostile = true
 end
 
 function critter_p_proc()
-    if visible == 0 then
+    if not visible then
         fallout.script_overrides()
     else
+        local self_obj = fallout.self_obj()
+        local dude_obj = fallout.dude_obj()
         if hostile then
-            hostile = 0
-            fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
+            hostile = false
+            fallout.attack(dude_obj, 0, 1, 0, 0, 30000, 0, 0)
         end
         if fallout.map_var(5) == 1 then
-            if (fallout.obj_can_hear_obj(fallout.self_obj(), fallout.dude_obj()) == 1) or (fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) == 1) then
+            if fallout.obj_can_hear_obj(self_obj, dude_obj) or fallout.obj_can_see_obj(self_obj, dude_obj) then
                 combat()
             end
         else
             if time.is_night() then
-                if (fallout.obj_can_hear_obj(fallout.self_obj(), fallout.dude_obj()) == 1) or (fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) == 1) and (threatened == 0) then
+                if fallout.obj_can_hear_obj(self_obj, dude_obj) or fallout.obj_can_see_obj(self_obj, dude_obj) and (threatened == 0) then
                     fallout.dialogue_system_enter()
                 end
                 if guardNumber == 1 then
-                    if fallout.tile_num(fallout.self_obj()) ~= 17119 then
-                        fallout.animate_move_obj_to_tile(fallout.self_obj(), 17119, 0)
+                    if fallout.tile_num(self_obj) ~= 17119 then
+                        fallout.animate_move_obj_to_tile(self_obj, 17119, 0)
                     end
-                else
-                    if guardNumber == 2 then
-                        if fallout.tile_num(fallout.self_obj()) ~= 16930 then
-                            fallout.animate_move_obj_to_tile(fallout.self_obj(), 16930, 0)
-                        end
+                elseif guardNumber == 2 then
+                    if fallout.tile_num(self_obj) ~= 16930 then
+                        fallout.animate_move_obj_to_tile(self_obj, 16930, 0)
                     end
                 end
             else
                 if guardNumber == 1 then
-                    if fallout.tile_num(fallout.self_obj()) ~= 19511 then
-                        fallout.animate_move_obj_to_tile(fallout.self_obj(), 19511, 0)
+                    if fallout.tile_num(self_obj) ~= 19511 then
+                        fallout.animate_move_obj_to_tile(self_obj, 19511, 0)
                     end
-                else
-                    if guardNumber == 2 then
-                        if fallout.tile_num(fallout.self_obj()) ~= 19935 then
-                            fallout.animate_move_obj_to_tile(fallout.self_obj(), 19935, 0)
-                        end
+                elseif guardNumber == 2 then
+                    if fallout.tile_num(self_obj) ~= 19935 then
+                        fallout.animate_move_obj_to_tile(self_obj, 19935, 0)
                     end
                 end
             end
@@ -112,16 +101,17 @@ end
 
 function pickup_p_proc()
     if fallout.source_obj() == fallout.dude_obj() then
-        hostile = 1
+        hostile = true
     end
 end
 
 function talk_p_proc()
     reaction.get_reaction()
-    if (fallout.game_time_hour() > 700) and (fallout.game_time_hour() < 1900) then
+    local game_time_hour = fallout.game_time_hour()
+    if game_time_hour > 700 and game_time_hour < 1900 then
         fallout.float_msg(fallout.self_obj(), fallout.message_str(583, fallout.random(101, 106)), 2)
     else
-        threatened = 1
+        threatened = true
         fallout.float_msg(fallout.self_obj(), fallout.message_str(583, fallout.random(107, 109)), 2)
         combat()
     end
