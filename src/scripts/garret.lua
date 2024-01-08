7a3e033e0acefed6ad35f1905ad8994311a88ee2
Alexander Batalov <alex.batalov@gmail.com>
2024-01-08 10:45:07 +0300

    Review garret.lua

--- src/scripts/garret.lua
+++ src/scripts/garret.lua
@@ -4,7 +4,12 @@ local reputation = require("lib.reputation")
 local time = require("lib.time")
 
 local start
-local do_dialogue
+local pickup_p_proc
+local talk_p_proc
+local critter_p_proc
+local damage_p_proc
+local destroy_p_proc
+local look_at_p_proc
 local garretend
 local garretcbt
 local goodstuff
@@ -25,158 +30,72 @@ local garret04
 local give_flare
 local give_cola
 
-local Shotgun_ptr = 0
-local Shells_ptr = 0
-local Cola_ptr = 0
-local Flare_ptr = 0
-local Hostile = 0
-local init = 0
-local maxleash = 9
-local noevent = 0
-local loopcount = 0
-local new_tile = 0
-local gavelist = 0
-local moving = 0
-local stuff = 0
-local indlog = 0
-local robbed = 0
+local hostile = false
+local initialized = false
+local moving = false
+local indlog = false
+local robbed = false
 local flare_count = 0
 local cola_count = 0
 local cash = 0
-local THRONE = 0
 local target_hex = 20113
-local my_hex = 0
-
-local exit_line = 0
 
 function start()
-    if not(init) then
+    if not initialized then
+        local self_obj = fallout.self_obj()
         if fallout.local_var(7) == 0 then
-            fallout.set_local_var(7, fallout.tile_num(fallout.self_obj()))
+            fallout.set_local_var(7, fallout.tile_num(self_obj))
         end
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 30)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 78)
-        fallout.set_external_var("Garret_ptr", fallout.self_obj())
-        init = 1
-    else
-        if fallout.script_action() == 14 then
-            fallout.set_global_var(249, 1)
-        else
-            if fallout.script_action() == 11 then
-                do_dialogue()
-            else
-                if fallout.script_action() == 21 then
-                    fallout.script_overrides()
-                    fallout.display_msg(fallout.message_str(102, 100))
-                else
-                    if fallout.script_action() == 4 then
-                        Hostile = 1
-                    else
-                        if fallout.script_action() == 12 then
-                            if fallout.global_var(249) and fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) then
-                                fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
-                            else
-                                my_hex = fallout.tile_num(fallout.self_obj())
-                                if Hostile > 0 then
-                                    fallout.set_global_var(249, 1)
-                                    fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
-                                end
-                                if fallout.global_var(60) & 1 then
-                                    if fallout.local_var(5) == 0 then
-                                        moving = 1
-                                        fallout.float_msg(fallout.self_obj(), fallout.message_str(102, 114), 0)
-                                        fallout.set_local_var(5, 1)
-                                    else
-                                        if fallout.local_var(5) == 1 then
-                                            moving = 1
-                                            if my_hex ~= target_hex then
-                                                fallout.animate_move_obj_to_tile(fallout.self_obj(), target_hex, 0)
-                                            else
-                                                fallout.set_local_var(5, 2)
-                                                fallout.use_obj(fallout.external_var("Fridge_ptr"))
-                                            end
-                                        else
-                                            if fallout.local_var(5) == 2 then
-                                                moving = 1
-                                                if fallout.tile_distance_objs(fallout.self_obj(), fallout.dude_obj()) < 5 then
-                                                    reward()
-                                                    fallout.set_local_var(5, 3)
-                                                    fallout.use_obj(fallout.external_var("Fridge_ptr"))
-                                                end
-                                            else
-                                                if fallout.local_var(5) == 3 then
-                                                    moving = 1
-                                                    target_hex = fallout.local_var(7)
-                                                    if my_hex ~= target_hex then
-                                                        fallout.animate_move_obj_to_tile(fallout.self_obj(), target_hex, 0)
-                                                    else
-                                                        done()
-                                                    end
-                                                end
-                                            end
-                                        end
-                                    end
-                                end
-                            end
-                        else
-                            if fallout.script_action() == 18 then
-                                fallout.set_global_var(249, 1)
-                                fallout.set_global_var(607, 3)
-                                reputation.inc_evil_critter()
-                            end
-                        end
-                    end
-                end
-            end
-        end
+        fallout.critter_add_trait(self_obj, 1, 6, 30)
+        fallout.critter_add_trait(self_obj, 1, 5, 78)
+        fallout.set_external_var("Garret_ptr", self_obj)
+        initialized = true
+    end
+
+    local script_action = fallout.script_action()
+    if script_action == 14 then
+        damage_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 21 then
+        look_at_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
     end
 end
 
-function do_dialogue()
-    if (fallout.local_var(8) == 0) and (fallout.global_var(60) & 2) then
+function pickup_p_proc()
+    hostile = true
+end
+
+local STUFF <const> = {
+    [38] = 1,
+    [39] = 1,
+    [87] = 1,
+    [53] = 1,
+    [40] = 2,
+    [25] = 2,
+    [26] = 2,
+    [27] = 2,
+    [35] = 4,
+    [36] = 4,
+}
+
+function talk_p_proc()
+    if fallout.local_var(8) == 0 and fallout.global_var(60) & 2 ~= 0 then
         fallout.set_local_var(8, 1)
         fallout.float_msg(fallout.self_obj(), fallout.message_str(102, 201), 0)
         fallout.display_msg(fallout.message_str(102, 202))
-        stuff = fallout.create_object_sid(38, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), stuff)
-        stuff = fallout.create_object_sid(39, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), stuff)
-        stuff = fallout.create_object_sid(87, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), stuff)
-        stuff = fallout.create_object_sid(53, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), stuff)
-        stuff = fallout.create_object_sid(40, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), stuff)
-        stuff = fallout.create_object_sid(40, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), stuff)
-        stuff = fallout.create_object_sid(25, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), stuff)
-        stuff = fallout.create_object_sid(25, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), stuff)
-        stuff = fallout.create_object_sid(26, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), stuff)
-        stuff = fallout.create_object_sid(26, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), stuff)
-        stuff = fallout.create_object_sid(27, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), stuff)
-        stuff = fallout.create_object_sid(27, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), stuff)
-        stuff = fallout.create_object_sid(35, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), stuff)
-        stuff = fallout.create_object_sid(35, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), stuff)
-        stuff = fallout.create_object_sid(35, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), stuff)
-        stuff = fallout.create_object_sid(35, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), stuff)
-        stuff = fallout.create_object_sid(36, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), stuff)
-        stuff = fallout.create_object_sid(36, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), stuff)
-        stuff = fallout.create_object_sid(36, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), stuff)
-        stuff = fallout.create_object_sid(36, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), stuff)
+
+        local dude_obj = fallout.dude_obj()
+        for pid, qty in ipairs(STUFF) do
+            local item_obj = fallout.create_object_sid(pid, 0, 0, -1)
+            fallout.add_mult_objs_to_inven(dude_obj, item_obj, qty)
+        end
     else
         if moving then
             fallout.display_msg(fallout.message_str(102, 200))
@@ -200,11 +119,70 @@ function do_dialogue()
     end
 end
 
+function critter_p_proc()
+    local self_obj = fallout.self_obj()
+    local dude_obj = fallout.dude_obj()
+    if fallout.global_var(249) and fallout.obj_can_see_obj(self_obj, dude_obj) then
+        fallout.attack(dude_obj, 0, 1, 0, 0, 30000, 0, 0)
+    else
+        local self_tile_num = fallout.tile_num(self_obj)
+        if hostile then
+            fallout.set_global_var(249, 1)
+            fallout.attack(dude_obj, 0, 1, 0, 0, 30000, 0, 0)
+        end
+        if fallout.global_var(60) & 1 ~= 0 then
+            if fallout.local_var(5) == 0 then
+                moving = true
+                fallout.float_msg(self_obj, fallout.message_str(102, 114), 0)
+                fallout.set_local_var(5, 1)
+            elseif fallout.local_var(5) == 1 then
+                moving = true
+                if self_tile_num ~= target_hex then
+                    fallout.animate_move_obj_to_tile(self_obj, target_hex, 0)
+                else
+                    fallout.set_local_var(5, 2)
+                    fallout.use_obj(fallout.external_var("Fridge_ptr"))
+                end
+            elseif fallout.local_var(5) == 2 then
+                moving = true
+                if fallout.tile_distance_objs(self_obj, fallout.dude_obj()) < 5 then
+                    reward()
+                    fallout.set_local_var(5, 3)
+                    fallout.use_obj(fallout.external_var("Fridge_ptr"))
+                end
+            elseif fallout.local_var(5) == 3 then
+                moving = true
+                target_hex = fallout.local_var(7)
+                if self_tile_num ~= target_hex then
+                    fallout.animate_move_obj_to_tile(self_obj, target_hex, 0)
+                else
+                    done()
+                end
+            end
+        end
+    end
+end
+
+function damage_p_proc()
+    fallout.set_global_var(249, 1)
+end
+
+function destroy_p_proc()
+    fallout.set_global_var(249, 1)
+    fallout.set_global_var(607, 3)
+    reputation.inc_evil_critter()
+end
+
+function look_at_p_proc()
+    fallout.script_overrides()
+    fallout.display_msg(fallout.message_str(102, 100))
+end
+
 function garretend()
 end
 
 function garretcbt()
-    Hostile = 1
+    hostile = true
 end
 
 function goodstuff()
@@ -215,30 +193,32 @@ function goodstuff()
 end
 
 function neutstuff()
+    local dude_obj = fallout.dude_obj()
+    local fridge_obj = fallout.external_var("Fridge_ptr")
     cash = cash + 50
-    fallout.add_mult_objs_to_inven(fallout.dude_obj(), fallout.create_object_sid(41, 0, 0, -1), 50)
-    if fallout.obj_is_carrying_obj_pid(fallout.external_var("Fridge_ptr"), 94) then
+    fallout.add_mult_objs_to_inven(dude_obj, fallout.create_object_sid(41, 0, 0, -1), 50)
+    if fallout.obj_is_carrying_obj_pid(fridge_obj, 94) ~= 0 then
         fallout.display_msg(fallout.message_str(102, 204))
-        Shotgun_ptr = fallout.obj_carrying_pid_obj(fallout.external_var("Fridge_ptr"), 94)
-        fallout.rm_obj_from_inven(fallout.external_var("Fridge_ptr"), Shotgun_ptr)
-        fallout.add_obj_to_inven(fallout.dude_obj(), Shotgun_ptr)
+        local shotgun_obj = fallout.obj_carrying_pid_obj(fridge_obj, 94)
+        fallout.rm_obj_from_inven(fridge_obj, shotgun_obj)
+        fallout.add_obj_to_inven(dude_obj, shotgun_obj)
     else
-        robbed = 1
+        robbed = true
     end
-    if fallout.obj_is_carrying_obj_pid(fallout.external_var("Fridge_ptr"), 95) then
+    if fallout.obj_is_carrying_obj_pid(fridge_obj, 95) ~= 0 then
         fallout.display_msg(fallout.message_str(102, 205))
-        Shells_ptr = fallout.obj_carrying_pid_obj(fallout.external_var("Fridge_ptr"), 95)
-        fallout.rm_obj_from_inven(fallout.external_var("Fridge_ptr"), Shells_ptr)
-        fallout.add_obj_to_inven(fallout.dude_obj(), Shells_ptr)
+        local shells_obj = fallout.obj_carrying_pid_obj(fridge_obj, 95)
+        fallout.rm_obj_from_inven(fridge_obj, shells_obj)
+        fallout.add_obj_to_inven(dude_obj, shells_obj)
     else
-        robbed = 1
+        robbed = true
     end
-    if fallout.obj_is_carrying_obj_pid(fallout.external_var("Fridge_ptr"), 95) then
-        Shells_ptr = fallout.obj_carrying_pid_obj(fallout.external_var("Fridge_ptr"), 95)
-        fallout.rm_obj_from_inven(fallout.external_var("Fridge_ptr"), Shells_ptr)
-        fallout.add_obj_to_inven(fallout.dude_obj(), Shells_ptr)
+    if fallout.obj_is_carrying_obj_pid(fridge_obj, 95) ~= 0 then
+        local shells_obj = fallout.obj_carrying_pid_obj(fridge_obj, 95)
+        fallout.rm_obj_from_inven(fridge_obj, shells_obj)
+        fallout.add_obj_to_inven(dude_obj, shells_obj)
     else
-        robbed = 1
+        robbed = true
     end
 end
 
@@ -259,25 +239,23 @@ end
 
 function reward()
     cash = 0
-    robbed = 0
+    robbed = false
     fallout.display_msg(fallout.message_str(102, 203))
     if fallout.local_var(1) >= 3 then
         goodstuff()
+    elseif fallout.local_var(1) >= 2 then
+        neutstuff()
     else
-        if fallout.local_var(1) >= 2 then
-            neutstuff()
-        else
-            badstuff()
-        end
+        badstuff()
     end
-    if flare_count then
+    if flare_count ~= 0 then
         if flare_count > 1 then
             fallout.display_msg(fallout.message_str(102, 206))
         else
             fallout.display_msg(fallout.message_str(102, 207))
         end
     end
-    if cola_count then
+    if cola_count ~= 0 then
         if cola_count > 1 then
             fallout.display_msg(fallout.message_str(102, 208))
         else
@@ -293,7 +271,7 @@ function reward()
 end
 
 function done()
-    moving = 0
+    moving = false
     fallout.set_local_var(5, 4)
 end
 
@@ -365,35 +343,43 @@ end
 function garret04()
     fallout.start_gdialog(102, fallout.self_obj(), 4, -1, -1)
     fallout.gsay_start()
-    indlog = 1
+    indlog = true
     garret00a()
-    indlog = 0
+    indlog = false
     fallout.gsay_end()
     fallout.end_dialogue()
 end
 
 function give_flare()
-    if fallout.obj_is_carrying_obj_pid(fallout.external_var("Fridge_ptr"), 79) then
+    local fridge_obj = fallout.external_var("Fridge_ptr")
+    if fallout.obj_is_carrying_obj_pid(fridge_obj, 79) ~= 0 then
         flare_count = flare_count + 1
-        Flare_ptr = fallout.obj_carrying_pid_obj(fallout.external_var("Fridge_ptr"), 79)
-        fallout.rm_obj_from_inven(fallout.external_var("Fridge_ptr"), Flare_ptr)
-        fallout.add_obj_to_inven(fallout.dude_obj(), Flare_ptr)
+        local flare_obj = fallout.obj_carrying_pid_obj(fridge_obj, 79)
+        fallout.rm_obj_from_inven(fridge_obj, flare_obj)
+        fallout.add_obj_to_inven(fallout.dude_obj(), flare_obj)
     else
-        robbed = 1
+        robbed = true
     end
 end
 
 function give_cola()
-    if fallout.obj_is_carrying_obj_pid(fallout.external_var("Fridge_ptr"), 106) then
+    local fridge_obj = fallout.external_var("Fridge_ptr")
+    if fallout.obj_is_carrying_obj_pid(fridge_obj, 106) ~= 0 then
         cola_count = cola_count + 1
-        Cola_ptr = fallout.obj_carrying_pid_obj(fallout.external_var("Fridge_ptr"), 106)
-        fallout.rm_obj_from_inven(fallout.external_var("Fridge_ptr"), Cola_ptr)
-        fallout.add_obj_to_inven(fallout.dude_obj(), Cola_ptr)
+        local cola_obj = fallout.obj_carrying_pid_obj(fridge_obj, 106)
+        fallout.rm_obj_from_inven(fridge_obj, cola_obj)
+        fallout.add_obj_to_inven(fallout.dude_obj(), cola_obj)
     else
-        robbed = 1
+        robbed = true
     end
 end
 
 local exports = {}
 exports.start = start
+exports.pickup_p_proc = pickup_p_proc
+exports.talk_p_proc = talk_p_proc
+exports.critter_p_proc = critter_p_proc
+exports.damage_p_proc = damage_p_proc
+exports.destroy_p_proc = destroy_p_proc
+exports.look_at_p_proc = look_at_p_proc
 return exports
