6b5ef59cf42031a5e062127fdba570057b2ff0b3
Alexander Batalov <alex.batalov@gmail.com>
2024-01-08 14:26:46 +0300

    Review gencop.lua

--- src/scripts/gencop.lua
+++ src/scripts/gencop.lua
@@ -15,55 +15,49 @@ local Cop00
 local Cop01
 local Cop02
 
-local hostile = 0
+local hostile = false
 local initialized = false
-
-local exit_line = 0
 
 function start()
     if not initialized then
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 40)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 86)
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 40)
+        fallout.critter_add_trait(self_obj, 1, 5, 86)
+        initialized = true
     end
-    if fallout.script_action() == 21 then
+
+    local script_action = fallout.script_action()
+    if script_action == 21 then
         look_at_p_proc()
-    else
-        if fallout.script_action() == 4 then
-            pickup_p_proc()
-        else
-            if fallout.script_action() == 11 then
-                talk_p_proc()
-            else
-                if fallout.script_action() == 12 then
-                    critter_p_proc()
-                else
-                    if fallout.script_action() == 18 then
-                        destroy_p_proc()
-                    end
-                end
-            end
-        end
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
     end
 end
 
 function combat()
-    hostile = 1
+    hostile = true
 end
 
 function critter_p_proc()
     if hostile then
-        hostile = 0
+        hostile = false
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     end
     if fallout.global_var(248) == 1 then
-        hostile = 1
+        hostile = true
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     end
 end
 
 function pickup_p_proc()
     if fallout.source_obj() == fallout.dude_obj() then
-        hostile = 1
+        hostile = true
     end
 end
 
@@ -71,12 +65,10 @@ function talk_p_proc()
     reaction.get_reaction()
     if fallout.cur_map_index() == 36 then
         Cop01()
+    elseif fallout.cur_map_index() == 38 then
+        Cop00()
     else
-        if fallout.cur_map_index() == 38 then
-            Cop00()
-        else
-            Cop02()
-        end
+        Cop02()
     end
 end
 
@@ -85,16 +77,15 @@ function destroy_p_proc()
 end
 
 function damage_p_proc()
-    local v0 = 0
-    v0 = fallout.obj_pid(fallout.source_obj())
-    if fallout.party_member_obj(v0) ~= 0 then
+    local pid = fallout.obj_pid(fallout.source_obj())
+    if fallout.party_member_obj(pid) ~= nil then
         fallout.set_global_var(248, 1)
     end
 end
 
 function combat_p_proc()
     if fallout.global_var(248) == 1 then
-        hostile = 1
+        hostile = true
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     end
 end
@@ -107,96 +98,64 @@ end
 function Cop00()
     if fallout.random(0, 3) == 1 then
         fallout.float_msg(fallout.self_obj(), fallout.message_str(573, fallout.random(101, 106)), 2)
-    else
-        if fallout.global_var(158) > 2 then
-            fallout.float_msg(fallout.self_obj(), fallout.message_str(573, fallout.random(113, 116)), 2)
+    elseif fallout.global_var(158) > 2 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(573, fallout.random(113, 116)), 2)
+    elseif reputation.has_rep_berserker() then
+        if fallout.get_critter_stat(fallout.dude_obj(), 34) == 0 then
+            fallout.float_msg(fallout.self_obj(), fallout.message_str(573, fallout.random(110, 112)), 2)
         else
-            if reputation.has_rep_berserker() then
-                if fallout.get_critter_stat(fallout.dude_obj(), 34) == 0 then
-                    fallout.float_msg(fallout.self_obj(), fallout.message_str(573, fallout.random(110, 112)), 2)
-                else
-                    fallout.float_msg(fallout.self_obj(), fallout.message_str(573, fallout.random(110, 111)), 2)
-                end
-            else
-                if reputation.has_rep_champion() then
-                    fallout.float_msg(fallout.self_obj(), fallout.message_str(573, fallout.random(117, 118)), 2)
-                else
-                    if (fallout.obj_item_subtype(fallout.critter_inven_obj(fallout.dude_obj(), 1)) == 3) or (fallout.obj_item_subtype(fallout.critter_inven_obj(fallout.dude_obj(), 2)) == 3) then
-                        fallout.float_msg(fallout.self_obj(), fallout.message_str(573, fallout.random(107, 109)), 2)
-                    else
-                        if fallout.global_var(44) == 2 then
-                            fallout.float_msg(fallout.self_obj(), fallout.message_str(573, 119), 2)
-                        else
-                            fallout.float_msg(fallout.self_obj(), fallout.message_str(573, fallout.random(101, 106)), 2)
-                        end
-                    end
-                end
-            end
+            fallout.float_msg(fallout.self_obj(), fallout.message_str(573, fallout.random(110, 111)), 2)
         end
+    elseif reputation.has_rep_champion() then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(573, fallout.random(117, 118)), 2)
+    elseif (fallout.obj_item_subtype(fallout.critter_inven_obj(fallout.dude_obj(), 1)) == 3) or (fallout.obj_item_subtype(fallout.critter_inven_obj(fallout.dude_obj(), 2)) == 3) then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(573, fallout.random(107, 109)), 2)
+    elseif fallout.global_var(44) == 2 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(573, 119), 2)
+    else
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(573, fallout.random(101, 106)), 2)
     end
 end
 
 function Cop01()
     if fallout.random(0, 3) == 1 then
         fallout.float_msg(fallout.self_obj(), fallout.message_str(573, fallout.random(120, 129)), 2)
+    elseif (fallout.obj_item_subtype(fallout.critter_inven_obj(fallout.dude_obj(), 1)) == 3) or (fallout.obj_item_subtype(fallout.critter_inven_obj(fallout.dude_obj(), 2)) == 3) then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(573, fallout.random(130, 133)), 2)
+    elseif fallout.obj_pid(fallout.critter_inven_obj(fallout.dude_obj(), 0)) == 3 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(573, 134), 2)
+    elseif fallout.global_var(198) == 1 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(573, fallout.random(135, 137)), 2)
+    elseif fallout.global_var(201) == 1 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(573, 138), 2)
+    elseif fallout.global_var(200) == 1 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(573, 139), 2)
+    elseif fallout.global_var(199) == 1 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(573, 140), 2)
+    elseif fallout.global_var(202) == 1 then
+        fallout.float_msg(fallout.self_obj(),
+            fallout.message_str(573, fallout.random(141, 143)), 2)
     else
-        if (fallout.obj_item_subtype(fallout.critter_inven_obj(fallout.dude_obj(), 1)) == 3) or (fallout.obj_item_subtype(fallout.critter_inven_obj(fallout.dude_obj(), 2)) == 3) then
-            fallout.float_msg(fallout.self_obj(), fallout.message_str(573, fallout.random(130, 133)), 2)
-        else
-            if fallout.obj_pid(fallout.critter_inven_obj(fallout.dude_obj(), 0)) == 3 then
-                fallout.float_msg(fallout.self_obj(), fallout.message_str(573, 134), 2)
-            else
-                if fallout.global_var(198) == 1 then
-                    fallout.float_msg(fallout.self_obj(), fallout.message_str(573, fallout.random(135, 137)), 2)
-                else
-                    if fallout.global_var(201) == 1 then
-                        fallout.float_msg(fallout.self_obj(), fallout.message_str(573, 138), 2)
-                    else
-                        if fallout.global_var(200) == 1 then
-                            fallout.float_msg(fallout.self_obj(), fallout.message_str(573, 139), 2)
-                        else
-                            if fallout.global_var(199) == 1 then
-                                fallout.float_msg(fallout.self_obj(), fallout.message_str(573, 140), 2)
-                            else
-                                if fallout.global_var(202) == 1 then
-                                    fallout.float_msg(fallout.self_obj(), fallout.message_str(573, fallout.random(141, 143)), 2)
-                                else
-                                    fallout.float_msg(fallout.self_obj(), fallout.message_str(573, fallout.random(120, 129)), 2)
-                                end
-                            end
-                        end
-                    end
-                end
-            end
-        end
+        fallout.float_msg(fallout.self_obj(),
+            fallout.message_str(573, fallout.random(120, 129)), 2)
     end
 end
 
 function Cop02()
     if fallout.random(0, 3) == 1 then
         fallout.float_msg(fallout.self_obj(), fallout.message_str(573, 144), 2)
+    elseif fallout.global_var(198) == 1 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(573, fallout.random(155, 157)), 2)
+    elseif fallout.global_var(201) == 1 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(573, 158), 2)
+    elseif fallout.global_var(200) == 1 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(573, 159), 2)
+    elseif fallout.global_var(199) == 1 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(573, 160), 2)
+    elseif fallout.global_var(202) == 1 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(573, fallout.random(161, 163)), 2)
     else
-        if fallout.global_var(198) == 1 then
-            fallout.float_msg(fallout.self_obj(), fallout.message_str(573, fallout.random(155, 157)), 2)
-        else
-            if fallout.global_var(201) == 1 then
-                fallout.float_msg(fallout.self_obj(), fallout.message_str(573, 158), 2)
-            else
-                if fallout.global_var(200) == 1 then
-                    fallout.float_msg(fallout.self_obj(), fallout.message_str(573, 159), 2)
-                else
-                    if fallout.global_var(199) == 1 then
-                        fallout.float_msg(fallout.self_obj(), fallout.message_str(573, 160), 2)
-                    else
-                        if fallout.global_var(202) == 1 then
-                            fallout.float_msg(fallout.self_obj(), fallout.message_str(573, fallout.random(161, 163)), 2)
-                        else
-                            fallout.float_msg(fallout.self_obj(), fallout.message_str(573, fallout.random(144, 154)), 2)
-                        end
-                    end
-                end
-            end
-        end
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(573, fallout.random(144, 154)), 2)
     end
 end
 
