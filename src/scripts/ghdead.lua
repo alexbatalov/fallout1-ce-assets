02165cc9a718175e580417f91476b89aaaaf8c46
Alexander Batalov <alex.batalov@gmail.com>
2024-01-08 10:28:36 +0300

    Review ghdead.lua

--- src/scripts/ghdead.lua
+++ src/scripts/ghdead.lua
@@ -4,18 +4,19 @@ local start
 local appear
 
 function start()
+    -- FIXME: Looks odd.
     if fallout.script_action() ~= 6 then
         if fallout.global_var(13) == 1 then
             appear()
         else
             fallout.script_overrides()
-            fallout.set_obj_visibility(fallout.self_obj(), 1)
+            fallout.set_obj_visibility(fallout.self_obj(), true)
         end
     end
 end
 
 function appear()
-    fallout.set_obj_visibility(fallout.self_obj(), 0)
+    fallout.set_obj_visibility(fallout.self_obj(), false)
 end
 
 local exports = {}
