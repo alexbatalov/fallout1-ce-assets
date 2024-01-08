4f95a19547e205f8a05835242e02a3919c29d811
Alexander Batalov <alex.batalov@gmail.com>
2024-01-08 09:17:33 +0300

    Review ghrefuge.lua

--- src/scripts/ghrefuge.lua
+++ src/scripts/ghrefuge.lua
@@ -3,7 +3,9 @@ local reaction = require("lib.reaction")
 local reputation = require("lib.reputation")
 
 local start
-local do_dialogue
+local talk_p_proc
+local destroy_p_proc
+local look_at_p_proc
 local ghoulend
 local ghoulcbt
 local ghoul00
@@ -27,53 +29,45 @@ local ghoul13
 local ghoul14
 local ghoul15
 local ghoul16
-local Critter_Action
+local critter_p_proc
 local timetomove
 local pickup_p_proc
 
-local Hostile = 0
-local init = 0
+local hostile = false
+local initialized = false
 local maxleash = 20
-local noevent = 0
+local noevent = false
 local loopcount = 0
 local new_tile = 0
 local HENRY = 0
 
-local exit_line = 0
-
 function start()
-    if not(init) then
+    if not initialized then
+        local self_obj = fallout.self_obj()
         if fallout.local_var(7) == 0 then
-            fallout.set_local_var(7, fallout.tile_num(fallout.self_obj()))
+            fallout.set_local_var(7, fallout.tile_num(self_obj))
             fallout.set_local_var(5, 0)
         end
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 30)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 40)
-        init = 1
+        fallout.critter_add_trait(self_obj, 1, 6, 30)
+        fallout.critter_add_trait(self_obj, 1, 5, 40)
+        initialized = true
     end
-    if fallout.script_action() == 11 then
-        do_dialogue()
-    else
-        if (fallout.script_action() == 21) or (fallout.script_action() == 3) then
-            fallout.script_overrides()
-            fallout.display_msg(fallout.message_str(74, 100))
-        else
-            if fallout.script_action() == 4 then
-                pickup_p_proc()
-            else
-                if fallout.script_action() == 12 then
-                    Critter_Action()
-                else
-                    if fallout.script_action() == 18 then
-                        reputation.inc_good_critter()
-                    end
-                end
-            end
-        end
+
+    local script_action = fallout.script_action()
+    if script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 21 or script_action == 3 then
+        look_at_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
     end
 end
 
-function do_dialogue()
+function talk_p_proc()
     reaction.get_reaction()
     fallout.start_gdialog(74, fallout.self_obj(), 4, -1, -1)
     fallout.gsay_start()
@@ -96,11 +90,20 @@ function do_dialogue()
     fallout.end_dialogue()
 end
 
+function destroy_p_proc()
+    reputation.inc_good_critter()
+end
+
+function look_at_p_proc()
+    fallout.script_overrides()
+    fallout.display_msg(fallout.message_str(74, 100))
+end
+
 function ghoulend()
 end
 
 function ghoulcbt()
-    Hostile = 1
+    hostile = true
 end
 
 function ghoul00()
@@ -239,17 +242,17 @@ function ghoul16()
     fallout.gsay_message(74, 146, 50)
 end
 
-function Critter_Action()
-    if Hostile > 0 then
-        Hostile = 0
+function critter_p_proc()
+    if hostile then
+        hostile = false
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     else
         if fallout.local_var(5) ~= 0 then
             loopcount = loopcount + 1
             if loopcount > 40 then
                 loopcount = 0
-                if noevent == 0 then
-                    noevent = 1
+                if not noevent then
+                    noevent = true
                     fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(fallout.random(5, 13)), 0)
                 end
             end
@@ -258,7 +261,7 @@ function Critter_Action()
 end
 
 function timetomove()
-    noevent = 0
+    noevent = false
     fallout.set_local_var(6, fallout.tile_num(fallout.self_obj()))
     new_tile = fallout.tile_num_in_direction(fallout.local_var(6), fallout.random(0, 5), 2)
     if fallout.tile_distance(fallout.local_var(7), new_tile) < maxleash then
@@ -268,11 +271,15 @@ end
 
 function pickup_p_proc()
     if fallout.source_obj() == fallout.dude_obj() then
-        Hostile = 1
+        hostile = true
     end
 end
 
 local exports = {}
 exports.start = start
 exports.pickup_p_proc = pickup_p_proc
+exports.talk_p_proc = talk_p_proc
+exports.critter_p_proc = critter_p_proc
+exports.destroy_p_proc = destroy_p_proc
+exports.look_at_p_proc = look_at_p_proc
 return exports
