123c2899ccf2aa1a5521d9ea48c4a4a3986955c4
Alexander Batalov <alex.batalov@gmail.com>
2024-01-10 21:52:39 +0300

    Review chanter.lua

--- src/scripts/chanter.lua
+++ src/scripts/chanter.lua
@@ -2,6 +2,11 @@ local fallout = require("fallout")
 local reputation = require("lib.reputation")
 
 local start
+local pickup_p_proc
+local talk_p_proc
+local critter_p_proc
+local destroy_p_proc
+local look_at_p_proc
 local Chant01
 local Chant02
 local Chant03
@@ -11,49 +16,64 @@ local Chant06
 local ChantEnd
 local Combat
 
-local Hostile = 0
+local hostile = false
 local initialized = false
 
 local Chant07
 
 function start()
     if not initialized then
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 20)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 69)
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 20)
+        fallout.critter_add_trait(self_obj, 1, 5, 69)
         initialized = true
     end
-    if (fallout.script_action() == 21) or (fallout.script_action() == 3) then
-        fallout.script_overrides()
-        fallout.display_msg(fallout.message_str(398, 100))
-    else
-        if fallout.script_action() == 12 then
-            if Hostile then
-                Hostile = 0
-                fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
-            end
-        else
-            if fallout.script_action() == 4 then
-                Hostile = 1
-            else
-                if fallout.script_action() == 18 then
-                    reputation.inc_good_critter()
-                else
-                    if fallout.script_action() == 11 then
-                        fallout.script_overrides()
-                        if fallout.global_var(195) == 1 then
-                            fallout.float_msg(fallout.self_obj(), fallout.message_str(398, 101), 0)
-                        else
-                            fallout.start_gdialog(398, fallout.self_obj(), 4, -1, -1)
-                            fallout.gsay_start()
-                            Chant01()
-                            fallout.gsay_end()
-                            fallout.end_dialogue()
-                        end
-                    end
-                end
-            end
-        end
+
+    local script_action = fallout.script_action()
+    if script_action == 21 or script_action == 3 then
+        look_at_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
     end
+end
+
+function pickup_p_proc()
+    hostile = true
+end
+
+function talk_p_proc()
+    fallout.script_overrides()
+    if fallout.global_var(195) == 1 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(398, 101), 0)
+    else
+        fallout.start_gdialog(398, fallout.self_obj(), 4, -1, -1)
+        fallout.gsay_start()
+        Chant01()
+        fallout.gsay_end()
+        fallout.end_dialogue()
+    end
+end
+
+function critter_p_proc()
+    if hostile then
+        hostile = false
+        fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
+    end
+end
+
+function destroy_p_proc()
+    reputation.inc_good_critter()
+end
+
+function look_at_p_proc()
+    fallout.script_overrides()
+    fallout.display_msg(fallout.message_str(398, 100))
 end
 
 function Chant01()
@@ -94,7 +114,7 @@ function ChantEnd()
 end
 
 function Combat()
-    Hostile = 1
+    hostile = true
 end
 
 function Chant07()
@@ -107,4 +127,9 @@ end
 
 local exports = {}
 exports.start = start
+exports.pickup_p_proc = pickup_p_proc
+exports.talk_p_proc = talk_p_proc
+exports.critter_p_proc = critter_p_proc
+exports.destroy_p_proc = destroy_p_proc
+exports.look_at_p_proc = look_at_p_proc
 return exports
