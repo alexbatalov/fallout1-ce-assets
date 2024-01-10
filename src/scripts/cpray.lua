a807518f76b628114f00424f3ab8b04e8c7a04a4
Alexander Batalov <alex.batalov@gmail.com>
2024-01-10 22:30:24 +0300

    Review cpray.lua

--- src/scripts/cpray.lua
+++ src/scripts/cpray.lua
@@ -11,59 +11,53 @@ local look_at_p_proc
 local zamin
 local goto00
 
-local hostile = 0
+local hostile = false
 local initialized = false
-local DISGUISED = 0
-local ARMED = 0
-local rndx = 0
+local disguised = false
+local armed = false
 
 function start()
     if not initialized then
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 34)
+        fallout.critter_add_trait(self_obj, 1, 5, 69)
         initialized = true
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 34)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 69)
     end
-    if fallout.script_action() == 21 then
+
+    local script_action = fallout.script_action()
+    if script_action == 21 then
         look_at_p_proc()
-    else
-        if fallout.script_action() == 4 then
-            pickup_p_proc()
-        else
-            if fallout.script_action() == 11 then
-                talk_p_proc()
-            else
-                if fallout.script_action() == 12 then
-                    critter_p_proc()
-                else
-                    if fallout.script_action() == 18 then
-                        destroy_p_proc()
-                    end
-                end
-            end
-        end
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
     end
 end
 
 function combat()
-    hostile = 1
+    hostile = true
 end
 
 function critter_p_proc()
     if hostile then
-        hostile = 0
+        hostile = false
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     end
 end
 
 function pickup_p_proc()
     if fallout.source_obj() == fallout.dude_obj() then
-        hostile = 1
+        hostile = true
     end
 end
 
 function talk_p_proc()
     zamin()
-    if (DISGUISED == 1) and (ARMED == 0) then
+    if disguised and not armed then
         goto00()
     end
 end
@@ -77,34 +71,32 @@ function look_at_p_proc()
 end
 
 function zamin()
-    DISGUISED = 0
-    ARMED = 0
+    disguised = false
+    armed = false
     if (fallout.obj_item_subtype(fallout.critter_inven_obj(fallout.dude_obj(), 1)) == 3) or (fallout.obj_item_subtype(fallout.critter_inven_obj(fallout.dude_obj(), 2)) == 3) then
-        ARMED = 1
+        armed = true
     end
     if fallout.obj_pid(fallout.critter_inven_obj(fallout.dude_obj(), 0)) == 113 then
         if fallout.metarule(16, 0) > 1 then
-            DISGUISED = 0
+            disguised = false
         else
-            DISGUISED = 1
+            disguised = true
         end
     end
 end
 
 function goto00()
-    rndx = fallout.random(1, 3)
+    local rndx = fallout.random(1, 3)
     if rndx == 3 then
         if fallout.get_critter_stat(fallout.dude_obj(), 34) == 0 then
             fallout.float_msg(fallout.self_obj(), fallout.message_str(711, 106), 2)
         else
             fallout.float_msg(fallout.self_obj(), fallout.message_str(711, 107), 2)
         end
+    elseif rndx == 2 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(711, 105), 2)
     else
-        if rndx == 2 then
-            fallout.float_msg(fallout.self_obj(), fallout.message_str(711, 105), 2)
-        else
-            fallout.float_msg(fallout.self_obj(), fallout.message_str(711, 108), 2)
-        end
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(711, 108), 2)
     end
 end
 
