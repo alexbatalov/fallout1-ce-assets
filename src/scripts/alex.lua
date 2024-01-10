60d41b82a20ed1171f21b63552bbfe39b2d7a163
Alexander Batalov <alex.batalov@gmail.com>
2024-01-10 21:43:28 +0300

    Review alex.lua

--- src/scripts/alex.lua
+++ src/scripts/alex.lua
@@ -20,54 +20,53 @@ local wake_time = 0
 local sleep_time = 0
 local home_tile = 0
 local sleep_tile = 0
-local hostile = 0
+local hostile = false
 local initialized = false
 
 function start()
     if not initialized then
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 49)
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 49)
         sleep_tile = 14275
         home_tile = 11300
         wake_time = 2030
         sleep_time = 730
         night_person = true
-        if fallout.obj_is_carrying_obj_pid(fallout.self_obj(), 41) == 0 then
-            fallout.item_caps_adjust(fallout.self_obj(), fallout.random(0, 20))
+        if fallout.obj_is_carrying_obj_pid(self_obj, 41) == 0 then
+            fallout.item_caps_adjust(self_obj, fallout.random(0, 20))
         end
         initialized = true
-    else
-        if fallout.script_action() == 12 then
-            critter_p_proc()
-        else
-            if fallout.script_action() == 18 then
-                destroy_p_proc()
-            else
-                if fallout.script_action() == 21 then
-                    look_at_p_proc()
-                else
-                    if fallout.script_action() == 11 then
-                        talk_p_proc()
-                    end
-                end
-            end
-        end
+    end
+
+    local script_action = fallout.script_action()
+    if script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
+    elseif script_action == 21 then
+        look_at_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
     end
 end
 
 function critter_p_proc()
-    if fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) then
+    local self_obj = fallout.self_obj()
+    local dude_obj = fallout.dude_obj()
+    if fallout.obj_can_see_obj(self_obj, dude_obj) then
         if hostile then
-            hostile = 0
-            fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
+            hostile = false
+            fallout.attack(dude_obj, 0, 1, 0, 0, 30000, 0, 0)
         else
             if fallout.local_var(0) == 0 then
-                if fallout.tile_distance_objs(fallout.self_obj(), fallout.dude_obj()) < 12 then
+                if fallout.tile_distance_objs(self_obj, dude_obj) < 12 then
                     fallout.dialogue_system_enter()
                 end
             end
-            if fallout.tile_distance(fallout.tile_num(fallout.dude_obj()), 10700) > fallout.tile_distance(fallout.tile_num(fallout.dude_obj()), 11900) then
+            local dude_tile_num = fallout.tile_num(dude_obj)
+            if fallout.tile_distance(dude_tile_num, 10700) > fallout.tile_distance(dude_tile_num, 11900) then
                 if fallout.global_var(128) < 2 then
-                    hostile = 1
+                    hostile = true
                 end
             end
         end
@@ -89,7 +88,8 @@ end
 
 function talk_p_proc()
     if fallout.global_var(128) == 2 then
-        fallout.float_msg(fallout.self_obj(), fallout.message_str(670, 101) .. fallout.proto_data(fallout.obj_pid(fallout.dude_obj()), 1), 0)
+        fallout.float_msg(fallout.self_obj(),
+            fallout.message_str(670, 101) .. fallout.proto_data(fallout.obj_pid(fallout.dude_obj()), 1), 0)
     else
         if fallout.local_var(0) ~= 0 then
             Alex01()
@@ -137,7 +137,7 @@ function Alex06()
 end
 
 function Alex07()
-    hostile = 1
+    hostile = true
     fallout.gsay_message(670, 113, 51)
 end
 
