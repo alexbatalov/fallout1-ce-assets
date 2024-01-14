8761fe3a77253ce43aaba6c632d3bf2e54fc80c5
Alexander Batalov <alex.batalov@gmail.com>
2024-01-14 21:42:01 +0300

    Review folinit.lua

--- src/scripts/folinit.lua
+++ src/scripts/folinit.lua
@@ -16,9 +16,10 @@ local initialized = false
 
 function start()
     if not initialized then
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 46)
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 46)
         if fallout.local_var(0) == 0 then
-            if fallout.get_critter_stat(fallout.self_obj(), 34) == 0 then
+            if fallout.get_critter_stat(self_obj, 34) == 0 then
                 fallout.set_local_var(0, fallout.random(100, 104))
             else
                 fallout.set_local_var(0, fallout.random(105, 109))
@@ -26,32 +27,25 @@ function start()
         end
         if fallout.global_var(129) == 2 then
             if fallout.random(0, 1) then
-                fallout.kill_critter(fallout.self_obj(), 59)
+                fallout.kill_critter(self_obj, 59)
             else
-                fallout.kill_critter(fallout.self_obj(), 57)
+                fallout.kill_critter(self_obj, 57)
             end
         end
         initialized = true
-    else
-        if fallout.script_action() == 12 then
-            critter_p_proc()
-        else
-            if fallout.script_action() == 14 then
-                damage_p_proc()
-            else
-                if fallout.script_action() == 18 then
-                    destroy_p_proc()
-                else
-                    if fallout.script_action() == 21 then
-                        look_at_p_proc()
-                    else
-                        if fallout.script_action() == 11 then
-                            talk_p_proc()
-                        end
-                    end
-                end
-            end
-        end
+    end
+
+    local script_action = fallout.script_action()
+    if script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 14 then
+        damage_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
+    elseif script_action == 21 then
+        look_at_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
     end
 end
 
@@ -59,7 +53,7 @@ function critter_p_proc()
     if fallout.global_var(129) == 2 then
         fallout.set_external_var("removal_ptr", fallout.self_obj())
     end
-    if fallout.local_var(1) or fallout.global_var(256) then
+    if fallout.local_var(1) ~= 0 or fallout.global_var(256) ~= 0 then
         if fallout.tile_distance_objs(fallout.dude_obj(), fallout.self_obj()) < 8 then
             behaviour.flee_dude(1)
         end
@@ -86,18 +80,14 @@ end
 
 function talk_p_proc()
     fallout.script_overrides()
-    if fallout.local_var(1) or fallout.global_var(256) then
+    if fallout.local_var(1) ~= 0 or fallout.global_var(256) ~= 0 then
         fallout.float_msg(fallout.self_obj(), fallout.message_str(669, fallout.random(100, 105)), 0)
+    elseif fallout.global_var(133) == 1 then
+        float_line1()
+    elseif fallout.global_var(132) == 1 then
+        float_line2()
     else
-        if fallout.global_var(133) == 1 then
-            float_line1()
-        else
-            if fallout.global_var(132) == 1 then
-                float_line2()
-            else
-                float_line0()
-            end
-        end
+        float_line0()
     end
 end
 
