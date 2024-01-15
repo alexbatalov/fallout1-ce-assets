bc21130f23d6053f72287416270ff7c6dc52b78c
Alexander Batalov <alex.batalov@gmail.com>
2024-01-15 21:51:38 +0300

    Review graffiti.lua

--- src/scripts/graffiti.lua
+++ src/scripts/graffiti.lua
@@ -1,20 +1,27 @@
 local fallout = require("fallout")
 
 local start
+local spatial_p_proc
 
-local HEREBEFORE = 0
+local once = false
 
 function start()
-    if fallout.script_action() == 2 then
-        if fallout.source_obj() == fallout.dude_obj() then
-            if not(HEREBEFORE) then
-                HEREBEFORE = 1
-                fallout.display_msg(fallout.message_str(91, 100))
-            end
+    local script_action = fallout.script_action()
+    if script_action == 2 then
+        spatial_p_proc()
+    end
+end
+
+function spatial_p_proc()
+    if fallout.source_obj() == fallout.dude_obj() then
+        if not once then
+            once = true
+            fallout.display_msg(fallout.message_str(91, 100))
         end
     end
 end
 
 local exports = {}
 exports.start = start
+exports.spatial_p_proc = spatial_p_proc
 return exports
