fae960fde59f755cdb974c931f25fe6d51ed2270
Alexander Batalov <alex.batalov@gmail.com>
2024-01-15 22:15:44 +0300

    Review hotspot.lua

--- src/scripts/hotspot.lua
+++ src/scripts/hotspot.lua
@@ -1,16 +1,19 @@
 local fallout = require("fallout")
 
 local start
-
-local Rads = 0
+local spatial_p_proc
 
 function start()
     if fallout.script_action() == 2 then
-        Rads = fallout.random(20, 50)
-        fallout.radiation_inc(fallout.dude_obj(), Rads)
+        spatial_p_proc()
     end
+end
+
+function spatial_p_proc()
+    fallout.radiation_inc(fallout.dude_obj(), fallout.random(20, 50))
 end
 
 local exports = {}
 exports.start = start
+exports.spatial_p_proc = spatial_p_proc
 return exports
