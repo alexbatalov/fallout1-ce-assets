fc157a07d32f8ecd7e409d37c4d870de095762fb
Alexander Batalov <alex.batalov@gmail.com>
2024-01-15 21:28:47 +0300

    Review glowtabl.lua

--- src/scripts/glowtabl.lua
+++ src/scripts/glowtabl.lua
@@ -1,14 +1,20 @@
 local fallout = require("fallout")
 
 local start
+local description_p_proc
 
 function start()
     if fallout.script_action() == 3 then
-        fallout.script_overrides()
-        fallout.display_msg(fallout.message_str(311, 100))
+        description_p_proc()
     end
+end
+
+function description_p_proc()
+    fallout.script_overrides()
+    fallout.display_msg(fallout.message_str(311, 100))
 end
 
 local exports = {}
 exports.start = start
+exports.description_p_proc = description_p_proc
 return exports
