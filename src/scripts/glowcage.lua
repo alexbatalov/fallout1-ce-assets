927a91be2ed51a7b29f1df8e876057d3999b9834
Alexander Batalov <alex.batalov@gmail.com>
2024-01-15 21:00:27 +0300

    Review glowcage.lua

--- src/scripts/glowcage.lua
+++ src/scripts/glowcage.lua
@@ -1,14 +1,21 @@
 local fallout = require("fallout")
 
 local start
+local description_p_proc
 
 function start()
-    if fallout.script_action() == 3 then
-        fallout.script_overrides()
-        fallout.display_msg(fallout.message_str(310, 100))
+    local script_action = fallout.script_action()
+    if script_action == 3 then
+        description_p_proc()
     end
+end
+
+function description_p_proc()
+    fallout.script_overrides()
+    fallout.display_msg(fallout.message_str(310, 100))
 end
 
 local exports = {}
 exports.start = start
+exports.description_p_proc = description_p_proc
 return exports
