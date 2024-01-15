501d9cabccc8321118ab2c064f2e15796fb365ad
Alexander Batalov <alex.batalov@gmail.com>
2024-01-15 21:06:41 +0300

    Review glowdesk.lua

--- src/scripts/glowdesk.lua
+++ src/scripts/glowdesk.lua
@@ -1,29 +1,40 @@
 local fallout = require("fallout")
 
 local start
+local pickup_p_proc
+local use_skill_on_p_proc
 
 function start()
-    if fallout.script_action() == 4 then
-        if fallout.local_var(0) == 0 then
-            fallout.script_overrides()
-            fallout.display_msg(fallout.message_str(331, 100))
+    local script_action = fallout.script_action()
+    if script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 8 then
+        use_skill_on_p_proc()
+    end
+end
+
+function pickup_p_proc()
+    if fallout.local_var(0) == 0 then
+        fallout.script_overrides()
+        fallout.display_msg(fallout.message_str(331, 100))
+    end
+end
+
+function use_skill_on_p_proc()
+    if fallout.action_being_used() == 9 then
+        if fallout.is_success(fallout.roll_vs_skill(fallout.dude_obj(), 9, 0)) then
+            fallout.set_local_var(0, 1)
+            fallout.display_msg(fallout.message_str(331, 101))
+        else
+            fallout.display_msg(fallout.message_str(331, 102))
         end
     else
-        if fallout.script_action() == 8 then
-            if fallout.action_being_used() == 9 then
-                if fallout.is_success(fallout.roll_vs_skill(fallout.dude_obj(), 9, 0)) then
-                    fallout.set_local_var(0, 1)
-                    fallout.display_msg(fallout.message_str(331, 101))
-                else
-                    fallout.display_msg(fallout.message_str(331, 102))
-                end
-            else
-                fallout.display_msg(fallout.message_str(331, 103))
-            end
-        end
+        fallout.display_msg(fallout.message_str(331, 103))
     end
 end
 
 local exports = {}
 exports.start = start
+exports.pickup_p_proc = pickup_p_proc
+exports.use_skill_on_p_proc = use_skill_on_p_proc
 return exports
