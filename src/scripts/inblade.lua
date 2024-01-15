5b5af77910f52ee282c12ded9c1eb2b530d71090
Alexander Batalov <alex.batalov@gmail.com>
2024-01-15 22:30:04 +0300

    Review inblade.lua

--- src/scripts/inblade.lua
+++ src/scripts/inblade.lua
@@ -2,16 +2,6 @@ local fallout = require("fallout")
 local reputation = require("lib.reputation")
 local time = require("lib.time")
 
---
--- Some unreferenced imported varables found.
--- Because of it it is impossible to specify
--- the real names of global variables.
---
-
-local g0 = 1
-local g1 = 0
-local g2 = 0
-
 local start
 local look_at_p_proc
 local description_p_proc
@@ -21,73 +11,42 @@ local damage_p_proc
 local destroy_p_proc
 local pickup_p_proc
 
--- ?import? variable RegGuard1
--- ?import? variable RegGuard2
--- ?import? variable Initialize
--- ?import? variable PsstTime
-
-local get_reaction
-local ReactToLevel
-local LevelToReact
-local UpReact
-local DownReact
-local BottomReact
-local TopReact
-local BigUpReact
-local BigDownReact
-local UpReactLevel
-local DownReactLevel
-local Goodbyes
-
--- ?import? variable exit_line
+local initialized = false
+local PsstTime = 0
 
 function start()
-    local v0 = 0
-    if g0 then
-        if fallout.obj_is_carrying_obj_pid(fallout.self_obj(), 41) == 0 then
-            fallout.item_caps_adjust(fallout.self_obj(), fallout.random(10, 100))
+    if not initialized then
+        local self_obj = fallout.self_obj()
+        if fallout.obj_is_carrying_obj_pid(self_obj, 41) == 0 then
+            fallout.item_caps_adjust(self_obj, fallout.random(10, 100))
         end
-        if (fallout.global_var(613) == 9103) or (fallout.global_var(613) == 9102) then
-            fallout.critter_add_trait(fallout.self_obj(), 1, 6, 0)
-            v0 = fallout.global_var(267)
+        if fallout.global_var(613) == 9103 or fallout.global_var(613) == 9102 then
+            fallout.critter_add_trait(self_obj, 1, 6, 0)
+            local v0 = fallout.global_var(267)
             if v0 == 0 then
-                fallout.set_external_var("InBladePtr1", fallout.self_obj())
-            else
-                if v0 == 1 then
-                    fallout.set_external_var("InBladePtr2", fallout.self_obj())
-                else
-                    if v0 == 2 then
-                        fallout.set_external_var("InBladePtr3", fallout.self_obj())
-                    else
-                        if v0 == 3 then
-                            fallout.set_external_var("InBladePtr4", fallout.self_obj())
-                        else
-                            if v0 == 4 then
-                                fallout.set_external_var("InBladePtr5", fallout.self_obj())
-                            else
-                                if v0 == 5 then
-                                    fallout.set_external_var("InBladePtr6", fallout.self_obj())
-                                else
-                                    if v0 == 6 then
-                                        fallout.set_external_var("InBladePtr7", fallout.self_obj())
-                                    else
-                                        if v0 == 7 then
-                                            fallout.set_external_var("InBladePtr8", fallout.self_obj())
-                                            fallout.set_global_var(266, 1)
-                                        end
-                                    end
-                                end
-                            end
-                        end
-                    end
-                end
+                fallout.set_external_var("InBladePtr1", self_obj)
+            elseif v0 == 1 then
+                fallout.set_external_var("InBladePtr2", self_obj)
+            elseif v0 == 2 then
+                fallout.set_external_var("InBladePtr3", self_obj)
+            elseif v0 == 3 then
+                fallout.set_external_var("InBladePtr4", self_obj)
+            elseif v0 == 4 then
+                fallout.set_external_var("InBladePtr5", self_obj)
+            elseif v0 == 5 then
+                fallout.set_external_var("InBladePtr6", self_obj)
+            elseif v0 == 6 then
+                fallout.set_external_var("InBladePtr7", self_obj)
+            elseif v0 == 7 then
+                fallout.set_external_var("InBladePtr8", self_obj)
+                fallout.set_global_var(266, 1)
             end
             v0 = v0 + 1
             fallout.set_global_var(267, v0)
         else
-            fallout.critter_add_trait(fallout.self_obj(), 1, 6, 47)
+            fallout.critter_add_trait(self_obj, 1, 6, 47)
         end
-        g0 = 0
+        initialized = true
     end
 end
 
@@ -104,23 +63,23 @@ end
 function talk_p_proc()
     if fallout.global_var(253) == 1 then
         fallout.float_msg(fallout.self_obj(), fallout.message_str(669, fallout.random(100, 105)), 2)
-    else
-        if fallout.global_var(613) == 2 then
-            fallout.float_msg(fallout.self_obj(), fallout.message_str(279, fallout.random(101, 115)), 0)
-        end
+    elseif fallout.global_var(613) == 2 then
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(279, fallout.random(101, 115)), 0)
     end
 end
 
 function critter_p_proc()
-    if ((time.game_time_in_seconds() - g1) >= 30) and (fallout.tile_distance_objs(fallout.self_obj(), fallout.dude_obj()) <= 4) and (fallout.global_var(253) == 0) then
+    local self_obj = fallout.self_obj()
+    local dude_obj = fallout.dude_obj()
+    if time.game_time_in_seconds() - PsstTime >= 30 and fallout.tile_distance_objs(self_obj, dude_obj) <= 4 and fallout.global_var(253) == 0 then
         if fallout.global_var(613) == 2 then
-            fallout.float_msg(fallout.self_obj(), fallout.message_str(279, fallout.random(101, 115)), 0)
+            fallout.float_msg(self_obj, fallout.message_str(279, fallout.random(101, 115)), 0)
         end
-        g1 = time.game_time_in_seconds()
+        PsstTime = time.game_time_in_seconds()
     end
-    if fallout.obj_can_see_obj(fallout.self_obj(), fallout.dude_obj()) then
+    if fallout.obj_can_see_obj(self_obj, dude_obj) then
         if fallout.global_var(253) == 1 then
-            fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
+            fallout.attack(dude_obj, 0, 1, 0, 0, 30000, 0, 0)
         end
     end
 end
@@ -142,113 +101,6 @@ end
 
 function pickup_p_proc()
     fallout.set_global_var(253, 1)
-end
-
-function get_reaction()
-    if fallout.local_var(2) == 0 then
-        fallout.set_local_var(0, 50)
-        fallout.set_local_var(1, 2)
-        fallout.set_local_var(2, 1)
-        fallout.set_local_var(0, fallout.local_var(0) + (5 * fallout.get_critter_stat(fallout.dude_obj(), 3)) - 25)
-        fallout.set_local_var(0, fallout.local_var(0) + (10 * fallout.has_trait(0, fallout.dude_obj(), 10)))
-        if fallout.has_trait(0, fallout.dude_obj(), 39) then
-            if fallout.global_var(155) > 0 then
-                fallout.set_local_var(0, fallout.local_var(0) + fallout.global_var(155))
-            else
-                fallout.set_local_var(0, fallout.local_var(0) - fallout.global_var(155))
-            end
-        else
-            if fallout.local_var(3) == 1 then
-                fallout.set_local_var(0, fallout.local_var(0) - fallout.global_var(155))
-            else
-                fallout.set_local_var(0, fallout.local_var(0) + fallout.global_var(155))
-            end
-        end
-        if fallout.global_var(158) > 2 then
-            fallout.set_local_var(0, fallout.local_var(0) - 30)
-        end
-        if reputation.has_rep_champion() then
-            fallout.set_local_var(0, fallout.local_var(0) + 20)
-        end
-        if reputation.has_rep_berserker() then
-            fallout.set_local_var(0, fallout.local_var(0) - 20)
-        end
-        ReactToLevel()
-    end
-end
-
-function ReactToLevel()
-    if fallout.local_var(0) <= 25 then
-        fallout.set_local_var(1, 1)
-    else
-        if fallout.local_var(0) <= 75 then
-            fallout.set_local_var(1, 2)
-        else
-            fallout.set_local_var(1, 3)
-        end
-    end
-end
-
-function LevelToReact()
-    if fallout.local_var(1) == 1 then
-        fallout.set_local_var(0, fallout.random(1, 25))
-    else
-        if fallout.local_var(1) == 2 then
-            fallout.set_local_var(0, fallout.random(26, 75))
-        else
-            fallout.set_local_var(0, fallout.random(76, 100))
-        end
-    end
-end
-
-function UpReact()
-    fallout.set_local_var(0, fallout.local_var(0) + 10)
-    ReactToLevel()
-end
-
-function DownReact()
-    fallout.set_local_var(0, fallout.local_var(0) - 10)
-    ReactToLevel()
-end
-
-function BottomReact()
-    fallout.set_local_var(1, 1)
-    fallout.set_local_var(0, 1)
-end
-
-function TopReact()
-    fallout.set_local_var(0, 100)
-    fallout.set_local_var(1, 3)
-end
-
-function BigUpReact()
-    fallout.set_local_var(0, fallout.local_var(0) + 25)
-    ReactToLevel()
-end
-
-function BigDownReact()
-    fallout.set_local_var(0, fallout.local_var(0) - 25)
-    ReactToLevel()
-end
-
-function UpReactLevel()
-    fallout.set_local_var(1, fallout.local_var(1) + 1)
-    if fallout.local_var(1) > 3 then
-        fallout.set_local_var(1, 3)
-    end
-    LevelToReact()
-end
-
-function DownReactLevel()
-    fallout.set_local_var(1, fallout.local_var(1) - 1)
-    if fallout.local_var(1) < 1 then
-        fallout.set_local_var(1, 1)
-    end
-    LevelToReact()
-end
-
-function Goodbyes()
-    g2 = fallout.message_str(634, fallout.random(100, 105))
 end
 
 local exports = {}
