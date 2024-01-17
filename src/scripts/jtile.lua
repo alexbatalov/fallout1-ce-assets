76960647753d90439f472843e7f361d85ec9dddb
Alexander Batalov <alex.batalov@gmail.com>
2024-01-17 22:15:48 +0300

    Review jtile.lua

--- src/scripts/jtile.lua
+++ src/scripts/jtile.lua
@@ -1,20 +1,25 @@
 local fallout = require("fallout")
 
 local start
+local spatial_p_proc
 local use_p_proc
+local timed_event_p_proc
 
-local ready = 1
+local ready = true
 
 function start()
-    if fallout.script_action() == 2 then
-        if fallout.source_obj() == fallout.dude_obj() then
-            if ready then
-                use_p_proc()
-            end
-        end
-    else
-        if fallout.script_action() == 22 then
-            ready = 1
+    local script_action = fallout.script_action()
+    if script_action == 2 then
+        spatial_p_proc()
+    elseif script_action == 22 then
+        timed_event_p_proc()
+    end
+end
+
+function spatial_p_proc()
+    if fallout.source_obj() == fallout.dude_obj() then
+        if ready then
+            use_p_proc()
         end
     end
 end
@@ -24,7 +29,13 @@ function use_p_proc()
     fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(4), 1)
 end
 
+function timed_event_p_proc()
+    ready = true
+end
+
 local exports = {}
 exports.start = start
+exports.spatial_p_proc = spatial_p_proc
 exports.use_p_proc = use_p_proc
+exports.timed_event_p_proc = timed_event_p_proc
 return exports
