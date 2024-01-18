d38583ff6cd797ecc4acc80105a24f59313ad5c8
Alexander Batalov <alex.batalov@gmail.com>
2024-01-18 21:50:22 +0300

    Review mvairloc.lua

--- src/scripts/mvairloc.lua
+++ src/scripts/mvairloc.lua
@@ -1,53 +1,62 @@
 local fallout = require("fallout")
 
 local start
-local use_stuff
-local pick_lock
+local use_p_proc
+local use_skill_on_p_proc
+local map_enter_p_proc
+local look_at_p_proc
 
-local unlocked = 0
+local unlocked = false
 
 function start()
-    if fallout.script_action() == 15 then
-        fallout.set_external_var("MVAirlock_ptr", fallout.self_obj())
-    else
-        if (fallout.script_action() == 21) or (fallout.script_action() == 3) then
-            fallout.script_overrides()
-            fallout.display_msg(fallout.message_str(427, 100))
-        else
-            if fallout.script_action() == 6 then
-                use_stuff()
-            else
-                if fallout.script_action() == 8 then
-                    if fallout.obj_pid(fallout.obj_being_used_with()) == 77 then
-                        pick_lock()
-                    end
-                end
-            end
-        end
+    local script_action = fallout.script_action()
+    if script_action == 15 then
+        map_enter_p_proc()
+    elseif script_action == 21 or script_action == 3 then
+        look_at_p_proc()
+    elseif script_action == 6 then
+        use_p_proc()
+    elseif script_action == 8 then
+        use_skill_on_p_proc()
     end
 end
 
-function use_stuff()
-    if not(unlocked) then
+function use_p_proc()
+    if not unlocked then
         fallout.script_overrides()
         fallout.display_msg(fallout.message_str(427, 101))
     end
 end
 
-function pick_lock()
-    fallout.script_overrides()
-    if unlocked then
-        fallout.display_msg(fallout.message_str(427, 102))
-    else
-        if fallout.is_success(fallout.roll_vs_skill(fallout.dude_obj(), 9, -30)) then
-            fallout.display_msg(fallout.message_str(427, 103))
-            unlocked = 1
+function use_skill_on_p_proc()
+    if fallout.obj_pid(fallout.obj_being_used_with()) == 77 then
+        fallout.script_overrides()
+        if unlocked then
+            fallout.display_msg(fallout.message_str(427, 102))
         else
-            fallout.display_msg(fallout.message_str(427, 104))
+            if fallout.is_success(fallout.roll_vs_skill(fallout.dude_obj(), 9, -30)) then
+                fallout.display_msg(fallout.message_str(427, 103))
+                unlocked = true
+            else
+                fallout.display_msg(fallout.message_str(427, 104))
+            end
         end
     end
 end
 
+function map_enter_p_proc()
+    fallout.set_external_var("MVAirlock_ptr", fallout.self_obj())
+end
+
+function look_at_p_proc()
+    fallout.script_overrides()
+    fallout.display_msg(fallout.message_str(427, 100))
+end
+
 local exports = {}
 exports.start = start
+exports.use_p_proc = use_p_proc
+exports.use_skill_on_p_proc = use_skill_on_p_proc
+exports.map_enter_p_proc = map_enter_p_proc
+exports.look_at_p_proc = look_at_p_proc
 return exports
