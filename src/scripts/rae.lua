37d040b850a855765f742a7676f429ef92a10810
Alexander Batalov <alex.batalov@gmail.com>
2024-01-18 22:32:08 +0300

    Review rae.lua

--- src/scripts/rae.lua
+++ src/scripts/rae.lua
@@ -9,6 +9,7 @@ local pickup_p_proc
 local talk_p_proc
 local destroy_p_proc
 local look_at_p_proc
+local timed_event_p_proc
 local special_end
 local raejoin
 local raetym
@@ -55,58 +56,47 @@ local rae13c
 local rae14
 local dialog_end
 
-local hostile = 0
+local hostile = false
 local initialized = false
-local movie = 0
-
-local exit_line = 0
+local movie = false
 
 function start()
     if not initialized then
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 44)
         initialized = true
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 44)
     end
-    if fallout.script_action() == 21 then
+
+    local script_action = fallout.script_action()
+    if script_action == 21 then
         look_at_p_proc()
-    else
-        if fallout.script_action() == 4 then
-            pickup_p_proc()
-        else
-            if fallout.script_action() == 11 then
-                talk_p_proc()
-            else
-                if fallout.script_action() == 22 then
-                    if fallout.tile_distance_objs(fallout.dude_obj(), fallout.self_obj()) < 6 then
-                        combat()
-                    end
-                else
-                    if fallout.script_action() == 12 then
-                        critter_p_proc()
-                    else
-                        if fallout.script_action() == 18 then
-                            destroy_p_proc()
-                        end
-                    end
-                end
-            end
-        end
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 22 then
+        timed_event_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
     end
 end
 
 function combat()
-    hostile = 1
+    hostile = true
 end
 
 function critter_p_proc()
     if hostile then
-        hostile = 0
+        hostile = false
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     end
 end
 
 function pickup_p_proc()
     if fallout.source_obj() == fallout.dude_obj() then
-        hostile = 1
+        hostile = true
     end
 end
 
@@ -122,7 +112,7 @@ function talk_p_proc()
     fallout.set_local_var(4, 1)
     fallout.gsay_end()
     fallout.end_dialogue()
-    if movie == 1 then
+    if movie then
         fallout.play_gmovie(4)
         fallout.metarule(13, 0)
     end
@@ -137,8 +127,14 @@ function look_at_p_proc()
     fallout.display_msg(fallout.message_str(671, 100))
 end
 
+function timed_event_p_proc()
+    if fallout.tile_distance_objs(fallout.dude_obj(), fallout.self_obj()) < 6 then
+        combat()
+    end
+end
+
 function special_end()
-    movie = 1
+    movie = true
 end
 
 function raejoin()
@@ -494,4 +490,5 @@ exports.pickup_p_proc = pickup_p_proc
 exports.talk_p_proc = talk_p_proc
 exports.destroy_p_proc = destroy_p_proc
 exports.look_at_p_proc = look_at_p_proc
+exports.timed_event_p_proc = timed_event_p_proc
 return exports
