ac037c05ed2c471a46338f1e701250499d6ca284
Alexander Batalov <alex.batalov@gmail.com>
2024-01-19 22:07:46 +0300

    Review term4.lua

--- src/scripts/term4.lua
+++ src/scripts/term4.lua
@@ -1,29 +1,29 @@
 local fallout = require("fallout")
 
 local start
+local use_p_proc
 local map_update_p_proc
 local map_enter_p_proc
 local look_at_p_proc
 
 function start()
-    if fallout.script_action() == 15 then
+    local script_action = fallout.script_action()
+    if script_action == 15 then
         map_enter_p_proc()
+    elseif script_action == 23 then
+        map_update_p_proc()
+    elseif script_action == 21 then
+        look_at_p_proc()
+    elseif script_action == 6 then
+        use_p_proc()
+    end
+end
+
+function use_p_proc()
+    if fallout.source_obj() ~= fallout.external_var("Vree_ptr") then
+        fallout.display_msg(fallout.message_str(725, 101))
     else
-        if fallout.script_action() == 23 then
-            map_update_p_proc()
-        else
-            if fallout.script_action() == 21 then
-                look_at_p_proc()
-            else
-                if fallout.script_action() == 6 then
-                    if fallout.source_obj() ~= fallout.external_var("Vree_ptr") then
-                        fallout.display_msg(fallout.message_str(725, 101))
-                    else
-                        fallout.float_msg(fallout.self_obj(), fallout.message_str(58, fallout.random(300, 308)), 8)
-                    end
-                end
-            end
-        end
+        fallout.float_msg(fallout.self_obj(), fallout.message_str(58, fallout.random(300, 308)), 8)
     end
 end
 
@@ -42,6 +42,7 @@ end
 
 local exports = {}
 exports.start = start
+exports.use_p_proc = use_p_proc
 exports.map_update_p_proc = map_update_p_proc
 exports.map_enter_p_proc = map_enter_p_proc
 exports.look_at_p_proc = look_at_p_proc
