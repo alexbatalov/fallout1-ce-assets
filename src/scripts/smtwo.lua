08d84dd5e48659423b4a20da3a4a6924d90eaee4
Alexander Batalov <alex.batalov@gmail.com>
2024-01-19 21:31:51 +0300

    Review smtwo.lua

--- src/scripts/smtwo.lua
+++ src/scripts/smtwo.lua
@@ -6,73 +6,71 @@ local combat
 local critter_p_proc
 local pickup_p_proc
 local talk_p_proc
+local map_enter_p_proc
 local destroy_p_proc
 local look_at_p_proc
+local map_update_p_proc
 local whoami
 
-local my_tile = 0
-local hostile = 0
+local hostile = false
 local initialized = false
 
 function start()
     if not initialized then
+        local self_obj = fallout.self_obj()
+        fallout.critter_add_trait(self_obj, 1, 6, 34)
+        fallout.critter_add_trait(self_obj, 1, 5, 47)
         initialized = true
-        fallout.critter_add_trait(fallout.self_obj(), 1, 6, 34)
-        fallout.critter_add_trait(fallout.self_obj(), 1, 5, 47)
     end
-    if fallout.script_action() == 15 then
-        whoami()
-    end
-    if fallout.script_action() == 23 then
-        whoami()
-    end
-    if fallout.script_action() == 21 then
+
+    local script_action = fallout.script_action()
+    if script_action == 15 then
+        map_enter_p_proc()
+    elseif script_action == 23 then
+        map_update_p_proc()
+    elseif script_action == 21 then
         look_at_p_proc()
-    else
-        if fallout.script_action() == 4 then
-            pickup_p_proc()
-        else
-            if fallout.script_action() == 11 then
-                talk_p_proc()
-            else
-                if fallout.script_action() == 12 then
-                    critter_p_proc()
-                else
-                    if fallout.script_action() == 18 then
-                        destroy_p_proc()
-                    end
-                end
-            end
-        end
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
     end
 end
 
 function combat()
-    hostile = 1
+    hostile = true
 end
 
 function critter_p_proc()
     if hostile then
-        hostile = 0
+        hostile = false
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     end
-    my_tile = fallout.tile_num(fallout.self_obj())
-    if my_tile == 12098 then
+    local self_obj = fallout.self_obj()
+    if fallout.tile_num(self_obj) == 12098 then
         fallout.set_external_var("lets_go", 0)
-        fallout.move_to(fallout.self_obj(), 7000, 0)
+        fallout.move_to(self_obj, 7000, 0)
     end
     if fallout.external_var("lets_go") == 1 then
-        fallout.animate_move_obj_to_tile(fallout.self_obj(), 12098, 0)
+        fallout.animate_move_obj_to_tile(self_obj, 12098, 0)
     end
 end
 
 function pickup_p_proc()
     if fallout.source_obj() == fallout.dude_obj() then
-        hostile = 1
+        hostile = true
     end
 end
 
 function talk_p_proc()
+end
+
+function map_enter_p_proc()
+    whoami()
 end
 
 function destroy_p_proc()
@@ -81,6 +79,10 @@ end
 
 function look_at_p_proc()
     fallout.script_overrides()
+end
+
+function map_update_p_proc()
+    whoami()
 end
 
 function whoami()
@@ -92,6 +94,8 @@ exports.start = start
 exports.critter_p_proc = critter_p_proc
 exports.pickup_p_proc = pickup_p_proc
 exports.talk_p_proc = talk_p_proc
+exports.map_enter_p_proc = map_enter_p_proc
 exports.destroy_p_proc = destroy_p_proc
 exports.look_at_p_proc = look_at_p_proc
+exports.map_update_p_proc = map_update_p_proc
 return exports
