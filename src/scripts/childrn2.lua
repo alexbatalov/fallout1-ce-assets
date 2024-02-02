deebeec37fc83ff6ea4c4e06894704b3e7378064
Alexander Batalov <alex.batalov@gmail.com>
2024-02-02 21:44:06 +0300

    Review childrn2.lua

--- src/scripts/childrn2.lua
+++ src/scripts/childrn2.lua
@@ -5,11 +5,15 @@ local party = require("lib.party")
 local time = require("lib.time")
 
 local start
+local map_enter_p_proc
+local map_exit_p_proc
+local map_update_p_proc
 
 fallout.create_external_var("Master_Ptr")
 
+fallout.set_external_var("Master_Ptr", nil)
+
 local party_elevation = 0
-local dude_start_hex = 0
 
 fallout.create_external_var("Ian_ptr")
 fallout.create_external_var("Dog_ptr")
@@ -17,50 +21,64 @@ fallout.create_external_var("Tycho_ptr")
 fallout.create_external_var("Katja_ptr")
 fallout.create_external_var("Tandi_ptr")
 
-local Player_Elevation = 0
-local cur_count = 0
+fallout.set_external_var("Ian_ptr", nil)
+fallout.set_external_var("Dog_ptr", nil)
+fallout.set_external_var("Tycho_ptr", nil)
+fallout.set_external_var("Katja_ptr", nil)
+fallout.set_external_var("Tandi_ptr", nil)
+
 local prev_count = 0
 
 function start()
-    if fallout.script_action() == 15 then
-        light.lighting()
-        Player_Elevation = fallout.elevation(fallout.dude_obj())
-        if fallout.global_var(32) == 0 then
-            fallout.override_map_start(101, 95, 0, 2)
-        else
-            if fallout.global_var(32) == 1 then
-                fallout.override_map_start(101, 95, 1, 2)
-            else
-                if fallout.global_var(32) == 2 then
-                    fallout.override_map_start(94, 96, 2, 2)
-                else
-                    fallout.override_map_start(101, 95, 0, 2)
-                end
-            end
+    local script_action = fallout.script_action()
+    if script_action == 15 then
+        map_enter_p_proc()
+    elseif script_action == 23 then
+        map_update_p_proc()
+    elseif script_action == 16 then
+        map_exit_p_proc()
+    end
+end
+
+function map_enter_p_proc()
+    light.lighting()
+    Player_Elevation = fallout.elevation(fallout.dude_obj())
+    if fallout.global_var(32) == 0 then
+        fallout.override_map_start(101, 95, 0, 2)
+    elseif fallout.global_var(32) == 1 then
+        fallout.override_map_start(101, 95, 1, 2)
+    elseif fallout.global_var(32) == 2 then
+        fallout.override_map_start(94, 96, 2, 2)
+    else
+        fallout.override_map_start(101, 95, 0, 2)
+    end
+    party_elevation = party.add_party()
+end
+
+function map_exit_p_proc()
+    party.remove_party()
+end
+
+function map_update_p_proc()
+    light.lighting()
+    if fallout.global_var(55) ~= 0 then
+        local cur_count = 240 - (time.game_time_in_seconds() - fallout.global_var(55))
+        if cur_count ~= prev_count then
+            fallout.display_msg(fallout.message_str(447, 100) .. cur_count .. fallout.message_str(447, 101))
+            prev_count = cur_count
         end
-        party_elevation = party.add_party()
-    end
-    if fallout.script_action() == 23 then
-        light.lighting()
-        if fallout.global_var(55) ~= 0 then
-            cur_count = 240 - (time.game_time_in_seconds() - fallout.global_var(55))
-            if cur_count ~= prev_count then
-                fallout.display_msg(fallout.message_str(447, 100) .. cur_count .. fallout.message_str(447, 101))
-                prev_count = cur_count
-            end
-            if (time.game_time_in_seconds() - fallout.global_var(55)) > 240 then
-                fallout.display_msg(fallout.message_str(445, 106))
-                fallout.play_gmovie(4)
-                misc.signal_end_game()
-            end
+        if time.game_time_in_seconds() - fallout.global_var(55) > 240 then
+            fallout.display_msg(fallout.message_str(445, 106))
+            fallout.play_gmovie(4)
+            misc.signal_end_game()
         end
-        party_elevation = party.update_party(party_elevation)
     end
-    if fallout.script_action() == 16 then
-        party.remove_party()
-    end
+    party_elevation = party.update_party(party_elevation)
 end
 
 local exports = {}
 exports.start = start
+exports.map_enter_p_proc = map_enter_p_proc
+exports.map_exit_p_proc = map_exit_p_proc
+exports.map_update_p_proc = map_update_p_proc
 return exports
