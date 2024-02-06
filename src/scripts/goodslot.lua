ebdf12003875022d6064093bd956e3d8644851f3
Alexander Batalov <alex.batalov@gmail.com>
2024-02-06 20:38:25 +0300

    Review goodslot.lua

--- src/scripts/goodslot.lua
+++ src/scripts/goodslot.lua
@@ -5,22 +5,14 @@ local look_at_p_proc
 local map_enter_p_proc
 local use_p_proc
 
-local rndx = 0
-local bonus = 0
-local test = 0
-local money = 0
-
 function start()
-    if fallout.script_action() == 21 then
+    local script_action = fallout.script_action()
+    if script_action == 21 then
         look_at_p_proc()
-    else
-        if fallout.script_action() == 15 then
-            map_enter_p_proc()
-        else
-            if fallout.script_action() == 6 then
-                use_p_proc()
-            end
-        end
+    elseif script_action == 15 then
+        map_enter_p_proc()
+    elseif script_action == 6 then
+        use_p_proc()
     end
 end
 
@@ -42,19 +34,20 @@ function use_p_proc()
     else
         fallout.display_msg(fallout.message_str(355, 101))
         fallout.item_caps_adjust(fallout.dude_obj(), -20)
-        bonus = fallout.random(1, 50) - fallout.random(20, 50)
-        test = fallout.roll_vs_skill(fallout.dude_obj(), 16, bonus)
-        if fallout.is_success(test) then
-            if fallout.is_critical(test) then
-                rndx = fallout.random(30, 40)
+        local bonus = fallout.random(1, 50) - fallout.random(20, 50)
+        local roll = fallout.roll_vs_skill(fallout.dude_obj(), 16, bonus)
+        if fallout.is_success(roll) then
+            local amount
+            if fallout.is_critical(roll) then
+                amount = fallout.random(30, 40)
                 fallout.float_msg(fallout.self_obj(), fallout.message_str(355, 102), 0)
-                fallout.display_msg(fallout.message_str(355, 103) .. rndx .. fallout.message_str(355, 104))
+                fallout.display_msg(fallout.message_str(355, 103) .. amount .. fallout.message_str(355, 104))
             else
                 fallout.float_msg(fallout.self_obj(), fallout.message_str(355, 105), 0)
                 fallout.display_msg(fallout.message_str(355, 106))
-                rndx = 30
+                amount = 30
             end
-            fallout.item_caps_adjust(fallout.dude_obj(), rndx)
+            fallout.item_caps_adjust(fallout.dude_obj(), amount)
         else
             fallout.display_msg(fallout.message_str(355, 107))
         end
