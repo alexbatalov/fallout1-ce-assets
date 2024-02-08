a24d57c092ba36a201a22f1f0eb80622acc2fb69
Alexander Batalov <alex.batalov@gmail.com>
2024-02-08 21:06:23 +0300

    Review sink.lua

--- src/scripts/sink.lua
+++ src/scripts/sink.lua
@@ -7,20 +7,15 @@ local look_at_p_proc
 local use_obj_on_p_proc
 
 function start()
-    if fallout.script_action() == 21 then
+    local script_action = fallout.script_action()
+    if script_action == 21 then
         look_at_p_proc()
-    else
-        if fallout.script_action() == 6 then
-            use_p_proc()
-        else
-            if fallout.script_action() == 4 then
-                pickup_p_proc()
-            else
-                if fallout.script_action() == 7 then
-                    use_obj_on_p_proc()
-                end
-            end
-        end
+    elseif script_action == 6 then
+        use_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 7 then
+        use_obj_on_p_proc()
     end
 end
 
@@ -40,7 +35,8 @@ end
 
 function use_obj_on_p_proc()
     fallout.script_overrides()
-    fallout.display_msg(fallout.message_str(739, 102) .. fallout.proto_data(fallout.obj_pid(fallout.obj_being_used_with()), 1) .. fallout.message_str(739, 103))
+    fallout.display_msg(fallout.message_str(739, 102) ..
+        fallout.proto_data(fallout.obj_pid(fallout.obj_being_used_with()), 1) .. fallout.message_str(739, 103))
 end
 
 local exports = {}
