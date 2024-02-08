4c1f1bd0a58fe88c71dc30d4e38b170709c2589f
Alexander Batalov <alex.batalov@gmail.com>
2024-02-08 21:13:15 +0300

    Review lorri.lua

--- src/scripts/lorri.lua
+++ src/scripts/lorri.lua
@@ -2,7 +2,9 @@ local fallout = require("fallout")
 local reaction = require("lib.reaction")
 
 local start
-local do_dialogue
+local talk_p_proc
+local look_at_p_proc
+local timed_event_p_proc
 local Lorri01
 local Lorri02
 local Lorri03
@@ -31,35 +33,25 @@ local Lorri25
 local Lorri26
 local Lorri27
 
-local known = 0
-local line21flag = 0
-local line22flag = 0
-local line23flag = 0
-local line24flag = 0
-local line25flag = 0
-
-local exit_line = 0
+local known = false
+local line21flag = false
+local line22flag = false
+local line23flag = false
+local line24flag = false
+local line25flag = false
 
 function start()
-    if fallout.script_action() == 11 then
-        do_dialogue()
-    else
-        if (fallout.script_action() == 21) or (fallout.script_action() == 3) then
-            fallout.script_overrides()
-            if not(known) then
-                fallout.display_msg(fallout.message_str(0, 100))
-            else
-                fallout.display_msg(fallout.message_str(0, 101))
-            end
-        else
-            if fallout.script_action() == 22 then
-                fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
-            end
-        end
+    local script_action = fallout.script_action()
+    if script_action == 11 then
+        talk_p_proc()
+    elseif script_action == 21 or script_action == 3 then
+        look_at_p_proc()
+    elseif script_action == 22 then
+        timed_event_p_proc()
     end
 end
 
-function do_dialogue()
+function talk_p_proc()
     fallout.start_gdialog(-1, fallout.self_obj(), 4, -1, -1)
     fallout.gsay_start()
     if known then
@@ -71,16 +63,31 @@ function do_dialogue()
     fallout.end_dialogue()
 end
 
+function look_at_p_proc()
+    fallout.script_overrides()
+    if not known then
+        fallout.display_msg(fallout.message_str(0, 100))
+    else
+        fallout.display_msg(fallout.message_str(0, 101))
+    end
+end
+
+function timed_event_p_proc()
+    fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
+end
+
 function Lorri01()
     fallout.gsay_reply(0, 102)
     fallout.giq_option(4, 0, 103, Lorri02, 50)
-    fallout.giq_option(4, 0, fallout.message_str(0, 104) .. fallout.proto_data(fallout.obj_pid(fallout.dude_obj()), 1) .. fallout.message_str(0, 105), Lorri03, 50)
+    fallout.giq_option(4, 0,
+        fallout.message_str(0, 104) ..
+        fallout.proto_data(fallout.obj_pid(fallout.dude_obj()), 1) .. fallout.message_str(0, 105), Lorri03, 50)
     fallout.giq_option(-3, 0, fallout.message_str(0, 106), Lorri15, 50)
 end
 
 function Lorri02()
     fallout.gsay_reply(0, 107)
-    if not(known) then
+    if not known then
         fallout.giq_option(4, 0, 108, Lorri03, 50)
     end
     fallout.giq_option(4, 0, 109, Lorri16, 50)
@@ -90,11 +97,10 @@ function Lorri02()
 end
 
 function Lorri03()
-    known = 1
+    known = true
     fallout.gsay_reply(0, 113)
     fallout.giq_option(4, 0, 114, Lorri02, 50)
-    exit_line = reaction.Goodbyes()
-    fallout.giq_option(4, 0, exit_line, Lorri05, 50)
+    fallout.giq_option(4, 0, reaction.Goodbyes(), Lorri05, 50)
 end
 
 function Lorri04()
@@ -202,19 +208,19 @@ end
 
 function Lorri20()
     fallout.gsay_reply(0, 170)
-    if not(line21flag) then
+    if not line21flag then
         fallout.giq_option(4, 0, 171, Lorri21, 50)
     end
-    if not(line22flag) then
+    if not line22flag then
         fallout.giq_option(4, 0, 172, Lorri22, 50)
     end
-    if not(line23flag) then
+    if not line23flag then
         fallout.giq_option(4, 0, 173, Lorri23, 50)
     end
-    if not(line24flag) then
+    if not line24flag then
         fallout.giq_option(4, 0, 174, Lorri24, 50)
     end
-    if not(line25flag) then
+    if not line25flag then
         fallout.giq_option(4, 0, 175, Lorri25, 50)
     end
     fallout.giq_option(4, 0, 176, Lorri05, 50)
@@ -222,39 +228,38 @@ end
 
 function Lorri21()
     fallout.gsay_reply(0, 177)
-    line21flag = 1
+    line21flag = true
     Lorri26()
 end
 
 function Lorri22()
     fallout.gsay_reply(0, 178)
-    line22flag = 1
+    line22flag = true
     Lorri26()
 end
 
 function Lorri23()
     fallout.gsay_reply(0, 179)
-    line23flag = 1
+    line23flag = true
     Lorri26()
 end
 
 function Lorri24()
     fallout.gsay_reply(0, 180)
-    line24flag = 1
+    line24flag = true
     Lorri26()
 end
 
 function Lorri25()
     fallout.gsay_reply(0, 181)
-    line25flag = 1
+    line25flag = true
     Lorri26()
 end
 
 function Lorri26()
     fallout.giq_option(4, 0, 182, Lorri20, 50)
     fallout.giq_option(4, 0, 183, Lorri02, 50)
-    exit_line = reaction.Goodbyes()
-    fallout.giq_option(4, 0, exit_line, Lorri05, 50)
+    fallout.giq_option(4, 0, reaction.Goodbyes(), Lorri05, 50)
 end
 
 function Lorri27()
@@ -263,4 +268,7 @@ end
 
 local exports = {}
 exports.start = start
+exports.talk_p_proc = talk_p_proc
+exports.look_at_p_proc = look_at_p_proc
+exports.timed_event_p_proc = timed_event_p_proc
 return exports
