0ef6b4325cd27d5a482211019be5afe2f4f9c229
Alexander Batalov <alex.batalov@gmail.com>
2024-02-09 21:29:06 +0300

    Review lance.lua

--- src/scripts/lance.lua
+++ src/scripts/lance.lua
@@ -69,46 +69,45 @@ local LanceCombat
 local LanceEnd
 local LanceLoot
 
-local hostile = 0
+local hostile = false
 local initialized = false
-local known = 0
-local go_to_Shady = 0
-local go_to_Raiders = 0
-
-local exit_line = 0
+local known = false
+local go_to_Shady = false
+local go_to_Raiders = false
 
 function start()
     if not initialized then
-        misc.set_team(fallout.self_obj(), 2)
-        misc.set_ai(fallout.self_obj(), 4)
-        hostile = fallout.global_var(334)
+        local self_obj = fallout.self_obj()
+        misc.set_team(self_obj, 2)
+        misc.set_ai(self_obj, 4)
+        hostile = fallout.global_var(334) ~= 0
         initialized = true
-    else
-        if fallout.script_action() == 12 then
-            critter_p_proc()
-        else
-            if fallout.script_action() == 18 then
-                destroy_p_proc()
-            else
-                if fallout.script_action() == 4 then
-                    pickup_p_proc()
-                else
-                    if fallout.script_action() == 11 then
-                        talk_p_proc()
-                    end
-                end
-            end
-        end
+    end
+
+    local script_action = fallout.script_action()
+    if script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
     end
 end
 
 function critter_p_proc()
     if hostile then
-        hostile = 0
+        hostile = false
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     else
-        if known and (fallout.tile_num(fallout.self_obj()) < 20000) then
-            fallout.animate_move_obj_to_tile(fallout.self_obj(), fallout.tile_num_in_direction(fallout.self_obj(), 0, 3), 0)
+        if known then
+            local self_obj = fallout.self_obj()
+            local self_tile_num = fallout.tile_num(self_obj)
+            if self_tile_num < 20000 then
+                local dest = fallout.tile_num_in_direction(self_tile_num, 0, 3)
+                fallout.animate_move_obj_to_tile(self_obj, dest, 0)
+            end
         end
     end
 end
@@ -119,7 +118,7 @@ function destroy_p_proc()
 end
 
 function pickup_p_proc()
-    hostile = 1
+    hostile = true
     fallout.set_global_var(334, 1)
 end
 
@@ -147,8 +146,8 @@ function talk_p_proc()
                 fallout.end_dialogue()
             end
         else
-            if fallout.global_var(246) or (fallout.global_var(155) < -30) or (reputation.has_rep_berserker()) or (fallout.global_var(158) > 2) then
-                if (fallout.global_var(103) == 2) and (fallout.global_var(43) == 2) then
+            if fallout.global_var(246) ~= 0 or fallout.global_var(155) < -30 or reputation.has_rep_berserker() or fallout.global_var(158) > 2 then
+                if fallout.global_var(103) == 2 and fallout.global_var(43) == 2 then
                     fallout.start_gdialog(699, fallout.self_obj(), 4, -1, -1)
                     fallout.gsay_start()
                     Lance00()
@@ -196,7 +195,7 @@ end
 
 function Lance00()
     reaction.BottomReact()
-    known = 1
+    known = true
     fallout.gsay_reply(699, 102)
     fallout.giq_option(4, 699, 103, Lance00a, 51)
     fallout.giq_option(4, 699, 104, Lance10, 50)
@@ -206,7 +205,12 @@ function Lance00()
 end
 
 function Lance00a()
-    if (fallout.get_critter_stat(fallout.dude_obj(), 0) > 7) or fallout.obj_is_carrying_obj_pid(fallout.dude_obj(), 15) or fallout.obj_is_carrying_obj_pid(fallout.dude_obj(), 118) or fallout.obj_is_carrying_obj_pid(fallout.dude_obj(), 13) or fallout.obj_is_carrying_obj_pid(fallout.dude_obj(), 12) then
+    local dude_obj = fallout.dude_obj()
+    if fallout.get_critter_stat(dude_obj, 0) > 7
+        or fallout.obj_is_carrying_obj_pid(dude_obj, 15) ~= 0
+        or fallout.obj_is_carrying_obj_pid(dude_obj, 118) ~= 0
+        or fallout.obj_is_carrying_obj_pid(dude_obj, 13) ~= 0
+        or fallout.obj_is_carrying_obj_pid(dude_obj, 12) ~= 0 then
         Lance08()
     else
         Lance09()
@@ -216,7 +220,7 @@ end
 function Lance01()
     reaction.BottomReact()
     fallout.float_msg(fallout.self_obj(), fallout.message_str(699, 108), 0)
-    hostile = 1
+    hostile = true
     fallout.set_global_var(334, 1)
 end
 
@@ -226,10 +230,10 @@ end
 
 function Lance03()
     fallout.gsay_reply(699, 110)
-    if (fallout.global_var(101) == 1) and (fallout.obj_is_carrying_obj_pid(fallout.dude_obj(), 55) == 0) then
+    if fallout.global_var(101) == 1 and fallout.obj_is_carrying_obj_pid(fallout.dude_obj(), 55) == 0 then
         fallout.giq_option(4, 699, 111, Lance14, 50)
     end
-    if (fallout.global_var(26) == 1) and (fallout.global_var(103) == 1) then
+    if fallout.global_var(26) == 1 and fallout.global_var(103) == 1 then
         fallout.giq_option(4, 699, 112, Lance15, 50)
     end
     if fallout.global_var(103) == 2 then
@@ -240,9 +244,9 @@ function Lance03()
 end
 
 function Lance04()
-    known = 1
+    known = true
     fallout.gsay_reply(699, 116)
-    if (fallout.global_var(101) == 1) and (fallout.obj_is_carrying_obj_pid(fallout.dude_obj(), 55) == 0) then
+    if fallout.global_var(101) == 1 and fallout.obj_is_carrying_obj_pid(fallout.dude_obj(), 55) == 0 then
         fallout.giq_option(4, 699, 111, Lance14, 50)
     end
     fallout.giq_option(4, 699, 118, Lance04a, 50)
@@ -264,7 +268,9 @@ function Lance05()
 end
 
 function Lance06()
-    fallout.gsay_reply(699, fallout.message_str(699, 123) .. fallout.proto_data(fallout.obj_pid(fallout.dude_obj()), 1) .. fallout.message_str(699, 124))
+    fallout.gsay_reply(699,
+        fallout.message_str(699, 123) ..
+        fallout.proto_data(fallout.obj_pid(fallout.dude_obj()), 1) .. fallout.message_str(699, 124))
     fallout.giq_option(4, 699, 125, Lance25, 50)
     fallout.giq_option(4, 699, 113, Lance16, 50)
     fallout.giq_option(4, 699, 127, Lance26, 50)
@@ -273,11 +279,13 @@ function Lance06()
 end
 
 function Lance07()
-    known = 1
-    fallout.gsay_reply(699, fallout.message_str(699, 123) .. fallout.proto_data(fallout.obj_pid(fallout.dude_obj()), 1) .. fallout.message_str(699, 130))
+    known = true
+    fallout.gsay_reply(699,
+        fallout.message_str(699, 123) ..
+        fallout.proto_data(fallout.obj_pid(fallout.dude_obj()), 1) .. fallout.message_str(699, 130))
     fallout.giq_option(4, 699, 131, Lance29, 50)
     fallout.giq_option(4, 699, 113, Lance16, 50)
-    if (fallout.global_var(101) == 1) and (fallout.obj_is_carrying_obj_pid(fallout.dude_obj(), 55) == 0) then
+    if fallout.global_var(101) == 1 and fallout.obj_is_carrying_obj_pid(fallout.dude_obj(), 55) == 0 then
         fallout.giq_option(4, 699, 111, Lance14, 50)
     end
     fallout.giq_option(4, 699, 132, Lance00a, 51)
@@ -292,7 +300,7 @@ end
 
 function Lance09()
     fallout.gsay_message(699, 135, 51)
-    hostile = 1
+    hostile = true
     fallout.set_global_var(334, 1)
 end
 
@@ -321,7 +329,7 @@ end
 function Lance14()
     fallout.gsay_reply(699, 142)
     fallout.giq_option(4, 699, 143, Lance14, 50)
-    if (fallout.global_var(103) == 1) and (fallout.global_var(26) == 1) then
+    if fallout.global_var(103) == 1 and fallout.global_var(26) == 1 then
         fallout.giq_option(4, 699, 112, Lance15, 50)
     end
     if fallout.global_var(103) == 2 then
@@ -351,10 +359,10 @@ end
 
 function Lance17()
     fallout.gsay_reply(699, 154)
-    if (fallout.global_var(101) == 1) and (fallout.obj_is_carrying_obj_pid(fallout.dude_obj(), 55) == 0) then
+    if fallout.global_var(101) == 1 and fallout.obj_is_carrying_obj_pid(fallout.dude_obj(), 55) == 0 then
         fallout.giq_option(4, 699, 111, Lance14, 50)
     end
-    if (fallout.global_var(103) == 1) and (fallout.global_var(26) == 1) then
+    if fallout.global_var(103) == 1 and fallout.global_var(26) == 1 then
         fallout.giq_option(4, 699, 112, Lance15, 50)
     end
     if fallout.global_var(103) == 2 then
@@ -372,7 +380,7 @@ function Lance19()
     fallout.gsay_reply(699, 158)
     fallout.giq_option(4, 699, 159, Lance19a, 50)
     fallout.giq_option(4, 699, 160, Lance42, 50)
-    if (fallout.global_var(101) == 1) and (fallout.obj_is_carrying_obj_pid(fallout.dude_obj(), 55) == 0) then
+    if fallout.global_var(101) == 1 and fallout.obj_is_carrying_obj_pid(fallout.dude_obj(), 55) == 0 then
         fallout.giq_option(4, 699, 161, Lance43, 50)
     end
     fallout.giq_option(4, 699, 162, Lance48, 50)
@@ -460,10 +468,10 @@ end
 
 function Lance32()
     fallout.gsay_reply(699, 182)
-    if (fallout.global_var(101) == 1) and (fallout.obj_is_carrying_obj_pid(fallout.dude_obj(), 55) == 0) then
+    if fallout.global_var(101) == 1 and fallout.obj_is_carrying_obj_pid(fallout.dude_obj(), 55) == 0 then
         fallout.giq_option(4, 699, 161, Lance43, 50)
     end
-    if (fallout.global_var(103) == 1) and (fallout.global_var(26) == 1) then
+    if fallout.global_var(103) == 1 and fallout.global_var(26) == 1 then
         fallout.giq_option(4, 699, 112, Lance15, 50)
     end
     if fallout.global_var(103) == 2 then
@@ -475,12 +483,12 @@ end
 
 function Lance33()
     fallout.gsay_message(699, 184, 50)
-    go_to_Shady = 1
+    go_to_Shady = true
 end
 
 function Lance34()
     fallout.gsay_message(699, 185, 50)
-    go_to_Raiders = 1
+    go_to_Raiders = true
 end
 
 function Lance35()
@@ -506,7 +514,7 @@ end
 
 function Lance40()
     fallout.gsay_message(699, 191, 50)
-    go_to_Shady = 1
+    go_to_Shady = true
 end
 
 function Lance41()
@@ -533,7 +541,7 @@ end
 
 function Lance46()
     fallout.gsay_message(699, 197, 50)
-    go_to_Shady = 1
+    go_to_Shady = true
 end
 
 function Lance47()
@@ -545,7 +553,7 @@ function Lance48()
 end
 
 function Lance49()
-    known = 1
+    known = true
     fallout.gsay_reply(699, 200)
     fallout.giq_option(4, 699, 201, Lance50, 50)
     fallout.giq_option(4, 699, 202, Lance51, 50)
@@ -567,7 +575,7 @@ function Lance52()
 end
 
 function LanceCombat()
-    hostile = 1
+    hostile = true
     fallout.set_global_var(334, 1)
 end
 
