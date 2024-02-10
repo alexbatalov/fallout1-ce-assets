78881b4ec94f2a089a96be0da814e966b5e6f9a6
Alexander Batalov <alex.batalov@gmail.com>
2024-02-10 21:59:36 +0300

    Review computer.lua

--- src/scripts/computer.lua
+++ src/scripts/computer.lua
@@ -1,42 +1,49 @@
 local fallout = require("fallout")
 
 local start
-local computer00
-
-local test = 0
+local pickup_p_proc
+local look_at_p_proc
 
 function start()
-    if fallout.script_action() == 4 then
-        fallout.script_overrides()
-        computer00()
-    else
-        if fallout.script_action() == 21 then
-            fallout.script_overrides()
-            fallout.display_msg(fallout.message_str(23, 100))
-        end
+    local script_action = fallout.script_action()
+    if script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 21 then
+        look_at_p_proc()
     end
 end
 
-function computer00()
-    if fallout.has_skill(fallout.dude_obj(), 12) > fallout.has_skill(fallout.dude_obj(), 13) then
-        test = fallout.roll_vs_skill(fallout.dude_obj(), 12, 0)
+function pickup_p_proc()
+    fallout.script_overrides()
+
+    local dude_obj = fallout.dude_obj()
+    local roll
+    if fallout.has_skill(dude_obj, 12) > fallout.has_skill(dude_obj, 13) then
+        roll = fallout.roll_vs_skill(dude_obj, 12, 0)
     else
-        test = fallout.roll_vs_skill(fallout.dude_obj(), 13, 0)
+        roll = fallout.roll_vs_skill(dude_obj, 13, 0)
     end
-    if fallout.is_success(test) then
+    if fallout.is_success(roll) then
         fallout.display_msg(fallout.message_str(23, 101))
-        if fallout.is_critical(test) then
+        if fallout.is_critical(roll) then
             fallout.display_msg(fallout.message_str(23, 102))
         end
     else
         fallout.display_msg(fallout.message_str(23, 103))
-        if fallout.is_critical(test) then
+        if fallout.is_critical(roll) then
             fallout.display_msg(fallout.message_str(23, 104))
-            fallout.critter_dmg(fallout.dude_obj(), 1, 0)
+            fallout.critter_dmg(dude_obj, 1, 0)
         end
     end
 end
 
+function look_at_p_proc()
+    fallout.script_overrides()
+    fallout.display_msg(fallout.message_str(23, 100))
+end
+
 local exports = {}
 exports.start = start
+exports.pickup_p_proc = pickup_p_proc
+exports.look_at_p_proc = look_at_p_proc
 return exports
