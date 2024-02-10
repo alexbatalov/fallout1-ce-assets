2b9c74d925550df90135f6a0f4be28e9d54cb3af
Alexander Batalov <alex.batalov@gmail.com>
2024-02-10 22:01:32 +0300

    Review coyote.lua

--- src/scripts/coyote.lua
+++ src/scripts/coyote.lua
@@ -9,18 +9,18 @@ local initialized = false
 
 function start()
     if not initialized then
-        misc.set_team(fallout.self_obj(), 21)
-        misc.set_ai(fallout.self_obj(), 8)
-        fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(fallout.random(3, 30)), 0)
+        local self_obj = fallout.self_obj()
+        misc.set_team(self_obj, 21)
+        misc.set_ai(self_obj, 8)
+        fallout.add_timer_event(self_obj, fallout.game_ticks(fallout.random(3, 30)), 0)
         initialized = true
-    else
-        if fallout.script_action() == 8 then
-            use_skill_on_p_proc()
-        else
-            if fallout.script_action() == 22 then
-                timed_event_p_proc()
-            end
-        end
+    end
+
+    local script_action = fallout.script_action()
+    if script_action == 8 then
+        use_skill_on_p_proc()
+    elseif script_action == 22 then
+        timed_event_p_proc()
     end
 end
 
@@ -31,8 +31,12 @@ function use_skill_on_p_proc()
 end
 
 function timed_event_p_proc()
-    fallout.animate_move_obj_to_tile(fallout.self_obj(), fallout.tile_num_in_direction(fallout.tile_num(fallout.self_obj()), fallout.random(0, 5), fallout.random(1, 3)), 0)
-    fallout.add_timer_event(fallout.self_obj(), fallout.game_ticks(fallout.random(3, 30)), 0)
+    local self_obj = fallout.self_obj()
+    local rotation = fallout.random(0, 5)
+    local distance = fallout.random(1, 3)
+    fallout.animate_move_obj_to_tile(self_obj,
+        fallout.tile_num_in_direction(fallout.tile_num(self_obj), rotation, distance), 0)
+    fallout.add_timer_event(self_obj, fallout.game_ticks(fallout.random(3, 30)), 0)
 end
 
 local exports = {}
