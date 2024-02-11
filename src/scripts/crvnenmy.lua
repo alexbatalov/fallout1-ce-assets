432b314070a866517b5e96945077ba8595260277
Alexander Batalov <alex.batalov@gmail.com>
2024-02-11 21:05:58 +0300

    Review crvnenmy.lua

--- src/scripts/crvnenmy.lua
+++ src/scripts/crvnenmy.lua
@@ -5,28 +5,29 @@ local reputation = require("lib.reputation")
 local start
 local critter_p_proc
 local destroy_p_proc
-local initialize_p_proc
 
 local initialized = false
-local Hostile = 1
+local hostile = true
 
 function start()
     if not initialized then
-        initialize_p_proc()
-    else
-        if fallout.script_action() == 12 then
-            critter_p_proc()
-        else
-            if fallout.script_action() == 18 then
-                destroy_p_proc()
-            end
-        end
+        local self_obj = fallout.self_obj()
+        misc.set_team(self_obj, 86)
+        misc.set_ai(self_obj, 1)
+        initialized = true
+    end
+
+    local script_action = fallout.script_action()
+    if script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
     end
 end
 
 function critter_p_proc()
-    if Hostile and (fallout.elevation(fallout.self_obj()) == fallout.elevation(fallout.dude_obj())) then
-        Hostile = 0
+    if hostile and fallout.elevation(fallout.self_obj()) == fallout.elevation(fallout.dude_obj()) then
+        hostile = false
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     end
 end
@@ -35,15 +36,8 @@ function destroy_p_proc()
     reputation.inc_evil_critter()
 end
 
-function initialize_p_proc()
-    initialized = true
-    misc.set_team(fallout.self_obj(), 86)
-    misc.set_ai(fallout.self_obj(), 1)
-end
-
 local exports = {}
 exports.start = start
 exports.critter_p_proc = critter_p_proc
 exports.destroy_p_proc = destroy_p_proc
-exports.initialize_p_proc = initialize_p_proc
 return exports
