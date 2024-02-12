cc298434409380317e287867e758ce6f94f1c57f
Alexander Batalov <alex.batalov@gmail.com>
2024-02-12 21:19:12 +0300

    Review map_init.lua

--- src/scripts/map_init.lua
+++ src/scripts/map_init.lua
@@ -1,24 +1,28 @@
 local fallout = require("fallout")
 
 local start
+local map_enter_p_proc
 
 function start()
-    local v0 = 0
     if fallout.script_action() == 15 then
-        v0 = fallout.create_object_sid(4, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), v0)
-        v0 = fallout.create_object_sid(18, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), v0)
-        v0 = fallout.create_object_sid(31, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), v0)
-        v0 = fallout.create_object_sid(40, 0, 0, -1)
-        fallout.add_obj_to_inven(fallout.dude_obj(), v0)
-    else
-        if fallout.script_action() == 16 then
-        end
+        map_enter_p_proc()
     end
+end
+
+function map_enter_p_proc()
+    local dude_obj = fallout.dude_obj()
+    local item_obj
+    item_obj = fallout.create_object_sid(4, 0, 0, -1)
+    fallout.add_obj_to_inven(dude_obj, item_obj)
+    item_obj = fallout.create_object_sid(18, 0, 0, -1)
+    fallout.add_obj_to_inven(dude_obj, item_obj)
+    item_obj = fallout.create_object_sid(31, 0, 0, -1)
+    fallout.add_obj_to_inven(dude_obj, item_obj)
+    item_obj = fallout.create_object_sid(40, 0, 0, -1)
+    fallout.add_obj_to_inven(dude_obj, item_obj)
 end
 
 local exports = {}
 exports.start = start
+exports.map_enter_p_proc = map_enter_p_proc
 return exports
