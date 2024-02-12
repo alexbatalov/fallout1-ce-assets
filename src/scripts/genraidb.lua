9a49d6652574b6cc9280757c0123a2bad984a1ba
Alexander Batalov <alex.batalov@gmail.com>
2024-02-12 21:16:04 +0300

    Review genraidb.lua

--- src/scripts/genraidb.lua
+++ src/scripts/genraidb.lua
@@ -9,37 +9,33 @@ local pickup_p_proc
 local talk_p_proc
 
 local initialized = false
-local hostile = 0
-local scared = 0
+local hostile = false
+local scared = false
 
 function start()
     if not initialized then
-        misc.set_team(fallout.self_obj(), 6)
-        misc.set_ai(fallout.self_obj(), 20)
+        local self_obj = fallout.self_obj()
+        misc.set_team(self_obj, 6)
+        misc.set_ai(self_obj, 20)
         initialized = true
-    else
-        if fallout.script_action() == 12 then
-            critter_p_proc()
-        else
-            if fallout.script_action() == 18 then
-                destroy_p_proc()
-            else
-                if fallout.script_action() == 4 then
-                    pickup_p_proc()
-                else
-                    if fallout.script_action() == 11 then
-                        talk_p_proc()
-                    end
-                end
-            end
-        end
+    end
+
+    local script_action = fallout.script_action()
+    if script_action == 12 then
+        critter_p_proc()
+    elseif script_action == 18 then
+        destroy_p_proc()
+    elseif script_action == 4 then
+        pickup_p_proc()
+    elseif script_action == 11 then
+        talk_p_proc()
     end
 end
 
 function critter_p_proc()
     if hostile then
-        hostile = 0
-        scared = 1
+        hostile = false
+        scared = true
         fallout.attack(fallout.dude_obj(), 0, 1, 0, 0, 30000, 0, 0)
     end
 end
@@ -53,7 +49,7 @@ function destroy_p_proc()
 end
 
 function pickup_p_proc()
-    hostile = 1
+    hostile = true
 end
 
 function talk_p_proc()
